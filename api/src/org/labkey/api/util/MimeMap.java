effb431e9e46a50000522e48ab27abefa4e60b4d
Matthew Bellew <mbellew@labkey.com>
2008-05-15 17:37:36 +0000

    merge static code in MimeMap and file in mime.types into one text file mime.txt mime.txt is easy to edit in Excel

--- api/src/org/labkey/api/util/MimeMap.java
+++ api/src/org/labkey/api/util/MimeMap.java
@@ -15,9 +15,11 @@
  */
 package org.labkey.api.util;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import org.apache.log4j.Logger;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
 import java.net.*;
 import java.util.*;
 
@@ -56,177 +58,45 @@ public class MimeMap implements FileNameMap
         {
             return _inlineImage;
         }
+
+        @Override
+        public String toString()
+        {
+            return _contentType;
+        }
     }
 
     static
     {
-        defaultMap.put("txt", new MimeType("text/plain"));
-        defaultMap.put("log", new MimeType("text/plain"));
-        defaultMap.put("html", new MimeType("text/html"));
-        defaultMap.put("htm", new MimeType("text/html"));
-        defaultMap.put("gif", new MimeType("image/gif", true));
-        defaultMap.put("jpg", new MimeType("image/jpeg", true));
-        defaultMap.put("jpe", new MimeType("image/jpeg", true));
-        defaultMap.put("jpeg", new MimeType("image/jpeg", true));
-        defaultMap.put("png", new MimeType("image/png", true));
-        defaultMap.put("ico", new MimeType("image/vnd.microsoft.icon"));
-        defaultMap.put("iqy", new MimeType("text/x-ms-iqy"));
-        defaultMap.put("java", new MimeType("text/plain"));
-        defaultMap.put("properties", new MimeType("text/plain"));
-        defaultMap.put("sql", new MimeType("text/plain"));
-        defaultMap.put("Log", new MimeType("text/plain"));
-        defaultMap.put("body", new MimeType("text/html"));
-        defaultMap.put("rtx", new MimeType("text/richtext"));
-        defaultMap.put("tsv", new MimeType("text/tab-separated-values"));
-        defaultMap.put("etx", new MimeType("text/x-setext"));
-        defaultMap.put("ps", new MimeType("application/x-postscript"));
-        defaultMap.put("class", new MimeType("application/java"));
-        defaultMap.put("csh", new MimeType("application/x-csh"));
-        defaultMap.put("sh", new MimeType("application/x-sh"));
-        defaultMap.put("tcl", new MimeType("application/x-tcl"));
-        defaultMap.put("tex", new MimeType("application/x-tex"));
-        defaultMap.put("texinfo", new MimeType("application/x-texinfo"));
-        defaultMap.put("texi", new MimeType("application/x-texinfo"));
-        defaultMap.put("t", new MimeType("application/x-troff"));
-        defaultMap.put("tr", new MimeType("application/x-troff"));
-        defaultMap.put("roff", new MimeType("application/x-troff"));
-        defaultMap.put("man", new MimeType("application/x-troff-man"));
-        defaultMap.put("me", new MimeType("application/x-troff-me"));
-        defaultMap.put("ms", new MimeType("application/x-wais-source"));
-        defaultMap.put("src", new MimeType("application/x-wais-source"));
-        defaultMap.put("zip", new MimeType("application/zip"));
-        defaultMap.put("bcpio", new MimeType("application/x-bcpio"));
-        defaultMap.put("cpio", new MimeType("application/x-cpio"));
-        defaultMap.put("gtar", new MimeType("application/x-gtar"));
-        defaultMap.put("shar", new MimeType("application/x-shar"));
-        defaultMap.put("sv4cpio", new MimeType("application/x-sv4cpio"));
-        defaultMap.put("sv4crc", new MimeType("application/x-sv4crc"));
-        defaultMap.put("tar", new MimeType("application/x-tar"));
-        defaultMap.put("ustar", new MimeType("application/x-ustar"));
-        defaultMap.put("dvi", new MimeType("application/x-dvi"));
-        defaultMap.put("hdf", new MimeType("application/x-hdf"));
-        defaultMap.put("latex", new MimeType("application/x-latex"));
-        defaultMap.put("bin", new MimeType("application/octet-stream"));
-        defaultMap.put("oda", new MimeType("application/oda"));
-        defaultMap.put("pdf", new MimeType("application/pdf"));
-        defaultMap.put("ps", new MimeType("application/postscript"));
-        defaultMap.put("eps", new MimeType("application/postscript"));
-        defaultMap.put("ai", new MimeType("application/postscript"));
-        defaultMap.put("rtf", new MimeType("application/rtf"));
-        defaultMap.put("nc", new MimeType("application/x-netcdf"));
-        defaultMap.put("cdf", new MimeType("application/x-netcdf"));
-        defaultMap.put("cer", new MimeType("application/x-x509-ca-cert"));
-        defaultMap.put("exe", new MimeType("application/octet-stream"));
-        defaultMap.put("gz", new MimeType("application/x-gzip"));
-        defaultMap.put("Z", new MimeType("application/x-compress"));
-        defaultMap.put("z", new MimeType("application/x-compress"));
-        defaultMap.put("hqx", new MimeType("application/mac-binhex40"));
-        defaultMap.put("mif", new MimeType("application/x-mif"));
-        defaultMap.put("ief", new MimeType("image/ief"));
-        defaultMap.put("tiff", new MimeType("image/tiff"));
-        defaultMap.put("tif", new MimeType("image/tiff"));
-        defaultMap.put("ras", new MimeType("image/x-cmu-raster"));
-        defaultMap.put("pnm", new MimeType("image/x-portable-anymap"));
-        defaultMap.put("pbm", new MimeType("image/x-portable-bitmap"));
-        defaultMap.put("pgm", new MimeType("image/x-portable-graymap"));
-        defaultMap.put("ppm", new MimeType("image/x-portable-pixmap"));
-        defaultMap.put("rgb", new MimeType("image/x-rgb"));
-        defaultMap.put("xbm", new MimeType("image/x-xbitmap"));
-        defaultMap.put("xpm", new MimeType("image/x-xpixmap"));
-        defaultMap.put("xwd", new MimeType("image/x-xwindowdump"));
-        defaultMap.put("au", new MimeType("audio/basic"));
-        defaultMap.put("snd", new MimeType("audio/basic"));
-        defaultMap.put("aif", new MimeType("audio/x-aiff"));
-        defaultMap.put("aiff", new MimeType("audio/x-aiff"));
-        defaultMap.put("aifc", new MimeType("audio/x-aiff"));
-        defaultMap.put("wav", new MimeType("audio/x-wav"));
-        defaultMap.put("mpeg", new MimeType("video/mpeg"));
-        defaultMap.put("mpg", new MimeType("video/mpeg"));
-        defaultMap.put("mpe", new MimeType("video/mpeg"));
-        defaultMap.put("qt", new MimeType("video/quicktime"));
-        defaultMap.put("mov", new MimeType("video/quicktime"));
-        defaultMap.put("avi", new MimeType("video/x-msvideo"));
-        defaultMap.put("movie", new MimeType("video/x-sgi-movie"));
-        defaultMap.put("avx", new MimeType("video/x-rad-screenplay"));
-        defaultMap.put("wrl", new MimeType("x-world/x-vrml"));
-        defaultMap.put("mpv2", new MimeType("video/mpeg2"));
-
-        /* Add XML related MIMEs */
-
-        defaultMap.put("xml", new MimeType("text/xml"));
-        defaultMap.put("xsl", new MimeType("text/xml"));
-        defaultMap.put("svg", new MimeType("image/svg+xml"));
-        defaultMap.put("svgz", new MimeType("image/svg+xml"));
-        defaultMap.put("wbmp", new MimeType("image/vnd.wap.wbmp"));
-        defaultMap.put("wml", new MimeType("text/vnd.wap.wml"));
-        defaultMap.put("wmlc", new MimeType("application/vnd.wap.wmlc"));
-        defaultMap.put("wmls", new MimeType("text/vnd.wap.wmlscript"));
-        defaultMap.put("wmlscriptc", new MimeType("application/vnd.wap.wmlscriptc"));
-        defaultMap.put("mzxml", new MimeType("application/mzxml"));
-
-        /* open office */
-
-        defaultMap.put("odt", new MimeType("application/vnd.oasis.opendocument.text"));
-        defaultMap.put("ott", new MimeType("application/vnd.oasis.opendocument.text-template ott"));
-        defaultMap.put("oth", new MimeType("application/vnd.oasis.opendocument.text-web"));
-        defaultMap.put("odm", new MimeType("application/vnd.oasis.opendocument.text-master"));
-        defaultMap.put("odg", new MimeType("application/vnd.oasis.opendocument.graphics odg"));
-        defaultMap.put("otg", new MimeType("application/vnd.oasis.opendocument.graphics-template"));
-        defaultMap.put("odp", new MimeType("application/vnd.oasis.opendocument.presentation"));
-        defaultMap.put("otp", new MimeType("application/vnd.oasis.opendocument.presentation-template"));
-        defaultMap.put("ods", new MimeType("application/vnd.oasis.opendocument.spreadsheet"));
-        defaultMap.put("ots", new MimeType("application/vnd.oasis.opendocument.spreadsheet-template"));
-        defaultMap.put("odc", new MimeType("application/vnd.oasis.opendocument.chart"));
-        defaultMap.put("odf", new MimeType("application/vnd.oasis.opendocument.formula"));
-        defaultMap.put("odb", new MimeType("application/vnd.oasis.opendocument.database"));
-        defaultMap.put("odi", new MimeType("application/vnd.oasis.opendocument.image"));
-        defaultMap.put("oxt", new MimeType("application/vnd.openofficeorg.extension"));
-
-        /* office 2007 */
-
-        defaultMap.put("docm", new MimeType("application/vnd.ms-word.document.macroEnabled.12"));
-        defaultMap.put("docx", new MimeType("application/vnd.openxmlformats-officedocument.wordprocessingml.document"));
-        defaultMap.put("dotm", new MimeType("application/vnd.ms-word.template.macroEnabled.12"));
-        defaultMap.put("dotx", new MimeType("application/vnd.openxmlformats-officedocument.wordprocessingml.template"));
-        defaultMap.put("potm", new MimeType("application/vnd.ms-powerpoint.template.macroEnabled.12"));
-        defaultMap.put("potx", new MimeType("application/vnd.openxmlformats-officedocument.presentationml.template"));
-        defaultMap.put("ppam", new MimeType("application/vnd.ms-powerpoint.addin.macroEnabled.12"));
-        defaultMap.put("ppsm", new MimeType("application/vnd.ms-powerpoint.slideshow.macroEnabled.12"));
-        defaultMap.put("ppsx", new MimeType("application/vnd.openxmlformats-officedocument.presentationml.slideshow"));
-        defaultMap.put("pptm", new MimeType("application/vnd.ms-powerpoint.presentation.macroEnabled.12"));
-        defaultMap.put("pptx", new MimeType("application/vnd.openxmlformats-officedocument.presentationml.presentation"));
-        defaultMap.put("xlam", new MimeType("application/vnd.ms-excel.addin.macroEnabled.12"));
-        defaultMap.put("xlsb", new MimeType("application/vnd.ms-excel.sheet.binary.macroEnabled.12"));
-        defaultMap.put("xlsm", new MimeType("application/vnd.ms-excel.sheet.macroEnabled.12"));
-        defaultMap.put("xlsx", new MimeType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
-        defaultMap.put("xltm", new MimeType("application/vnd.ms-excel.template.macroEnabled.12"));
-        defaultMap.put("xltx", new MimeType("application/vnd.openxmlformats-officedocument.spreadsheetml.template"));
-
-        // mime.types
+        Map<String,MimeType> mimetypes = new HashMap<String, MimeType>();
+        defaultMap.put("image/gif", new MimeType("image/gif", true));
+        defaultMap.put("image/jpeg", new MimeType("image/jpeg", true));
+        defaultMap.put("image/png", new MimeType("image/png", true));
+        
         try
         {
-            ClassLoader cl = MimeMap.class.getClassLoader();
-            InputStream is = cl.getResourceAsStream("org/labkey/api/util/mime.types");
-            if (null == is)
-                is = cl.getResourceAsStream("mime.types");
-            BufferedReader in = new BufferedReader(new InputStreamReader(is));
-
-            String s;
-            while (null != (s = in.readLine()))
+            // tab delimited file is easier to edit in a spreadsheet
+            InputStream is = MimeMap.class.getResourceAsStream("mime.txt");
+            List<String> lines = IOUtils.readLines(is);
+            for (String line : lines)
             {
-                String[] a = s.split("\\s");
-                if (a.length < 2) continue;
-                for (int i = 1; i < a.length; i++)
+                int tab = StringUtils.indexOfAny(line, "\t ");
+                if (tab < 0) continue;
+                String key = StringUtils.trimToNull(line.substring(0,tab));
+                String value = StringUtils.trimToNull(line.substring(tab+1));
+                if (null != key && null != value)
                 {
-                    MimeType existingType = defaultMap.get(a[i]);
-                    boolean inlineImage = existingType == null ? false : existingType.isInlineImage();
-                    defaultMap.put(a[i], new MimeType(a[0], inlineImage));
+                    MimeType mt = mimetypes.get(value);
+                    if (null == mt)
+                        mimetypes.put(value, (mt = new MimeType(value)));
+                    defaultMap.put(key, mt);
                 }
             }
+            is.close();
         }
-        catch (Exception x)
+        catch (Exception e)
         {
-            x.printStackTrace();
+            Logger.getInstance(MimeMap.class).error("unexpected error", e);
         }
     }
 
@@ -327,5 +197,4 @@ public class MimeMap implements FileNameMap
             return null;
         }
     }
-
 }
