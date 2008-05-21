56a3de21588ea3fecd0a5ad44d4b0b6d2ec6caf8
Adam Rauch <adam@labkey.com>
2008-05-21 17:15:19 +0000

    Insert mini license in our .js files

--- query/webapp/query/queryDesigner.js
+++ query/webapp/query/queryDesigner.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2006-2008 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 LABKEY.requiresScript('filter.js');
 LABKEY.requiresScript('utils/dialogBox.js');
 var nsQuery = "http://query.labkey.org/design";
