037ba243a1c45e439e38b47cd0cb15b8f2d68708
Adam Rauch <adam@labkey.com>
2008-07-11 16:59:57 +0000

    Update copyrights

--- experiment/src/META-INF/experiment/scripts/postgresql/exp-create.sql
+++ experiment/src/META-INF/experiment/scripts/postgresql/exp-create.sql
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2008 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 CREATE VIEW exp.ProtocolActionStepDetailsView AS
     SELECT Protocol_P.LSID AS ParentProtocolLSID, Protocol_C.LSID AS LSID, Protocol_C.LSID AS ChildProtocolLSID, exp.ProtocolAction.Sequence AS Sequence, exp.ProtocolAction.Sequence AS ActionSequence,
         exp.ProtocolAction.RowId AS ActionId, Protocol_C.RowId AS RowId, Protocol_C.Name AS Name,
