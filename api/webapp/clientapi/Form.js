d8b530c21a1bf15e2a02d7dd1a7075fab52c111e
Dave Stearns <daves@labkey.com>
2009-06-10 19:00:20 +0000

    8083 : Need documentation for LABKEY.Form JavaScript client API class

--- api/webapp/clientapi/Form.js
+++ api/webapp/clientapi/Form.js
@@ -19,8 +19,25 @@
  */
 
 /**
- * @namespace LabKey Form Helper class.
- * This class provides several methods for working with HTML forms
+ * Constructs a LABKEY.Form object. This object may be used to track the dirty state of an HTML form
+ * and warn the user about unsaved changes if the user leaves the page before saving.
+ * @class A utility class for tracking HTML form dirty state and warning the user about unsaved changes.
+ * @constructor
+ * @param {Object} config A configuration ojbect containing the following properties:
+ * @param {Element} config.formElement A reference to the HTML form element you want to track.
+ * @param {String} [config.warningMessage] An optional warning message to display if the user
+ * attempts to leave the page while the form is still dirty. If not supplied, a default message is used.
+ * @param {Boolean} [config.showWarningMessage] Set to false to stop this from displaying the warning message
+ * when the user attempts to leave the page while the form is still dirty. If you only want to use this class
+ * to track the dirty state only and not to warn on unload, set this to false.
+ * @example
+ * &lt;form id='form-preferences'&gt; ... &lt;/form&gt;
+ * &lt;script type="text/javascript"&gt;
+    var _form = new LABKEY.Form({
+        formElement: 'form-preferences'
+    });
+&lt;/script&gt;
+
  */
 LABKEY.Form = function(config) {
     //if the config doesn't have a property named formElement
@@ -47,7 +64,7 @@ LABKEY.Form = function(config) {
         this.setClean();
     }, this);
 
-    if(false !== config.trackDirty)
+    if(false !== config.showWarningMessage)
     {
         Ext.EventManager.on(window, "beforeunload", function(evt){
             if(this.isDirty())
@@ -56,18 +73,30 @@ LABKEY.Form = function(config) {
     }
 };
 
+/**
+ * Returns true if the form is currently dirty, false otherwise.
+ */
 LABKEY.Form.prototype.isDirty = function() {
     return this._isDirty;
 };
 
+/**
+ * Sets the form's dirty state to clean
+ */
 LABKEY.Form.prototype.setClean = function() {
     this._isDirty = false;
 };
 
+/**
+ * Sets the form's dirty state to dirty
+ */
 LABKEY.Form.prototype.setDirty = function() {
     this._isDirty = true;
 };
 
+/**
+ * @private
+ */
 LABKEY.Form.prototype.onElemChange = function() {
     this._isDirty = true;
 };
