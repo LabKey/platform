743ae147f774aa1d6f79c900787f85c80e7802f9
Adam Rauch <adam@labkey.com>
2009-07-23 17:14:48 +0000

    License updates

--- study/src/org/labkey/study/model/SpecimenTypeSummaryRow.java
+++ study/src/org/labkey/study/model/SpecimenTypeSummaryRow.java
@@ -1,7 +1,7 @@
 package org.labkey.study.model;
 
 /**
- * Copyright (c) 2008 LabKey Corporation
+ * Copyright (c) 2008-2009 LabKey Corporation
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
