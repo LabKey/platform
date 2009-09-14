826d1a63aaafb2404d6f98d837e97a637f5c7f9e
Adam Rauch <adam@labkey.com>
2009-09-14 16:54:02 +0000

    License updates - mostly just obsolete scripts that got moved

--- api/src/org/labkey/api/security/RequiresPermission.java
+++ api/src/org/labkey/api/security/RequiresPermission.java
@@ -16,14 +16,11 @@
 
 package org.labkey.api.security;
 
-import org.labkey.api.security.roles.HasContextualRoles;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Created by IntelliJ IDEA.
  * User: matthewb
  * Date: Jan 11, 2007
  * Time: 11:35:28 AM
