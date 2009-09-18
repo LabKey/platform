0bd7ca46fb52a42725b8a628ab41bf6cf63cab81
Kevin Krouse <kevink@labkey.com>
2009-09-18 17:48:33 +0000

    viability assay work in progress

--- study/src/org/labkey/study/assay/ModuleAssayProvider.java
+++ study/src/org/labkey/study/assay/ModuleAssayProvider.java
@@ -88,7 +88,8 @@ public class ModuleAssayProvider extends TsvAssayProvider
     public ModuleAssayProvider(String name, File baseDir, ProviderType providerConfig)
     {
         super(name + "Protocol", name + "Run");
-        _tableMetadata = new AssayTableMetadata(_tableMetadata.getSpecimenDetailParentFieldKey(), _tableMetadata.getRunFieldKeyFromResults(), _tableMetadata.getResultRowIdFieldKey())        {
+        _tableMetadata = new AssayTableMetadata(_tableMetadata.getSpecimenDetailParentFieldKey(), _tableMetadata.getRunFieldKeyFromResults(), _tableMetadata.getResultRowIdFieldKey())
+        {
             @Override
             public FieldKey getParticipantIDFieldKey()
             {
