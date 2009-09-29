c08956be4abc4caa93b4bbbcbb6ac4895973d061
Adam Rauch <adam@labkey.com>
2009-09-29 23:04:41 +0000

    Export attachments with lists...

--- experiment/src/org/labkey/experiment/list/ListWriter.java
+++ experiment/src/org/labkey/experiment/list/ListWriter.java
@@ -17,23 +17,29 @@
 package org.labkey.experiment.list;
 
 import org.apache.log4j.Logger;
+import org.labkey.api.attachments.AttachmentParent;
+import org.labkey.api.attachments.AttachmentService;
 import org.labkey.api.data.*;
+import org.labkey.api.exp.PropertyType;
 import org.labkey.api.exp.list.ListDefinition;
 import org.labkey.api.exp.list.ListService;
 import org.labkey.api.exp.property.DomainProperty;
-import org.labkey.api.exp.PropertyType;
 import org.labkey.api.query.QueryService;
 import org.labkey.api.study.ExternalStudyWriter;
 import org.labkey.api.study.ExternalStudyWriterFactory;
 import org.labkey.api.study.Study;
 import org.labkey.api.study.StudyContext;
+import org.labkey.api.util.FileUtil;
+import org.labkey.api.util.ResultSetUtil;
 import org.labkey.api.writer.VirtualFile;
 import org.labkey.data.xml.ColumnType;
 import org.labkey.data.xml.TableType;
 import org.labkey.data.xml.TablesDocument;
+import org.labkey.experiment.controllers.list.ListItemAttachmentParent;
 
-import java.io.PrintWriter;
+import java.io.*;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 /*
@@ -69,15 +75,15 @@ public class ListWriter implements ExternalStudyWriter
             for (Map.Entry<String, ListDefinition> entry : lists.entrySet())
             {
                 ListDefinition def = entry.getValue();
-                TableInfo tinfo = def.getTable(ctx.getUser());
+                TableInfo ti = def.getTable(ctx.getUser());
 
                 // Write meta data
                 TableType tableXml = tablesXml.addNewTable();
-                ListTableInfoWriter xmlWriter = new ListTableInfoWriter(tinfo, def, getColumnsToExport(tinfo, true));
+                ListTableInfoWriter xmlWriter = new ListTableInfoWriter(ti, def, getColumnsToExport(ti, true));
                 xmlWriter.writeTable(tableXml);
 
                 // Write data
-                Collection<ColumnInfo> columns = getColumnsToExport(tinfo, false);
+                Collection<ColumnInfo> columns = getColumnsToExport(ti, false);
 
                 if (!columns.isEmpty())
                 {
@@ -86,15 +92,92 @@ public class ListWriter implements ExternalStudyWriter
                     for (ColumnInfo col : columns)
                         displayColumns.add(new ListExportDataColumn(col));
 
-                    ResultSet rs = QueryService.get().select(tinfo, columns, null, null);
+                    ResultSet rs = QueryService.get().select(ti, columns, null, null);
                     TSVGridWriter tsvWriter = new TSVGridWriter(rs, displayColumns);
                     tsvWriter.setColumnHeaderType(TSVGridWriter.ColumnHeaderType.propertyName);
                     PrintWriter out = listsDir.getPrintWriter(def.getName() + ".tsv");
                     tsvWriter.write(out);     // NOTE: TSVGridWriter closes PrintWriter and ResultSet
+
+                    writeAttachments(ti, def, c, listsDir);
                 }
             }
 
             listsDir.saveXmlBean(SCHEMA_FILENAME, tablesDoc);
+        }
+    }
+
+    private void writeAttachments(TableInfo ti, ListDefinition def, Container c, VirtualFile dir) throws SQLException, IOException
+    {
+        List<ColumnInfo> attachmentColumns = new ArrayList<ColumnInfo>();
+
+        for (DomainProperty prop : def.getDomain().getProperties())
+            if (prop.getPropertyDescriptor().getPropertyType() == PropertyType.ATTACHMENT)
+                attachmentColumns.add(ti.getColumn(prop.getName()));
+
+        if (!attachmentColumns.isEmpty())
+        {
+            Map<String, FileNameUniquifier> uniquifiers = new HashMap<String, FileNameUniquifier>();
+
+            for (ColumnInfo attachmentColumn : attachmentColumns)
+                uniquifiers.put(attachmentColumn.getName(), new FileNameUniquifier());
+
+            List<ColumnInfo> selectColumns = new ArrayList<ColumnInfo>(attachmentColumns);
+            selectColumns.add(0, ti.getColumn("EntityId"));
+
+            ResultSet rs = null; 
+
+            try
+            {
+                rs = QueryService.get().select(ti, selectColumns, null, null);
+
+                while(rs.next())
+                {
+                    String entityId = rs.getString(1);
+                    AttachmentParent listItemParent = new ListItemAttachmentParent(entityId, c);
+
+                    rs.getString(1);
+                    int sqlColumn = 2;
+
+                    for (ColumnInfo attachmentColumn : attachmentColumns)
+                    {
+                        String filename = rs.getString(sqlColumn++);
+
+                        // Item might not have an attachment in this column
+                        if (null == filename)
+                            continue;
+
+                        String columnName = attachmentColumn.getColumnName();
+                        VirtualFile columnDir = dir.getDir(columnName);
+
+                        InputStream is = null;
+                        OutputStream os = null;
+
+                        try
+                        {
+                            is = AttachmentService.get().getInputStream(listItemParent, filename);
+                            FileNameUniquifier uniquifier = uniquifiers.get(columnName);
+                            os = columnDir.getOutputStream(uniquifier.uniquify(filename));
+                            FileUtil.copyData(is, os);
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            // Shouldn't happen... but just skip this file in production if it does
+                            assert false;
+                        }
+                        finally
+                        {
+                            if (null != is)
+                                is.close();
+                            if (null != os)
+                                os.close();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                ResultSetUtil.close(rs);
+            }
         }
     }
 
@@ -109,6 +192,37 @@ public class ListWriter implements ExternalStudyWriter
         return columns;
     }
 
+
+    // Uniquify filenames by inserting _n immediately before the extension
+    private static class FileNameUniquifier
+    {
+        private Set<String> _previous = new HashSet<String>();
+
+        private String uniquify(String name)
+        {
+            int i = 1;
+            String candidateName = name;
+
+            // Insert _2, _3, _4, _5, etc. before the extension until we have a unique identifier
+            while (_previous.contains(candidateName))
+            {
+                i++;
+                int dot = name.lastIndexOf('.');
+
+                if (-1 == dot)
+                    candidateName = name + '_' + i;
+                else
+                    candidateName = name.substring(0, dot) + '_' + i + name.substring(dot);
+            }
+
+            _previous.add(candidateName);
+
+            return candidateName;
+        }
+    }
+
+
+    // We just want the underlying value, not the lookup
     private static class ListExportDataColumn extends DataColumn
     {
         private ListExportDataColumn(ColumnInfo col)
