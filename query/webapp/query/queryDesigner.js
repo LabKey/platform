bb059519dd6809762559fd64254b519006bd21d5
Matthew Bellew <mbellew@labkey.com>
2009-12-14 19:29:16 +0000

    combine filter.js and DataRegion.js

--- query/webapp/query/queryDesigner.js
+++ query/webapp/query/queryDesigner.js
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
-LABKEY.requiresScript('filter.js');
+LABKEY.requiresScript('DataRegion.js');
 LABKEY.requiresScript('utils/dialogBox.js');
 var nsQuery = "http://query.labkey.org/design";
 var nsData = "http://labkey.org/data/xml";
