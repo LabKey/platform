53eb3bd40aae3c9fa18ad2d4b76f83a272e4367a
Dave Stearns <daves@labkey.com>
2010-02-03 21:43:39 +0000

    8585 : Remove client side permissions check for adding new users as project admin

--- core/webapp/SecurityAdmin.js
+++ core/webapp/SecurityAdmin.js
@@ -861,15 +861,6 @@ var UserInfoPopup = Ext.extend(Ext.Window,{
     {
         if (!email)
             return;
-        if (!LABKEY.Security.currentUser.isSystemAdmin)
-        {
-            this.addPrincipalComboBox.suspendEvents();
-            Ext.Msg.alert("Unauthorized", "You do not have permission to create new user accounts. Please choose an existing user.");
-            this.addPrincipalComboBox.clearValue();
-            this.addPrincipalComboBox.resumeEvents();
-            return;
-        }
-
         this.cache.createNewUser(email, true, function(user)
         {
             var groupid = this.user.UserId;
