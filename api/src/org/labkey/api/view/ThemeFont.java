98299ef8b82efbb3dffed067c9c59b4b89c49d0b
Matthew Bellew <mbellew@labkey.com>
2010-03-01 21:25:31 +0000

    for your consideration, since we don't seem to have font size between "smallest on the internet" (aka small) and old man VGA (aka medium).
    I played with the themes a little so that MEDIUM is 13px font (seems to be pretty standard) and SMALL and LARGE are smaller increments.
    
    XSMALL is almost identical to old small
    SMALL is one step smaller than MEDIUM
    MEDIUM is almost identical to old theme
    LARGE is much smaller than the previous clown font
    XLARGE is also smaller than the previous LARGE, but still big enough to not make the dropdown.
    
    the old themes are still in the dropdown, and the whole thing may be easily reverted.

--- api/src/org/labkey/api/view/ThemeFont.java
+++ api/src/org/labkey/api/view/ThemeFont.java
@@ -25,9 +25,15 @@ import java.util.*;
 public class ThemeFont
 {
     //protected final static ThemeFont VERYSMALL = new ThemeFont("Very small", "7pt", "13pt", "10pt", "18pt", "9pt", "9pt", "10", "11");
-    protected final static ThemeFont SMALL = new ThemeFont("Small", "8pt", "10pt", "14pt", "14pt", "19pt", "10pt", "10pt", "11", "12");
-    protected final static ThemeFont MEDIUM = new ThemeFont("Medium", "10pt", "10pt", "16pt", "13pt", "21pt", "12pt", "12pt", "13", "14");
-    protected final static ThemeFont LARGE = new ThemeFont("Large", "12pt", "12pt", "18pt", "15pt", "23pt", "14pt", "14pt", "15", "16");
+    protected final static ThemeFont _SMALL  = new ThemeFont("Small 10.3",  "8pt", "10pt", "14pt", "14pt", "19pt", "10pt", "10pt", "11", "12");
+    protected final static ThemeFont _MEDIUM = new ThemeFont("Medium 10.3","10pt", "10pt", "16pt", "13pt", "21pt", "12pt", "12pt", "13", "14");
+    protected final static ThemeFont _LARGE  = new ThemeFont("Large 10.3", "12pt", "12pt", "18pt", "15pt", "23pt", "14pt", "14pt", "15", "16");
+
+    protected final static ThemeFont XSMALL   = new ThemeFont("Smallest",  "11px", "13px", "18px", "18px", "25px", "13px", "13px", "11", "12");
+    protected final static ThemeFont SMALL   = new ThemeFont("Small",      "12px", "13px", "18px", "18px", "25px", "13px", "13px", "12", "13");
+    protected final static ThemeFont MEDIUM  = new ThemeFont("Medium",     "13px", "13px", "20px", "18px", "27px", "15px", "15px", "13", "14");
+    protected final static ThemeFont LARGE   = new ThemeFont("Large",      "14px", "14px", "22px", "19px", "28px", "18px", "18px", "14", "15");
+//    protected final static ThemeFont XLARGE   = new ThemeFont("Large",     "15px", "15px", "22px", "19px", "28px", "18px", "18px", "15", "16");
 
     private final String _friendlyName;
     private final String _sizeNormal;
@@ -120,15 +126,22 @@ public class ThemeFont
 
     static
     {
+        webThemeFontMap.put(XSMALL.getFriendlyName(), XSMALL);
         webThemeFontMap.put(SMALL.getFriendlyName(), SMALL);
         webThemeFontMap.put(MEDIUM.getFriendlyName(), MEDIUM);
         webThemeFontMap.put(LARGE.getFriendlyName(), LARGE);
+//        webThemeFontMap.put(XLARGE.getFriendlyName(), XLARGE);
+
+        webThemeFontMap.put(_SMALL.getFriendlyName(), _SMALL);
+        webThemeFontMap.put(_MEDIUM.getFriendlyName(), _MEDIUM);
+        webThemeFontMap.put(_LARGE.getFriendlyName(), _LARGE);
     }
 
     public static ThemeFont getThemeFont(Container c)
     {
         LookAndFeelProperties laf = LookAndFeelProperties.getInstance(c);
-        return ThemeFont.getThemeFont(laf.getThemeFont());
+        ThemeFont tf = ThemeFont.getThemeFont(laf.getThemeFont());
+        return null==tf ? DEFAULT_THEME_FONT : tf;
     }
 
     public static ThemeFont getThemeFont(String themeFont)
