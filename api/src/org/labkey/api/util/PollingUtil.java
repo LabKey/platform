45e959b45a6dfa0e4f5425d0da607e8b8ec37ec5
Matthew Bellew <mbellew@labkey.com>
2010-03-07 22:25:39 +0000

    PollingUtil very lightweight communcation for status of asynchronouse tasks

---
+++ api/src/org/labkey/api/util/PollingUtil.java
@@ -0,0 +1,251 @@
+package org.labkey.api.util;
+
+import org.labkey.api.settings.AppProps;
+import org.json.JSONObject;
+import org.jetbrains.annotations.Nullable;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matthew
+ * Date: Feb 20, 2010
+ * Time: 3:22:42 PM
+ *
+ * requires PollingFilter to be installed in web.xml
+ *
+ *
+ * Lifetime issue is a little tricky.  I think this is a reasonable rule.  As long as the owning object exists
+ * the PollKey won't be deleted (except by explicit done()).  Once the object is no longer owned, it will
+ * go away if it has not been access for > N minutes.
+ */
+public class PollingUtil
+{
+    private static final String ATTR_NAME = "PollingUtil.map";
+    private static final Map<String,PollKey> globalMap = newMap();
+
+
+    public static PollKey createKey(@Nullable HttpSession session)
+    {
+        return createKey(session, null, null);
+    }
+
+
+    public static PollKey createKey(@Nullable HttpSession session, @Nullable String key,Object scope)
+    {
+        Map<String,PollKey> m = getMap(session);
+        return new PollKey(m, key, scope);
+    }
+
+    
+    public static class PollKey
+    {
+        private final WeakReference<Object> ref;
+        private final String key;
+        private final Map map;
+        private boolean done = false;
+        private boolean clearOnRead;
+        private long lastAccessed;
+        private Object json = null;
+
+
+        private PollKey(Map<String,PollKey> m, String key, Object ref)
+        {
+            this.ref = new WeakReference<Object>(ref);
+            this.key = null==key ? GUID.makeHash() : key;
+            this.map = m;
+            accessed();
+            m.put(this.key, this);
+        }
+
+
+        public void done()
+        {
+            done = true;
+        }
+
+
+        private boolean isHeld()
+        {
+            return !done && null != ref.get();
+        }
+
+        public synchronized void setJson(Object o)
+        {
+            setJson(o, false);
+        }
+
+        public synchronized void setJson(Object o, boolean clearOnRead)
+        {
+            accessed();
+            json = o;
+            this.clearOnRead = clearOnRead;
+            notifyAll();
+        }
+
+        public synchronized Object getJson()
+        {
+            accessed();
+            Object ret = json;
+            if (null != ret && clearOnRead)
+            {
+                json = null;
+                if (!isHeld())
+                    map.remove(key);
+            }
+            return ret;
+        }
+
+        public synchronized Object getJson(long ms)
+        {
+            accessed();
+            if (null == json)
+            {
+                try
+                {
+                    wait(Math.min(ms,1000));
+                }
+                catch (InterruptedException x) { /* */ }
+            }
+            return json;
+        }
+
+        void accessed()
+        {
+            lastAccessed = System.currentTimeMillis();
+        }
+
+        public String getUrl()
+        {
+            return AppProps.getInstance().getContextPath() + Path.parse(key).encode("/","") + ".poll";
+        }
+
+
+        public void close()
+        {
+            map.remove(this.key);
+        }
+    }
+
+
+    private static class PollingMap extends LinkedHashMap<String,PollKey>
+    {
+        PollingMap()
+        {
+            super(10,0.5F,true);
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<String, PollKey> eldest)
+        {
+            if (size()<=1)
+                return false;
+            PollKey key = eldest.getValue();
+            // if key is still 'owned' bump to front of queue
+            if (key.isHeld())
+            {
+                key.map.get(key.key);
+                return false;
+            }
+            long age = (System.currentTimeMillis() - key.lastAccessed)/(60*1000);
+            return age > 30;
+        }
+    }
+    
+
+    private static Map<String,PollKey>  newMap()
+    {
+        return Collections.synchronizedMap(new PollingMap());
+    }
+
+
+    private static Map<String,PollKey> getMap(HttpSession session)
+    {
+        if (null == session)
+            return globalMap;
+        
+        synchronized (session)
+        {
+            Map<String,PollKey> m = (Map<String,PollKey>)session.getAttribute(ATTR_NAME);
+            if (null == m)
+            {
+                m = newMap();
+                session.setAttribute(ATTR_NAME, Collections.synchronizedMap(m));
+            }
+            return m;
+        }
+    }
+
+
+    /**
+     * NOTE: caller should be prepared for SC_NOT_FOUND, it will often mean the process in question has simply completed
+     */
+    public static class PollingFilter implements javax.servlet.Filter
+    {
+        public void init(FilterConfig filterConfig) throws ServletException
+        {
+        }
+
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+        {
+            String servletPath = ((HttpServletRequest)request).getServletPath();    // decoded already, leading '/'
+            if (servletPath.endsWith(".poll"))
+                servletPath = servletPath.substring(0,servletPath.length()-".poll".length());
+            String key = StringUtils.stripStart(servletPath,"/");
+
+            HttpSession s = ((HttpServletRequest)request).getSession();
+            Map<String,PollKey> m = getMap(s);
+            PollKey pk = null==m ? null : m.get(key);
+                                                         
+            if (null == pk)
+            {
+                m = getMap(null);
+                pk = m.get(key);
+            }
+
+            if (null == pk)
+            {
+                ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+
+            response.setContentType("application/json;charset=UTF-8");
+            ((HttpServletResponse)response).setHeader("Expires", "Sun, 01 Jan 2000 00:00:00 GMT");
+            PrintWriter out = response.getWriter();
+
+            Object json = pk.getJson();
+            if (json == null)
+            {
+                out.write("{}");
+            }
+            else if (json instanceof String)
+            {
+                out.write((String)json);
+                out.write((String)json);
+            }
+            else if (json instanceof JSONObject)
+            {
+                out.write(json.toString());
+            }
+            else
+            {
+                out.write(( new JSONObject(json)).toString());
+            }
+            out.flush();
+        }
+
+        public void destroy()
+        {
+        }
+    }
+}
