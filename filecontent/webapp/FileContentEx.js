fadac80ca3d45b4077f6dff758c562a586f08bff
Adam Rauch <adam@labkey.com>
2010-03-11 01:38:31 +0000

    Update licenses.

--- filecontent/webapp/FileContentEx.js
+++ filecontent/webapp/FileContentEx.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2010 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 LABKEY.FilesWebPartPanelEx = Ext.extend(LABKEY.FilesWebPartPanel, {
     propertiesSelectResults:null,
     annotationConfig:null,
