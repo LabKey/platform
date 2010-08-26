01d8ebdf92484d9ae64a0949da2b247a6011599e
Adam Rauch <adam@labkey.com>
2010-08-26 22:33:27 +0000

    Add a devmode-only option to expire passwords every five seconds.  Allows testing the password expiration feature.

--- api/src/org/labkey/api/security/PasswordExpiration.java
+++ api/src/org/labkey/api/security/PasswordExpiration.java
@@ -17,9 +17,13 @@ package org.labkey.api.security;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import org.labkey.api.settings.AppProps;
 
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * User: adam
@@ -34,6 +38,21 @@ public enum PasswordExpiration
         {
             return false;
         }},
+    FiveSeconds(0, "Every five seconds -- for testing purposes only") {
+        @Override
+        public boolean hasExpired(Date lastChanged)
+        {
+            Calendar cutoff = Calendar.getInstance();
+            cutoff.add(Calendar.SECOND, -5);
+
+            return lastChanged.compareTo(cutoff.getTime()) < 0;
+        }
+
+        @Override
+        protected boolean display()
+        {
+            return AppProps.getInstance().isDevMode();
+        }},
     ThreeMonths(3, "Every three months"),
     SixMonths(6, "Every six months"),
     OneYear(12, "Every 12 months");
@@ -41,10 +60,28 @@ public enum PasswordExpiration
     private final int _months;
     private final String _description;
 
+    // Return the options that admins can see.  Will omit the "Every five seconds" rule on production servers.
+    public static Collection<PasswordExpiration> displayValues()
+    {
+        List<PasswordExpiration> list = new LinkedList<PasswordExpiration>();
+
+        for (PasswordExpiration pe : values())
+            if (pe.display())
+                list.add(pe);
+
+        return list;
+    }
+
     private PasswordExpiration(int months, String description)
     {
         _months = months;
         _description = description;
+    }
+
+    // All options get displayed by default.
+    protected boolean display()
+    {
+        return true;
     }
 
     public int getMonths()
@@ -82,8 +119,9 @@ public enum PasswordExpiration
         {
             testExpiration(PasswordExpiration.Never, 60, 0, 0);
             testExpiration(PasswordExpiration.OneYear, 60, 7, 7);
-            testExpiration(PasswordExpiration.SixMonths, 60, 33, 34);    // # of weeks with an expired password differs depending on if February in the range
-            testExpiration(PasswordExpiration.ThreeMonths, 60, 46, 47);  // # of weeks with an expired password differs depending on if February in the range
+            testExpiration(PasswordExpiration.SixMonths, 60, 33, 34);    // # of weeks with an expired password differs depending on if February is in the range
+            testExpiration(PasswordExpiration.ThreeMonths, 60, 46, 47);  // # of weeks with an expired password differs depending on if February is in the range
+            testExpiration(PasswordExpiration.FiveSeconds, 60, 60, 60);
         }
 
         // Test expiration every week for the specified number of weeks
