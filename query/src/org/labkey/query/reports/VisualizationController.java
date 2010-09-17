43de84d471fa03e6cf9b46c10bd51a3ed8e1b9ad
Adam Rauch <adam@labkey.com>
2010-09-17 21:05:59 +0000

    Update licenses.

--- query/src/org/labkey/query/reports/VisualizationController.java
+++ query/src/org/labkey/query/reports/VisualizationController.java
@@ -34,7 +34,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Copyright (c) 2008-2010 LabKey Corporation
+ * Copyright (c) 2010 LabKey Corporation
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
