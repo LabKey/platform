c0817676c4700b9375ac7898869114116498ec81
Adam Rauch <adam@labkey.com>
2011-02-09 01:01:58 +0000

    Update licenses

--- visualization/src/org/labkey/visualization/sql/VisualizationAggregateColumn.java
+++ visualization/src/org/labkey/visualization/sql/VisualizationAggregateColumn.java
@@ -6,7 +6,7 @@ import org.labkey.api.view.ViewContext;
 import java.util.Map;
 
 /**
-* Copyright (c) 2008-2010 LabKey Corporation
+* Copyright (c) 2011 LabKey Corporation
 * <p/>
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
--- visualization/src/org/labkey/visualization/sql/VisualizationSQLGenerator.java
+++ visualization/src/org/labkey/visualization/sql/VisualizationSQLGenerator.java
@@ -12,7 +12,7 @@ import org.labkey.visualization.VisualizationController;
 import java.util.*;
 
 /**
- * Copyright (c) 2008-2010 LabKey Corporation
+ * Copyright (c) 2011 LabKey Corporation
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
