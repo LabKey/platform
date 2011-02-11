7d9aa2df7e32bcb6f0e8964dc5672cdcf6207bac
Cory Nathe <cnathe@labkey.com>
2011-02-11 16:33:26 +0000

    changes to prepare for saving visualization: - remove unnecessary data from this.chartInfo - this.chartInfo to include only items needed to save and retrieve a visualization

--- visualization/resources/web/vis/timeChartPanel.js
+++ visualization/resources/web/vis/timeChartPanel.js
@@ -28,14 +28,11 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
         this.chartInfo = {
             measures: [
                 {axis: {multiSelect: "false", name: "x-axis", label: "", timeAxis: "true"}, dateOptions: {}, measure: {}},
-                {axis: {multiSelect: "false", name: "y-axis", label: ""}, measure: {}}
+                {axis: {multiSelect: "false", name: "y-axis", label: "", scale: "linear"}, measure: {}}
             ],
-            yAxisScale: 'linear',
-            plotChar: 'circle',
             layout: 'single',
-            subject: {selection: []},
-            title: '',
-            data: null
+            subject: {},
+            title: ''
         };
 
         LABKEY.vis.TimeChartPanel.superclass.constructor.call(this, config);
@@ -129,7 +126,7 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                 listeners: {
                     scope: this,
                     'yAxisScaleChanged': function(newValue) {
-                        this.chartInfo.yAxisScale = newValue;
+                        this.chartInfo.measures[this.yAxisMeasureIndex].axis.scale = newValue;
                         this.renderLineChart();
                     },
                     'yAxisLabelChanged': function(newValue) {
@@ -191,10 +188,10 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                     scope: this,
                     'selectionChange': function(selModel){
                         // add the selected subjects to the chartInfo
-                        this.chartInfo.subject.selection = [];
+                        this.subjectSelections = [];
                         var selectedRecords = selModel.getSelections();
                         for(var i = 0; i < selectedRecords.length; i++) {
-                            this.chartInfo.subject.selection.push(selectedRecords[i].get('value'));
+                            this.subjectSelections.push(selectedRecords[i].get('value'));
                         }
                         // redraw the line chart
                         this.renderLineChart();
@@ -256,7 +253,7 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                     scope: this,
                     'resize': function(cmp){
                         // only call renderLineChart if the data object is available
-                        if(this.chartInfo.data) {
+                        if(this.chartSubjectData) {
                             this.renderLineChart();
                         }
                     }
@@ -301,7 +298,7 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
         this.chartInfo.measures[this.yAxisMeasureIndex].axis.label = measure.label;
         // reset y-axis scale to linear
         Ext.getCmp('y-axis-scale-combo').setValue('linear');
-        this.chartInfo.yAxisScale = 'linear';
+        this.chartInfo.measures[this.yAxisMeasureIndex].axis.scale = 'linear';
         // reset the y-axis range to automatic
         Ext.getCmp('y-axis-range-automatic-radio').setValue(true);
 
@@ -349,14 +346,15 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                 // put the dimension values into the chartInfo object for the given measure
                 // note: 2 version are needed. the values version is needed for the getData call for pivoting the data
                 //          and the jsonValues version is needed for the Ext GridPanel
+                this.yAxisDimension = new Object();
                 this.chartInfo.measures[this.yAxisMeasureIndex].dimension.values = new Array();
-                this.chartInfo.measures[this.yAxisMeasureIndex].dimension.jsonValues = new Array();
+                this.yAxisDimension.jsonValues = new Array();
                 for(var i = 0; i < o.records.length; i++) {
                     this.chartInfo.measures[this.yAxisMeasureIndex].dimension.values.push(o.records[i].data.value);
-                    this.chartInfo.measures[this.yAxisMeasureIndex].dimension.jsonValues.push({value: o.records[i].data.value});
+                    this.yAxisDimension.jsonValues.push({value: o.records[i].data.value});
                 }
                 // initially selected = all values
-                this.chartInfo.measures[this.yAxisMeasureIndex].dimension.selected = this.chartInfo.measures[this.yAxisMeasureIndex].dimension.values;
+                this.yAxisDimension.selected = this.chartInfo.measures[this.yAxisMeasureIndex].dimension.values;
 
                 // put the dimension values into a list view for the user to enable/disable series
                 var sm = new  Ext.grid.CheckboxSelectionModel({
@@ -364,10 +362,10 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                         scope: this,
                         'selectionChange': function(selModel){
                             // add the selected dimension values to the chartInfo
-                            this.chartInfo.measures[this.yAxisMeasureIndex].dimension.selected = new Array();
+                            this.yAxisDimension.selected = new Array();
                             var selectedRecords = selModel.getSelections();
                             for(var i = 0; i < selectedRecords.length; i++) {
-                                this.chartInfo.measures[this.yAxisMeasureIndex].dimension.selected.push(selectedRecords[i].get('value'));
+                                this.yAxisDimension.selected.push(selectedRecords[i].get('value'));
                             }
                             // redraw the line chart with new selections
                             this.renderLineChart();                                        }
@@ -384,7 +382,7 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                             store: new Ext.data.JsonStore({
                                 root: 'jsonValues',
                                 fields: ['value'],
-                                data: this.chartInfo.measures[this.yAxisMeasureIndex].dimension
+                                data: this.yAxisDimension
                             }),
                             viewConfig: {forceFit: true},
                             border: false,
@@ -403,7 +401,7 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                                     var dimSelModel = grid.getSelectionModel();
                                     dimSelModel.suspendEvents(false);
                                     dimSelModel.selectRange(0, grid.getStore().getTotalCount()-1, false);
-                                    this.chartInfo.measures[this.yAxisMeasureIndex].dimension.selected = this.chartInfo.measures[this.yAxisMeasureIndex].dimension.values;
+                                    this.yAxisDimension.selected = this.chartInfo.measures[this.yAxisMeasureIndex].dimension.values;
                                     dimSelModel.resumeEvents();
                                     grid.show();
                                 }
@@ -461,27 +459,55 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
         // add all of the values to the subject list view store
         Ext.getCmp('subject-list-view').getStore().add(o.records);
         // initially select all of the subject values from the list view (but suspend events during selection)
+        this.subjectSelections = [];
         var subjectSelModel = Ext.getCmp('subject-list-view').getSelectionModel();
         subjectSelModel.suspendEvents(false);
         subjectSelModel.selectRange(0, o.records.length, false);
         for(var i = 0; i < o.records.length; i++) {
-            this.chartInfo.subject.selection.push(o.records[i].data.value != undefined ? o.records[i].data.value : o.records[i].data);
+            this.subjectSelections.push(o.records[i].data.value != undefined ? o.records[i].data.value : o.records[i].data);
         }
         subjectSelModel.resumeEvents();
         // now that the subject values are loaded, show the list view
         Ext.getCmp('subject-list-view').show();
-        //Ext.getCmp('subject-list-view').setAutoScroll(true);
     },
 
     getChartData: function() {
         this.chart.getEl().mask("Loading...", "x-mask-loading");
         LABKEY.Visualization.getData({
             successCallback: function(data){
-                // store the data in the chartInfo object
-                this.chartInfo.data = data;
+                // store the data in an object by subject for use later when it comes time to render the line chart
+                this.chartSubjectData = new Object();
+                Ext.each(data.rows, function(row){
+                    // get the subject id from the data row
+                    var rowSubject = row[data.measureToColumn[this.viewInfo.subjectColumn]];
+                    if(rowSubject.value ) rowSubject = rowSubject.value;
+
+                    // if this is a new subject to the chartSubjectData object, then initialize it
+                    if(!this.chartSubjectData[rowSubject]) {
+                        this.chartSubjectData[rowSubject] = new Object();
+
+                        // initialize an array for each element in measureToColumn
+                        Ext.iterate(data.measureToColumn, function(key, value, obj){
+                            this.chartSubjectData[rowSubject][key] = new Array();
+                        }, this);
+                    }
+
+                    // add the data value and interval value to the appropriate place in the chartSubjectData object
+                    Ext.iterate(data.measureToColumn, function(key, value, obj){
+                        var dataValue = row[value];
+                        if(typeof dataValue != "object") {
+                            dataValue = {value: dataValue};
+                        }
+
+                        this.chartSubjectData[rowSubject][key].push({
+                            interval: row[this.chartInfo.measures[this.xAxisMeasureIndex].dateOptions.interval],
+                            dataValue: dataValue
+                        });
+                    }, this);
+                }, this);
 
                 // store the temp schema and query name for the data grid
-                this.chartInfo.tempGrid = {schema: data.schemaName, query: data.queryName};
+                this.tempGridInfo = {schema: data.schemaName, query: data.queryName};
 
                 // now that we have the temp grid info, enable the View Grid button
                 // and make sure that the view charts button is hidden
@@ -518,46 +544,26 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
 
 	    // one series per subject/measure/dimensionvalue combination
 	    var seriesList = [];
-	    if(this.chartInfo.measures[this.yAxisMeasureIndex].dimension && this.chartInfo.measures[this.yAxisMeasureIndex].dimension.selected) {
-	        seriesList = this.chartInfo.measures[this.yAxisMeasureIndex].dimension.selected;
+	    if(this.yAxisDimension && this.yAxisDimension.selected) {
+	        seriesList = this.yAxisDimension.selected;
 	    }
 	    else {
 	        seriesList = [this.chartInfo.measures[this.yAxisMeasureIndex].measure.name];
 	    }
 
         var series = [];
-        for(var j = 0; j < this.chartInfo.subject.selection.length; j++)
+        for(var j = 0; j < this.subjectSelections.length; j++)
         {
         	for(var i = 0; i < seriesList.length; i++)
         	{
                 var yAxisSeries = seriesList[i];
-                var subject = this.chartInfo.subject.selection[j];
-                var singleSeriesData = [];
-
-                var subjectColName = this.viewInfo.subjectColumn;
-                Ext.each(this.chartInfo.data.rows, function(row){
-                    var rowSubject = row[this.chartInfo.data.measureToColumn[subjectColName]];
-                    if(rowSubject.value ) rowSubject = rowSubject.value;
-
-                    if(rowSubject == subject)
-                    {
-                        var dataValue = row[this.chartInfo.data.measureToColumn[yAxisSeries]];
-                        if(typeof dataValue != "object") {
-                            dataValue = {value: dataValue};
-                        }
-
-                        singleSeriesData.push({
-                            interval: row[this.chartInfo.measures[this.xAxisMeasureIndex].dateOptions.interval],
-                            dataValue: dataValue
-                        });
-                    }
-                }, this);
+                var subject = this.subjectSelections[j];
 
                 series.push({
                     subject: subject,
                     yAxisSeries: yAxisSeries,
                     caption: subject + " " + yAxisSeries,
-                    data: singleSeriesData,
+                    data: this.chartSubjectData[subject][yAxisSeries],
                     xProperty:"interval",
                     yProperty: "dataValue",
                     dotShape: 'circle'
@@ -571,7 +577,7 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
 	    // three options: all series on one chart, one chart per subject, or one chart per measure/dimension
         var charts = [];
         if(this.chartInfo.layout == "per_subject") {
-        	Ext.each(this.chartInfo.subject.selection, function(subject) {
+        	Ext.each(this.subjectSelections, function(subject) {
         		charts.push(this.newLineChart(size, series, {parameter: "subject", value: subject}, subject));
         	}, this);
         }
@@ -613,7 +619,7 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
                     min: (this.chartInfo.measures[this.yAxisMeasureIndex].axis.min ? this.chartInfo.measures[this.yAxisMeasureIndex].axis.min : undefined),
                     max: (this.chartInfo.measures[this.yAxisMeasureIndex].axis.max ? this.chartInfo.measures[this.yAxisMeasureIndex].axis.max : undefined),
                     caption: this.chartInfo.measures[this.yAxisMeasureIndex].axis.label,
-                    scale: this.chartInfo.yAxisScale
+                    scale: this.chartInfo.measures[this.yAxisMeasureIndex].axis.scale
                 },
                 x: {
                     min: (this.chartInfo.measures[this.xAxisMeasureIndex].axis.min ? this.chartInfo.measures[this.xAxisMeasureIndex].axis.min : undefined),
@@ -640,7 +646,8 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
     },
 
     viewDataGrid: function() {
-        if(typeof this.chartInfo.tempGrid == "object") {
+        // make sure the tempGridInfo is available
+        if(typeof this.tempGridInfo == "object") {
             // mask panel and remove the chart(s)
             this.chart.getEl().mask("Loading...", "x-mask-loading");
             this.chart.removeAll();
@@ -661,8 +668,8 @@ LABKEY.vis.TimeChartPanel = Ext.extend(Ext.Panel, {
             // create the queryWebpart using the temp grid schema and query name
             var chartQueryWebPart = new LABKEY.QueryWebPart({
                 renderTo: gridPanelId,
-                schemaName: this.chartInfo.tempGrid.schema,
-                queryName: this.chartInfo.tempGrid.query,
+                schemaName: this.tempGridInfo.schema,
+                queryName: this.tempGridInfo.query,
                 allowChooseQuery: false,
                 allowChooseView: false,
                 title: "",
