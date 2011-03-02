1d906f3bcafe2632ff5e72f75137e50fefda0854
Adam Rauch <adam@labkey.com>
2011-03-02 02:13:28 +0000

    Issue 11660: External Data Source with Field Named 'Owner' Becomes a Lookup Field
    Update MySQL JDBC driver 5.1.13 -> 5.1.15.  Fixes for MySQL 5.5.

--- query/src/org/labkey/query/data/SimpleUserSchema.java
+++ query/src/org/labkey/query/data/SimpleUserSchema.java
@@ -144,9 +144,11 @@ public class SimpleUserSchema extends UserSchema
                 wrap.setReadOnly(col.isReadOnly());
 
                 final String colName = col.getName();
-                if (colName.equalsIgnoreCase("owner") ||
-                        colName.equalsIgnoreCase("createdby") ||
-                        colName.equalsIgnoreCase("modifiedby"))
+
+                // Add an FK to the Users table for special fields... but ONLY if for type integer and in the LabKey data source.  #11660
+                if (JdbcType.INTEGER == col.getJdbcType() &&
+                   (colName.equalsIgnoreCase("owner") || colName.equalsIgnoreCase("createdby") || colName.equalsIgnoreCase("modifiedby")) &&
+                   (DbScope.isLabKeyScope(_userSchema.getDbSchema().getScope())))
                 {
                     wrap.setFk(new UserIdQueryForeignKey(_userSchema.getUser(), _userSchema.getContainer()));
                 }
