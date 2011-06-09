5df79ab186fc84f27106aa3fecd348d7c7994030
Adam Rauch <adam@labkey.com>
2011-06-09 18:54:56 +0000

    More logging

--- api/src/org/labkey/api/data/RenderContext.java
+++ api/src/org/labkey/api/data/RenderContext.java
@@ -206,6 +206,9 @@ public class RenderContext extends BoundMap // extends ViewContext
                 if (null == displayColumn)
                     continue;
                 displayColumn.addQueryColumns(ret);
+
+                // TODO: Remove this check -- it's overkill, but should help narrow down a TeamCity failure
+                Table.checkAllColumns(tinfo, ret, "RenderContext.getSelectColumns(), after adding " + displayColumn.getName());
             }
 
             // add any additional columns specified by FieldKey
