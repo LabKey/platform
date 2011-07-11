205ce0b6cc57e89c178072f1eda517696431798c
Adam Rauch <adam@labkey.com>
2011-07-11 19:46:48 +0000

    Update licenses

--- study/src/org/labkey/study/query/ParticipantCategoryTable.java
+++ study/src/org/labkey/study/query/ParticipantCategoryTable.java
@@ -9,7 +9,7 @@ import org.labkey.api.study.StudyService;
 import org.labkey.study.StudySchema;
 
 /**
- * Copyright (c) 2008-2010 LabKey Corporation
+ * Copyright (c) 2011 LabKey Corporation
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
