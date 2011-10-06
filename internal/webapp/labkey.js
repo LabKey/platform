f67fec9ae9a570194c23b560d2858628e4472da2
Karl Lum <klum@labkey.com>
2011-10-06 20:38:59 +0000

    add ext4 sandbox : an earlier download of 4.0.2a did not contain the separate sandbox file, so we created our own version based on the 4.0.1 version. The latest download contains the files missing earlier (including a minified version).

--- internal/webapp/labkey.js
+++ internal/webapp/labkey.js
@@ -260,7 +260,7 @@ LABKEY.requiresExt4Sandbox = function(immediate)
     if (arguments.length < 1) immediate = true;
 
     LABKEY.requiresCss(LABKEY.extJsRoot_40 + '/resources/css/ext-sandbox.css', true);
-    LABKEY.requiresScript(LABKEY.extJsRoot_40 + "/ext-all-dev.js", immediate);
+    LABKEY.requiresScript(LABKEY.extJsRoot_40 + "/ext-all-sandbox" + (LABKEY.devMode ?  "-debug.js" : ".js"), immediate);
     LABKEY.requiresScript(LABKEY.extJsRoot_40 + "/ext-patches.js", immediate);
 };
 
