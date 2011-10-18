b7139df29b8b3709d253dfb35e4466d59f167328
Adam Rauch <adam@labkey.com>
2011-10-18 01:24:55 +0000

    Update licenses

--- study/webapp/study/DataViewsPanel.js
+++ study/webapp/study/DataViewsPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 LABKEY.requiresExt4Sandbox(true);
 
 Ext4.define('LABKEY.ext4.DataViewsPanel', {
