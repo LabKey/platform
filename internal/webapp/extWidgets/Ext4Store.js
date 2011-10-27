c26546bb809e3da9467dec8fb9a573f30cc6ee06
Adam Rauch <adam@labkey.com>
2011-10-27 23:53:40 +0000

    Update licenses

--- internal/webapp/extWidgets/Ext4Store.js
+++ internal/webapp/extWidgets/Ext4Store.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 LABKEY.requiresExt4Sandbox(true);
 LABKEY.requiresScript('/extWidgets/MetaHelper.js');
 
--- internal/webapp/extWidgets/ImportWizard.js
+++ internal/webapp/extWidgets/ImportWizard.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 //experimental
 
 Ext4.define('LABKEY.ext.ImportWizard', {
