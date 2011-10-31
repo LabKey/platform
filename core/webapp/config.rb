4f58092fb1efae2624db4ff39b1c86b8692edb16
Nick Kerr <nicka@labkey.com>
2011-10-31 23:22:30 +0000

    Example .scss file - modify servlet to work with windows

---
+++ core/webapp/config.rb
@@ -0,0 +1,14 @@
+# sass_path: the directory your Sass files are in. THIS file should also be in the Sass folder
+# Generally this will be in a resources/sass folder
+# <root>/resources/sass
+sass_path = File.dirname(__FILE__)
+
+# css_path: the directory you want your CSS files to be.
+# Generally this is a folder in the parent directory of your Sass files
+# <root>/resources/css
+css_path = sass_path
+
+# output_style: The output style for your compiled CSS
+# nested, expanded, compact, compressed
+# More information can be found here http://sass-lang.com/docs/yardoc/file.SASS_REFERENCE.html#output_style
+output_style = :expanded
