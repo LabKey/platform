6c5c60d7fc2115980dcdda33a43d309d66348fea
Alan Vezina <alanv@labkey.com>
2011-11-09 01:26:43 +0000

    Issue 12952: For issues custom fields are character varying(200), while others are character varying(32)  - Type, Area, Milestone, and Resolutions are now character varying(200) on postgres and NVARCHAR(200) on SQLServer.

--- issues/src/org/labkey/issue/IssuesModule.java
+++ issues/src/org/labkey/issue/IssuesModule.java
@@ -67,7 +67,7 @@ public class IssuesModule extends DefaultModule implements SearchService.Documen
 
     public double getVersion()
     {
-        return 11.20;
+        return 11.21;
     }
 
     protected void init()
