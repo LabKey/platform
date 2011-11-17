a9cb4f9bd6dee6400ba0f791b7926021a5fdeb25
Nick Kerr <nicka@labkey.com>
2011-11-17 20:25:33 +0000

    Remove Ext-4.0.2a - Update build and credits for 4.0.7

--- internal/webapp/labkey.js
+++ internal/webapp/labkey.js
@@ -14,7 +14,6 @@ if (typeof LABKEY == "undefined")
     LABKEY.devMode = false;
     LABKEY.demoMode = false;
     LABKEY.extJsRoot = "ext-3.4.0";
-//    LABKEY.extJsRoot_40 = "ext-4.0.2a";
     LABKEY.extJsRoot_40 = "ext-4.0.7";
     LABKEY.verbose = false;
     LABKEY.widget = {};
