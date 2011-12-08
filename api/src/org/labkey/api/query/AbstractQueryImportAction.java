e6036362648e59104ad9a9d1cbdb496ed785f118
Josh Eckels <jeckels@labkey.com>
2011-12-08 21:43:34 +0000

    Use specialized sample set import UI for Import Data target
    Don't try to commit transaction in the case of errors in sample set QueryUpdateService

--- api/src/org/labkey/api/query/AbstractQueryImportAction.java
+++ api/src/org/labkey/api/query/AbstractQueryImportAction.java
@@ -269,9 +269,9 @@ public abstract class AbstractQueryImportAction<FORM> extends FormApiAction<FORM
             scope.beginTransaction();
             List res = _updateService.insertRows(getViewContext().getUser(), getViewContext().getContainer(), dl.load(), errors, new HashMap<String, Object>());
 //            List res = _updateService.importRows(getViewContext().getUser(), getViewContext().getContainer(), dl.getDataIterator(errors), errors, new HashMap<String, Object>());
-            scope.commitTransaction();
             if (errors.hasErrors())
                 return 0;
+            scope.commitTransaction();
             return res.size();
         }
         catch (BatchValidationException x)
