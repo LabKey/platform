982131dd5493fe7535d81d3529d3a59832de03ed
Adam Rauch <adam@labkey.com>
2011-12-09 01:30:39 +0000

    Consolidate SQL scripts (module version bumps will come after branching)

--- api/src/org/labkey/api/util/Formats.java
+++ api/src/org/labkey/api/util/Formats.java
@@ -30,7 +30,9 @@ public class Formats
     public static DecimalFormat f2 = new DecimalFormat("0.00");
     public static DecimalFormat fv2 = new DecimalFormat("#.##");
     public static DecimalFormat f3 = new DecimalFormat("0.000");
+    public static DecimalFormat fv3 = new DecimalFormat("#.###");
     public static DecimalFormat f4 = new DecimalFormat("0.0000");
+    public static DecimalFormat fv4 = new DecimalFormat("#.####");
     public static DecimalFormat signf4 = new DecimalFormat("+0.0000;-0.0000");
     public static DecimalFormat percent = new DecimalFormat("0%");
     public static DecimalFormat percent1 = new DecimalFormat("0.0%");
