e87d96a299f56f906a935979ee6be04a6d8138e4
Adam Rauch <adam@labkey.com>
2011-12-23 13:48:36 +0000

    Update licenses

--- api/src/org/labkey/api/nab/NabUrls.java
+++ api/src/org/labkey/api/nab/NabUrls.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2010-2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.labkey.api.nab;
 
 import org.labkey.api.action.UrlProvider;
@@ -6,21 +21,6 @@ import org.labkey.api.exp.api.ExpProtocol;
 import org.labkey.api.view.ActionURL;
 
 /**
- * Copyright (c) 2010-2011 LabKey Corporation
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p/>
- * <p/>
  * User: brittp
  * Date: Aug 19, 2010 10:57:31 AM
  */
