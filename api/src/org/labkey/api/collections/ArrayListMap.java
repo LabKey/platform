617e4d2d4a2878a20f38fe2c11e8e8a64db3d862
Adam Rauch <adam@labkey.com>
2012-03-21 15:59:38 +0000

    Update licenses

--- api/src/org/labkey/api/collections/ArrayListMap.java
+++ api/src/org/labkey/api/collections/ArrayListMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2011 Fred Hutchinson Cancer Research Center
+ * Copyright (c) 2003-2012 Fred Hutchinson Cancer Research Center
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
