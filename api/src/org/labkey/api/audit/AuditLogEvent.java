38b6ec5c6ddf1a17ccb5701163f7b8b963ddca1a
Matthew Bellew <mbellew@labkey.com>
2012-03-28 16:05:19 +0000

    14454: Access Modification History for Guest user at Site level shows strange list of events AuditLogEvent intkeys NULLABLE

--- api/src/org/labkey/api/audit/AuditLogEvent.java
+++ api/src/org/labkey/api/audit/AuditLogEvent.java
@@ -39,9 +39,9 @@ public class AuditLogEvent
     private String _key1;
     private String _key2;
     private String _key3;
-    private int _intKey1;
-    private int _intKey2;
-    private int _intKey3;
+    private Integer _intKey1;
+    private Integer _intKey2;
+    private Integer _intKey3;
     private String _lsid;
 
     public int getRowId()
@@ -149,10 +149,10 @@ public class AuditLogEvent
     public void setKey1(String key){_key1 = key;}
     public String getKey2(){return _key2;}
     public void setKey2(String key){_key2 = key;}
-    public int getIntKey1(){return _intKey1;}
-    public void setIntKey1(int key){_intKey1 = key;}
-    public int getIntKey2(){return _intKey2;}
-    public void setIntKey2(int key){_intKey2 = key;}
+    public Integer getIntKey1(){return _intKey1;}
+    public void setIntKey1(Integer key){_intKey1 = key;}
+    public Integer getIntKey2(){return _intKey2;}
+    public void setIntKey2(Integer key){_intKey2 = key;}
     public String getLsid(){return _lsid;}
     public void setLsid(String lsid){_lsid = lsid;}
 
@@ -166,12 +166,12 @@ public class AuditLogEvent
         _key3 = key3;
     }
 
-    public int getIntKey3()
+    public Integer getIntKey3()
     {
         return _intKey3;
     }
 
-    public void setIntKey3(int intKey3)
+    public void setIntKey3(Integer intKey3)
     {
         _intKey3 = intKey3;
     }
