a3fb8eb99ee05b9deb63d04438bba7e9331f3718
Alan Vezina <alanv@labkey.com>
2012-05-09 23:25:04 +0000

    Added functions to LABKEY.vis.Stat 	-Q1, Q2, Q3 		- Generate the 1st, 2nd, and 3rd quartiles. 		- Q2 is also aliased to median 	-sortNumericAscending(data, accessor) 		- Sorts a numeric set of data in ascending order 	-sortNumericDescending(data, accessor) 		- Sorts a numeric set of data in descending order 	-summary(data, accessor) 		- Returns max, min, Q1, Q2, Q3, IQR, and a sorted array of the numeric values (ascending).

--- internal/webapp/vis/src/stat.js
+++ internal/webapp/vis/src/stat.js
@@ -17,3 +17,50 @@ if(!LABKEY.vis){
 if(!LABKEY.vis.Stat){
 	LABKEY.vis.Stat = {};
 }
+
+LABKEY.vis.Stat.summary = function(data, accessor){
+    /*
+        Returns an object with the min, max, Q1, Q2 (median), Q3, interquartile range, and the sorted array of values.
+     */
+    var summary = {};
+
+    summary.sortedValues = LABKEY.vis.Stat.sortNumericAscending(data, accessor);
+    summary.min = summary.sortedValues[0];
+    summary.max = summary.sortedValues[summary.sortedValues.length -1];
+    summary.Q1 = LABKEY.vis.Stat.Q1(summary.sortedValues);
+    summary.Q2 = LABKEY.vis.Stat.Q2(summary.sortedValues);
+    summary.Q3 = LABKEY.vis.Stat.Q3(summary.sortedValues);
+    summary.IQR = summary.Q3 - summary.Q1;
+
+    return summary;
+};
+
+LABKEY.vis.Stat.Q1 = function(numbers){
+    return d3.quantile(numbers,0.25);
+};
+
+LABKEY.vis.Stat.Q2 = LABKEY.vis.Stat.median = function(numbers){
+    return d3.quantile(numbers,0.5);
+};
+
+LABKEY.vis.Stat.Q3 = function(numbers){
+    return d3.quantile(numbers,0.75);
+};
+
+LABKEY.vis.Stat.sortNumericAscending = function(data, accessor){
+    var numbers = [];
+    for(var i = 0; i < data.length; i++){
+        numbers.push(accessor(data[i]));
+    }
+    numbers.sort(function(a, b){return a-b;});
+    return numbers;
+};
+
+LABKEY.vis.Stat.sortNumericDescending = function(data, accessor){
+    var numbers = [];
+    for(var i = 0; i < data.length; i++){
+        numbers.push(accessor(data[i]));
+    }
+    numbers.sort(function(a, b){return b-a;});
+    return numbers;
+};
