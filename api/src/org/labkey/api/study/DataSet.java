b858270c679f58899e97c55ae5fd36406648b85c
Kevin Krouse <kevink@labkey.com>
2012-06-05 00:49:13 +0000

    Issue 15119: Assay datasets offer join to all other assay datasets - treat assay backed datasets as ptid/sequencenum type joins

--- api/src/org/labkey/api/study/DataSet.java
+++ api/src/org/labkey/api/study/DataSet.java
@@ -108,7 +108,7 @@ public interface DataSet<T extends DataSet> extends StudyEntity, StudyCachable<T
     String getKeyTypeDescription();
 
     /**
-     * Compares the extra key for this dataset with the passed in dataset
+     * Compares the extra key for this dataset with the passed in dataset.
      * @param pkDataSet dataset to compare
      * @return true if the extra key for this DataSet matches the extra key for the passed in dataset
      */
