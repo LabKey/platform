2cc8f7bd9d42c8312062b6a111932665c5f70acc
Ben Bimber <bbimber@labkey.com>
2012-06-28 16:27:01 +0000

    add file missed w/ previous checkin

--- experiment/src/org/labkey/experiment/ExperimentModule.java
+++ experiment/src/org/labkey/experiment/ExperimentModule.java
@@ -95,7 +95,7 @@ public class ExperimentModule extends SpringModule implements SearchService.Docu
 
     public double getVersion()
     {
-        return 12.15;
+        return 12.16;
     }
 
     protected void init()
