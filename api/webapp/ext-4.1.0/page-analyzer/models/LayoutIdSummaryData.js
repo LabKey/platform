3b9f55e0a7b5af9a24b63f71620aed581b55417b
Ben Bimber <bbimber@labkey.com>
2012-06-29 22:24:21 +0000

    modify Ext4 PageAnalyzer to work w/ sandbox

---
+++ api/webapp/ext-4.1.0/page-analyzer/models/LayoutIdSummaryData.js
@@ -0,0 +1,13 @@
+(function(Ext){
+
+Ext.define('PageAnalyzer.models.LayoutIdSummaryData', {
+    extend: 'Ext.data.Model',
+    fields: [
+        { name: 'type', type: 'string' },
+        { name: 'duration', type: 'float', defaultValue: 0.0 },
+        { name: 'count', type: 'int', defaultValue: 0 }
+    ]
+
+});
+
+})(Ext4);
