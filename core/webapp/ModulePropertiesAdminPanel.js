9a26c0e26c9e67c53b4f30c5507bf348d0cd5b62
Adam Rauch <adam@labkey.com>
2012-07-13 00:04:37 +0000

    Update licenses

--- core/webapp/ModulePropertiesAdminPanel.js
+++ core/webapp/ModulePropertiesAdminPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.ns('LABKEY.ext');
 
 Ext4.define('LABKEY.ext.ModulePropertiesAdminPanel', {
