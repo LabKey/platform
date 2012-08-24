1eb4765d6909d30f70de12e7576a989c303858a3
Adam Rauch <adam@labkey.com>
2012-08-24 00:03:03 +0000

    Update licenses

--- core/resources/scripts/ExtAdapter.js
+++ core/resources/scripts/ExtAdapter.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 var Ext = require("Ext4").Ext;
 var console = require("console");
 
