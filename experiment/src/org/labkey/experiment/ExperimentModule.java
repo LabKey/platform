fe9b6b4366b9666df5fedf2a506230adc0b76f4f
Josh Eckels <jeckels@labkey.com>
2012-08-28 17:38:57 +0000

    Issue 14159: SQLException from org.labkey.api.exp.OntologyManager.getPropertiesForType()
    Removed index on DomainURI, Container. Flipped column order on other index so that it's now DomainURI, Project. It's more common to query based just on DomainURI than it is to go on Project.

--- experiment/src/org/labkey/experiment/ExperimentModule.java
+++ experiment/src/org/labkey/experiment/ExperimentModule.java
@@ -94,7 +94,7 @@ public class ExperimentModule extends SpringModule implements SearchService.Docu
 
     public double getVersion()
     {
-        return 12.22;
+        return 12.23;
     }
 
     protected void init()
