36530e8cf4be13096512a3b1885ecba27e497735
Adam Rauch <adam@labkey.com>
2012-11-15 01:55:39 +0000

    Update licenses

--- internal/webapp/SQVSelector.js
+++ internal/webapp/SQVSelector.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 /**
  * Created with IntelliJ IDEA.
  * User: Rylan
