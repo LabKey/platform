dcbf1c5f42607916548e34d8a0babf25cf71fab5
Cory Nathe <cnathe@labkey.com>
2012-12-13 16:22:28 +0000

    Support card layout for survey display (in addition to collapsible panels for the survey sections)

--- survey/web/survey/SurveyPanel.js
+++ survey/web/survey/SurveyPanel.js
@@ -13,13 +13,15 @@ Ext4.define('LABKEY.ext4.SurveyPanel', {
 
     constructor : function(config){
 
-        Ext4.applyIf(config, {
+        Ext4.apply(config, {
             border: true,
-            bodyStyle: 'background-color: transparent;',
-            width: 740,
-            items: [],
-            buttons: [],
-            buttonAlign: 'center'
+            width: 770,
+            layout: {
+                type: 'hbox',
+                pack: 'start',
+                align: 'stretchmax'
+            },
+            items: []
         });
 
         this.callParent([config]);
@@ -29,6 +31,7 @@ Ext4.define('LABKEY.ext4.SurveyPanel', {
 
         if (this.surveyDesignId)
         {
+            // query the DB for the survey design metadata from the given survey design ID
             Ext4.Ajax.request({
                 url     : LABKEY.ActionURL.buildURL('survey', 'getSurveyTemplate.api'),
                 method  : 'POST',
@@ -39,6 +42,7 @@ Ext4.define('LABKEY.ext4.SurveyPanel', {
                     if (o.success)
                     {
                         var metadata = Ext4.JSON.decode(o.survey.metadata);
+                        this.getSurveyLayout(metadata);
                         this.generateSurveySections(metadata);
                     }
                 },
@@ -52,76 +56,78 @@ Ext4.define('LABKEY.ext4.SurveyPanel', {
         this.callParent();
     },
 
-    setSurveyLayout: function(layout) {
-
-        // in card layout, we need to add previous/next buttons
-        this.layout = layout;
-        if (this.layout == 'card')
-        {
-            this.buttons.add({
-                text: 'Previous',
-                disabled: true
-            });
-            this.buttons.add({
-                text: 'Next',
-                disabled: true
-            });
-        }
+    getSurveyLayout : function(surveyConfig) {
+        this.surveyLayout = 'auto';
+        if (surveyConfig.survey && surveyConfig.survey.layout)
+            this.surveyLayout = surveyConfig.survey.layout;
     },
 
-    generateSurveySections: function(surveyConfig) {
+    generateSurveySections : function(surveyConfig) {
 
         // TODO: what survey config validation is needed here? (verify survey, layout, sections, etc.)
+        this.sections = [];
 
-        this.setSurveyLayout(surveyConfig.survey.layout);
+        this.addSurveyStartPanel(surveyConfig.survey.labelCaption);
 
-        // at the top level each item is a panel for a given survey section
-        Ext4.each(surveyConfig.survey.sections, function(section){
-            var sectionPanel = Ext4.create('Ext.form.Panel', {
-                border: false,
-                bodyStyle: 'background-color: transparent; padding: 5px;',
-                header: section.header != undefined ? section.header : true,
-                collapsible: section.collapsible != undefined ? section.collapsible : true,
-                title: section.title || '',
-                titleCollapse: true,
-                defaults: {
-                    labelWidth: section.defaultLabelWidth || 250,
-                    labelSeparator: '',
-                    padding: 10
-                },
-                items: []
-            });
+        // add each of the survey sections as a panel to the sections array
+        if (surveyConfig.survey)
+        {
+            Ext4.each(surveyConfig.survey.sections, function(section){
+                var sectionPanel = Ext4.create('Ext.form.Panel', {
+                    border: false,
+                    header: this.surveyLayout == 'card' ? false :
+                            (section.header != undefined ? section.header : true),
+                    collapsed: section.collapsed != undefined ? section.collapsed : false,
+                    collapsible: section.collapsible != undefined ? section.collapsible : true,
+                    title: section.title || '',
+                    titleCollapse: true,
+                    defaults: {
+                        labelWidth: section.defaultLabelWidth || 250,
+                        labelSeparator: '',
+                        padding: 10
+                    },
+                    items: []
+                });
 
-            // there is a section description, add it as the first item for that section
-            if (section.description)
-            {
-                sectionPanel.add({
-                    xtype: 'displayfield',
-                    hideLabel: true,
-                    value: section.description
-                })
-            }
-
-            // each section can have a set of questions to be added using the FormHelper
-            if (section.questions)
-            {
-                for (var i = 0; i < section.questions.length; i++)
+                // for card layout, add the section title as a displayfield instead of a panel header
+                if (this.surveyLayout == 'card' && section.title)
                 {
-                    var question = section.questions[i];
+                    var txt = Ext.DomHelper.markup({tag:'div', cls:'labkey-nav-page-header', html: section.title}) +
+                            Ext.DomHelper.markup({tag:'div', html:'&nbsp;'});
+                    sectionPanel.add({xtype:'displayfield', value: txt});
+                }
 
-                    // not currently using helpPopups for the survey question field labels
-                    question.helpPopup = [];
+                // there is a section description, add it as the first item for that section
+                if (section.description)
+                {
+                    sectionPanel.add({
+                        xtype: 'displayfield',
+                        hideLabel: true,
+                        value: section.description
+                    })
+                }
 
-                    var config;
-                    if (question.extConfig)
+                // each section can have a set of questions to be added using the FormHelper
+                if (section.questions)
+                {
+                    for (var i = 0; i < section.questions.length; i++)
                     {
-                        // if the question is defined as an ext component, pass it through
-                        config = question.extConfig;
-                    }
-                    else
-                    {
-                        // if the question is not defined as ext, use the column metadata form helper
-                        config = LABKEY.ext.Ext4Helper.getFormEditorConfig(question);
+                        var question = section.questions[i];
+
+                        // not currently using helpPopups for the survey question field labels
+                        question.helpPopup = [];
+
+                        var config;
+                        if (question.extConfig)
+                        {
+                            // if the question is defined as an ext component, pass it through
+                            config = question.extConfig;
+                        }
+                        else
+                        {
+                            // if the question is not defined as ext, use the column metadata form helper
+                            config = LABKEY.ext.Ext4Helper.getFormEditorConfig(question);
+                        }
 
                         // make the field label for required questions bold and end with an *
                         if (question.nullable != undefined && !question.nullable)
@@ -132,22 +138,165 @@ Ext4.define('LABKEY.ext4.SurveyPanel', {
                         }
 
                         // TODO: any question customizations?
+
+                        sectionPanel.add(config);
                     }
-
-                    sectionPanel.add(config);
                 }
-            }
 
-            this.add(sectionPanel);
-        }, this);
+                this.sections.push(sectionPanel);
+            }, this);
+        }
+
+        this.addSurveyEndPanel();
+
+        this.setSurveyLayout(surveyConfig);
+    },
+
+    addSurveyStartPanel : function(labelCaption) {
+
+        // add an initial panel that has the survey label text field
+        this.sections.push(Ext4.create('Ext.form.Panel', {
+            title: 'Begin Survey',
+            header: false,
+            border: false,
+            minHeight: 60,
+            items: [{
+                xtype: 'textfield',
+                fieldLabel: (labelCaption || 'Survey Label') + '*',
+                labelStyle: 'font-weight: bold;',
+                labelWidth: 250,
+                labelSeparator: '',
+                padding: 10,
+                allowBlank: false,
+                width: 700
+            }]
+        }));
+    },
+
+    addSurveyEndPanel : function() {
+
+        // add a final panel that has the Save/Submit buttons and required field checks
+        // TODO: the layout of this panel will have to change when we add text about which required fields are missing
+        this.sections.push(Ext4.create('Ext.panel.Panel', {
+            title: 'End Survey',
+            layout: {
+                type: 'hbox',
+                align: 'middle',
+                pack: 'center'
+            },
+            header: false,
+            border: false,
+            minHeight: 100,
+            items: [{
+                xtype: 'button',
+                text: 'Save',
+                disabled: true,
+                width: 150,
+                height: 30
+            },{
+                xtype: 'label',
+                width: 100,
+                value: null
+            },{
+                xtype: 'button',
+                text: 'Submit completed form',
+                disabled: true,
+                width: 250,
+                height: 30
+            }]
+        }));
+    },
+
+    setSurveyLayout: function(surveyConfig) {
+
+        this.currentStep = 0;
+
+        // in card layout, we add a side bar with the section titles and next/previous buttons
+        var bbar = [];
+        if (this.surveyLayout == 'card')
+        {
+            this.width += 250;
+
+            this.sideBar = Ext4.create('Ext.panel.Panel', {
+                name: 'sidebar',
+                width: 250,
+                border: false,
+                cls: 'extContainer',
+                tpl: [
+                    '<div class="labkey-ancillary-wizard-background">',
+                    '<ol class="labkey-ancillary-wizard-steps">',
+                    '<tpl for="steps">',
+
+                        '<tpl if="values.currentStep == true">',
+                        '<li class="labkey-ancillary-wizard-active-step">{value}</li>',
+                        '</tpl>',
+
+                        '<tpl if="values.currentStep == false">',
+                        '<li>{value}</li>',
+                        '</tpl>',
+
+                    '</tpl>',
+                    '</ol>',
+                    '</div>'
+                ],
+                data: {steps: this.getStepsDataArr()}
+            });
+            this.add(this.sideBar);
+
+            this.prevBtn = Ext4.create('Ext.button.Button', {
+                text: 'Previous',
+                disabled: true,
+                scope: this,
+                handler: function(cmp){
+                    this.currentStep--;
+                    this.updateStep();
+            }});
+
+            this.nextBtn = Ext4.create('Ext.button.Button', {
+                text: 'Next',
+                scope: this,
+                handler: function(cmp){
+                    this.currentStep++;
+                    this.updateStep();
+            }});
+            bbar = ['->', this.prevBtn, this.nextBtn];
+        }
+
+        this.centerPanel = Ext4.create('Ext.panel.Panel', {
+            layout: this.surveyLayout,
+            border: false,
+            minHeight: this.surveyLayout == 'card' ? 500 : undefined,
+            bodyStyle : 'padding: 20px;',
+            activeItem: 0,
+            flex: 1,
+            items: this.sections,
+            bbar: bbar.length > 0 ? bbar : undefined
+        });
+        this.add(this.centerPanel);
+    },
+
+    getStepsDataArr : function() {
+        var steps = [];
+        for (var i = 0; i < this.sections.length; i++)
+        {
+            steps.push({value: this.sections[i].title, currentStep: i == this.currentStep});
+        }
+        return steps;
+    },
+
+    updateStep : function() {
+        this.sideBar.update({steps: this.getStepsDataArr()});
+        this.centerPanel.getLayout().setActiveItem(this.currentStep);
+
+        this.prevBtn.setDisabled(this.currentStep == 0);
+        this.nextBtn.setDisabled(this.currentStep == this.sections.length-1);
     },
 
     onFailure : function(resp){
         var error = Ext4.decode(resp.responseText).exception;
-        if(error){
-            Ext.MessageBox.alert('Error', error);
-        } else {
-            Ext.MessageBox.alert('Error', 'An unknown error has ocurred, unable to save the chart.');
-        }
+        if (error)
+            Ext4.MessageBox.alert('Error', error);
+        else
+            Ext4.MessageBox.alert('Error', 'An unknown error has ocurred, unable to save the survey.');
     }
 });
