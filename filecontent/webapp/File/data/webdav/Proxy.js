f13644589e59f3c7db50f3886935d5a99b0fea2b
Adam Rauch <adam@labkey.com>
2012-12-27 19:38:57 +0000

    Update licenses

--- filecontent/webapp/File/data/webdav/Proxy.js
+++ filecontent/webapp/File/data/webdav/Proxy.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.define('File.data.webdav.Proxy', {
     extend : 'Ext.data.proxy.Ajax',
     alias : 'proxy.webdav',
--- filecontent/webapp/File/panel/Browser.js
+++ filecontent/webapp/File/panel/Browser.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 LABKEY.requiresCss("_images/icons.css");
 LABKEY.requiresCss("study/DataViewsPanel.css");
 
