b747e0e9949bde7e5cf253d1743630af2a3a1a33
Alan Vezina <alanv@labkey.com>
2013-01-12 01:05:08 +0000

    Update core.PortalWebParts to support saving of webpart permissions.

--- core/src/org/labkey/core/CoreModule.java
+++ core/src/org/labkey/core/CoreModule.java
@@ -196,7 +196,7 @@ public class CoreModule extends SpringModule implements SearchService.DocumentPr
     @Override
     public double getVersion()
     {
-        return 12.32;
+        return 12.33;
     }
 
     @Override
