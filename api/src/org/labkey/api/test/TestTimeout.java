6f2a044bb6e875a9c80ee5b219e8f7283f37f45b
Josh Eckels <jeckels@labkey.com>
2013-01-16 17:41:43 +0000

    Add annotation for setting non-default timeout for unit tests

---
+++ api/src/org/labkey/api/test/TestTimeout.java
@@ -0,0 +1,20 @@
+package org.labkey.api.test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Timeout for server-side unit tests, in seconds
+ * User: jeckels
+ * Date: 1/14/13
+ */
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface TestTimeout
+{
+    /** One minute is our standard timeout */
+    public static final int DEFAULT = 60;
+
+    int value() default DEFAULT;
+}
