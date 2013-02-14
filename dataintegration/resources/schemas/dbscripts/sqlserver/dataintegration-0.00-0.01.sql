e942a8733f3c2d19616811f707d5ac35e44fea4d
Matthew Bellew <mbellew@labkey.com>
2013-02-14 19:17:44 +0000

    dataintegration.addDataIntegrationColumns

--- dataintegration/resources/schemas/dbscripts/sqlserver/dataintegration-0.00-0.01.sql
+++ dataintegration/resources/schemas/dbscripts/sqlserver/dataintegration-0.00-0.01.sql
@@ -10,4 +10,8 @@ SELECT @sql = 'ALTER TABLE [' + @schemaName + '].[' + @tableName + '] ADD  ' +
      '_txTransactionId INT, ' +
      '_txNote NVARCHAR(1000)';
 EXEC sp_executesql @sql;
+SELECT @sql = 'ALTER TABLE [' + @schemaName + '].[' + @tableName + '] ADD CONSTRAINT [_DF_' + @tableName + '_updated] ' +
+    'DEFAULT getutcdate() FOR [_txLastUpdated]';
+EXEC sp_executesql @sql;
+
 GO
