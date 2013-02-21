55bfccd42da5a6407e7d079eccd4caf0db422f1d
Nick Kerr <nicka@labkey.com>
2013-02-21 21:35:27 +0000

    ITN: Get Cohort Filters to replace properly in AJAX cases.

--- study/src/org/labkey/study/model/CohortManager.java
+++ study/src/org/labkey/study/model/CohortManager.java
@@ -260,14 +260,11 @@ public class CohortManager
 
                     for (CohortFilter.Type type : CohortFilter.Type.values())
                     {
-                        CohortFilter filter = new SingleCohortFilter(type, cohort);
-                        ActionURL url = filter.addURLParameters(study, baseURL.clone(), dataRegionName);
-//                        SingleCohortFilter filter = new SingleCohortFilter(type, cohort);
-//                        Pair<FieldKey, String> filterColValue = filter.getURLFilter(study);
-//                        NavTree typeItem = new NavTree(type.getTitle());
-//
-//                        typeItem.setScript(getSelectionScript(dataRegionName, filterColValue));
-                        NavTree typeItem = new NavTree(type.getTitle(), url.toString());
+                        SingleCohortFilter filter = new SingleCohortFilter(type, cohort);
+                        Pair<FieldKey, String> filterColValue = filter.getURLFilter(study);
+                        NavTree typeItem = new NavTree(type.getTitle());
+
+                        typeItem.setScript(getSelectionScript(dataRegionName, filterColValue));
                         typeItem.setId(cohort.getLabel() + ":" + typeItem.getText());
                         if (filter.equals(currentCohortFilter))
                             typeItem.setSelected(true);
@@ -281,14 +278,11 @@ public class CohortManager
             {
                 for (CohortImpl cohort : cohorts)
                 {
-                    CohortFilter filter = new SingleCohortFilter(CohortFilter.Type.PTID_CURRENT, cohort);
-                    ActionURL url = filter.addURLParameters(study, baseURL.clone(), dataRegionName);
-                    NavTree item = new NavTree(cohort.getLabel(), url.toString());
-//                    SingleCohortFilter filter = new SingleCohortFilter(CohortFilter.Type.PTID_CURRENT, cohort);
-//                    Pair<FieldKey, String> filterColValue = filter.getURLFilter(study);
-//                    NavTree item = new NavTree(cohort.getLabel());
-//
-//                    item.setScript(getSelectionScript(dataRegionName, filterColValue));
+                    SingleCohortFilter filter = new SingleCohortFilter(CohortFilter.Type.PTID_CURRENT, cohort);
+                    Pair<FieldKey, String> filterColValue = filter.getURLFilter(study);
+                    NavTree item = new NavTree(cohort.getLabel());
+
+                    item.setScript(getSelectionScript(dataRegionName, filterColValue));
                     item.setId(caption + ":" + item.getText());
                     if (filter.equals(currentCohortFilter))
                         item.setSelected(true);
@@ -301,11 +295,12 @@ public class CohortManager
     private String getSelectionScript(String dataRegionName, Pair<FieldKey, String> filterColValue)
     {
         StringBuilder script = new StringBuilder();
-        script.append("LABKEY.DataRegions['").append(dataRegionName).append("'].replaceFilter(")
+        script.append("(function() { ")
+              .append("LABKEY.DataRegions['").append(dataRegionName).append("'].replaceAdvCohortFilter(")
               .append("LABKEY.Filter.create('")
               .append(filterColValue.first).append("', '").append(filterColValue.second)
               .append("', LABKEY.Filter.Types.EQUAL)")
-              .append(");");
+              .append("); })(); ");
         return script.toString();
     }
 
