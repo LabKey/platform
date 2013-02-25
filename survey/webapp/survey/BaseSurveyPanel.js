4f0e44d11eb0be0ae8f9e1b625effb8948b30818
Adam Rauch <adam@labkey.com>
2013-02-25 23:06:41 +0000

    Update licenses

--- survey/webapp/survey/BaseSurveyPanel.js
+++ survey/webapp/survey/BaseSurveyPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 LABKEY.requiresExt4ClientAPI();
 LABKEY.requiresScript("/extWidgets/Ext4Helper.js");
