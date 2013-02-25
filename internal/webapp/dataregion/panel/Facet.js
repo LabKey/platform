6f65c4629f623dcfffef932885d9ff3630b16028
Adam Rauch <adam@labkey.com>
2013-02-25 23:06:41 +0000

    Update licenses

--- internal/webapp/dataregion/panel/Facet.js
+++ internal/webapp/dataregion/panel/Facet.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.define('LABKEY.dataregion.panel.Facet', {
 
     extend : 'Ext.Panel',
