4f6026346e2e309b4c64466f4dae0af1b2a841ef
Cory Nathe <cnathe@labkey.com>
2013-03-21 14:15:51 +0000

    Fix SurveyTest

--- survey/webapp/survey/SurveyPanel.js
+++ survey/webapp/survey/SurveyPanel.js
@@ -568,7 +568,7 @@ Ext4.define('LABKEY.ext4.SurveyPanel', {
     },
 
     updateSubmitInfo : function() {
-        var msg = "";
+        var msg = this.surveyLabel == null ? "-" + this.labelCaption : "";
         for (var name in this.validStatus)
         {
             if (!this.validStatus[name])
