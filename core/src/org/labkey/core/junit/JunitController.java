ccce1d25dcf0292b80fc5ebec3d99cb70fe9c3c7
Trey Chadick <tchad@labkey.com>
2013-03-25 17:51:37 +0000

    Add some temporary logging to track down JUnit failure

--- core/src/org/labkey/core/junit/JunitController.java
+++ core/src/org/labkey/core/junit/JunitController.java
@@ -400,7 +400,14 @@ public class JunitController extends SpringActionController
             JSONObject json = new JSONObject(map);
 
             HttpServletResponse response = getViewContext().getResponse();
-            response.reset();
+            try{
+                response.reset();
+            }
+            catch (IllegalStateException ex)
+            {
+                //TODO: Remove. It is temporary logging to investigate failure from ContainerDisplayColumn
+                logger.error(json.toString(2));
+            }
             response.setStatus(status);
 
             PrintWriter out = response.getWriter();
