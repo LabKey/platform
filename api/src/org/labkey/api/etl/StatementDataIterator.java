eacc59bf51b9d578b875abf3011d12a92fa975e9
Matthew Bellew <mbellew@labkey.com>
2013-05-12 16:26:23 +0000

    reselect fix

--- api/src/org/labkey/api/etl/StatementDataIterator.java
+++ api/src/org/labkey/api/etl/StatementDataIterator.java
@@ -258,7 +258,7 @@ class StatementDataIterator extends AbstractDataIterator
                 {
                     _currentStmt.execute();
                 }
-                if (_useAsynchronousExecute && _stmts.length > 1)
+                else if (_useAsynchronousExecute && _stmts.length > 1)
                 {
                     _currentStmt = _queue.swapFullForEmpty(_currentStmt);
                     _currentBinding = (_currentStmt == _stmts[0] ? _bindings[0] : _bindings[1]);
