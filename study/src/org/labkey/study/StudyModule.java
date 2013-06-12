71112d14eefd74b04388c6eeedd1cc634ae8d824
Josh Eckels <jeckels@labkey.com>
2013-06-12 23:17:42 +0000

    Issue 15707: **Deadlock found on customer server - improve indices on study.participantvisit

--- study/src/org/labkey/study/StudyModule.java
+++ study/src/org/labkey/study/StudyModule.java
@@ -226,7 +226,7 @@ public class StudyModule extends SpringModule implements SearchService.DocumentP
 
     public double getVersion()
     {
-        return 13.11;
+        return 13.12;
     }
 
     protected void init()
