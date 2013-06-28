4b24c21fcb14e3ad4d26103e28e297efb6c62893
Rylan Bauermeister <rylanb@labkey.com>
2013-06-28 02:51:22 +0000

    Bug fixing on the Experiment Workbook

--- core/webapp/editInPlaceElement.js
+++ core/webapp/editInPlaceElement.js
@@ -137,7 +137,7 @@ LABKEY.ext.EditInPlaceElement = Ext.extend(Ext.util.Observable, {
               'padding-right', 'line-height', 'font-size',
               'font-family', 'font-weight', 'font-style');
 
-        styles.width = this.el.getWidth() + "px";
+        styles.width = this.el.getWidth()-25 + "px";
         this.editor.setStyle(styles);
         this.sizingDiv.setStyle(styles);
 
