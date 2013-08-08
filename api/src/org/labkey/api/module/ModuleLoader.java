33f4307c546e82896130ef5ec89f197ae2ce57be
Adam Rauch <adam@labkey.com>
2013-08-08 23:12:32 +0000

    Set minimum upgrade version to 11.2
    Consolidate all bootstrap scripts to 0.00-11.20. Obsolete 0.00-11.10, 11.10-11.20, etc. scripts. Remove 11.10-11.20 java upgrade code (no longer needed).
    
    Obsolete all incremental scripts before 13.10.
    
    New consolidate feature to choose whether to include/exclude single-script schemas

--- api/src/org/labkey/api/module/ModuleLoader.java
+++ api/src/org/labkey/api/module/ModuleLoader.java
@@ -104,7 +104,7 @@ import java.util.TreeSet;
  */
 public class ModuleLoader implements Filter
 {
-    private static final double EARLIEST_UPGRADE_VERSION = 11.1;
+    private static final double EARLIEST_UPGRADE_VERSION = 11.2;
     private static final Logger _log = Logger.getLogger(ModuleLoader.class);
     private static final Map<String, Throwable> _moduleFailures = new HashMap<>();
     private static final Map<String, Module> _controllerNameToModule = new HashMap<>();
