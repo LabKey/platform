0afe33561b0efcfa1c7819a45fda538f7739f78c
Adam Rauch <adam@labkey.com>
2013-08-08 03:59:49 +0000

    Update licenses

--- api/webapp/clientapi/ext3/FilterDialog.js
+++ api/webapp/clientapi/ext3/FilterDialog.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 LABKEY.FilterDialog = Ext.extend(Ext.Window, {
 
     autoHeight: true,
