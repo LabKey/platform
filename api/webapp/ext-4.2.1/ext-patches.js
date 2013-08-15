33b53056608e472f2682bd0d8a41d2638b930a8b
Nick Kerr <nicka@labkey.com>
2013-08-15 22:24:55 +0000

    Ext 4.2.1: More fixes. - Test fixes for ChartingAPI and DataReports

--- api/webapp/ext-4.2.1/ext-patches.js
+++ api/webapp/ext-4.2.1/ext-patches.js
@@ -142,10 +142,57 @@ Ext4.override(Ext4.form.FieldContainer, {
 /**
  * @Override
  * Sencha Issue: http://www.sencha.com/forum/showthread.php?269116-Ext-4.2.1.883-Sandbox-getRowStyleTableEl
+ * Version: 4.2.1
  */
 Ext4.override(Ext4.view.Table, {
     getRowStyleTableEl : function(item) {
         return this.el.down('table.' + Ext4.baseCSSPrefix  + 'grid-table');
+    }
+});
+
+/**
+ * @Override
+ * Sencha Issue: http://www.sencha.com/forum/showthread.php?258397-4.2.0-RC-Selecting-a-grid-s-row-with-a-buffered-store-causes-a-JavaScript-error
+ * Version: 4.2.1
+ */
+Ext4.override(Ext4.selection.Model, {
+    /**
+     * this override fixes multiple bugs when store is a buffered type
+     * and fixed a bug that causes the initial hasId to not work because
+     * the store.gerById() method expects an ID and not the record
+     * also forcing the double verification when the record is not found because
+     * it places both checks in a single if statement
+     * @param record
+     * @returns {boolean}
+     */
+    storeHasSelected: function(record) {
+        var store = this.store,
+                records,
+                len, id, i, m;
+
+
+        if (record.hasId()) {
+            return store.getById(record.getId());
+        } else {
+            if (store.buffered) {//on buffered stores the map holds the data items
+                records = [];
+                for (m in store.data.map) {
+                    records = records.concat(store.data.map[m].value);
+                }
+            } else {
+                records = store.data.items;
+            }
+            len = records.length;
+            id = record.internalId;
+
+
+            for (i = 0; i < len; ++i) {
+                if (id === records[i].internalId) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 });
 
