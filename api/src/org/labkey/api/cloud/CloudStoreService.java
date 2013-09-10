67151e15874530c38f2379a1967e8803a02d9740
Kevin Krouse <kevink@labkey.com>
2013-09-10 20:50:46 +0000

    Issue 18520: Attaching a cloud webdav provider should not required enabling module in folder Issue 18518: cloud: delete files on container delete Issue 18605: IllegalArgumentException from query audit log parameters

--- api/src/org/labkey/api/cloud/CloudStoreService.java
+++ api/src/org/labkey/api/cloud/CloudStoreService.java
@@ -35,11 +35,6 @@ public interface CloudStoreService
     Iterable<Pair<String, String>> providers();
 
     /**
-     * Returns true if the Cloud module is enabled in the container.
-     */
-    boolean isEnabled(Container container);
-
-    /**
      * Returns true if store is enabled at the site level.
      */
     boolean isEnabled(String storeName);
