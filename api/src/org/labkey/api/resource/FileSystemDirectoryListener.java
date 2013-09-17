9b1ac2e856f01e1c993584d6301b28436e02cd03
Adam Rauch <adam@labkey.com>
2013-09-17 22:48:15 +0000

    More accurate names for listener methods
    Close the file watcher on shutdown

--- api/src/org/labkey/api/resource/FileSystemDirectoryListener.java
+++ api/src/org/labkey/api/resource/FileSystemDirectoryListener.java
@@ -12,11 +12,11 @@ import java.nio.file.Path;
 public interface FileSystemDirectoryListener
 {
     // A new entry (file or directory) was created in the watched directory. Invoked only if the event was listed at registration time.
-    public void fileCreated(Path directory, Path entry);
+    public void entryCreated(Path directory, Path entry);
     // An entry (file or directory) in the directory was deleted. Invoked only if the event was listed at registration time.
-    public void fileDeleted(Path directory, Path entry);
+    public void entryDeleted(Path directory, Path entry);
     // An entry (file or directory) in the directory was changed. Invoked only if the event was listed at registration time.
-    public void fileModified(Path directory, Path entry);
+    public void entryModified(Path directory, Path entry);
 
     // Indicates that events might have been lost or discarded. The method is always invoked when overflow occurs (regardless of registration).
     public void overflow();
