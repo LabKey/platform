a82942e47d138f7d36d468295837cb8f1adb9dfb
Adam Rauch <adam@labkey.com>
2013-10-03 17:41:12 +0000

    Update licenses

--- visualization/resources/web/vis/baseExportScriptPanel.js
+++ visualization/resources/web/vis/baseExportScriptPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 Ext4.define('LABKEY.vis.BaseExportScriptPanel', {
     extend: 'Ext.Panel',
