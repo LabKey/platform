7282e4d5d7c22bc691e004fe63cbe8d9148ea4fd
Tony Galuhn <tgaluhn@labkey.com>
2013-10-22 04:38:21 +0000

    Add javadoc comments

--- dataintegration/src/org/labkey/di/steps/StepProvider.java
+++ dataintegration/src/org/labkey/di/steps/StepProvider.java
@@ -28,9 +28,39 @@ import java.util.List;
  */
 public interface StepProvider
 {
+    /**
+     * The prefered type name. Used in the transform type attribute
+     * @return The name
+     */
     public String getName();
+
+    /**
+     *  Optional list of alternate names for the type
+     *  For example, SimpleQueryTransformStep is the default for a blank (null) value,
+     *  and in the initial rollout we were using fully qualified class names
+     * @return  List of alternate names
+     */
     public List<String> getLegacyNames();
+
+    /**
+     *  The class for the Step
+     * @return The .class
+     */
     public Class getStepClass();
+
+    /**
+     * Instantiate the correct StepMeta subclass
+     * @return An instance of the StepMeta corresponding to the type
+     */
     public StepMeta createMetaInstance();
+
+    /**
+     * Instantiate the correct Step subclass.
+     * @param f
+     * @param job
+     * @param meta Should be cast to the correct StepMeta subclass in the constructor call
+     * @param context
+     * @return An instance of the Step class corresponding to the type
+     */
     public TransformTask createStepInstance(TransformTaskFactory f, PipelineJob job, StepMeta meta, TransformJobContext context);
 }
