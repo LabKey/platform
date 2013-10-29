ff79854e8945e085773443ff79a7d979a2197e2a
Greg Taylor <gktaylor@labkey.com>
2013-10-29 23:06:55 +0000

    OConnor ETL - Integrate External Data from EHR Issue 18893 - deleting remote connections was broken

---
+++ internal/src/org/labkey/remoteapi/RemoteConnections.java
@@ -0,0 +1,17 @@
+package org.labkey.remoteapi;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gktaylor
+ * Date: 10/29/13
+ * Time: 3:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RemoteConnections
+{
+    public static String REMOTE_CONNECTIONS_CATEGORY = "remote-connections";
+    public static String FIELD_URL = "URL";
+    public static String FIELD_USER = "user";
+    public static String FIELD_PASSWORD = "password";
+    public static String FIELD_CONTAINER = "container";
+}
