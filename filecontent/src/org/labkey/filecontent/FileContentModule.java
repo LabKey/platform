d3d5921ce667513f398d7b4bbcb2968355d46607
Dax Hawkins <dax@labkey.com>
2013-11-07 19:49:40 +0000

    merge from modules13.3  (29362-29373) including release13.3 (29361-29371)

--- filecontent/src/org/labkey/filecontent/FileContentModule.java
+++ filecontent/src/org/labkey/filecontent/FileContentModule.java
@@ -70,8 +70,8 @@ public class FileContentModule extends DefaultModule
     {
         return new ArrayList<WebPartFactory>(Arrays.asList(
                 new FilesWebPart.Factory(WebPartFactory.LOCATION_RIGHT),
-                new FilesWebPart.Factory(WebPartFactory.LOCATION_BODY),
-                new FilesWebPart.Ext4Factory(WebPartFactory.LOCATION_BODY) // Disable web part for 13.1 release
+                new FilesWebPart.Factory(WebPartFactory.LOCATION_BODY)
+//                new FilesWebPart.Ext4Factory(WebPartFactory.LOCATION_BODY)
         ));
     }
 
