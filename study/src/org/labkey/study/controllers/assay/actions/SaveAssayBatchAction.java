d7bc27fdee865da0ed14edffa03ddadea9c0e9ea
Dax Hawkins <dax@labkey.com>
2013-11-15 17:01:43 +0000

    icemr:  add ability to upload multiple batches for daily maintenance.  This includes a new javascript client api saveBatches().  This change also requires that a new template be used for all daily maintenance uploads.  Also added automated tests to support new scenarios as well as fixup existing tests to support the new UI.

--- study/src/org/labkey/study/controllers/assay/actions/SaveAssayBatchAction.java
+++ study/src/org/labkey/study/controllers/assay/actions/SaveAssayBatchAction.java
@@ -15,6 +15,7 @@
  */
 package org.labkey.study.controllers.assay.actions;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.labkey.api.action.ApiResponse;
 import org.labkey.api.action.ApiVersion;
@@ -42,21 +43,60 @@ public class SaveAssayBatchAction extends AbstractAssayAPIAction<SimpleApiJsonFo
     {
         JSONObject rootJsonObject = form.getJsonObject();
 
-        JSONObject batchJsonObject = rootJsonObject.getJSONObject(AssayJSONConverter.BATCH);
-        if (batchJsonObject == null)
-        {
-            throw new IllegalArgumentException("No batch object found");
-        }
+        // A user can send in either an array of batches or just a batch but not both.  If a user sends in an array of batches
+        // then it must have at least one batch
+        JSONObject batchJsonObject = null;
+        JSONArray batchesJsonArray = null;
+
+        if (rootJsonObject.has(AssayJSONConverter.BATCH))
+            batchJsonObject = rootJsonObject.getJSONObject(AssayJSONConverter.BATCH);
+
+        if (rootJsonObject.has(AssayJSONConverter.BATCHES))
+            batchesJsonArray = rootJsonObject.getJSONArray(AssayJSONConverter.BATCHES);
+
+        if (batchJsonObject == null && batchesJsonArray == null)
+            throw new IllegalArgumentException("No batch object or batches array found");
+
+        if ((null != batchesJsonArray) && (batchesJsonArray.length() == 0))
+            throw new IllegalArgumentException("You must provide at least one batch in your batches array");
+
+        if ((null != batchJsonObject) && (null != batchesJsonArray))
+            throw new IllegalArgumentException("You cannot specify both a batch object and a batches array");
 
         AssaySaveHandler saveHandler = provider.getSaveHandler();
-
         if (null == saveHandler)
-        {
             throw new IllegalArgumentException("SaveAssayBatch is not supported for assay provider: " + provider);
+
+        if (null != batchJsonObject)
+            return executeAction(saveHandler, protocol, provider, rootJsonObject, batchJsonObject);
+
+        return executeAction(saveHandler, protocol, provider, rootJsonObject, batchesJsonArray);
+    }
+
+    private ApiResponse executeAction(AssaySaveHandler saveHandler, ExpProtocol protocol, AssayProvider provider,
+                                      JSONObject rootJsonObject, JSONArray batchesJsonArray) throws Exception
+    {
+        saveHandler.beforeSave(getViewContext(), rootJsonObject, protocol);
+        ExpExperiment[] batches = new ExpExperiment[batchesJsonArray.length()];
+        try (DbScope.Transaction transaction = ExperimentService.get().ensureTransaction())
+        {
+            for (int i = 0; i < batchesJsonArray.length(); i++)
+            {
+                JSONObject batchJsonObject = batchesJsonArray.getJSONObject(i);
+                batches[i] = saveHandler.handleBatch(getViewContext(), batchJsonObject, protocol);
+            }
+
+            transaction.commit();
         }
+        saveHandler.afterSave(getViewContext(), batches, protocol);
+        return AssayJSONConverter.serializeResult(provider, protocol, batches, getViewContext().getUser());
 
+    }
+
+    private ApiResponse executeAction(AssaySaveHandler saveHandler, ExpProtocol protocol, AssayProvider provider,
+                                      JSONObject rootJsonObject, JSONObject batchJsonObject) throws Exception
+    {
         ExpExperiment batch;
-
         saveHandler.beforeSave(getViewContext(), rootJsonObject, protocol);
         try (DbScope.Transaction transaction = ExperimentService.get().ensureTransaction())
         {
