e0c991b4a8bf0dc890f7c9feee5f5df9eb21ef9c
Josh Eckels <jeckels@labkey.com>
2013-11-19 21:15:33 +0000

    Issue 19039: **email template is getting truncated
    Remove limit on size for property store values

--- core/src/org/labkey/core/CoreModule.java
+++ core/src/org/labkey/core/CoreModule.java
@@ -219,7 +219,7 @@ public class CoreModule extends SpringModule implements SearchService.DocumentPr
     @Override
     public double getVersion()
     {
-        return 13.30;
+        return 13.31;
     }
 
     @Override
