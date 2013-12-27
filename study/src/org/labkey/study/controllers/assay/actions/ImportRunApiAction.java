2dce5311707ac00eddde79d312ddb10ccc99aabb
Kevin Krouse <kevink@labkey.com>
2013-12-27 23:55:04 +0000

    refactor ImportRunApiUploadContext and HPLCRunUploadContext into AssayRunUploadContextImpl

--- study/src/org/labkey/study/controllers/assay/actions/ImportRunApiAction.java
+++ study/src/org/labkey/study/controllers/assay/actions/ImportRunApiAction.java
@@ -16,7 +16,6 @@
 
 package org.labkey.study.controllers.assay.actions;
 
-import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 import org.labkey.api.action.ApiResponse;
 import org.labkey.api.action.ApiSimpleResponse;
@@ -31,29 +30,18 @@ import org.labkey.api.exp.api.ExpExperiment;
 import org.labkey.api.exp.api.ExpProtocol;
 import org.labkey.api.exp.api.ExpRun;
 import org.labkey.api.exp.api.ExperimentJSONConverter;
-import org.labkey.api.exp.property.Domain;
-import org.labkey.api.exp.property.DomainProperty;
-import org.labkey.api.qc.DefaultTransformResult;
-import org.labkey.api.qc.TransformResult;
 import org.labkey.api.security.ActionNames;
 import org.labkey.api.security.RequiresPermissionClass;
-import org.labkey.api.security.User;
 import org.labkey.api.security.permissions.InsertPermission;
-import org.labkey.api.study.assay.AssayDataCollector;
 import org.labkey.api.study.assay.AssayProvider;
 import org.labkey.api.study.assay.AssayRunUploadContext;
+import org.labkey.api.study.assay.AssayRunUploadContextImpl;
 import org.labkey.api.study.assay.AssayUrls;
-import org.labkey.api.study.assay.FileUploadDataCollector;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.util.Pair;
 import org.labkey.api.view.ActionURL;
-import org.labkey.api.view.ViewContext;
 import org.springframework.validation.BindException;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -66,8 +54,6 @@ import java.util.Map;
 @ApiVersion(12.3)
 public class ImportRunApiAction<ProviderType extends AssayProvider> extends MutatingApiAction<ImportRunApiAction.ImportRunApiForm>
 {
-    private static final String FILE_INPUT_NAME = "file";
-
     @Override
     public ApiResponse execute(ImportRunApiForm form, BindException errors) throws Exception
     {
@@ -91,16 +77,19 @@ public class ImportRunApiAction<ProviderType extends AssayProvider> extends Muta
             String targetStudy = json.optString("targetStudy");
             Integer reRunId = json.containsKey("reRunId") ? json.optInt("reRunId") : null;
 
-            uploadContext = new ImportRunApiUploadContext<>(
+            AssayRunUploadContextImpl.Factory factory = new AssayRunUploadContextImpl.Factory<>(
                     protocol,
                     (ProviderType)provider,
-                    getViewContext(),
-                    name,
-                    comments,
-                    runProperties,
-                    batchProperties,
-                    targetStudy,
-                    reRunId);
+                    getViewContext());
+
+            factory.setName(name)
+                   .setComments(comments)
+                   .setRunProperties(runProperties)
+                   .setBatchProperties(batchProperties)
+                   .setTargetStudy(targetStudy)
+                   .setReRunId(reRunId);
+
+            uploadContext = factory.create();
         }
         else
         {
@@ -110,16 +99,19 @@ public class ImportRunApiAction<ProviderType extends AssayProvider> extends Muta
 
             batchId = form.getBatchId();
 
-            uploadContext = new ImportRunApiUploadContext<>(
+            AssayRunUploadContextImpl.Factory factory = new AssayRunUploadContextImpl.Factory<>(
                     protocol,
                     (ProviderType)provider,
-                    getViewContext(),
-                    form.getName(),
-                    form.getComment(),
-                    form.getProperties(),
-                    form.getBatchProperties(),
-                    form.getTargetStudy(),
-                    form.getReRunId());
+                    getViewContext());
+
+            factory.setName(form.getName())
+                    .setComments(form.getComment())
+                    .setRunProperties(form.getProperties())
+                    .setBatchProperties(form.getBatchProperties())
+                    .setTargetStudy(form.getTargetStudy())
+                    .setReRunId(form.getReRunId());
+
+            uploadContext = factory.create();
         }
 
         try
@@ -268,210 +260,4 @@ public class ImportRunApiAction<ProviderType extends AssayProvider> extends Muta
         }
     }
 
-    protected static class ImportRunApiUploadContext<ProviderType extends AssayProvider> implements AssayRunUploadContext<ProviderType>
-    {
-        private ExpProtocol _protocol;
-        private ProviderType _provider;
-        private ViewContext _context;
-        private Map<String, File> _uploadedData;
-        private String _comments;
-        private String _name;
-        private String _targetStudy;
-        private Integer _reRunId;
-
-        private Map<String, String> _rawRunProperties;
-        private Map<String, String> _rawBatchProperties;
-        private Map<DomainProperty, String> _runProperties;
-        private Map<DomainProperty, String> _batchProperties;
-
-        private TransformResult _transformResult;
-
-        public ImportRunApiUploadContext(
-                @NotNull ExpProtocol protocol, @NotNull ProviderType provider, ViewContext context,
-                String name, String comment,
-                Map<String, String> runProperties,
-                Map<String, String> batchProperties,
-                String targetStudy,
-                Integer reRunId)
-        {
-            _protocol = protocol;
-            _provider = provider;
-            _context = context;
-
-            _name = name;
-            _comments = comment;
-
-            _rawRunProperties = runProperties;
-            _rawBatchProperties = batchProperties;
-
-            _reRunId = reRunId;
-            _targetStudy = targetStudy;
-        }
-
-        @NotNull
-        public ExpProtocol getProtocol()
-        {
-            return _protocol;
-        }
-
-        public Map<DomainProperty, String> getRunProperties() throws ExperimentException
-        {
-            if (_runProperties == null)
-            {
-                Map<DomainProperty, String> properties = new HashMap<>();
-                if (_rawRunProperties != null)
-                {
-                    for (DomainProperty prop : _provider.getRunDomain(_protocol).getProperties())
-                    {
-                        String value;
-                        if (_rawRunProperties.containsKey(prop.getName()))
-                            value = _rawRunProperties.get(prop.getName());
-                        else
-                            value = _rawRunProperties.get(prop.getPropertyURI());
-                        properties.put(prop, value);
-                    }
-
-                }
-                _runProperties = properties;
-            }
-            return _runProperties;
-        }
-
-        public Map<DomainProperty, String> getBatchProperties()
-        {
-            if (_batchProperties == null)
-            {
-                Map<DomainProperty, String> properties = new HashMap<>();
-                if (_rawBatchProperties != null)
-                {
-                    for (DomainProperty prop : _provider.getBatchDomain(_protocol).getProperties())
-                    {
-                        String value;
-                        if (_rawBatchProperties.containsKey(prop.getName()))
-                            value = _rawBatchProperties.get(prop.getName());
-                        else
-                            value = _rawBatchProperties.get(prop.getPropertyURI());
-                        properties.put(prop, value);
-                    }
-
-                }
-                _batchProperties = properties;
-            }
-            return _batchProperties;
-        }
-
-        public String getComments()
-        {
-            return _comments;
-        }
-
-        public String getName()
-        {
-            return _name;
-        }
-
-        public User getUser()
-        {
-            return _context.getUser();
-        }
-
-        @NotNull
-        public Container getContainer()
-        {
-            return _context.getContainer();
-        }
-
-        public HttpServletRequest getRequest()
-        {
-            return _context.getRequest();
-        }
-
-        public ActionURL getActionURL()
-        {
-            return _context.getActionURL();
-        }
-
-        @NotNull
-        public Map<String, File> getUploadedData() throws ExperimentException
-        {
-            if (_uploadedData == null)
-            {
-                try
-                {
-                    AssayDataCollector<ImportRunApiUploadContext> collector = new FileUploadDataCollector(1, Collections.emptyMap(), FILE_INPUT_NAME);
-                    Map<String, File> files = collector.createData(this);
-                    // HACK: rekey the map using PRIMARY_FILE instead of FILE_INPUT_NAME
-                    _uploadedData = Collections.singletonMap(AssayDataCollector.PRIMARY_FILE, files.get(FILE_INPUT_NAME));
-                }
-                catch (IOException e)
-                {
-                    throw new ExperimentException(e);
-                }
-            }
-            return _uploadedData;
-        }
-
-        public ProviderType getProvider()
-        {
-            return _provider;
-        }
-
-        public Map<DomainProperty, Object> getDefaultValues(Domain domain, String scope) throws ExperimentException
-        {
-            throw new UnsupportedOperationException("Not Supported");
-        }
-
-        public Map<DomainProperty, Object> getDefaultValues(Domain domain) throws ExperimentException
-        {
-            throw new UnsupportedOperationException("Not Supported");
-        }
-
-        public void saveDefaultValues(Map<DomainProperty, String> values, String scope) throws ExperimentException
-        {
-            throw new UnsupportedOperationException("Not Supported");
-        }
-
-        public void saveDefaultBatchValues() throws ExperimentException
-        {
-            throw new UnsupportedOperationException("Not Supported");
-        }
-
-        public void saveDefaultRunValues() throws ExperimentException
-        {
-            throw new UnsupportedOperationException("Not Supported");
-        }
-
-        public void clearDefaultValues(Domain domain) throws ExperimentException
-        {
-            throw new UnsupportedOperationException("Not Supported");
-        }
-
-        @Override
-        public Integer getReRunId()
-        {
-            return _reRunId;
-        }
-
-        public String getTargetStudy()
-        {
-            return _targetStudy;
-        }
-
-        public TransformResult getTransformResult()
-        {
-            return _transformResult == null ? DefaultTransformResult.createEmptyResult() : _transformResult;
-        }
-
-        @Override
-        public void setTransformResult(TransformResult result)
-        {
-            _transformResult = result;
-        }
-        
-        @Override
-        public void uploadComplete(ExpRun run) throws ExperimentException
-        {
-            // no-op
-        }
-    }
 }
