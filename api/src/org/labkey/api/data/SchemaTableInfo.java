8619242a4483c4efe108bc8846757ef0b8c685d7
Adam Rauch <adam@labkey.com>
2014-01-10 16:02:41 +0000

    These are no longer used and have been removed from the TableInfo interface

--- api/src/org/labkey/api/data/SchemaTableInfo.java
+++ api/src/org/labkey/api/data/SchemaTableInfo.java
@@ -938,19 +938,6 @@ public class SchemaTableInfo implements TableInfo, UpdateableTableInfo
         return _notificationKey;
     }
 
-    @Override
-    public String getDefaultDateFormat()
-    {
-        return null;
-    }
-
-    @Override
-    public String getDefaultNumberFormat()
-    {
-        return null;
-    }
-
-
     private void checkLocked()
     {
         if (_locked)
