05fe6b161dc15334438aa6fd7386a951276ac6c6
Adam Rauch <adam@labkey.com>
2014-03-10 00:28:42 +0000

    Upgrade only from 12.10 and beyond (not 11.30), per our upgrade support policy
    Consolidate all bootstrap scripts to 0.00-12.10. Obsolete unreachable scripts and incremental scripts from 13.3.
    
    Remove no longer needed StudyUpgradeCode.migrateReportProperties()

--- api/src/org/labkey/api/module/ModuleLoader.java
+++ api/src/org/labkey/api/module/ModuleLoader.java
@@ -114,7 +114,7 @@ import java.util.TreeSet;
  */
 public class ModuleLoader implements Filter
 {
-    private static final double EARLIEST_UPGRADE_VERSION = 11.3;
+    private static final double EARLIEST_UPGRADE_VERSION = 12.1;
     private static final Logger _log = Logger.getLogger(ModuleLoader.class);
     private static final Map<String, Throwable> _moduleFailures = new HashMap<>();
     private static final Map<String, Module> _controllerNameToModule = new HashMap<>();
