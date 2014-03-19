2b53ca321ba7ed632bbd00e78e0a251832d1596f
Alan Vezina <alanv@labkey.com>
2014-03-19 19:33:22 +0000

    Fix spacing in olap.js

--- query/webapp/query/olap.js
+++ query/webapp/query/olap.js
@@ -672,10 +672,10 @@ Ext4.define('LABKEY.query.olap.MDX', {
         return this.getDimension(name);
     },
 
-    clearNamedFilter : function(name) {
-        if (this._filter[name]) {
+    clearNamedFilter : function(name)
+    {
+        if (this._filter[name])
             delete this._filter[name];
-        }
     },
 
     setNamedFilter : function(name, filter)
