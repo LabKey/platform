85e25e098b96c1803118f1f8c920fac318279b20
Matthew Bellew <mbellew@labkey.com>
2014-04-04 20:17:07 +0000

    19953: Allow Shared Datasets is still set as an experimental feature

--- study/src/org/labkey/study/StudyModule.java
+++ study/src/org/labkey/study/StudyModule.java
@@ -213,8 +213,6 @@ public class StudyModule extends SpringModule implements SearchService.DocumentP
 {
     public static final String MODULE_NAME = "Study";
 
-    public static final String EXPERIMENTALFEATURE_SHARED_DATASET = "SharedDatasets";
-
     public static final BaseWebPartFactory reportsPartFactory = new ReportsWebPartFactory();
     public static final WebPartFactory reportsWidePartFactory = new ReportsWideWebPartFactory();
     public static final WebPartFactory samplesPartFactory = new SamplesWebPartFactory(WebPartFactory.LOCATION_RIGHT);
@@ -467,9 +465,6 @@ public class StudyModule extends SpringModule implements SearchService.DocumentP
 
         AdminConsole.addExperimentalFeatureFlag(CreateChildStudyAction.CREATE_SPECIMEN_STUDY, "Create Specimen Study",
             "Adds a button to the specimen request details page that creates a new child study containing the selected specimens, associated participants, and selected datasets.", false);
-
-        AdminConsole.addExperimentalFeatureFlag(EXPERIMENTALFEATURE_SHARED_DATASET, "Allow Shared Datasets",
-                "Allows study dataset definitions created at the project root to be used by studies in sub-folders.", false);
     }
 
     @Override
