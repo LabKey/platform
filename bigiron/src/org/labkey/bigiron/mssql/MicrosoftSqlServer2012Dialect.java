a329c5b044ac0a4998a610fe4fb8d80a187e5dbb
Adam Rauch <adam@labkey.com>
2014-04-14 18:41:04 +0000

    Don't use OFFSET/FETCH when we just need to limit rows... OFFSET/FETCH requires ORDER BY but TOP does not.
    Null check for filter.

--- bigiron/src/org/labkey/bigiron/mssql/MicrosoftSqlServer2012Dialect.java
+++ bigiron/src/org/labkey/bigiron/mssql/MicrosoftSqlServer2012Dialect.java
@@ -25,31 +25,31 @@ import org.labkey.api.data.Table;
  */
 public class MicrosoftSqlServer2012Dialect extends MicrosoftSqlServer2008R2Dialect
 {
-    @Override          // Consider: remove this; just use TOP in this maxRows-only case
-    public SQLFragment limitRows(SQLFragment frag, int maxRows)
-    {
-        if (Table.ALL_ROWS == maxRows)
-            return frag;
-
-        String sql = frag.getSQL();
-        if (!sql.substring(0, 6).equalsIgnoreCase("SELECT"))
-            throw new IllegalArgumentException("ERROR: Limit SQL doesn't start with SELECT: " + sql);
-
-        // FETCH NEXT doesn't seem to work with 0 rows, so fallback to TOP in this case
-        if (Table.NO_ROWS == maxRows)
-        {
-            int offset = 6;
-            if (sql.substring(0, 15).equalsIgnoreCase("SELECT DISTINCT"))
-                offset = 15;
-            frag.insert(offset, " TOP 0");
-        }
-        else
-        {
-            frag.append(" OFFSET 0 ROWS FETCH NEXT ").append(maxRows).append(" ROWS ONLY");
-        }
-
-        return frag;
-    }
+//    @Override          // Consider: remove this; just use TOP in this maxRows-only case
+//    public SQLFragment limitRows(SQLFragment frag, int maxRows)
+//    {
+//        if (Table.ALL_ROWS == maxRows)
+//            return frag;
+//
+//        String sql = frag.getSQL();
+//        if (!sql.substring(0, 6).equalsIgnoreCase("SELECT"))
+//            throw new IllegalArgumentException("ERROR: Limit SQL doesn't start with SELECT: " + sql);
+//
+//        // FETCH NEXT doesn't seem to work with 0 rows, so fallback to TOP in this case
+//        if (Table.NO_ROWS == maxRows)
+//        {
+//            int offset = 6;
+//            if (sql.substring(0, 15).equalsIgnoreCase("SELECT DISTINCT"))
+//                offset = 15;
+//            frag.insert(offset, " TOP 0");
+//        }
+//        else
+//        {
+//            frag.append(" OFFSET 0 ROWS FETCH NEXT ").append(maxRows).append(" ROWS ONLY");
+//        }
+//
+//        return frag;
+//    }
 
     // Called only if rowCount and offset are both > 0
     @Override
@@ -57,7 +57,7 @@ public class MicrosoftSqlServer2012Dialect extends MicrosoftSqlServer2008R2Diale
     {
         SQLFragment sql = new SQLFragment(select);
         sql.append("\n").append(from);
-        if (!filter.isEmpty()) sql.append("\n").append(filter);
+        if (null != filter && !filter.isEmpty()) sql.append("\n").append(filter);
         if (groupBy != null) sql.append("\n").append(groupBy);
         if (order != null) sql.append("\n").append(order);
         sql.append("\nOFFSET ").append(offset).append(" ROWS FETCH NEXT ").append(maxRows).append(" ROWS ONLY");
