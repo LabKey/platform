9477239d8b566e64a97bc7dfe190b57c18264cce
Alan Vezina <alanv@labkey.com>
2014-04-29 22:42:15 +0000

    Fix test failures related to LABKEY.Ajax changes.  - Wrap ExtAdapter.Ajax.request in Ajax.footer.js  - No longer serialize null or undefined in ActionURL.queryString

--- core/script-fragments/labkey/Ajax.footer.js
+++ core/script-fragments/labkey/Ajax.footer.js
@@ -4,4 +4,13 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
-exports.Ajax = LABKEY.Ajax;
+// Rhino does not support XMLHttpRequest, so here we wrap the one from our ExtAdapter.
+exports.Ajax = {
+    request : function (config)
+    {
+        var o = LABKEY.ExtAdapter.Ajax.request(config);
+        if (LABKEY.ExtAdapter.isObject(o))
+            return o.responseJSON || o.responseXML || o.responseText;
+        return o;
+    }
+};
