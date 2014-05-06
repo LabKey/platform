26d3149b9fa9bee80422c7a29d319e4d081ec8fa
Dave Bradlee <dave@labkey.com>
2014-05-06 19:06:23 +0000

    20306: PSQLException when changing project folder type from Study to Dataspace

--- study/src/org/labkey/study/DataspaceStudyFolderType.java
+++ study/src/org/labkey/study/DataspaceStudyFolderType.java
@@ -41,32 +41,16 @@ public class DataspaceStudyFolderType extends StudyFolderType
     public void configureContainer(Container container, User user)
     {
         super.configureContainer(container, user);
-        StudyImpl study = new StudyImpl(container, container.getTitle());
-        study.setTimepointType(TimepointType.VISIT);
-//        s.setStartDate(new Date(DateUtil.parseDateTime(container, "2014-01-01")));
-        study.setSubjectColumnName("SubjectID");
-        study.setSubjectNounPlural("Subjects");
-        study.setSubjectNounSingular("Subject");
-        study.setShareDatasetDefinitions(Boolean.TRUE);
-        StudyManager.getInstance().createStudy(user, study);
-    }
-
-    @Override
-    public void unconfigureContainer(Container container, User user)
-    {
         StudyImpl study = StudyManager.getInstance().getStudy(container);
-        if (null != study)
+        if (null == study)
         {
-            try
-            {
-                StudyManager.getInstance().deleteAllStudyData(container, user);     // TODO: should we retain study data?
-            }
-            catch (SQLException e)
-            {
-                throw new RuntimeSQLException(e);
-            }
+            study = new StudyImpl(container, container.getTitle());
+            study.setTimepointType(TimepointType.VISIT);
+            study.setSubjectColumnName("SubjectID");
+            study.setSubjectNounPlural("Subjects");
+            study.setSubjectNounSingular("Subject");
+            study.setShareDatasetDefinitions(Boolean.TRUE);
+            StudyManager.getInstance().createStudy(user, study);
         }
-        super.unconfigureContainer(container, user);
     }
-
 }
