648adab3bf75c1b26d4fe1e915ca527b9b4e8853
Aaron Robinson <aaronr@labkey.com>
2014-06-30 22:27:07 +0000

    20890: If a bad user name is entered into the notify list it is accepted. This fixes a few different edge cases that can occur due to bad data that was allowed into this field. We will fix the schema in a future branch.

--- api/src/org/labkey/api/security/UserManager.java
+++ api/src/org/labkey/api/security/UserManager.java
@@ -631,7 +631,7 @@ public class UserManager
         for (User user : users)
         {
             final String fullName = StringUtils.defaultString(user.getFirstName()) + " " + StringUtils.defaultString(user.getLastName());
-            String completionValue = showEmailAddresses ? user.getEmail() : user.getDisplayName(currentUser);
+            String completionValue = user.getDisplayName(currentUser);
 
             // Output the most human-friendly names possible for the pick list, and append the email addresses if the user has permission to see them
 
