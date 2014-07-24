c81f4de9b781b1a6e09ffd8f91801cd30e11e0cc
Tony Galuhn <tgaluhn@labkey.com>
2014-07-24 16:55:47 +0000

    OLAP Cube maintenance via ETL + generic TaskrefTask for ETLs

---
+++ api/src/org/labkey/api/di/TaskrefTask.java
@@ -0,0 +1,64 @@
+package org.labkey.api.di;
+
+import org.apache.log4j.Logger;
+import org.labkey.api.pipeline.PipelineJobException;
+import org.labkey.api.pipeline.RecordedActionSet;
+import org.labkey.api.writer.ContainerUser;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * User: tgaluhn
+ * Date: 7/22/2014
+ */
+
+/**
+ * A more generic interface for ETL transform task types that aren't going to be fully defined in the etl.xsd
+ * The fully qualified classname of an implementor of this interface can be specified in an ETL transform taskref
+ * element, with configuration in settings/setting child elements. The taskref element is compatible with the
+ * definition in pipeline.xsd
+ *
+ * Ultimately we'd like to be able to share tasks between ETL and Pipeline; as a step in that direction
+ * the signature of the run() method here intentionally matches that of PipelineJob.Task<TaskFactory>.run().
+ * Also as such, implementors of this interface should ideally be decoupled from api.di and dataintegration. This is why
+ * we downcast the TransformJobContext to the ContainerUser interface when passing it to the task.
+ *
+ */
+public interface TaskrefTask
+{
+    /**
+     * The method that does the real work in the implementing class. Signature matches PipelineJob.Task<TaskFactory>.run()
+     * @return RecordedActionSet Any parameters of any RecordedActions in the set get persisted in the job parameter set in
+     *          dataintegration.TransformConfiguration.TransformState
+     * @throws PipelineJobException
+     */
+    public RecordedActionSet run() throws PipelineJobException;
+
+    /**
+     * For upfront validation; if any of these are missing, we won't even try to run the task
+     */
+    public List<String> getRequiredSettings();
+
+    /**
+     * Settings read from the etl xml (or elsewhere). Eventually would be good to use TaskFactorySettings instead.
+     */
+    public void setSettings(Map<String, String> settings);
+
+    /**
+     * Downcast from the ScheduledPipelineJobContext or TransformJobContext that the etl job will be otherwise passing
+     * around. There's probably a more pipeline-y way of passing this context info to a task, so this method may eventually
+     * go away.
+     *
+     */
+    public void setContainerUser(ContainerUser containerUser);
+
+    /**
+     * A log4j logger. In this context it is from the ETL job so messages are logged into the ETL job log. As with the
+     * ContainerUser context, there may be a more pipeline-y way of passing this.
+     *
+     */
+    public void setLogger(Logger logger);
+
+    public String getRecordedActionName();
+}
--- api/src/org/labkey/api/query/QueryService.java
+++ api/src/org/labkey/api/query/QueryService.java
@@ -369,7 +369,7 @@ abstract public class QueryService
     /** OLAP **/
     // could make this a separate service
     abstract public void cubeDataChanged(Container c);    // TODO could be more specific than "something in this container"
-    abstract public void warmCube(User user, Container container, String schemaName, String configId, String cubeName);
+    abstract public String warmCube(User user, Container container, String schemaName, String configId, String cubeName);
 
     abstract public void saveNamedSet(String setName, List<String> setList);
     abstract public void deleteNamedSet(String setName);
