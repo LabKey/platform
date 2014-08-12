d0bb554c4cf9b5ccd44d126d1d4be746e73713b2
Matthew Bellew <mbellew@labkey.com>
2014-08-12 22:14:51 +0000

    LABKEY.help.Tour
    moved into api/webapp/clientapi/dom/

---
+++ api/webapp/clientapi/dom/Tour.js
@@ -0,0 +1,63 @@
+
+if (!('help' in LABKEY))
+    LABKEY.help = {};
+
+LABKEY.help.Tour =
+{
+    _tours : {},
+
+    register : function(config)
+    {
+        this._tours[config.id] = config;
+    },
+
+    show : function(idOrConfig)
+    {
+        var config = idOrConfig;
+        if (typeof idOrConfig == "string")
+            config = this._tours[idOrConfig];
+
+        var hopscotchSrc = "/hopscotch/js/hopscotch.js";
+        LABKEY.requiresScript(hopscotchSrc, true, function(){
+            hopscotch.startTour(config);
+            this.markSeen(config.id);
+        }, this);
+    },
+
+    autoShow : function(idOrConfig)
+    {
+        var id = idOrConfig;
+        var config = idOrConfig;
+        if (typeof idOrConfig == "string")
+            config = this._tours[idOrConfig];
+        else
+            id = config.id;
+
+        if (!config || !id)
+            return;
+
+        if (this.seen(id))
+            return;
+        this.show(config);
+    },
+
+    seen : function(id)
+    {
+        // use one item for all tours, this is a little more complicated, but makes it easier to reset state
+        var state = {};
+        var v = localStorage.getItem("tours");
+        if (v)
+            state = LABKEY.Utils.decode(v);
+        return "seen" == state[id];
+    },
+
+    markSeen : function(id)
+    {
+        var state = {};
+        var v = localStorage.getItem("tours");
+        if (v)
+            state = LABKEY.Utils.decode(v);
+        state[id] = "seen";
+        localStorage.setItem("tours", LABKEY.Utils.encode(state));
+    }
+};
