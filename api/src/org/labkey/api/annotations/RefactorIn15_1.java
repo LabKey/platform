725030f237c58e548306d5bc2cecbae2dcf2eb78
Adam Rauch <adam@labkey.com>
2014-09-03 00:27:56 +0000

    21225: Remove option to export/import studies using legacy format
    Remove legacy format option on export. Eliminate testing of legacy formats (StudyExportTest). Add deprecation warning to imports that use legacy formats.
    
    @RefactorIn15_1 marker annotation

---
+++ api/src/org/labkey/api/annotations/RefactorIn15_1.java
@@ -0,0 +1,16 @@
+package org.labkey.api.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * User: adam
+ * Date: 9/1/2014
+ * Time: 6:06 PM
+ */
+
+// Used to mark code that should be removed or refactored in LabKey version 15.1.
+@Retention(RetentionPolicy.SOURCE)
+public @interface RefactorIn15_1
+{
+}
