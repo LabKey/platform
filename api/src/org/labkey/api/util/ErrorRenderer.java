7812c2abdaf4b666cea9abcb247fd250c293ae0e
Josh Eckels <jeckels@labkey.com>
2014-09-03 21:25:54 +0000

    Eliminate trivial dependency on Ext

--- api/src/org/labkey/api/util/ErrorRenderer.java
+++ api/src/org/labkey/api/util/ErrorRenderer.java
@@ -121,7 +121,7 @@ public class ErrorRenderer
 
             if (!showDetails)
             {
-                out.println("<p><div id='togglePanel' style='cursor:pointer' onclick='Ext.getDom(\"contentPanel\").style.display=\"block\";Ext.getDom(\"togglePanel\").style.display=\"none\";'>" + PageFlowUtil.textLink("Show more details", "#details") + "</div>\n" +
+                out.println("<p><div id='togglePanel' style='cursor:pointer' onclick='document.getElementById(\"contentPanel\").style.display=\"block\";document.getElementById(\"togglePanel\").style.display=\"none\";'>" + PageFlowUtil.textLink("Show more details", "#details") + "</div>\n" +
                         "<div id=\"contentPanel\" style=\"display:none;\">");
             }
 
