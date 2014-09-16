eff668af8a6089adbf9a1d19e2f7d932359d67e2
Matthew Bellew <mbellew@labkey.com>
2014-09-16 16:10:22 +0000

    LABKEY.help.Tour.resume()

--- api/webapp/clientapi/dom/Tour.js
+++ api/webapp/clientapi/dom/Tour.js
@@ -9,66 +9,122 @@ if (!('help' in LABKEY))
 
 LABKEY.help.Tour =
 {
+    _localStorageProperty : "LABKEY.tours.state",
     _hopscotchSrc : "/hopscotch/js/hopscotch.js",
     _hopscotchCss : "/hopscotch/css/hopscotch.css",
     _tours : {},
 
-    register : function(config)
-    {
-        this._tours[config.id] = config;
-    },
-
-    _initHopscotch: function(fn,scope)
+    _initHopscotch : function(fn,scope)
     {
         LABKEY.requiresScript(this._hopscotchSrc, true, fn, scope);
         LABKEY.requiresCss(this._hopscotchCss);
     },
 
-    show : function(idOrConfig)
+
+    _get : function(idOrConfig)
     {
         var config = idOrConfig;
         if (typeof idOrConfig == "string")
             config = this._tours[idOrConfig];
-        this._initHopscotch(function(){
-            hopscotch.startTour(config);
+        if (!config || !config.id)
+        {
+            console.warn("tour not found, or not configured properly: " + idOrConfig);
+            return null;
+        }
+        return config;
+    },
+
+
+    /**
+     * Define a tour
+     */
+    register : function(config)
+    {
+        this._tours[config.id] = config;
+    },
+
+
+    /**
+     * Show tour starting at the beginning
+     * Always loads hopscotch.js
+     */
+    show : function(idOrConfig, step)
+    {
+        var config = this._get(idOrConfig);
+        if (!config)
+            return;
+        this._initHopscotch(function()
+        {
+            hopscotch.startTour(config, step||0);
             this.markSeen(config.id);
         }, this);
     },
 
+
+    /**
+     * Countinue tour if it is currently on the indicated step, useful for multi-page tours
+     * Always loads hopscotch.js
+     */
+    resume : function(idOrConfig, step)
+    {
+        var config = this._get(idOrConfig);
+        if (!config)
+            return;
+        var testState = config.id + ":" + step;
+        this._initHopscotch(function()
+        {
+            if (hopscotch.getState() == testState)
+                hopscotch.startTour(config, step);
+        },this);
+    },
+
+
+    /**
+     * Show tour if it has never been shown before.
+     * Conditionally loads hopscotch.js if the tour needs to be shown.
+     */
     autoShow : function(idOrConfig)
     {
-        var id = idOrConfig;
-        var config = idOrConfig;
-        if (typeof idOrConfig == "string")
-            config = this._tours[idOrConfig];
-        else
-            id = config.id;
-
-        if (!config || !id)
+        var config = this._get(idOrConfig);
+        if (!config)
             return;
-
-        if (this.seen(id))
+        if (this.seen(config.id))
             return;
         this.show(config);
     },
+
 
     seen : function(id)
     {
         // use one item for all tours, this is a little more complicated, but makes it easier to reset state
         var state = {};
-        var v = localStorage.getItem("tours");
+        var v = localStorage.getItem(this._localStorageProperty);
         if (v)
             state = LABKEY.Utils.decode(v);
         return "seen" == state[id];
     },
 
+
+    /**
+     * Mark tour as seen so autoShow() will no longer show this tour
+     */
     markSeen : function(id)
     {
         var state = {};
-        var v = localStorage.getItem("tours");
+        var v = localStorage.getItem(this._localStorageProperty);
         if (v)
             state = LABKEY.Utils.decode(v);
         state[id] = "seen";
-        localStorage.setItem("tours", LABKEY.Utils.encode(state));
+        localStorage.setItem(this._localStorageProperty, LABKEY.Utils.encode(state));
+    },
+
+
+    reset : function()
+    {
+        localStorage.setItem(this._localStorageProperty, "{}");
+        this._initHopscotch(function()
+        {
+            hopscotch.endTour(true,false);
+        });
     }
 };
