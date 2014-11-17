da9f139ab3ce373ab003cabb4255a7c408353f44
Adam Rauch <adam@labkey.com>
2014-11-17 00:11:41 +0000

    Add BEGIN/END to a few SQL Server CREATE PROCEDURE definitions. Not required (by the database), but improves our ability to parse the definitions.

--- dataintegration/resources/schemas/dbscripts/sqlserver/dataintegration-0.00-13.10.sql
+++ dataintegration/resources/schemas/dbscripts/sqlserver/dataintegration-0.00-13.10.sql
@@ -20,16 +20,18 @@ GO
 
 CREATE PROCEDURE dataintegration.addDataIntegrationColumns @schemaName NVARCHAR(100), @tableName NVARCHAR(100)
 AS
-DECLARE @sql NVARCHAR(1000)
-SELECT @sql = 'ALTER TABLE [' + @schemaName + '].[' + @tableName + '] ADD  ' +
-     '_txRowVersion ROWVERSION, ' +
-     '_txLastUpdated DATETIME, ' +
-     '_txTransactionId INT, ' +
-     '_txNote NVARCHAR(1000)';
-EXEC sp_executesql @sql;
-SELECT @sql = 'ALTER TABLE [' + @schemaName + '].[' + @tableName + '] ADD CONSTRAINT [_DF_' + @tableName + '_updated] ' +
-    'DEFAULT getutcdate() FOR [_txLastUpdated]';
-EXEC sp_executesql @sql;
+BEGIN
+    DECLARE @sql NVARCHAR(1000)
+    SELECT @sql = 'ALTER TABLE [' + @schemaName + '].[' + @tableName + '] ADD  ' +
+        '_txRowVersion ROWVERSION, ' +
+        '_txLastUpdated DATETIME, ' +
+        '_txTransactionId INT, ' +
+        '_txNote NVARCHAR(1000)';
+    EXEC sp_executesql @sql;
+    SELECT @sql = 'ALTER TABLE [' + @schemaName + '].[' + @tableName + '] ADD CONSTRAINT [_DF_' + @tableName + '_updated] ' +
+        'DEFAULT getutcdate() FOR [_txLastUpdated]';
+    EXEC sp_executesql @sql;
+END
 
 GO
 
