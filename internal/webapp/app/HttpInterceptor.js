7ee8b0702947f5b64c8e9861395c270e3e26667b
Adam Rauch <adam@labkey.com>
2014-12-18 20:10:21 +0000

    Update licenses

--- internal/webapp/app/HttpInterceptor.js
+++ internal/webapp/app/HttpInterceptor.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext.define('LABKEY.app.controller.HttpInterceptor', {
 
     extend : 'Ext.app.Controller',
