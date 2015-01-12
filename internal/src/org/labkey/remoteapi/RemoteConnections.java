bfd1f982981709d8725fb85080be6d1f8f74644c
Dave Bradlee <dave@labkey.com>
2015-01-12 17:58:42 +0000

    Refactor Remote Connections so that it can be used for webdav/labkey integrations (files) in addition to current use for queries.

--- internal/src/org/labkey/remoteapi/RemoteConnections.java
+++ internal/src/org/labkey/remoteapi/RemoteConnections.java
@@ -15,15 +15,228 @@
  */
 package org.labkey.remoteapi;
 
+import org.apache.commons.lang3.StringUtils;
+import org.labkey.api.action.LabkeyError;
+import org.labkey.api.data.Container;
+import org.labkey.api.data.PropertyManager;
+import org.labkey.api.security.ValidEmail;
+import org.springframework.validation.BindException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Map;
+
 /**
  * User: gktaylor
  * Date: 10/29/13
  */
 public class RemoteConnections
 {
-    public static String REMOTE_CONNECTIONS_CATEGORY = "remote-connections";
+    public static String REMOTE_QUERY_CONNECTIONS_CATEGORY = "remote-connections";
+    public static String REMOTE_FILE_CONNECTIONS_CATEGORY = "remote-file-connections";
     public static String FIELD_URL = "URL";
     public static String FIELD_USER = "user";
     public static String FIELD_PASSWORD = "password";
     public static String FIELD_CONTAINER = "container";
+
+    public static String CONNECTION_KIND_QUERY = "query";
+    public static String CONNECTION_KIND_FILE = "file";
+
+    public static Map<String, String> getRemoteConnection(String connectionKind, String name, Container container)
+    {
+        return PropertyManager.getEncryptedStore().getProperties(container,
+                RemoteConnections.makeRemoteConnectionKey(connectionKind, name));
+    }
+
+    public static boolean createOrEditRemoteConnection(RemoteConnectionForm remoteConnectionForm, Container container, BindException errors)
+    {
+        String name = remoteConnectionForm.getConnectionName();
+        String newName = remoteConnectionForm.getNewConnectionName();
+        boolean editing = StringUtils.isNotEmpty(name);
+        boolean changingName = editing && !name.equals(newName);
+        if (!StringUtils.isNotBlank(newName))
+        {
+            errors.addError(new LabkeyError("Connection name may not be blank."));
+            return false;
+        }
+
+        String url = remoteConnectionForm.getUrl();
+        String user = remoteConnectionForm.getUser();
+        String password = remoteConnectionForm.getPassword();
+        String folderPath = remoteConnectionForm.getContainer();
+        String connectionKind = remoteConnectionForm.getConnectionKind();
+
+        if (url == null || user == null || password == null || (CONNECTION_KIND_QUERY.equals(connectionKind) && folderPath == null))
+        {
+            errors.addError(new LabkeyError("All fields must be filled in."));
+            return false;
+        }
+
+        // validate the url string and connection
+        try
+        {
+            URL urlObj = new URL(url);
+            URLConnection conn = urlObj.openConnection();
+            conn.connect();
+        }
+        catch (MalformedURLException e)
+        {
+            errors.addError(new LabkeyError("The entered URL is not valid."));
+            return false;
+        }
+        catch (IOException e)
+        {
+            errors.addError(new LabkeyError("A connection to the entered URL could not be established."));
+            return false;
+        }
+
+        // validate the user
+        try
+        {
+            ValidEmail validEmail = new ValidEmail(user);
+        }
+        catch(ValidEmail.InvalidEmailException e)
+        {
+            errors.addError(new LabkeyError("The entered user is not a valid email address."));
+            return false;
+        }
+
+        // save the connection name in connectionMap
+        String connectionsCategory = CONNECTION_KIND_QUERY.equals(connectionKind) ? REMOTE_QUERY_CONNECTIONS_CATEGORY : REMOTE_FILE_CONNECTIONS_CATEGORY;
+        Map<String, String> connectionMap = PropertyManager.getEncryptedStore().getWritableProperties(container, connectionsCategory, true);
+        if ((!editing || changingName) && connectionMap.containsKey(makeRemoteConnectionKey(connectionKind, newName)))
+        {
+            errors.addError(new LabkeyError("There is already a remote connection with the name '" + newName + "'."));
+            return false;
+        }
+
+        if (changingName)
+        {
+            String oldNameKey = makeRemoteConnectionKey(connectionKind, name);
+            connectionMap.remove(oldNameKey);        // Remove old name
+            PropertyManager.getEncryptedStore().deletePropertySet(container, oldNameKey);
+        }
+
+        String newNameKey = makeRemoteConnectionKey(connectionKind, newName);
+        connectionMap.put(newNameKey, newName);
+        PropertyManager.getEncryptedStore().saveProperties(connectionMap);
+
+        // save the properties for the individual connection in the encrypted property store
+        Map <String, String> singleConnectionMap = PropertyManager.getEncryptedStore().getWritableProperties(container, newNameKey, true);
+        singleConnectionMap.put(RemoteConnections.FIELD_URL, url);
+        singleConnectionMap.put(RemoteConnections.FIELD_USER, user);
+        singleConnectionMap.put(RemoteConnections.FIELD_PASSWORD, password);
+        if (CONNECTION_KIND_QUERY.equals(connectionKind))
+            singleConnectionMap.put(RemoteConnections.FIELD_CONTAINER, folderPath);
+        PropertyManager.getEncryptedStore().saveProperties(singleConnectionMap);
+        return true;
+    }
+
+    public static boolean deleteRemoteConnection(RemoteConnectionForm remoteConnectionForm, Container container)
+    {
+        String name = remoteConnectionForm.getConnectionName();
+
+        // delete the index
+        String connectionsCategory = CONNECTION_KIND_QUERY.equals(remoteConnectionForm.getConnectionKind()) ? REMOTE_QUERY_CONNECTIONS_CATEGORY : REMOTE_FILE_CONNECTIONS_CATEGORY;
+        Map<String, String> connectionMap = PropertyManager.getEncryptedStore().getWritableProperties(container, connectionsCategory, false);
+        connectionMap.remove(makeRemoteConnectionKey(remoteConnectionForm.getConnectionKind(), name));
+        PropertyManager.getEncryptedStore().saveProperties(connectionMap);
+
+        // delete the underlying property set
+        PropertyManager.getEncryptedStore().deletePropertySet(container, makeRemoteConnectionKey(remoteConnectionForm.getConnectionKind(), name));
+
+        return true;
+    }
+
+    public static class RemoteConnectionForm
+    {
+        private String _connectionName;
+        private String _url;
+        private String _user;
+        private String _password;
+        private String _container;
+        private String _newConnectionName;
+        private String _connectionKind;
+
+        public RemoteConnectionForm()
+        {
+        }
+
+        public String getConnectionName()
+        {
+            return _connectionName;
+        }
+
+        public void setConnectionName(String connectionName)
+        {
+            _connectionName = connectionName;
+        }
+
+        public String getUrl()
+        {
+            return _url;
+        }
+
+        public void setUrl(String url)
+        {
+            _url = url;
+        }
+
+        public String getUser()
+        {
+            return _user;
+        }
+
+        public void setUser(String user)
+        {
+            _user = user;
+        }
+
+        public String getPassword()
+        {
+            return _password;
+        }
+
+        public void setPassword(String password)
+        {
+            _password = password;
+        }
+
+        public String getContainer()
+        {
+            return _container;
+        }
+
+        public void setContainer(String container)
+        {
+            _container = container;
+        }
+
+        public String getNewConnectionName()
+        {
+            return _newConnectionName;
+        }
+
+        public void setNewConnectionName(String newConnectionName)
+        {
+            _newConnectionName = newConnectionName;
+        }
+
+        public String getConnectionKind()
+        {
+            return _connectionKind;
+        }
+
+        public void setConnectionKind(String connectionKind)
+        {
+            _connectionKind = connectionKind;
+        }
+    }
+
+    private static String makeRemoteConnectionKey(String connectionKind, String name)
+    {
+        return connectionKind + ":" + name;
+    }
 }
