6ec021504caf325dbef40f325f1aa2bfa54e3885
Adam Rauch <adam@labkey.com>
2015-01-17 18:01:01 +0000

    Update licenses

--- internal/webapp/app/Application.js
+++ internal/webapp/app/Application.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext.define('LABKEY.app.Application', {
     extend: 'Ext.app.Application',
 
