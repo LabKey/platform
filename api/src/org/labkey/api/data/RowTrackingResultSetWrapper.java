cd0269d213c71f6d2e958fff3a5299834b29d5c6
Adam Rauch <adam@labkey.com>
2015-02-24 20:01:53 +0000

    Issue 22527: Need to implement ResultSet.getRow() for SAS
    afterLast(), beforeFirst(), stop checking isValid, junit test

--- api/src/org/labkey/api/data/RowTrackingResultSetWrapper.java
+++ api/src/org/labkey/api/data/RowTrackingResultSetWrapper.java
@@ -15,19 +15,21 @@
  */
 package org.labkey.api.data;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
+ * ResultSet wrapper that tracks the current row based on navigation operations. This is needed for JDBC drivers that don't
+ * correctly implement getRow(), most notably, the SAS JDBC driver.
+ *
  * User: adam
  * Date: 2/21/2015
  * Time: 1:17 PM
  */
 
-// ResultSet wrapper that tracks the current row based on navigation operations. This is needed for JDBC drivers that don't
-// correctly implement getRow(), most notably, the SAS JDBC driver.
-//
-// Implementation follows the same basic pattern as LoggingResultSet.
 public class RowTrackingResultSetWrapper extends ResultSetWrapper
 {
     private int _currentRow = 0;
@@ -43,59 +45,101 @@ public class RowTrackingResultSetWrapper extends ResultSetWrapper
         return _currentRow;
     }
 
-    @Override
-    public boolean next() throws SQLException
-    {
-        boolean isNext = resultset.next();
-        if (isNext)
-            _currentRow++;
-        return isNext;
-    }
+    // These are the absolute positioning methods that we support
 
     @Override
     public boolean first() throws SQLException
     {
-        boolean isValid = resultset.first();
-        if (isValid)
-            _currentRow = 1;
-        return isValid;
+        _currentRow = 1;
+        return resultset.first();
     }
+
+    @Override
+    public boolean absolute(int i) throws SQLException
+    {
+        _currentRow = i;
+        return resultset.absolute(i);
+    }
+
+    @Override
+    public void beforeFirst() throws SQLException
+    {
+        _currentRow = 0;
+        resultset.beforeFirst();
+    }
+
+    @Override
+    public void afterLast() throws SQLException
+    {
+        _currentRow = 0;
+        resultset.afterLast();
+    }
+
+    // These are the relative positioning methods that we support
+
+    @Override
+    public boolean next() throws SQLException
+    {
+        _currentRow++;
+        return resultset.next();
+    }
+
+    @Override
+    public boolean previous() throws SQLException
+    {
+        _currentRow--;
+        return resultset.previous();
+    }
+
+    @Override
+    public boolean relative(int i) throws SQLException
+    {
+        _currentRow += i;
+        return resultset.relative(i);
+    }
+
+    // We don't support this method
 
     @Override
     public boolean last() throws SQLException
     {
         // We can't set _currentRow correctly, unless we enumerate every row, so just throw for now
         throw new SQLException("Not supported");
-//        boolean isValid = resultset.last();
-//        if (isValid)
-//            _currentRow = ??;
-//        return isValid;
     }
 
-    @Override
-    public boolean absolute(int i) throws SQLException
-    {
-        boolean isValid = resultset.absolute(i);
-        if (isValid)
-            _currentRow = i;
-        return isValid;
-    }
 
-    @Override
-    public boolean relative(int i) throws SQLException
+    // Test basic operations. This doesn't test edge conditions like previous() when before first, next when after last, absolute(-3), etc.
+    public static class TestCase extends Assert
     {
-        boolean isValid = resultset.relative(i);
-        if (isValid)
-            _currentRow += i;
-        return isValid;
-    }
+        @Test
+        public void test() throws SQLException
+        {
+            TableSelector selector = new TableSelector(CoreSchema.getInstance().getTableInfoModules());
 
-    @Override
-    public boolean previous() throws SQLException
-    {
-        boolean isValid = resultset.previous();
-        if (isValid)
-            _currentRow--;
-        return isValid;
+            try (ResultSet rs = selector.getResultSet(); ResultSet wrapper = new RowTrackingResultSetWrapper(rs))
+            {
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.next();
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.next();
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.previous();
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.absolute(7);
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.absolute(5);
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.relative(2);
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.relative(-2);
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.first();
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.beforeFirst();
+                assertEquals(rs.getRow(), wrapper.getRow());
+                wrapper.afterLast();
+                assertEquals(rs.getRow(), wrapper.getRow());
+            }
+        }
     }
 }
