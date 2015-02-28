a7db4399f24a82d8400e6726f18a9bb4dc293938
Tony Galuhn <tgaluhn@labkey.com>
2015-02-28 19:17:47 +0000

    Issue 22666 NPE in ETL when no pipeline root set

--- dataintegration/src/org/labkey/di/pipeline/TransformPipelineJob.java
+++ dataintegration/src/org/labkey/di/pipeline/TransformPipelineJob.java
@@ -63,12 +63,14 @@ public class TransformPipelineJob extends PipelineJob implements TransformJobSup
     public static final String ETL_PREFIX = "ETL Job: ";
 
 
-    public TransformPipelineJob(TransformJobContext info, TransformDescriptor etlDescriptor)
+    public TransformPipelineJob(@NotNull TransformJobContext info, TransformDescriptor etlDescriptor)
     {
         super(ETLPipelineProvider.NAME,
                 new ViewBackgroundInfo(info.getContainer(), info.getUser(), null),
                 PipelineService.get().findPipelineRoot(info.getContainer()));
         _etlDescriptor = etlDescriptor;
+        if (null == getPipeRoot())
+            throw new IllegalStateException("Pipeline root has not been set.");
         File etlLogDir = getPipeRoot().resolvePath("etlLogs");
         //TODO: The string replace is a temp workaround until we remove the braces from the module name and switch
         // to passing around a TaskId instead.
