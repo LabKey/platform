43a6dcecc4d1bba1f034bd9716e48cea1af55c9b
Adam Rauch <adam@labkey.com>
2015-03-01 00:18:28 +0000

    Update licenses

--- query/webapp/query/browser/view/SchemaDetails.js
+++ query/webapp/query/browser/view/SchemaDetails.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext4.define('LABKEY.query.browser.view.SchemaDetails', {
 
     extend: 'LABKEY.query.browser.view.Base',
