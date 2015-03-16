80a913931c159d10c8486b512e49231c4643f77f
Adam Rauch <adam@labkey.com>
2015-03-16 16:25:11 +0000

    Update licenses

--- internal/webapp/designer/FieldMetaStore.js
+++ internal/webapp/designer/FieldMetaStore.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 /**
  * An Ext.data.Store for LABKEY.ext.FieldMetaRecord json objects.
--- internal/webapp/designer/FieldTreeLoader.js
+++ internal/webapp/designer/FieldTreeLoader.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 // This TreeLoader returns TreeNodes for field metadata and is backed by a FieldMetaStore.
 LABKEY.ext.FieldTreeLoader = Ext.extend(Ext.tree.TreeLoader, {
