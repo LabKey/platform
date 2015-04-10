80a6af838124616bb697377a04d322f9c0b5f3d9
Adam Rauch <adam@labkey.com>
2015-04-10 18:49:19 +0000

    Update licenses

--- internal/webapp/internal/ViewDesigner/field/FilterTextValue.js
+++ internal/webapp/internal/ViewDesigner/field/FilterTextValue.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 Ext4.define('LABKEY.internal.ViewDesigner.field.FilterTextValue', {
 
