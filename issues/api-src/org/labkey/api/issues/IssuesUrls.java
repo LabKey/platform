90c0813ee18919ce53584d6367952f00b2dc1211
Kevin Krouse <kevink@labkey.com>
2015-04-17 22:00:35 +0000

    extract issues api

---
+++ issues/api-src/org/labkey/api/issues/IssuesUrls.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2010 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.api.issues;
+
+import org.labkey.api.action.UrlProvider;
+import org.labkey.api.data.Container;
+import org.labkey.api.view.ActionURL;
+
+/*
+* User: adam
+* Date: Nov 19, 2010
+* Time: 9:38:34 PM
+*/
+public interface IssuesUrls extends UrlProvider
+{
+    public ActionURL getDetailsURL(Container c);
+}
