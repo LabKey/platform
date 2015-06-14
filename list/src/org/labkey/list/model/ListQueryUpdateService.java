93481cc4c33a2096e3b01617bf917f0b44eaa85d
Adam Rauch <adam@labkey.com>
2015-06-14 23:34:17 +0000

    Remove warnings: unused imports, visibility

--- list/src/org/labkey/list/model/ListQueryUpdateService.java
+++ list/src/org/labkey/list/model/ListQueryUpdateService.java
@@ -72,7 +72,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
