411f9616e34aac29538d6a76463fd5ba5e49e429
Adam Rauch <adam@labkey.com>
2015-07-16 02:52:49 +0000

    Earliest upgrade version is now 13.20

--- api/src/org/labkey/api/module/ModuleLoader.java
+++ api/src/org/labkey/api/module/ModuleLoader.java
@@ -119,7 +119,7 @@ import java.util.regex.Pattern;
  */
 public class ModuleLoader implements Filter
 {
-    private static final double EARLIEST_UPGRADE_VERSION = 13.1;
+    private static final double EARLIEST_UPGRADE_VERSION = 13.2;
     private static final Logger _log = Logger.getLogger(ModuleLoader.class);
     private static final Map<String, Throwable> _moduleFailures = new HashMap<>();
     private static final Map<String, Module> _controllerNameToModule = new HashMap<>();
