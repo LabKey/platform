7f07b58685bab0fa43339c8781aa6d4a0b62f691
Adam Rauch <adam@labkey.com>
2015-08-02 04:12:49 +0000

    Update licenses

--- core/webapp/login.js
+++ core/webapp/login.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 (function($) {
 
  // bind triggers, page init, etc
