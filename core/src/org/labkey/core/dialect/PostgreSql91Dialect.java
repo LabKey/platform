13a8d262234b80766ceccd110ae89edd922b0f91
Matthew Bellew <mbellew@labkey.com>
2015-08-03 17:45:47 +0000

    revert

--- core/src/org/labkey/core/dialect/PostgreSql91Dialect.java
+++ core/src/org/labkey/core/dialect/PostgreSql91Dialect.java
@@ -1408,16 +1408,18 @@ public class PostgreSql91Dialect extends SqlDialect
     {
         if (hasReturn || assignResult)
             paramCount--; // this param isn't included in the argument list of the CALL statement
-
-        StringBuilder parameters = new StringBuilder();
-        String comma = "";
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        if (assignResult)
+            sb.append("? = ");
+        sb.append("CALL " + procSchema + "." + procName +"(");
         for (int i = 0; i < paramCount; i++)
         {
-            parameters.append(comma).append("?");
-            comma = ",";
+            sb.append("?,");
         }
-
-        return "SELECT " + procSchema + "." + procName + "(" + parameters.toString() + ")";
+        sb.setLength(sb.length() - 1); // Postgres chokes on the trailing comma
+        sb.append(")}");
+        return sb.toString();
     }
 
     @Override
