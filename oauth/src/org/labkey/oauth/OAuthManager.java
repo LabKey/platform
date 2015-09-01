eb3391e45556ae0cf54b2c347991cb18055e6d6f
Adam Rauch <adam@labkey.com>
2015-09-01 23:43:22 +0000

    Spec ID 24057: Split SAML, Duo, CAS, OAuth into separate modules Spec ID 24048: Allow customers to easily turn off footer Spec ID 24058: Split SQL Server synonyms into separate module
    Create modules for this sprint's feature split outs
    
    Fix initial version numbers in module template

---
+++ oauth/src/org/labkey/oauth/OAuthManager.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.oauth;
+
+public class OAuthManager
+{
+    private static final OAuthManager _instance = new OAuthManager();
+
+    private OAuthManager()
+    {
+        // prevent external construction with a private default constructor
+    }
+
+    public static OAuthManager get()
+    {
+        return _instance;
+    }
+}
---
+++ oauth/src/org/labkey/oauth/OAuthModule.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.oauth;
+
+import org.jetbrains.annotations.NotNull;
+import org.labkey.api.module.DefaultModule;
+import org.labkey.api.module.ModuleContext;
+import org.labkey.api.view.WebPartFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class OAuthModule extends DefaultModule
+{
+    public static final String NAME = "OAuth";
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public double getVersion()
+    {
+        return 15.21;
+    }
+
+    @Override
+    public boolean hasScripts()
+    {
+        return true;
+    }
+
+    @Override
+    @NotNull
+    protected Collection<WebPartFactory> createWebPartFactories()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected void init()
+    {
+    }
+
+    @Override
+    public void doStartup(ModuleContext moduleContext)
+    {
+    }
+}
