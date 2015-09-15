afbf44d9b5b94cc99624cc66ad62d0654f0db639
Tony Galuhn <tgaluhn@labkey.com>
2015-09-15 17:08:36 +0000

    24159 Convert tumor marker table to wide table with sparse columns. Necessary core support; bypass jTDS getColumns(), query metadata directly

--- study/src/org/labkey/study/query/DatasetTableImpl.java
+++ study/src/org/labkey/study/query/DatasetTableImpl.java
@@ -21,7 +21,6 @@ import org.jetbrains.annotations.NotNull;
 import org.labkey.api.collections.CaseInsensitiveHashSet;
 import org.labkey.api.data.AbstractForeignKey;
 import org.labkey.api.data.ColumnInfo;
-import org.labkey.api.data.CompareType;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerFilter;
 import org.labkey.api.data.ContainerFilterable;
@@ -74,7 +73,6 @@ import org.labkey.api.study.assay.SpecimenForeignKey;
 import org.labkey.api.util.ContainerContext;
 import org.labkey.api.util.DemoMode;
 import org.labkey.api.util.PageFlowUtil;
-import org.labkey.api.util.Pair;
 import org.labkey.api.util.StringExpression;
 import org.labkey.api.view.ActionURL;
 import org.labkey.data.xml.TableType;
@@ -87,7 +85,6 @@ import org.labkey.study.model.QCState;
 import org.labkey.study.model.StudyManager;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -717,6 +714,7 @@ public class DatasetTableImpl extends BaseStudyTable implements DatasetTable
     }
 
 
+    @NotNull
     @Override
     public SQLFragment getFromSQL(String alias, Set<FieldKey> cols)
     {
