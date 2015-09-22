8ed6ee1c9dea02c49b93b3ba9737c0cbe7b42de6
Adam Rauch <adam@labkey.com>
2015-09-22 04:34:53 +0000

    Update licenses

--- pipeline/src/org/labkey/pipeline/mule/routing/EPipelineQueueMessageSplitter.java
+++ pipeline/src/org/labkey/pipeline/mule/routing/EPipelineQueueMessageSplitter.java
@@ -1,7 +1,7 @@
 /*
  * $Id: FilteringListMessageSplitter.java 7976 2007-08-21 14:26:13Z dirk.olmes $
  * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ * Copyright (c) MuleSource, Inc. All rights reserved. http://www.mulesource.com
  *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
