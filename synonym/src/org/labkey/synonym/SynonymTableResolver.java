0b81e65a9f4e824bc856a95f4d25146e74b1a410
Adam Rauch <adam@labkey.com>
2015-10-12 01:14:54 +0000

    Issue 24314: Query Stack Traces -> Show execution plan causes NPE when using IN clause on postgres
    Translate JDBC array parameters back to Java array parameters for query profiler purposes. Stop ignoring exceptions from JDBC Array.free(); PostgreSQL driver now supports free().
    
    Simplify SynonymTableResolver by adding a few imports

--- synonym/src/org/labkey/synonym/SynonymTableResolver.java
+++ synonym/src/org/labkey/synonym/SynonymTableResolver.java
@@ -17,7 +17,7 @@ package org.labkey.synonym;
 
 import org.jetbrains.annotations.Nullable;
 import org.labkey.api.data.DatabaseTableType;
-import org.labkey.api.data.DbSchema;
+import org.labkey.api.data.DbSchema.TableMetaDataLoader;
 import org.labkey.api.data.DbScope;
 import org.labkey.api.data.SchemaTableInfoFactory;
 import org.labkey.api.data.StandardSchemaTableInfoFactory;
@@ -25,6 +25,9 @@ import org.labkey.api.data.dialect.ForeignKeyResolver;
 import org.labkey.api.data.dialect.JdbcMetaDataLocator;
 import org.labkey.api.data.dialect.StandardTableResolver;
 import org.labkey.api.util.Pair;
+import org.labkey.synonym.SynonymManager.Synonym;
+import org.labkey.synonym.SynonymManager.SynonymForeignKeyResolver;
+import org.labkey.synonym.SynonymManager.SynonymJdbcMetaDataLocator;
 
 import java.sql.SQLException;
 import java.util.Map;
@@ -37,14 +40,14 @@ public class SynonymTableResolver extends StandardTableResolver
     @Override
     public void addTableInfoFactories(Map<String, SchemaTableInfoFactory> map, DbScope scope, String schemaName) throws SQLException
     {
-        Map<String, SynonymManager.Synonym> synonymMap = SynonymManager.getSynonymMap(scope, schemaName);
+        Map<String, Synonym> synonymMap = SynonymManager.getSynonymMap(scope, schemaName);
 
         // Put a SchemaTableInfoFactory into the map for each synonym
         for (String synonymName : synonymMap.keySet())
         {
             try (JdbcMetaDataLocator locator = getJdbcMetaDataLocator(scope, schemaName, synonymName))
             {
-                new DbSchema.TableMetaDataLoader<SchemaTableInfoFactory>(locator, true)
+                new TableMetaDataLoader<SchemaTableInfoFactory>(locator, true)
                 {
                     @Override
                     protected void handleTable(String tableName, DatabaseTableType tableType, String description) throws SQLException
@@ -59,22 +62,22 @@ public class SynonymTableResolver extends StandardTableResolver
     @Override
     public JdbcMetaDataLocator getJdbcMetaDataLocator(DbScope scope, @Nullable String schemaName, @Nullable String requestedTableName) throws SQLException
     {
-        Pair<DbScope, SynonymManager.Synonym> pair = SynonymManager.getSynonym(scope, schemaName, requestedTableName);
+        Pair<DbScope, Synonym> pair = SynonymManager.getSynonym(scope, schemaName, requestedTableName);
 
         if (null == pair)
             return super.getJdbcMetaDataLocator(scope, schemaName, requestedTableName);      // Not a valid synonym, so return the standard locator
         else
-            return new SynonymManager.SynonymJdbcMetaDataLocator(pair.first, scope, pair.second);  // tableName is a synonym, so return a synonym locator
+            return new SynonymJdbcMetaDataLocator(pair.first, scope, pair.second);  // tableName is a synonym, so return a synonym locator
     }
 
     @Override
     public ForeignKeyResolver getForeignKeyResolver(DbScope scope, @Nullable String schemaName, @Nullable String tableName)
     {
-        Pair<DbScope, SynonymManager.Synonym> pair = SynonymManager.getSynonym(scope, schemaName, tableName);
+        Pair<DbScope, Synonym> pair = SynonymManager.getSynonym(scope, schemaName, tableName);
 
         if (null == pair)
             return super.getForeignKeyResolver(scope, schemaName, tableName);       // Not a synonym, so return the standard resolver
         else
-            return new SynonymManager.SynonymForeignKeyResolver(pair.first, scope, pair.second);   // tableName is a synonym, so return a synonym resolver
+            return new SynonymForeignKeyResolver(pair.first, scope, pair.second);   // tableName is a synonym, so return a synonym resolver
     }
 }
