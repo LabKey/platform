92c7b587af76e0a05525550c6809937b9fc3e451
Jim Piper <jimp@labkey.com>
2015-10-13 23:19:28 +0000

    Issue 24340: update license for the synonym module

--- synonym/src/org/labkey/synonym/SynonymManager.java
+++ synonym/src/org/labkey/synonym/SynonymManager.java
@@ -1,17 +1,6 @@
 /*
- * Copyright (c) 2015 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) 2015 LabKey Corporation. All rights reserved. No portion of this work may be reproduced in
+ * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
  */
 package org.labkey.synonym;
 
