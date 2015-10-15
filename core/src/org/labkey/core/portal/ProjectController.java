bb11edff90f335e1638d524db7feff17f0760c37
Ian Sigmon <ians@labkey.com>
2015-10-15 23:26:29 +0000

    Issue 23139: Data views webpart disappears after creation of new clinical/assay tab  - Added check for deleted page as part of returnURL  - Added delete scenarios to existing TabTest

--- core/src/org/labkey/core/portal/ProjectController.java
+++ core/src/org/labkey/core/portal/ProjectController.java
@@ -523,7 +523,13 @@ public class ProjectController extends SpringActionController
             handlePost(form, errors);
             URLHelper successURL = getSuccessURL(form);
             if (null != successURL)
+            {
+                //Don't return to the deleted page
+                if(form.getPageId().equalsIgnoreCase(successURL.getParameter("pageId")))
+                    successURL.deleteParameter("pageId");
+
                 return HttpView.redirect(successURL);
+            }
             return HttpView.redirect(getContainer().getStartURL(getUser()));
         }
 
