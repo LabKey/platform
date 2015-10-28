3e5b1db0400217f606d7bb607f15200eeeb2204e
Tony Galuhn <tgaluhn@labkey.com>
2015-10-28 02:19:08 +0000

    Fix memory leaks; ScopyQueryLoggingProfilerListener didn't need a ViewContext. Clear environments in background pipeline job threads

--- pipeline/src/org/labkey/pipeline/mule/PipelineJobRunner.java
+++ pipeline/src/org/labkey/pipeline/mule/PipelineJobRunner.java
@@ -35,7 +35,7 @@ public class PipelineJobRunner
         }
         finally
         {
-            DbScope.closeAllConnectionsForCurrentThread();
+            DbScope.finishedWithThread();
         }
     }
 }
