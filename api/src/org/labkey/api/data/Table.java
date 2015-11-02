d5e7647e910dfa9591a6ebdd8ff9ee91805c348e
Adam Rauch <adam@labkey.com>
2015-11-02 19:34:19 +0000

    Unused import

--- api/src/org/labkey/api/data/Table.java
+++ api/src/org/labkey/api/data/Table.java
@@ -45,7 +45,6 @@ import org.labkey.api.query.BatchValidationException;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.QueryService;
 import org.labkey.api.query.RuntimeValidationException;
-import org.labkey.api.query.ValidationException;
 import org.labkey.api.security.User;
 import org.labkey.api.util.GUID;
 import org.labkey.api.util.JunitUtil;
