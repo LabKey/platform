38b1c6fc3d7d3db82524bc64cfcd0fc3944159b4
Josh Eckels <jeckels@labkey.com>
2015-11-09 18:45:59 +0000

    Delete now-orphaned function, related to 24776

--- api/webapp/clientapi/ext3/FormPanel.js
+++ api/webapp/clientapi/ext3/FormPanel.js
@@ -689,14 +689,6 @@ LABKEY.ext.FormHelper =
         }
 
         return config;
-    },
-
-    twoDigit : function(num)
-    {
-        if (num < 10)
-            return "0" + num;
-        else
-            return "" + num;
     }
 };
 
