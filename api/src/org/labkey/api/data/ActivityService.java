241554889d529332bb0613429b690a77022fa8bf
Susan Hert <susanh@labkey.com>
2015-11-20 19:30:14 +0000

    Spec 24608: column chooser performance improvements by using getMeasuresStatic

---
+++ api/src/org/labkey/api/data/ActivityService.java
@@ -0,0 +1,11 @@
+package org.labkey.api.data;
+
+import org.labkey.api.view.ViewContext;
+
+/**
+ * Created by susanh on 11/20/15.
+ */
+public interface ActivityService
+{
+    PHI getPHI(ViewContext context);
+}
