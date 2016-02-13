6392577105a45d89f033bc73a23ef790dff8a5fb
Nick Kerr <nicka@labkey.com>
2016-02-13 22:44:02 +0000

    Update experiment/lineage.api - add initial ExpObject metadata - parent/children are array of objects describing edge (e.g. includes "role")

--- api/src/org/labkey/api/exp/api/ExpLineage.java
+++ api/src/org/labkey/api/exp/api/ExpLineage.java
@@ -1,5 +1,6 @@
 package org.labkey.api.exp.api;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.labkey.api.util.Pair;
 
@@ -17,18 +18,20 @@ public class ExpLineage
     private ExpProtocolOutput _seed;
     private Set<ExpData> _datas;
     private Set<ExpMaterial> _materials;
-    private Set<Pair<String, String>> _edges;
+    private Set<ExpRun> _runs;
+    private Set<Edge> _edges;
 
     public ExpLineage(ExpProtocolOutput seed)
     {
-        this(seed, Collections.emptySet(), Collections.emptySet(), Collections.emptySet());
+        this(seed, Collections.emptySet(), Collections.emptySet(), Collections.emptySet(), Collections.emptySet());
     }
 
-    public ExpLineage(ExpProtocolOutput seed, Set<ExpData> data, Set<ExpMaterial> materials, Set<Pair<String, String>> edges)
+    public ExpLineage(ExpProtocolOutput seed, Set<ExpData> data, Set<ExpMaterial> materials, Set<ExpRun> runs, Set<Edge> edges)
     {
         _seed = seed;
         _datas = data;
         _materials = materials;
+        _runs = runs;
         _edges = edges;
     }
 
@@ -42,66 +45,43 @@ public class ExpLineage
         return _materials;
     }
 
-    private Map<String, Pair<Set<String>, Set<String>>> processEdges(Set<Pair<String, String>> edges) throws IllegalStateException
+    private Map<String, ExpObject> processNodes()
     {
-        Map<String, Pair<Set<String>, Set<String>>> nodes = new HashMap<>();
+        Map<String, ExpObject> nodes = new HashMap<>();
 
-        // TODO: Replace this prefix filtering with something more robust and that captures all non-data/non-material types
-        // This loop is used solely to remap relationships. Edges can contain non-data/non-material types so this loop
-        // "looks through" those types to resolve parent/child relationships of datas/materials.
-        // Input: MatA -> Run1 -> MatB
-        // Output: MatA -> MatB
-        String prefix = "urn:lsid:labkey.com:Run.Folder";
-        Map<String, Pair<Set<String>, Set<String>>> invalidEdges = new HashMap<>();
-        for (Pair<String, String> edge : edges)
+        nodes.put(_seed.getLSID(), _seed);
+
+        for (ExpObject node : _datas)
+            nodes.put(node.getLSID(), node);
+
+        for (ExpObject node : _materials)
+            nodes.put(node.getLSID(), node);
+
+        for (ExpObject node : _runs)
+            nodes.put(node.getLSID(), node);
+
+        return nodes;
+    }
+
+    private Map<String, Pair<Set<Edge>, Set<Edge>>> processEdges()
+    {
+        Map<String, Pair<Set<Edge>, Set<Edge>>> nodes = new HashMap<>();
+
+        for (Edge edge : _edges)
         {
-            if (edge.first.equals(edge.second))
-                continue;
+            if (!nodes.containsKey(edge.parent))
+                nodes.put(edge.parent, Pair.of(new HashSet<>(), new HashSet<>()));
 
-            // an edge where the parent is invalid
-            if (edge.first.indexOf(prefix) == 0)
-            {
-                if (!invalidEdges.containsKey(edge.first))
-                    invalidEdges.put(edge.first, Pair.of(new HashSet<>(), new HashSet<>()));
-
-                invalidEdges.get(edge.first).second.add(edge.second);
-            }
-
-            // an edge where the child is invalid
-            if (edge.second.indexOf(prefix) == 0)
-            {
-                if (!invalidEdges.containsKey(edge.second))
-                    invalidEdges.put(edge.second, Pair.of(new HashSet<>(), new HashSet<>()));
-
-                invalidEdges.get(edge.second).first.add(edge.first);
-            }
-        }
-
-        for (Pair<String, String> edge : edges)
-        {
-            if (!invalidEdges.containsKey(edge.first) && !nodes.containsKey(edge.first))
-                nodes.put(edge.first, Pair.of(new HashSet<>(), new HashSet<>()));
-
-            if (!invalidEdges.containsKey(edge.second) && !nodes.containsKey(edge.second))
-                nodes.put(edge.second, Pair.of(new HashSet<>(), new HashSet<>()));
+            if (!nodes.containsKey(edge.child))
+                nodes.put(edge.child, Pair.of(new HashSet<>(), new HashSet<>()));
 
             // The edges parent now has a child of edge.second
-            if (!invalidEdges.containsKey(edge.first) && !edge.first.equals(edge.second)) // node cannot parent itself
-            {
-                if (invalidEdges.containsKey(edge.second))
-                    nodes.get(edge.first).second.addAll(invalidEdges.get(edge.second).second);
-                else
-                    nodes.get(edge.first).second.add(edge.second);
-            }
+            if (!edge.parent.equals(edge.child)) // node cannot parent itself
+                nodes.get(edge.parent).second.add(edge);
 
             // The edges child now has a parent of edge.first
-            if (!invalidEdges.containsKey(edge.second) && !edge.second.equals(edge.first)) // node cannot child itself
-            {
-                if (invalidEdges.containsKey(edge.first))
-                    nodes.get(edge.second).first.addAll(invalidEdges.get(edge.first).first);
-                else
-                    nodes.get(edge.second).first.add(edge.first);
-            }
+            if (!edge.child.equals(edge.parent)) // node cannot child itself
+                nodes.get(edge.child).first.add(edge);
         }
 
         return nodes;
@@ -109,48 +89,72 @@ public class ExpLineage
 
     public JSONObject toJSON()
     {
-        Map<String, Object> values = new HashMap<>();
+        Map<String, ExpObject> nodeMeta = processNodes();
+        ExpObject expObject;
 
+        Map<String, Object> values = new HashMap<>();
         JSONObject nodes = new JSONObject();
         Map<String, Object> json;
 
-        for (Map.Entry<String, Pair<Set<String>, Set<String>>> node : processEdges(_edges).entrySet())
+        for (Map.Entry<String, Pair<Set<Edge>, Set<Edge>>> node : processEdges().entrySet())
         {
             json = new JSONObject();
-            json.put("parents", node.getValue().first.toArray());
-            json.put("children", node.getValue().second.toArray());
+
+            JSONArray array = new JSONArray();
+            for (Edge edge : node.getValue().first)
+                array.put(edge.toParentJSON());
+            json.put("parents", array);
+
+            array = new JSONArray();
+            for (Edge edge : node.getValue().second)
+                array.put(edge.toChildJSON());
+            json.put("children", array);
+
+            if (nodeMeta.get(node.getKey()) != null)
+            {
+                expObject = nodeMeta.get(node.getKey());
+
+                json.put("name", expObject.getName());
+                json.put("url", expObject.detailsURL());
+                json.put("rowId", expObject.getRowId());
+            }
 
             nodes.put(node.getKey(), json);
         }
-
-//        for (ExpData output : getDatas())
-//        {
-//            json = new HashMap<>();
-//            json.put("dataClass", output.getDataClass());
-//            json.put("LSID", output.getLSID());
-//            json.put("created", output.getCreated());
-//            json.put("createdBy", output.getCreatedBy());
-//            json.put("modified", output.getModified());
-//            json.put("modifiedBy", output.getModifiedBy());
-//
-//            nodes.put(output.getLSID(), json);
-//        }
-//
-//        for (ExpMaterial output : getMaterials())
-//        {
-//            json = new HashMap<>();
-//            json.put("LSID", output.getLSID());
-//            json.put("created", output.getCreated());
-//            json.put("createdBy", output.getCreatedBy());
-//            json.put("modified", output.getModified());
-//            json.put("modifiedBy", output.getModifiedBy());
-//
-//            nodes.put(output.getLSID(), json);
-//        }
 
         values.put("seed", _seed.getLSID());
         values.put("nodes", nodes);
 
         return new JSONObject(values);
+    }
+
+    public static class Edge
+    {
+        public final String parent;
+        public final String child;
+        private String _role;
+
+        public Edge(String parentLSID, String childLSID, String role)
+        {
+            parent = parentLSID;
+            child = childLSID;
+            _role = role;
+        }
+
+        public JSONObject toParentJSON()
+        {
+            JSONObject json = new JSONObject();
+            json.put("lsid", parent);
+            json.put("role", _role);
+            return json;
+        }
+
+        public JSONObject toChildJSON()
+        {
+            JSONObject json = new JSONObject();
+            json.put("lsid", child);
+            json.put("role", _role);
+            return json;
+        }
     }
 }
