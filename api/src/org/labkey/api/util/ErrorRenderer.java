9f466a6e1ff2155811a0003a2fa07cbe46c7bbc9
Trey Chadick <tchad@labkey.com>
2016-02-16 16:50:36 +0000

    Fail faster when crawler triggers errors Workaround for 25589: AdminController.ResetPropertiesAction at root clears systemEmailAddress

--- api/src/org/labkey/api/util/ErrorRenderer.java
+++ api/src/org/labkey/api/util/ErrorRenderer.java
@@ -63,7 +63,7 @@ public class ErrorRenderer
 
     public void renderStart(PrintWriter out)
     {
-        out.println("<table cellpadding=4 style=\"width:100%; height:100%\"><tr><td style=\"background-color:#ffffff;\" valign=top align=left>");
+        out.println("<table class=\"server-error\" cellpadding=4 style=\"width:100%; height:100%\"><tr><td style=\"background-color:#ffffff;\" valign=top align=left>");
     }
 
     public void renderEnd(PrintWriter out)
