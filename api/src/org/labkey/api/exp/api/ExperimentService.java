f5a83fdf46f1ea43a4cde73198cec430bb391fad
Kevin Krouse <kevink@labkey.com>
2016-04-07 21:44:36 +0000

    update experiment service

--- api/src/org/labkey/api/exp/api/ExperimentService.java
+++ api/src/org/labkey/api/exp/api/ExperimentService.java
@@ -112,9 +112,6 @@ public class ExperimentService
         List<? extends ExpRun> getExpRunsForFilePathRoot(File filePathRoot);
         ExpRun createExperimentRun(Container container, String name);
 
-        List<String> getLsidsToIndex();
-        void addLsidToIndex(String lsid);
-        void clearLsidsToIndex();
         ExpData getExpData(int rowid);
         ExpData getExpData(String lsid);
         List<? extends ExpData> getExpDatas(int... rowid);
