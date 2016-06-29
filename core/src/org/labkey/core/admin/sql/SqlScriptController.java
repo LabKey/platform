bb4b574e656dbc1349cca48915637533a2b19e5d
Adam Rauch <adam@labkey.com>
2016-06-29 01:15:16 +0000

    Fix duplicate listing of gelcancer scripts on sql scripts admin page

--- core/src/org/labkey/core/admin/sql/SqlScriptController.java
+++ core/src/org/labkey/core/admin/sql/SqlScriptController.java
@@ -66,7 +66,6 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
