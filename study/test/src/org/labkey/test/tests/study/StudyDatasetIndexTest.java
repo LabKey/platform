0f2d061d8d5c4de753ccdb7bf194075706b06090
Ron Dashwood <rond@labkey.com>
2016-08-31 23:31:33 +0000

    Spec ID 27264: Indices and common columns in dataset_metadata.xml - Add automated tests covering shared columns and indices

---
+++ study/test/src/org/labkey/test/tests/study/StudyDatasetIndexTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2014-2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.labkey.test.tests.study;
+
+import org.junit.experimental.categories.Category;
+import org.labkey.test.Locator;
+import org.labkey.test.TestFileUtils;
+import org.labkey.test.categories.DailyA;
+import org.labkey.test.tests.StudyBaseTest;
+import org.labkey.test.util.LogMethod;
+
+import java.io.File;
+
+@Category({DailyA.class})
+public class StudyDatasetIndexTest extends StudyBaseTest
+{
+    private static final File STUDY_WITH_DATASET_INDEX = TestFileUtils.getSampleData("studies/StudyWithDatasetIndex.folder.zip");
+    private static final File STUDY_WITH_DATASET_SHARED_INDEX = TestFileUtils.getSampleData("studies/StudyWithDatasetSharedIndex.folder.zip");
+
+    protected String getProjectName()
+    {
+        return "StudyDatasetSharedColumnAndIndexProject";
+    }
+
+    protected String getFolderName()
+    {
+        return "Study Dataset Shared Column and Index";
+    }
+
+    @Override
+    @LogMethod
+    protected void doCreateSteps()
+    {
+        initializeFolder();
+        initializePipeline(null);
+        clickFolder(getFolderName());
+
+        log("Import study with index on dataset");
+        importFolderFromZip(STUDY_WITH_DATASET_INDEX);
+    }
+
+    @Override
+    @LogMethod
+    protected void doVerifySteps()
+    {
+        beginAt("/query/" + getProjectName() + "/" + getFolderName()  + "/schema.view?schemaName=study");
+        selectQueryInSubfolder("study","built-in queries & tables", "DEM-1");
+        waitForText(10000, "view raw table metadata");
+        clickAndWait(Locator.linkWithText("view raw table metadata"));
+        waitForText("dem_minus_1_indexedColumn");
+
+        reloadStudyFromZip(STUDY_WITH_DATASET_SHARED_INDEX);
+
+        beginAt("/query/" + getProjectName() + "/" + getFolderName()  + "/schema.view?schemaName=study");
+        selectQueryInSubfolder("study","built-in queries & tables", "DEM-1");
+        waitForText(10000, "view raw table metadata");
+        clickAndWait(Locator.linkWithText("view raw table metadata"));
+        waitForText("dem_minus_1_indexedColumn");
+        waitForText("dem_minus_1_sharedColumn");
+
+        beginAt("/query/" + getProjectName() + "/" + getFolderName()  + "/schema.view?schemaName=study");
+        selectQueryInSubfolder("study","built-in queries & tables", "DEM-2");
+        waitForText(10000, "view raw table metadata");
+        clickAndWait(Locator.linkWithText("view raw table metadata"));
+        assertTextNotPresent("indexedColumn");
+        waitForText("dem_minus_2_sharedColumn");
+    }
+}
