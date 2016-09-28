33a4d7be4c2d09ff90d45d5755c339f493ab8295
Tony Galuhn <tgaluhn@labkey.com>
2016-09-28 20:38:20 +0000

    Spec 27518 Automated test

--- dataintegration/src/org/labkey/di/columnTransforms/TestColumnTransform.java
+++ dataintegration/src/org/labkey/di/columnTransforms/TestColumnTransform.java
@@ -7,13 +7,16 @@ import org.labkey.api.di.columnTransform.AbstractColumnTransform;
  * Date: 9/26/2016
  *
  * Used in automated testing and gives an example of implementing a transform.
- * Prepends the value of the "id" column of the source query to the value of the source column specified in the ETL xml
+ * Prepends the value of the "id" column of the source query
+ * to the value of the source column specified in the ETL xml,
+ * then appends the value of the "myConstant" constant set in the xml.
  */
 public class TestColumnTransform extends AbstractColumnTransform
 {
     @Override
     protected Object doTransform(Object inputValue)
     {
-        return getInputValue("id") + "_" + inputValue;
+        String prefix = getInputValue("id").toString();
+        return prefix + "_" + inputValue + "_" + getConstantValue("myConstant");
     }
 }
