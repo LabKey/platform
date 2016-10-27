a1dce07493a7ea4bc410ce60f552ea925a488772
Ian Sigmon <ians@labkey.com>
2016-10-27 19:38:21 +0000

    Spec Issue #27487 Duke Development - Re-import assays without losing exclusions Issue 28261: Retain the Created and CreatedBy fields for re-imported Luminex Exclusions
    Inital-fix for
    Issue 28289: SQLException re-importing a run with a well exclusion when the same file has been previously uploaded as a different run

--- api/src/org/labkey/api/query/DefaultQueryUpdateService.java
+++ api/src/org/labkey/api/query/DefaultQueryUpdateService.java
@@ -302,10 +302,16 @@ public class DefaultQueryUpdateService extends AbstractQueryUpdateService
     protected Map<String, Object> updateRow(User user, Container container, Map<String, Object> row, @NotNull Map<String, Object> oldRow)
             throws InvalidKeyException, ValidationException, QueryUpdateServiceException, SQLException
     {
-        return updateRow(user, container, row, oldRow, false);
+        return updateRow(user, container, row, oldRow, false, false);
     }
 
     protected Map<String, Object> updateRow(User user, Container container, Map<String, Object> row, @NotNull Map<String, Object> oldRow, boolean allowOwner)
+            throws InvalidKeyException, ValidationException, QueryUpdateServiceException, SQLException
+    {
+        return updateRow(user, container, row, oldRow, allowOwner, false);
+    }
+
+    protected Map<String, Object> updateRow(User user, Container container, Map<String, Object> row, @NotNull Map<String, Object> oldRow, boolean allowOwner, boolean retainCreation)
             throws InvalidKeyException, ValidationException, QueryUpdateServiceException, SQLException
     {
         Map<String,Object> rowStripped = new CaseInsensitiveHashMap<>(row.size());
@@ -340,10 +346,9 @@ public class DefaultQueryUpdateService extends AbstractQueryUpdateService
             //unfortunately, the Table.update() method doesn't strip these, so we'll
             //do that here.
             // Owner, CreatedBy, Created, EntityId
-            if ((!allowOwner && name.equalsIgnoreCase("Owner")) ||
-                    name.equalsIgnoreCase("CreatedBy") ||
-                    name.equalsIgnoreCase("Created") ||
-                    name.equalsIgnoreCase("EntityId"))
+            if ((!retainCreation && (name.equalsIgnoreCase("CreatedBy") || name.equalsIgnoreCase("Created")))
+                || (!allowOwner && name.equalsIgnoreCase("Owner"))
+                || name.equalsIgnoreCase("EntityId"))
                 continue;
 
             // We want a map using the DbTable column names as keys, so figure out the right name to use
