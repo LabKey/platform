d0ad246066bbc9656050a40a17d3fe20952e925b
Xing Yang <xyang@labkey.com>
2017-01-05 18:21:46 +0000

    Issue 28492: Customized view should not allowing delete of module-based custom view

--- internal/webapp/internal/ViewDesigner/Designer.js
+++ internal/webapp/internal/ViewDesigner/Designer.js
@@ -663,7 +663,7 @@ Ext4.define('LABKEY.internal.ViewDesigner.Designer', {
         var deleteEnabled = canEdit && this.customView.deletable && this.customView.name && !this.customView.doesNotExist;
 
         // enabled for saved (non-session) editable views or customized default or overridable module based view (not new) views.
-        var revertEnabled = canEdit && (this.customView.revertable || this.customView.session);
+        var revertEnabled = canEdit && (this.customView.revertable || this.customView.session || (!this.customView.name && !this.customView.doesNotExist));
 
         var items = [{
             text: 'Delete',
