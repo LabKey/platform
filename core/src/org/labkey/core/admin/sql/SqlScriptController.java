a5d641fa0dfbf8baa47284b5a3baeec4fa35b6e0
Tony Galuhn <tgaluhn@labkey.com>
2017-01-31 17:58:19 +0000

    Spec 28570 Unclaimed install/uninstall script for new CLR Assembly in premium module

--- core/src/org/labkey/core/admin/sql/SqlScriptController.java
+++ core/src/org/labkey/core/admin/sql/SqlScriptController.java
@@ -985,10 +985,14 @@ public class SqlScriptController extends SpringActionController
 
             if (CoreSchema.getInstance().getSqlDialect().isSqlServer())
             {
+                // TODO: Since some of these come from other modules, create a registration mechanism.
                 unclaimedFiles.remove("group_concat_install.sql");
                 unclaimedFiles.remove("group_concat_install_1.00.23696.sql");
                 unclaimedFiles.remove("group_concat_uninstall.sql");
                 unclaimedFiles.remove("naturalize_install.sql");      // From the LDK module
+
+                unclaimedFiles.remove("PremiumStats_install_1.0.1.sql");
+                unclaimedFiles.remove("PremiumStats_uninstall.sql");
             }
 
             // specifically allow db scripts to have README files
