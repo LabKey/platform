5910be982ae799d4ae1f2821a74f9e9157ef9cbd
Adam Rauch <adam@labkey.com>
2017-02-15 17:50:00 +0000

    Eliminate warnings.
    Better descriptions for email template replacements.

--- api/src/org/labkey/api/util/emailTemplate/UserOriginatedEmailTemplate.java
+++ api/src/org/labkey/api/util/emailTemplate/UserOriginatedEmailTemplate.java
@@ -21,7 +21,6 @@ import org.labkey.api.data.Container;
 import org.labkey.api.security.User;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -50,17 +49,17 @@ public abstract class UserOriginatedEmailTemplate extends EmailTemplate
     public UserOriginatedEmailTemplate(@NotNull String name, String subject, String body, String description, @NotNull ContentType contentType, @Nullable String senderDisplayName)
     {
         super(name, subject, body, description, contentType, senderDisplayName);
-        _replacements.add(new ReplacementParam<String>("userFirstName", String.class, "First name of the user originated the action"){
+        _replacements.add(new ReplacementParam<String>("userFirstName", String.class, "First name of the user who originated the action"){
             public String getValue(Container c) {
                 return _originatingUser == null ? null : _originatingUser.getFirstName();
             }
         });
-        _replacements.add(new ReplacementParam<String>("userLastName", String.class, "Last name of the user originated the action"){
+        _replacements.add(new ReplacementParam<String>("userLastName", String.class, "Last name of the user who originated the action"){
             public String getValue(Container c) {
                 return _originatingUser == null ? null : _originatingUser.getLastName();
             }
         });
-        _replacements.add(new ReplacementParam<String>("userDisplayName", String.class, "Display name of the user originated the action"){
+        _replacements.add(new ReplacementParam<String>("userDisplayName", String.class, "Display name of the user who originated the action"){
             public String getValue(Container c) {
                 return _originatingUser == null ? null : _originatingUser.getFriendlyName();
             }
