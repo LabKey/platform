319a3274370d57c39ccef06e765a2c85dc334ba0
Adam Rauch <adam@labkey.com>
2017-02-18 12:35:40 +0000

    Remove old workflow model caching
    Remove no longer used module resource caching code (ModuleResourceCacheOld, ModuleResourceDirectory, StandardModuleResourceDirectory, etc.)

--- api/src/org/labkey/api/module/ModuleResourceCaches.java
+++ api/src/org/labkey/api/module/ModuleResourceCaches.java
@@ -15,20 +15,9 @@
  */
 package org.labkey.api.module;
 
-import org.labkey.api.files.FileSystemDirectoryListener;
-import org.labkey.api.files.FileSystemWatcher;
-import org.labkey.api.files.FileSystemWatchers;
-import org.labkey.api.resource.MergedDirectoryResource;
-import org.labkey.api.resource.Resource;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.util.Path;
 
-import java.nio.file.StandardWatchEventKinds;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -71,25 +60,6 @@ public class ModuleResourceCaches
     public static <T> QueryBasedModuleResourceCache<T> createQueryBasedCache(Path root, String description, QueryBasedModuleResourceCacheHandler<T> handler)
     {
         return new QueryBasedModuleResourceCache<>(root, description, handler);
-    }
-
-    /**
-     * Create a new ModuleResourceCache. This is the standard method to use in most situations, where each module has a
-     * single directory that contains files that represent a single object type.
-     *
-     * @param path Path representing the module resource directory
-     * @param description Short description of the cache
-     * @param handler ModuleResourceCacheHandler that customizes this cache's behavior
-     * @param <T> Object type that this cache handles
-     * @return A ModuleResourceCache
-     */
-    public static <T> ModuleResourceCacheOld<T> create(Path path, String description, ModuleResourceCacheHandlerOld<String, T> handler)
-    {
-        ModuleResourceDirectory directory = new StandardModuleResourceDirectory(path);
-        ModuleResourceCacheOld<T> cache = new ModuleResourceCacheOld<>(directory, description, handler);
-        directory.registerCache(cache);
-
-        return cache;
     }
 
 
@@ -159,106 +129,5 @@ public class ModuleResourceCaches
         String moduleName = matcher.group(1);
         String filename = matcher.group(2);
         return new CacheId(moduleName, filename);
-    }
-
-
-    private static class StandardModuleResourceDirectory implements ModuleResourceDirectory
-    {
-        private final Path _path;
-        private final FileSystemWatcher _watcher;
-        // A map of all modules that include the specified resource directory to the standard listener that handles that
-        // module + directory combination. This is initialized at construction time and never changed.
-        private final Map<Module, StandardModuleResourceListener> _moduleListeners = new ConcurrentHashMap<>();
-
-        private StandardModuleResourceDirectory(Path path)
-        {
-            _path = path;
-            _watcher = FileSystemWatchers.get();
-
-            for (Module module : ModuleLoader.getInstance().getModules())
-                registerModule(module);
-        }
-
-        @Override
-        public Path getPath()
-        {
-            return _path;
-        }
-
-        @Override
-        public Collection<Module> getModules()
-        {
-            return _moduleListeners.keySet();
-        }
-
-        @Override
-        public <T> void registerCache(ModuleResourceCacheOld<T> cache)
-        {
-            for (Module module : getModules())
-            {
-                StandardModuleResourceListener standardListener = _moduleListeners.get(module);
-                assert null != standardListener;
-                standardListener.addListener(cache.getListener(module));
-            }
-        }
-
-        // If this module includes the resource path then register a file listener to monitor for changes.
-        // The listener won't actually do anything useful yet; each cache will add the actual cache listeners when
-        // registerCache() is called.
-        public void registerModule(Module module)
-        {
-            Resource dirResource = module.getModuleResolver().lookup(_path);
-
-            if (null != dirResource && dirResource.isCollection())
-            {
-                // At construction time, we register an empty listener on the directory. Each cache associated with the
-                // directory will add its listener via addListener()
-                StandardModuleResourceListener emptyListener = new StandardModuleResourceListener();
-                _moduleListeners.put(module, emptyListener);
-
-                // TODO: Integrate this better with Resource
-                ((MergedDirectoryResource)dirResource).registerListener(_watcher, emptyListener,
-                        StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
-            }
-        }
-    }
-
-
-    private static class StandardModuleResourceListener implements FileSystemDirectoryListener
-    {
-        private final List<FileSystemDirectoryListener> _listeners = new CopyOnWriteArrayList<>();
-
-        public void addListener(FileSystemDirectoryListener listener)
-        {
-            _listeners.add(listener);
-        }
-
-        @Override
-        public void entryCreated(java.nio.file.Path directory, java.nio.file.Path entry)
-        {
-            for (FileSystemDirectoryListener listener : _listeners)
-                listener.entryCreated(directory, entry);
-        }
-
-        @Override
-        public void entryDeleted(java.nio.file.Path directory, java.nio.file.Path entry)
-        {
-            for (FileSystemDirectoryListener listener : _listeners)
-                listener.entryDeleted(directory, entry);
-        }
-
-        @Override
-        public void entryModified(java.nio.file.Path directory, java.nio.file.Path entry)
-        {
-            for (FileSystemDirectoryListener listener : _listeners)
-                listener.entryModified(directory, entry);
-        }
-
-        @Override
-        public void overflow()
-        {
-            for (FileSystemDirectoryListener listener : _listeners)
-                listener.overflow();
-        }
     }
 }
