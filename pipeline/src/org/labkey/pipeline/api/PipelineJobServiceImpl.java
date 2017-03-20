0af813fcc92ac58625763dc7879cef23c16546c4
Adam Rauch <adam@labkey.com>
2017-03-20 16:28:25 +0000

    Minor cleanup of warnings (unused imports, unnecessary null check, etc.)

--- pipeline/src/org/labkey/pipeline/api/PipelineJobServiceImpl.java
+++ pipeline/src/org/labkey/pipeline/api/PipelineJobServiceImpl.java
@@ -79,7 +79,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.stream.Collectors;
 
 /**
