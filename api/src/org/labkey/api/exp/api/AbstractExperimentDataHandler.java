1d3a79af188916f97d7ef0e58dbb6ffd7bef9d1d
Nick Kerr <nicka@labkey.com>
2017-05-20 02:53:03 +0000

    Experiment: remove redundant getPriority overrides

--- api/src/org/labkey/api/exp/api/AbstractExperimentDataHandler.java
+++ api/src/org/labkey/api/exp/api/AbstractExperimentDataHandler.java
@@ -25,7 +25,6 @@ import org.labkey.api.security.User;
 import org.labkey.api.util.NetworkDrive;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
