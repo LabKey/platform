28d2fc907b788dc364a0692f13163ea133e503c3
Nick Kerr <nicka@labkey.com>
2017-07-13 16:03:20 +0000

    disableOnClick for <labkey:button>

--- api/src/org/labkey/api/jsp/taglib/ButtonTag.java
+++ api/src/org/labkey/api/jsp/taglib/ButtonTag.java
@@ -16,15 +16,18 @@
 
 package org.labkey.api.jsp.taglib;
 
+import org.labkey.api.util.Button;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.view.ActionURL;
 
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ButtonTag extends SimpleTagBase
 {
+    Boolean _disableOnClick;
     String _text;
     String _href;
     String _alt;
@@ -37,58 +40,50 @@ public class ButtonTag extends SimpleTagBase
 
     public void doTag() throws JspException, IOException
     {
-        JspWriter out = getOut();
+        Map<String, String> attributes = new HashMap<>();
+        Button.ButtonBuilder button = PageFlowUtil.button(_text).id(_id);
+
+        // TODO: This shouldn't have inconsistent logic from Button.java, should just be a pass through
         if (_href != null)
         {
-            String attributes = "";
             if (null != _target)
-                attributes = "target='" + h(_target) + "'";
-            out.write(PageFlowUtil.button(_text).href(_href).onClick(_onclick).attributes(attributes).toString());
+                attributes.put("target", _target);
+
+            button.href(_href).onClick(_onclick).attributes(attributes);
         }
         else
         {
             if (_onclick != null && _action != null)
-            {
                 throw new IllegalArgumentException("onclick and action cannot both be set");
-            }
+
             String onClickScript = "";
-            StringBuilder attributes = new StringBuilder();
             if (_onclick != null)
-            {
                 onClickScript = (h(_onclick));
-            }
             if (_action != null)
-            {
                 onClickScript = ("this.form.action='" + _action + "';this.form.method='POST';");
-            }
+
             if (_name != null)
-            {
-                attributes.append(" name=\"");
-                attributes.append(h(_name));
-                attributes.append("\"");
-            }
+                attributes.put("name", _name);
             if (_value != null)
-            {
-                attributes.append(" value=\"");
-                attributes.append(h(_value));
-                attributes.append("\"");
-            }
-            if (_id != null)
-            {
-                attributes.append(" id=\"");
-                attributes.append(h(_id));
-                attributes.append("\"");
-            }
-            out.write(PageFlowUtil.button(_text).submit(true).onClick(onClickScript).attributes(attributes.toString()).toString());
+                attributes.put("value", _value);
+
+            button.submit(true).onClick(onClickScript).attributes(attributes);
         }
+
+        if (null != _disableOnClick)
+            button.disableOnClick(_disableOnClick);
+
+        getOut().write(button.toString());
+    }
+
+    public void setDisableOnClick(Boolean disableOnClick)
+    {
+        _disableOnClick = disableOnClick;
     }
 
     public void setHref(Object value)
     {
-        if (value != null)
-        {
-            _href = value.toString();
-        }
+        _href = value == null ? null : value.toString();
     }
 
     public void setText(String text)
@@ -115,10 +110,12 @@ public class ButtonTag extends SimpleTagBase
     {
         _action = action.toString() + ".view";
     }
+
     public void setName(String name)
     {
         _name = name;
     }
+
     public void setValue(String value)
     {
         _value = value;
