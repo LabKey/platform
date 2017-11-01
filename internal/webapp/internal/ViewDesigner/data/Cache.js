15af15d2a0a8f02c2da17e493b9fbbc7798d6edf
Ed  Younskevicius <eyounske@labkey.com>
2017-11-01 02:02:40 +0000

    Update copyrights for 17.3

--- internal/webapp/internal/ViewDesigner/data/Cache.js
+++ internal/webapp/internal/ViewDesigner/data/Cache.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 LabKey Corporation
+ * Copyright (c) 2015-2017 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
