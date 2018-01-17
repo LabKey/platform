45dd05464928d36b934443ae8e2ed041eab71357
Josh Eckels <jeckels@labkey.com>
2018-01-17 00:23:48 +0000

    Revert file that was accidentally included in 55960

--- api/src/org/labkey/api/data/JsonWriter.java
+++ api/src/org/labkey/api/data/JsonWriter.java
@@ -314,8 +314,6 @@ public class JsonWriter
                 schemaName = lookupTable.getSchema().getName();
             }
 
-            queryName = queryName.toLowerCase();
-
             // Duplicate info with different property names for backwards compatibility
             lookupInfo.put("queryName", queryName);
             lookupInfo.put("table", queryName);
