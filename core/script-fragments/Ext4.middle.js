ef3858ec9c352ac5fd45374029711eb4ca25e1b1
Nick Kerr <nicka@labkey.com>
2018-02-24 22:16:04 +0000

    32939: Trigger script code failing to resolve Ext.Error.raise()

--- core/script-fragments/Ext4.middle.js
+++ core/script-fragments/Ext4.middle.js
@@ -3,6 +3,19 @@
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
+/**
+ * 32939: Ext.Error.raise is defined using the Ext.Class "statics" features. We don't currently support
+ * Ext.Class on the server-side so the "statics" functions are not applied. Here are redefining Ext.Error.raise
+ * to be suitable for use on the server-side.
+ */
+Ext.Error.raise = function(err) {
+    err = err || {};
+    if (Ext.isString(err)) {
+        err = { msg: err };
+    }
+
+    throw new Ext.Error(err);
+};
 
 Ext.Msg = {
     alert : function (title, msg) {
