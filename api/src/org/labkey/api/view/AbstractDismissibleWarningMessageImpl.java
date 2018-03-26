1c31c41ff0d70787891a6aaa5d0c6490e3ceaf65
Adam Rauch <adam@labkey.com>
2018-03-26 20:38:16 +0000

    Move evaluationContent expiration warning out of core into evaluationContent module
    Eliminate EvaluationService/EvaluationServiceImpl

--- api/src/org/labkey/api/view/AbstractDismissibleWarningMessageImpl.java
+++ api/src/org/labkey/api/view/AbstractDismissibleWarningMessageImpl.java
@@ -7,24 +7,25 @@ import org.labkey.api.util.PageFlowUtil;
 public abstract class AbstractDismissibleWarningMessageImpl implements DismissibleWarningMessage
 {
     //Use with String.format(DISMISSAL_SCRIPT_FORMAT, "dismissMyWarning", PageFlowUtil.jsString(dismissURL), myMessageText, myId);
-    protected final static String DISMISSAL_SCRIPT_FORMAT = "<script type=\"text/javascript\">\n" +
-            "                (function($) {\n" +
-            "                    function %1$s() {\n" +
-            "                        var config = {\n" +
-            "                            url: %2$s,\n" +
-            "                            method: 'POST',\n" +
-            "                            success: function () {$(\".lk-dismissable-warn\").hide()},\n" +
-            "                            failure: LABKEY.Utils.displayAjaxErrorResponse\n" +
-            "                        };\n" +
-            "                        LABKEY.Ajax.request(config); \n" +
-            "                        return false;\n" +
-            "                    }\n" +
-            "                   $('body').on('click', 'a.lk-dismissable-warn-close', function() {\n" +
-            "                       %1$s();\n" +
-            "                   });" +
-            "                })(jQuery);\n" +
-            "            </script>\n" +
-            "<div id=\"%4$s\">%3$s</div>";
+    protected final static String DISMISSAL_SCRIPT_FORMAT =
+        "<script type=\"text/javascript\">\n" +
+        "    (function($) {\n" +
+        "        function %1$s() {\n" +
+        "            var config = {\n" +
+        "                url: %2$s,\n" +
+        "                method: 'POST',\n" +
+        "                success: function () {$(\".lk-dismissable-warn\").hide()},\n" +
+        "                failure: LABKEY.Utils.displayAjaxErrorResponse\n" +
+        "            };\n" +
+        "            LABKEY.Ajax.request(config); \n" +
+        "            return false;\n" +
+        "        }\n" +
+        "       $('body').on('click', 'a.lk-dismissable-warn-close', function() {\n" +
+        "           %1$s();\n" +
+        "       });" +
+        "    })(jQuery);\n" +
+        "</script>\n" +
+        "<div id=\"%4$s\">%3$s</div>";
 
     /**
      * Get the HTML string for the ribbon
