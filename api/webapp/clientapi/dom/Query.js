5b986179d92ada11bdc22dd8e9ab2599d67d6526
Cory Nathe <cnathe@labkey.com>
2018-04-09 02:09:50 +0000

    Spec #33665: LABKEY.Query.columnSelectInput

--- api/webapp/clientapi/dom/Query.js
+++ api/webapp/clientapi/dom/Query.js
@@ -158,6 +158,12 @@ LABKEY.Query = new function(impl, $) {
         select.prop('disabled', false);
     }
 
+    function sortObjectArrayByTitle(a, b){
+        var aTitle = a.title ? a.title : a.caption;
+        var bTitle = b.title ? b.title : b.caption;
+        return ((aTitle < bTitle) ? -1 : ((aTitle > bTitle) ? 1 : 0));
+    }
+
     var SCHEMA_QUERIES_CACHE = {}; // cache of queries by schema
     function loadQueries(schemaSelect, querySelect, selectedSchema, initialValue) {
         schemaSelect.prop('disabled', true);
@@ -173,36 +179,94 @@ LABKEY.Query = new function(impl, $) {
                 includeColumns: false,
                 success: function(data) {
                     // add the sorted set of queries for this schema to the cache
-                    SCHEMA_QUERIES_CACHE[selectedSchema] = data.queries.sort(function (a, b){
-                        var aTitle = a.title.toLowerCase();
-                        var bTitle = b.title.toLowerCase();
-                        return ((aTitle < bTitle) ? -1 : ((aTitle > bTitle) ? 1 : 0));
-                    });
+                    SCHEMA_QUERIES_CACHE[selectedSchema] = data.queries.sort(sortObjectArrayByTitle);
 
                     populateSelect(querySelect, SCHEMA_QUERIES_CACHE[selectedSchema], 'name', 'title', initialValue);
                     schemaSelect.prop('disabled', false);
+
+                    // if there is a selected query, fire the change event
+                    if (querySelect.val()) {
+                        querySelect.trigger('change');
+                    }
                 }
             });
+        }
+    }
+
+    var QUERY_COLUMNS_CACHE = {}; // cache of columns by schema|query
+    function loadColumns(select, schemaName, queryName, filterFn, initValue) {
+        loadingSelect(select);
+
+        var queryKey = schemaName + '|' + queryName;
+        if (QUERY_COLUMNS_CACHE[queryKey]) {
+            populateColumnsWithFilterFn(select, QUERY_COLUMNS_CACHE[queryKey], filterFn, initValue);
+        }
+        else {
+            LABKEY.Query.getQueryDetails({
+                schemaName: schemaName,
+                queryName: queryName,
+                success: function(data) {
+                    // find the default view from the views array returned
+                    // NOTE: in the future if we allow this to work for view other than the default, this logic will need to change
+                    var queryView = null;
+                    $.each(data.views, function(i, view) {
+                        if (view.default) {
+                            queryView = view;
+                            return false;
+                        }
+                    });
+
+                    QUERY_COLUMNS_CACHE[queryKey] = [];
+                    if (queryView) {
+                        QUERY_COLUMNS_CACHE[queryKey] = queryView.fields.sort(sortObjectArrayByTitle);
+                    }
+
+                    populateColumnsWithFilterFn(select, QUERY_COLUMNS_CACHE[queryKey], filterFn, initValue);
+                }
+            });
+        }
+    }
+
+    function populateColumnsWithFilterFn(select, origFields, filterFn, initValue) {
+        var fields = [];
+        $.each(origFields, function(i, field) {
+            var includeField = true;
+
+            // allow for a filter function to be called for each field
+            if (filterFn && LABKEY.Utils.isFunction(filterFn)) {
+                includeField = filterFn.call(this, field);
+            }
+
+            if (includeField) {
+                fields.push($.extend({}, field));
+            }
+        });
+
+        if (fields.length > 0) {
+            populateSelect(select, fields, 'name', 'caption', initValue);
+        }
+        else {
+            select.empty().append($('<option>', {text: 'No columns available'}));
         }
     }
 
     /**
      * Load the set of user visible schemas from the given container into a standard <select> input element.
      * @param config An object which contains the following configuration properties.
-     * @param {String} config.inputName name of the <select> input to load the LabKey queries into.
+     * @param {String} config.renderTo the id of the <select> input to load the LabKey queries into.
      * @param {String} config.initValue the initial value to try and set the <select> element value after it loads.
      */
     impl.schemaSelectInput = function(config) {
         var SCHEMA_SELECT;
 
-        if (!config || !config.inputName) {
-            console.error('Invalid config object. Missing inputName property for the <select> element.');
+        if (!config || !config.renderTo) {
+            console.error('Invalid config object. Missing renderTo property for the <select> element.');
             return;
         }
 
-        SCHEMA_SELECT = $("select[name='" + config.inputName + "']");
+        SCHEMA_SELECT = $("select[id='" + config.renderTo + "']");
         if (SCHEMA_SELECT.length !== 1) {
-            console.error('Invalid config object. Expect to find exactly one <select> element for the inputName provided (found: ' + SCHEMA_SELECT.length + ').');
+            console.error('Invalid config object. Expect to find exactly one <select> element for the renderTo provided (found: ' + SCHEMA_SELECT.length + ').');
             return;
         }
 
@@ -212,7 +276,7 @@ LABKEY.Query = new function(impl, $) {
             success: function(data) {
                 populateSelect(SCHEMA_SELECT, data.schemas.sort(), null, null, config.initValue);
 
-                // if there is a selected schema, fire the change event to load the queries
+                // if there is a selected schema, fire the change event
                 if (SCHEMA_SELECT.val()) {
                     SCHEMA_SELECT.trigger('change', [SCHEMA_SELECT.val()]);
                 }
@@ -226,32 +290,32 @@ LABKEY.Query = new function(impl, $) {
      * also then associates the two <select> inputs so that a selection change in the schema input will update the
      * query input accordingly.
      * @param config An object which contains the following configuration properties.
-     * @param {String} config.inputName name of the <select> input to load the LabKey queries into.
-     * @param {String} config.schemaInputName name of the <select> input to load the LabKey schemas into.
+     * @param {String} config.renderTo the id of the <select> input to load the LabKey queries into.
+     * @param {String} config.schemaInputId the id of the <select> input to load the LabKey schemas into.
      * @param {String} config.initValue the initial value to try and set the <select> element value after it loads.
      */
     impl.querySelectInput = function(config) {
         var SCHEMA_SELECT, QUERY_SELECT;
 
-        if (!config || !config.inputName || !config.schemaInputName) {
+        if (!config || !config.renderTo || !config.schemaInputId) {
             var msg = 'Invalid config object. ';
-            if (!config.inputName) {
-                msg += 'Missing inputName property for the <select> element. ';
+            if (!config.renderTo) {
+                msg += 'Missing renderTo property for the <select> element. ';
             }
-            if (!config.schemaInputName) {
-                msg += 'Missing schemaInputName property for the parent <select> element. ';
+            if (!config.schemaInputId) {
+                msg += 'Missing schemaInputId property for the parent <select> element. ';
             }
             console.error(msg);
             return;
         }
 
-        QUERY_SELECT = $("select[name='" + config.inputName + "']");
+        QUERY_SELECT = $("select[id='" + config.renderTo + "']");
         if (QUERY_SELECT.length !== 1) {
             console.error('Invalid config object. Expect to find exactly one <select> element with the name provided (found: ' + QUERY_SELECT.length + ').');
             return;
         }
 
-        SCHEMA_SELECT = $("select[name='" + config.schemaInputName + "']");
+        SCHEMA_SELECT = $("select[id='" + config.schemaInputId + "']");
         if (SCHEMA_SELECT.length !== 1) {
             console.error('Invalid config object. Expect to find exactly one <select> element with the name provided (found: ' + SCHEMA_SELECT.length + ').');
             return;
@@ -260,6 +324,43 @@ LABKEY.Query = new function(impl, $) {
         SCHEMA_SELECT.on('change', function (event, schemaName) {
             loadQueries(SCHEMA_SELECT, QUERY_SELECT, schemaName || event.target.value, config.initValue);
         });
+    };
+
+    /**
+     * Load the set of columns for a given schema/query into a standard <select> input. The config object
+     * must define the schemaName and queryName to be used to source the column listing.
+     * @param config An object which contains the following configuration properties.
+     * @param {String} config.renderTo the id of the <select> input to load the LabKey queries into. Required.
+     * @param {String} config.schemaName the name of the schema. Required.
+     * @param {String} config.queryName the name of the query. Required.
+     * @param {String} config.initValue the initial value to try and set the <select> element value after it loads. Optional.
+     * @param {String} config.filterFn a function to call to filter the column set (ex. by data type). Optional.
+     */
+    impl.columnSelectInput = function(config) {
+        var COLUMN_SELECT;
+
+        if (!config || !config.renderTo || !config.schemaName || !config.queryName) {
+            var msg = 'Invalid config object. ';
+            if (!config.renderTo) {
+                msg += 'Missing renderTo property for the <select> element. ';
+            }
+            if (!config.schemaName) {
+                msg += 'Missing schemaName property. ';
+            }
+            if (!config.queryName) {
+                msg += 'Missing queryName property. ';
+            }
+            console.error(msg);
+            return;
+        }
+
+        COLUMN_SELECT = $("select[id='" + config.renderTo + "']");
+        if (COLUMN_SELECT.length !== 1) {
+            console.error('Invalid config object. Expect to find exactly one <select> element with the name provided (found: ' + COLUMN_SELECT.length + ').');
+            return;
+        }
+
+        loadColumns(COLUMN_SELECT, config.schemaName, config.queryName, config.filterFn, config.initValue);
     };
 
     return impl;
