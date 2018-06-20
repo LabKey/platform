083b4179e82390e70ed8b03304b149e8bb4418f6
Marty Pradere <martyp@labkey.com>
2018-06-20 20:39:48 +0000

    Issue 34605: Add Socket read failed to Client abort exceptions

--- api/src/org/labkey/api/util/ExceptionUtil.java
+++ api/src/org/labkey/api/util/ExceptionUtil.java
@@ -527,7 +527,7 @@ public class ExceptionUtil
                 return true;
             }
             // Bug 15371
-            if (ex.getClass().equals(IOException.class) && ex.getMessage() != null && ex.getMessage().contains("disconnected client"))
+            if (ex.getClass().equals(IOException.class) && ex.getMessage() != null && (ex.getMessage().contains("disconnected client") || ex.getMessage().contains("Socket read failed")))
             {
                 return true;
             }
