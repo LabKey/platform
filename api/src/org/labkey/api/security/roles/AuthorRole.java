c6f4da576ad3b2b4112d56cedb3416fbc95d1a3a
Karl Lum <klum@labkey.com>
2018-06-29 17:25:25 +0000

    automation code review feedback

--- api/src/org/labkey/api/security/roles/AuthorRole.java
+++ api/src/org/labkey/api/security/roles/AuthorRole.java
@@ -29,7 +29,8 @@ public class AuthorRole extends AbstractRole
 {
     public AuthorRole()
     {
-        super("Author", "Authors may read and add information in some cases, but may update and delete only information they added.",
+        super("Author", "Authors may read and add information in some cases, but may update and delete only information they added. Supported for only " +
+                        "Message Boards and Issues. See the online documentation for details.",
                 ReadPermission.class,
                 ReadSomePermission.class,
                 InsertPermission.class,
