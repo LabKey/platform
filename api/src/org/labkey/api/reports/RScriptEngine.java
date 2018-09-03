c090802f3da3253389ecb04f99d7f740bc5db12a
Cory Nathe <cnathe@labkey.com>
2018-09-03 18:29:45 +0000

    Issue 31747: Knitr R Reports set as markdown affect style of the page when the report is rendered

--- api/src/org/labkey/api/reports/RScriptEngine.java
+++ api/src/org/labkey/api/reports/RScriptEngine.java
@@ -238,8 +238,8 @@ public class RScriptEngine extends ExternalScriptEngine
                 }
                 else
                 {
-                    // TODO: should the output_options list params match between these and the ones above (note missing theme=NULL and css=NULL here)
-                    sb.append("output_options=list(keep_md=TRUE, self_contained=FALSE, fig_caption=TRUE, smart=TRUE, highlight=\"default\"), ");
+                    sb.append("output_options=list(keep_md=TRUE, self_contained=FALSE, fig_caption=TRUE, " +
+                            "theme=NULL, css=NULL, smart=TRUE, highlight=\"default\"), ");
                 }
             }
             else
