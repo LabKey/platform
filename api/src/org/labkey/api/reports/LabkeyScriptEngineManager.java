773da928de0b5fcf6e47c8f7893ea429b973d3d7
Emily Zhai <emilyz@labkey.com>
2018-09-06 00:28:39 +0000

    Nestle R engine association - Buddy testing bug fixes (part 1)

--- api/src/org/labkey/api/reports/LabkeyScriptEngineManager.java
+++ api/src/org/labkey/api/reports/LabkeyScriptEngineManager.java
@@ -31,7 +31,7 @@ public interface LabkeyScriptEngineManager
     @Deprecated
     ScriptEngine getEngineByExtension(Container c, String extension, boolean requestRemote, boolean requestDocker);
 
-    ArrayList<String> getEngineScope(Container c);
+    List<ExternalScriptEngineDefinition> getScopedEngines(Container container);
     void setEngineScope(Container c, ExternalScriptEngineDefinition def);
     void removeEngineScope(Container c, ExternalScriptEngineDefinition def);
 
@@ -41,6 +41,7 @@ public interface LabkeyScriptEngineManager
 
     List<ExternalScriptEngineDefinition> getEngineDefinitions();
     List<ExternalScriptEngineDefinition> getEngineDefinitions(ExternalScriptEngineDefinition.Type type);
+    List<ExternalScriptEngineDefinition> getEngineDefinitions(ExternalScriptEngineDefinition.Type type, boolean enabled);
     ExternalScriptEngineDefinition getEngineDefinition(String name, ExternalScriptEngineDefinition.Type type);
     ExternalScriptEngineDefinition getEngineDefinition(int rowId, ExternalScriptEngineDefinition.Type type);
 
