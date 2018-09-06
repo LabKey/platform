bba2b35090c5eb77c8fe54ae804cdf3a1af75559
Ian Sigmon <ians@labkey.com>
2018-09-06 17:34:00 +0000

    Fix MSSQL build break.

--- core/resources/schemas/dbscripts/sqlserver/core-18.24-18.25.sql
+++ core/resources/schemas/dbscripts/sqlserver/core-18.24-18.25.sql
@@ -2,8 +2,9 @@ CREATE TABLE core.PrincipalRelations
 (
   userid ENTITYID NOT NULL,
   otherid ENTITYID NOT NULL,
-  relationship VARCHAR(100) NULL,
-  created TIMESTAMP,
+  relationship VARCHAR(100) NOT NULL,
+  created DATETIME,
 
   CONSTRAINT PK_PrincipalRelations PRIMARY KEY (userid, otherid, relationship)
 );
+
