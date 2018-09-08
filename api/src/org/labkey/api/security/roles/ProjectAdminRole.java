5f5009c90997b8b1d70c97ae33dc8221366b67b4
Adam Rauch <adam@labkey.com>
2018-09-08 21:58:26 +0000

    Secure Issue 32893: Actual access level of ProjectAdminRole is obfuscated
    Clarify Project Administrator role description

--- api/src/org/labkey/api/security/roles/ProjectAdminRole.java
+++ api/src/org/labkey/api/security/roles/ProjectAdminRole.java
@@ -32,8 +32,7 @@ public class ProjectAdminRole extends AbstractRole
     public ProjectAdminRole()
     {
         super("Project Administrator",
-                "Project Administrators have full control over the project, but not the entire system. " +
-                        "By default, Project Administrators are added to the Folder Administrator role within all project folders.",
+                "Project Administrators have full control over the project, but not the entire system.",
                 ReadPermission.class,
                 ReadSomePermission.class,
                 InsertPermission.class,
