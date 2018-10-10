cd10e6a4d9e0c4b019e1b2afb30df1b8297b9cd9
Matthew Bellew <mbellew@labkey.com>
2018-10-10 16:50:02 +0000

    missed date form field

--- study/resources/web/study/assayPublish.js
+++ study/resources/web/study/assayPublish.js
@@ -1,55 +1,12 @@
-/* reshape a bunch of array valued fields into a tsv */
+/*
+ * Copyright (c) 2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
-function formDataToTSV(formDataIN, fieldNamesArray)
-{
-    var fieldNamesSet = {};
-    var gridNames = [];
-    var gridValues = [];
-    var len = 0;
-    var n;
-    for (n=0 ; n<fieldNamesArray.length ; n++)
-    {
-        var name = fieldNamesArray[n];
-        fieldNamesSet[name] = name;
-        var values = formDataIN.getAll(name);
-        if (values.length === 0)
-            continue;
-        if (len !== 0 && values.length !== len)
-            throw "badly formed FORM, can't post";
-        gridNames.push(name);
-        gridValues.push(values);
-        len = values.length;
-    }
-    var tsv = [];
-    tsv.push(gridNames.join("\t"));
-    var row;
-    var width = gridValues.length;
-    for (i=0 ; i<len ; i++)
-    {
-        row = [];
-        for (n=0 ; n<width ; n++)
-            row.push(gridValues[n][i]);
-        tsv.push(row.join("\t"));
-    }
-    tsv = tsv.join("\n");
-
-    // OK, pick up all the other values
-    var formDataOUT = new FormData();
-    formDataOUT.append("tsv", tsv);
-    var entries = Array.from(formDataIN.entries());
-    for (var pair of formDataIN.entries())
-    {
-        var key = pair[0];
-        if (key.charAt(0) === '.')
-            continue;
-        if (key === fieldNamesSet[key])
-            continue;
-        formDataOUT.append(key, pair[1]);
-    }
-    return formDataOUT;
-}
-
-
+/*
+ * basically, change a=1&a=2=&a=3 to a=1\t2\t3 and resubmit the form
+ */
 function assayPublish_onCopyToStudy(el, fieldNames)
 {
     var formEl = jQuery(el).closest("FORM")[0];
