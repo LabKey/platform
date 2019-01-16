b30837849b0f1ddf67d0eec9cdf18c96be566a59
Xing Yang <xyang@labkey.com>
2019-01-16 00:06:48 +0000

    Item #4684: QC trend reports, updates to metrics dropdown - measure inference on importing legacy xar

--- experiment/src/org/labkey/experiment/api/property/PropertyServiceImpl.java
+++ experiment/src/org/labkey/experiment/api/property/PropertyServiceImpl.java
@@ -409,8 +409,10 @@ public class PropertyServiceImpl implements PropertyService
         List<ConditionalFormat> formats = ConditionalFormat.convertFromXML(xProp.getConditionalFormats());
         prop.setConditionalFormats(formats);
 
-        prop.setMeasure(xProp.getMeasure());
-        prop.setDimension(xProp.getDimension());
+        if (null != xProp.xgetMeasure())
+            prop.setMeasure(xProp.getMeasure());
+        if (null != xProp.xgetDimension())
+            prop.setDimension(xProp.getDimension());
         prop.setRecommendedVariable(xProp.getRecommendedVariable());
 
         return prop;
