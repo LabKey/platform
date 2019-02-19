9b8739f70eb2f8c457169af2f13edbb39208aeae
Ankur Juneja <ankurj@labkey.com>
2019-02-19 15:14:04 +0000

    Item 5199: qc code review changes

--- api/src/org/labkey/api/visualization/Stats.java
+++ api/src/org/labkey/api/visualization/Stats.java
@@ -1,94 +1,101 @@
 package org.labkey.api.visualization;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Stats
 {
 
-    public static double MOVING_RANG_EPSILON = 0.0000001;
-    public static int  CUSUM_CONTROL_LIMIT = 5;
-    public static double MOVING_RANGE_UPPER_LIMIT_WEIGHT = 3.268;
+    public static final double MOVING_RANG_EPSILON = 0.0000001;
+    public static final int CUSUM_CONTROL_LIMIT = 5;
+    public static final double MOVING_RANGE_UPPER_LIMIT_WEIGHT = 3.268;
 
-    public static Stats _instance = new Stats();
-
-    public static Stats get()
-    {
-        return _instance;
-    }
 
     /**
      * CUSUM_WEIGHT_FACTOR of 0.5 and CUSUM_CONTROL_LIMIT of 5 to achieve a 3*stdDev boundary
      */
-    public static double CUSUM_WEIGHT_FACTOR = 0.5;
-    public static double CUSUM_EPSILON = 0.0000001;
+    public static final double CUSUM_WEIGHT_FACTOR = 0.5;
+    public static final double CUSUM_EPSILON = 0.0000001;
 
     /**
      * Returns the average value.
+     *
      * @param values An array of double.
      * @returns {double}
      */
-    public double getMean(Double[] values)
+    public static double getMean(Double[] values)
     {
-        if (values == null || values.length ==0)
+        if (values == null || values.length == 0)
             return 0;
 
-        double sum = Arrays.stream(values).mapToDouble(i->i).sum();
-        return sum/values.length;
+        double sum = Arrays.stream(values).filter(Objects::nonNull).mapToDouble(i -> i).sum();
+        return sum / values.length;
     }
 
     /**
      * Returns the standard deviation.
+     *
      * @param values An array of double.
      * @returns {double}
      */
-    public double getStdDev(Double[] values)
+    public static double getStdDev(Double[] values)
     {
-        if (values == null || values.length ==0)
+        if (values == null || values.length == 0)
             return 0;
 
         double mean = getMean(values);
         Double[] squareDiffs = new Double[values.length];
-        for(int i =0; i< values.length; i++)
+        for (int i = 0; i < values.length; i++)
         {
-            Double diff = values[i] - mean;
-            squareDiffs[i] = diff*diff;
+            if(values[i] != null)
+            {
+                Double diff = values[i] - mean;
+                squareDiffs[i] = diff * diff;
+            }
         }
         return Math.sqrt(getMean(squareDiffs));
     }
 
     /**
      * Calculates a variety of cumulative sums for a data array.
-     * @param values Array of data values to calculate from
-     * @param negative True to calculate CUSUM-, false to calculate CUSUM+. (default to false)
-     * @param transform True to calculate CUSUMv (Variability CUSUM), false to calculate CUSUMm (Mean CUSUM). (default to false)
+     *
+     * @param values              Array of data values to calculate from
+     * @param negative            True to calculate CUSUM-, false to calculate CUSUM+. (default to false)
+     * @param transform           True to calculate CUSUMv (Variability CUSUM), false to calculate CUSUMm (Mean CUSUM). (default to false)
      * @param forcePositiveResult True to force all result values to be no less than a specified positive value, usually used for log scale. (default to false)
-     * @param epsilon The smallest value that all returned value can be, only used if forcePositiveResult is true. (default to LABKEY.vis.Stat.CUSUM_EPSILON)
+     * @param epsilon             The smallest value that all returned value can be, only used if forcePositiveResult is true. (default to LABKEY.vis.Stat.CUSUM_EPSILON)
      * @returns {number[]}
      */
-    public double[] getCUSUMS(Double[] values, boolean negative, boolean transform, boolean forcePositiveResult, Double epsilon)
+    public static double[] getCUSUMS(Double[] values, boolean negative, boolean transform, boolean forcePositiveResult, Double epsilon)
     {
-        if (values == null || values.length < 2)
+            if (values == null || values.length < 2)
             return new double[0];
 
         double mean = getMean(values);
         double stdDEV = getStdDev(values);
-        if(stdDEV == 0) // in the case when all values are equal, calculation has to abort, special case CUSUM to all be 0
+        if (stdDEV == 0) // in the case when all values are equal, calculation has to abort, special case CUSUM to all be 0
         {
             return new double[values.length];
         }
 
-        double[] cusums = new double[values.length];
+        double[] cusums = new double[values.length+1];
         cusums[0] = 0;
-        for(int i=1; i< values.length; i++)
+        for (int i = 1; i <= values.length; i++)
         {
-            double standardized = (values[i]-mean) / stdDEV; //standard value (z-score)
-            if (transform)
-                standardized = (Math.sqrt(Math.abs(standardized)) - 0.822) / 0.349; //the transformed standardize normal quantity value so that it is sensitive to variability changes
-            if (negative)
-                standardized = standardized * -1;
-            double cusum = Math.max(0, standardized - CUSUM_WEIGHT_FACTOR + cusums[i-1]);
-            cusums[i] = cusum;
+            if(values[i-1] != null)
+            {
+                double standardized = (values[i-1] - mean) / stdDEV; //standard value (z-score)
+                if (transform)
+                    standardized = (Math.sqrt(Math.abs(standardized)) - 0.822) / 0.349; //the transformed standardize normal quantity value so that it is sensitive to variability changes
+                if (negative)
+                    standardized = standardized * -1;
+                double cusum = Math.max(0, standardized - CUSUM_WEIGHT_FACTOR + cusums[i - 1]);
+                cusums[i] = cusum;
+            }
         }
+        int n = cusums.length-1;
+        double[] shiftCusums = new double[n];
+        System.arraycopy(cusums, 1, shiftCusums, 0, n);
         if (forcePositiveResult)
         {
             double lowerBound = epsilon != null ? epsilon : CUSUM_EPSILON;
@@ -97,31 +104,34 @@ public class Stats
                 cusums[j] = Math.max(cusums[j], lowerBound);
             }
         }
-        return cusums;
+        return shiftCusums;
     }
 
     /**
      * Calculate the moving range values for a data array, which are sequential differences between two successive values.
-     * @param values Array of data values to calculate from
+     *
+     * @param values              Array of data values to calculate from
      * @param forcePositiveResult True to force all result values to be no less than a specified positive value, usually used for log scale. (default to false)
-     * @param epsilon The smallest value that all returned value can be, only used if forcePositiveResult is true. (default to LABKEY.vis.Stat.MOVING_RANGE_EPSILON)
+     * @param epsilon             The smallest value that all returned value can be, only used if forcePositiveResult is true. (default to LABKEY.vis.Stat.MOVING_RANGE_EPSILON)
      * @returns {double[]}
      */
-    public Double[] getMovingRanges(Double[] values, boolean forcePositiveResult, Double epsilon)
+    public static Double[] getMovingRanges(Double[] values, boolean forcePositiveResult, Double epsilon)
     {
-        if(values == null || values.length <= 1)
+        if (values == null || values.length <= 1)
             return new Double[0];
 
         Double[] mR = new Double[values.length];
         mR[0] = Double.valueOf(0); // mR[0] is always 0
-        for(int i=1; i<values.length; i++)
+        for (int i = 1; i < values.length; i++)
         {
-            mR[i] = Math.abs(values[i] - values[i-1]);
+            if(values[i] != null && values[i-1] != null)
+                mR[i] = Math.abs(values[i] - values[i - 1]);
         }
 
-        if(forcePositiveResult) {
+        if (forcePositiveResult)
+        {
             double lowerBound = epsilon != null ? epsilon : MOVING_RANG_EPSILON;
-            for(int j = 0; j < mR.length; j++)
+            for (int j = 0; j < mR.length; j++)
             {
                 mR[j] = Math.max(lowerBound, mR[j]);
             }
