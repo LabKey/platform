80c081df7850470290cd19985e77ddd7a44bd34b
Brad Holland <bradh@labkey.com>
2019-02-26 17:42:05 +0000

    Epic #5164 - Code Review feedback

--- api/gwtsrc/org/labkey/api/gwt/client/assay/model/GWTPropertyDescriptorMixin.java
+++ api/gwtsrc/org/labkey/api/gwt/client/assay/model/GWTPropertyDescriptorMixin.java
@@ -4,7 +4,7 @@ package org.labkey.api.gwt.client.assay.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
- * Configures the fields to be returned when serializing a GWTPropertyDescriptor.
+ * Configures the fields that are not returned when serializing a GWTPropertyDescriptor.
  * Ideally we would just add the @JsonIgnore annotations to GWTPropertyDescriptor directly,
  * but the GWT compiler would need to have jackson on the classpath which isn't
  * necessary.
