1512b7b66d835298941d0004a9bd656d4aebc3f5
Brad Holland <bradh@labkey.com>
2019-02-28 22:47:17 +0000

    Epic #5164 - Use Jackson marshalling for all LABKEY.Domain calls (infer, create) and handle upper case property names

--- api/gwtsrc/org/labkey/api/gwt/client/assay/model/GWTPropertyDescriptorMixin.java
+++ api/gwtsrc/org/labkey/api/gwt/client/assay/model/GWTPropertyDescriptorMixin.java
@@ -2,6 +2,8 @@ package org.labkey.api.gwt.client.assay.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.labkey.api.gwt.client.util.StringProperty;
 
 /**
  * Configures the fields that are not returned when serializing a GWTPropertyDescriptor.
@@ -16,6 +18,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
         "lookupDescription",
         "fileType"
 })
-public interface GWTPropertyDescriptorMixin
+public abstract class GWTPropertyDescriptorMixin
 {
+    GWTPropertyDescriptorMixin(@JsonProperty("PHI") StringProperty phi, @JsonProperty("URL") StringProperty url)
+    { }
+    @JsonProperty("PHI")
+    abstract void setPHI(String phi); // rename property on deserialize
+    @JsonProperty("URL")
+    abstract void setURL(String url); // rename property on deserialize
 }
