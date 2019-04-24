af588841447e1c0fa8de9d83a5672aa6b29b610a
labkey-adam <adam@labkey.com>
2019-04-24 10:41:30 -0700

    Attempt to fix issues with LabKeyJspWriter and taglibs (#71)
    We're seeing intermittent problems where FormTag.doEnd() output sometimes goes nowhere. For example, the </form> tag and csrf token might not get output on the site settings page. Attempt to fix by stashing our wrapped JspWriter and implementing popBody().
    
    Also, register a ShutdownListener that reverts back to the default JspFactory; that prevents us from wrapping our own LabKeyJspFactory on every Tomcat reload.

--- api/src/org/labkey/api/ApiModule.java
+++ api/src/org/labkey/api/ApiModule.java
@@ -35,6 +35,7 @@ import org.labkey.api.security.ApiKeyManager;
 import org.labkey.api.security.ApiKeyManager.ApiKeyMaintenanceTask;
 import org.labkey.api.security.AuthenticationLogoType;
 import org.labkey.api.security.AvatarType;
+import org.labkey.api.util.ContextListener;
 import org.labkey.api.util.SystemMaintenance;
 import org.labkey.api.view.WebPartFactory;
 
@@ -64,8 +65,9 @@ public class ApiModule extends CodeOnlyModule
             AttachmentService.get().registerAttachmentType(ExpRunAttachmentType.get());
 
         // Replace the default JspFactory with a custom factory that injects our own JspWriter implementation
-        JspFactory factory = JspFactory.getDefaultFactory();
-        JspFactory.setDefaultFactory(new LabKeyJspFactory(factory));
+        LabKeyJspFactory factory = new LabKeyJspFactory(JspFactory.getDefaultFactory());
+        JspFactory.setDefaultFactory(factory);
+        ContextListener.addShutdownListener(factory);
     }
 
     @NotNull
