9553338746cc9b3f8f6c8d01ba13a874b91f67c4
Chris Joosse <chrisj@labkey.com>
2019-06-10 15:42:49 -0700

    QC States export-import round-trip testing + page classes to support it

--- study/test/src/org/labkey/test/tests/study/AssayQCTest.java
+++ study/test/src/org/labkey/test/tests/study/AssayQCTest.java
@@ -10,18 +10,24 @@ import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.InDevelopment;
 import org.labkey.test.components.CustomizeView;
 import org.labkey.test.pages.AssayDesignerPage;
+import org.labkey.test.pages.admin.ExportFolderPage;
+import org.labkey.test.pages.admin.ImportFolderPage;
 import org.labkey.test.pages.assay.AssayImportPage;
 import org.labkey.test.pages.assay.AssayRunsPage;
 import org.labkey.test.pages.assay.ManageAssayQCStatesPage;
+import org.labkey.test.pages.study.QCStateTableRow;
 import org.labkey.test.params.FieldDefinition;
 import org.labkey.test.util.DataRegionTable;
+import org.labkey.test.util.PortalHelper;
 import org.labkey.test.util.TestDataGenerator;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @Category({InDevelopment.class})
 public class AssayQCTest extends BaseWebDriverTest
@@ -187,6 +193,168 @@ public class AssayQCTest extends BaseWebDriverTest
 
         // clean up the subfolder on success
         _containerHelper.deleteFolder(getProjectName(), "QCStateVisibilityTest");
+    }
+
+    @Test
+    public void testQCStateRoundTrip() throws Exception
+    {
+        String importDestProjectName = "AssayQCTest_exportDestination";
+        String destSubfolder= "ImportExportedAssaySubdir";
+        // create the destination project and subfolder
+        _containerHelper.addCreatedProject(importDestProjectName);
+        _containerHelper.createProject(importDestProjectName, null);
+        _containerHelper.createSubfolder(importDestProjectName, destSubfolder);
+
+        String assayName = "RoundTripQCStateTest_assay";
+        generateAssay("RoundTripQCStateTest", assayName)
+                .addRunField("Color", "Color", FieldDefinition.ColumnType.String)
+                .addRunField("Concentration", "Concentration", FieldDefinition.ColumnType.Double)
+                .enableQCStates(true)
+                .saveAndClose();
+        List<FieldDefinition> resultsFieldset = List.of(
+                TestDataGenerator.simpleFieldDef("ParticipantID",FieldDefinition.ColumnType.String),
+                TestDataGenerator.simpleFieldDef("Date", FieldDefinition.ColumnType.DateTime),
+                TestDataGenerator.simpleFieldDef("Color", FieldDefinition.ColumnType.String),
+                TestDataGenerator.simpleFieldDef("Concentration", FieldDefinition.ColumnType.Double));
+        FieldDefinition.LookupInfo assayLookup = new FieldDefinition.LookupInfo(getProjectName() + "/" + "RoundTripQCStateTest",
+                "assay.General.RoundTripQCStateTest_assay", "Runs");
+
+        clickAndWait(Locator.linkWithText(assayName));
+        DataRegionTable.DataRegion(getDriver()).withName("Runs").find().clickHeaderMenu("QC State", "Manage states");
+        new ManageAssayQCStatesPage(getDriver())
+                .addStateRow("Seems shady", "Better review this one", true)
+                .addStateRow("Totally legit", "Looks good", true)
+                .addStateRow("WTF", "What, was this found on the lab floor somewhere?", false)
+                .clickSave();
+
+        TestDataGenerator dgen1 = new TestDataGenerator(assayLookup)
+                .withColumnSet(resultsFieldset)
+                .addCustomRow(Map.of("ParticipantID", "Jeff", "Date", "11/11/2018", "Color", "Green", "Concentration", 12.5))
+                .addCustomRow(Map.of("ParticipantID", "Jim", "Date", "11/12/2018", "Color", "Red", "Concentration", 14.5))
+                .addCustomRow(Map.of("ParticipantID", "Billy", "Date", "11/13/2018", "Color", "Yellow", "Concentration", 17.5))
+                .addCustomRow(Map.of("ParticipantID", "Michael", "Date", "11/14/2018", "Color", "Orange", "Concentration", 11.5));
+        String pasteData1 = dgen1.writeTsvContents();
+
+        TestDataGenerator dgen2 = new TestDataGenerator(assayLookup)
+                .withColumnSet(resultsFieldset)
+                .addCustomRow(Map.of("ParticipantID", "Harry", "Date", "10/11/2018", "Color", "Green", "Concentration", 12.5))
+                .addCustomRow(Map.of("ParticipantID", "William", "Date", "10/12/2018", "Color", "Red", "Concentration", 14.5))
+                .addCustomRow(Map.of("ParticipantID", "Jenny", "Date", "10/13/2018", "Color", "Yellow", "Concentration", 17.5))
+                .addCustomRow(Map.of("ParticipantID", "Hermione", "Date", "10/14/2018", "Color", "Orange", "Concentration", 11.5));
+        String pasteData2 = dgen2.writeTsvContents();
+
+        TestDataGenerator dgen3 = new TestDataGenerator(assayLookup)
+                .withColumnSet(resultsFieldset)
+                .addCustomRow(Map.of("ParticipantID", "George", "Date", "10/11/2018", "Color", "Green", "Concentration", 12.5))
+                .addCustomRow(Map.of("ParticipantID", "Arthur", "Date", "10/12/2018", "Color", "Red", "Concentration", 14.5))
+                .addCustomRow(Map.of("ParticipantID", "Colin", "Date", "10/13/2018", "Color", "Yellow", "Concentration", 17.5))
+                .addCustomRow(Map.of("ParticipantID", "Ronald", "Date", "10/14/2018", "Color", "Orange", "Concentration", 11.5));
+        String pasteData3 = dgen3.writeTsvContents();
+
+        DataRegionTable runsTable = DataRegionTable.DataRegion(getDriver()).withName("Runs").find();
+        runsTable.clickHeaderButton("Import Data");
+        clickButton("Next");
+
+        // insert 3 runs
+        new AssayImportPage(getDriver()).setNamedTextAreaValue("TextAreaDataCollector.textArea", pasteData1);
+        clickButton("Save and Import Another Run");
+        new AssayImportPage(getDriver()).setNamedTextAreaValue("TextAreaDataCollector.textArea", pasteData2);
+        clickButton("Save and Import Another Run");
+        new AssayImportPage(getDriver()).setNamedTextAreaValue("TextAreaDataCollector.textArea", pasteData3);
+        clickButton("Save and Finish");
+
+        AssayRunsPage runsPage = new AssayRunsPage(getDriver());
+        CustomizeView customView = runsPage.getTable().openCustomizeGrid();
+        customView.addColumn("QCFLAGS/LABEL", "Label");
+        customView.addColumn("QCFLAGS/DESCRIPTION", "Description");
+        customView.addColumn("QCFLAGS/PUBLICDATA", "Public Data");
+        customView.clickViewGrid();
+
+        // now set each row to a different QC state
+        runsPage = new AssayRunsPage(getDriver());
+        runsPage = runsPage.setRowQcStatus(0, "Seems shady", "Not so sure about this one");
+        runsPage = runsPage.setRowQcStatus(1, "Totally legit", "Yeah, I trust this");
+        runsPage = runsPage.setRowQcStatus(2, "WTF", "No way is this legit");
+
+        Map<String, String> run1Data = runsPage.getTable().getRowDataAsMap(0);
+        Map<String, String> run2Data = runsPage.getTable().getRowDataAsMap(1);
+        Map<String, String> run3Data = runsPage.getTable().getRowDataAsMap(2);
+
+        // validate expected visibility
+        assertEquals("Seems shady", run1Data.get("QCFlags/Label"));
+        assertEquals("Better review this one", run1Data.get("QCFlags/Description"));
+        assertEquals("true", run1Data.get("QCFlags/PublicData"));
+
+        assertEquals("Totally legit", run2Data.get("QCFlags/Label"));
+        assertEquals("Looks good", run2Data.get("QCFlags/Description"));
+        assertEquals("true", run2Data.get("QCFlags/PublicData"));
+
+        assertEquals("WTF", run3Data.get("QCFlags/Label"));
+        assertEquals("What, was this found on the lab floor somewhere?", run3Data.get("QCFlags/Description"));
+        assertEquals("false", run3Data.get("QCFlags/PublicData"));
+
+
+        // now export the assay to a zip archive
+        goToFolderManagement()
+                .goToExportTab();    // todo: make a FolderExportPage  and implement
+        new ExportFolderPage(getDriver())
+                .selectExperimentsAndRuns(true);
+        File exportArchive = clickAndWaitForDownload(Locator.linkWithSpan("Export"));
+
+        // navigate into the destination folder and import there
+        ImportFolderPage.beginAt(this, importDestProjectName +"/"+ destSubfolder)
+                .selectLocalZipArchive()
+                .chooseFile(exportArchive)
+                .clickImportFolder();
+        waitForPipelineJobsToFinish(1);
+
+        navigateToFolder(importDestProjectName, destSubfolder);
+
+        navBar().enterPageAdminMode();
+        new PortalHelper(getDriver()).addBodyWebPart("Assay List");
+        navBar().exitPageAdminMode();
+        clickAndWait(Locator.linkWithText(assayName));
+
+        AssayRunsPage importedRunsPage = new AssayRunsPage(getDriver());
+        CustomizeView importedView = importedRunsPage.getTable().openCustomizeGrid();
+        importedView.addColumn("QCFLAGS/LABEL", "Label");
+        importedView.addColumn("QCFLAGS/DESCRIPTION", "Description");
+        importedView.addColumn("QCFLAGS/PUBLICDATA", "Public Data");
+        importedView.clickViewGrid();
+
+        ManageAssayQCStatesPage importedStatesPage = importedRunsPage.manageQCStates();
+        List<QCStateTableRow> states = importedStatesPage.getStateRows();
+        assertNotNull("expect qc states to have imported with data",
+                states.stream().filter(a->a.getState().equals("Seems shady")).findFirst().orElse(null));
+        assertNotNull("expect qc states to have imported with data",
+                states.stream().filter(a->a.getState().equals("Totally legit")).findFirst().orElse(null));
+        assertNotNull("expect qc states to have imported with data",
+                states.stream().filter(a->a.getState().equals("WTF")).findFirst().orElse(null));
+        importedStatesPage.clickCancel();
+
+        // now verify expected qc states
+        importedRunsPage = new AssayRunsPage(getDriver());
+        Map<String, String> imported1 = importedRunsPage.getTable().getRowDataAsMap(0);
+        Map<String, String> imported2 = importedRunsPage.getTable().getRowDataAsMap(1);
+        Map<String, String> imported3 = importedRunsPage.getTable().getRowDataAsMap(2);
+
+        // validate expected visibility
+        assertEquals("Seems shady", imported1.get("QCFlags/Label"));
+        assertEquals("Better review this one", imported1.get("QCFlags/Description"));
+        assertEquals("true", imported1.get("QCFlags/PublicData"));
+
+        assertEquals("Totally legit", imported2.get("QCFlags/Label"));
+        assertEquals("Looks good", imported2.get("QCFlags/Description"));
+        assertEquals("true", imported2.get("QCFlags/PublicData"));
+
+        assertEquals("WTF", imported3.get("QCFlags/Label"));
+        assertEquals("What, was this found on the lab floor somewhere?", imported3.get("QCFlags/Description"));
+        assertEquals("false", imported3.get("QCFlags/PublicData"));
+
+        // clean up the subfolder on success
+        _containerHelper.deleteFolder(getProjectName(), "QCStateVisibilityTest");
+        // also the import destination project
+        _containerHelper.deleteProject(importDestProjectName);
     }
 
     @Override
