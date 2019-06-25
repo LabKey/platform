59c41cff905cd21224ae244e008f82f6ded23986
Binal <binalp@labkey.com>
2019-06-25 13:18:37 -0700

    Removed IsSerializable to test whether it is needed after changing LockedPropertyType variable to be of StringProperty type in GWTPropertyDescriptor.

--- api/gwtsrc/org/labkey/api/gwt/client/LockedPropertyType.java
+++ api/gwtsrc/org/labkey/api/gwt/client/LockedPropertyType.java
@@ -1,12 +1,10 @@
 package org.labkey.api.gwt.client;
 
-import com.google.gwt.user.client.rpc.IsSerializable;
-
 /**
  * Enum to set a type of lock on a column, since binary values of either fully locked or not locked is not sufficient to handle
  * combined cases of various domain designs, esp. when a column is partially locked.
 **/
-public enum LockedPropertyType implements IsSerializable
+public enum LockedPropertyType
 {
     FullyLocked("Fully Locked"), // can't change any properties
     PartiallyLocked("Partially Locked"), // can't change name and type, for example, but can change other properties
