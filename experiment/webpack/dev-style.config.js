e557320e4beba7e5f7b85d22cebbeff45a835172
labkey-ankurj <39965420+labkey-ankurj@users.noreply.github.com>
2019-06-26 09:16:26 -0700

    Item 5760: update copyrights to 2019

--- experiment/webpack/dev-style.config.js
+++ experiment/webpack/dev-style.config.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const constants = require('./constants');
 
--- internal/webapp/extWidgets/BootstrapTabPanel.js
+++ internal/webapp/extWidgets/BootstrapTabPanel.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 LabKey Corporation
+ * Copyright (c) 2018-2019 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
