9b7216de5968a237a94194fbe329f7dcdde4b2c4
labkey-tchad <tchad@labkey.com>
2019-07-24 07:58:08 -0700

    Code review updates

--- api/src/org/labkey/api/data/BeanObjectFactory.java
+++ api/src/org/labkey/api/data/BeanObjectFactory.java
@@ -133,39 +133,43 @@ public class BeanObjectFactory<K> implements ObjectFactory<K> // implements Resu
 
         for (String prop : _writeableProperties)
         {
-            Object value = null;
-            try
+            // If the map contains the key, assuming that we should use the map's value, even if it's null.
+            // Otherwise, don't set a value on the bean.
+            if (m.containsKey(prop))
             {
+                Object value = m.get(prop);
                 try
                 {
-                    // If the map contains the key, assuming that we should use the map's value, even if it's null.
-                    // Otherwise, don't set a value on the bean.
-                    if (m.containsKey(prop))
+                    try
                     {
-                        value = m.get(prop);
                         BeanUtils.copyProperty(bean, prop, value);
                     }
-
+                    catch (InvocationTargetException x)
+                    {
+                        // Unwrap exceptions for bad parameters
+                        Throwable targetException = x.getTargetException();
+                        if (targetException instanceof IllegalArgumentException)
+                        {
+                            throw (IllegalArgumentException) targetException;
+                        }
+                        else if (targetException instanceof ConversionException)
+                        {
+                            throw (ConversionException) targetException;
+                        }
+                        else
+                        {
+                            throw x;
+                        }
+                    }
                 }
-                catch (InvocationTargetException x)
+                catch (IllegalAccessException | InvocationTargetException x)
                 {
-                    // Unwrap exceptions for bad parameters
-                    Throwable targetException = x.getTargetException();
-                    if (targetException instanceof IllegalArgumentException)
-                        throw (IllegalAccessException) targetException;
-                    else if (targetException instanceof ConversionException)
-                        throw (ConversionException) targetException;
-                    else
-                        throw x;
+                    throw new UnexpectedException(x);
                 }
-            }
-            catch (IllegalAccessException | InvocationTargetException x)
-            {
-                throw new UnexpectedException(x);
-            }
-            catch (IllegalArgumentException | ConversionException x)
-            {
-                _log.warn("Bean [" + bean.getClass().getName() + "] could not set property: " + prop + "=" + String.valueOf(value) + ": " + x.getMessage());
+                catch (IllegalArgumentException | ConversionException x)
+                {
+                    _log.warn("Bean [" + bean.getClass().getName() + "] could not set property: " + prop + "=" + String.valueOf(value) + ": " + x.getMessage());
+                }
             }
         }
 
