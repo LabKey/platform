2ff7baf90ea10e7edc12210d18eb4112479bc018
Adam Rauch <adam@labkey.com>
2019-08-18 13:20:15 -0700

    Extract BaseScanner out of SqlScanner, implement new subclass JavaScanner
    Add devtools JspFinderAction, which uses JavaScanner to find JSP file references in our code and cross reference them with JSP source files, highlighting incorrect JSP paths and unused JSP files.

--- devtools/src/org/labkey/devtools/ToolsController.java
+++ devtools/src/org/labkey/devtools/ToolsController.java
@@ -1,5 +1,6 @@
 package org.labkey.devtools;
 
+import org.jetbrains.annotations.Nullable;
 import org.labkey.api.action.SimpleViewAction;
 import org.labkey.api.action.SpringActionController;
 import org.labkey.api.module.Module;
@@ -7,6 +8,7 @@ import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.security.RequiresPermission;
 import org.labkey.api.security.permissions.AdminPermission;
 import org.labkey.api.util.PageFlowUtil;
+import org.labkey.api.util.Pair;
 import org.labkey.api.util.StringUtilsLabKey;
 import org.labkey.api.view.ActionURL;
 import org.labkey.api.view.HttpView;
@@ -19,8 +21,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import static org.labkey.api.util.PageFlowUtil.filter;
 
 public class ToolsController extends SpringActionController
 {
@@ -56,15 +70,23 @@ public class ToolsController extends SpringActionController
     }
 
 
+    /**
+     * This action "validates" our two non-trivial .gitattributes files (in platform and commonAssays) by outputting all
+     * filenames in those files that don't exist in the source code. This highlights files that have been moved, renamed,
+     * or deleted, making it easy to update the .gitattributes files with their new locations.
+     */
+    @SuppressWarnings("unused")
     @RequiresPermission(AdminPermission.class)
     public class GitAttributesAction extends SimpleViewAction<Object>
     {
         @Override
         public ModelAndView getView(Object o, BindException errors)
         {
+            // The .gitattributes files we care about live at the root of the platform and commonAssays repos. Pick an
+            // arbitrary module from each of these repos and use it to locate each root and .gitattributes file.
             return new VBox(
-                new GitAttributesView("core"),
-                new GitAttributesView("ms2")
+                new GitAttributesView("core"), // Use the "core" module to locate the platform repo source root
+                new GitAttributesView("ms2")   // Use the "ms2" module to locate the commonAssays repo source root
             );
         }
 
@@ -119,7 +141,7 @@ public class ToolsController extends SpringActionController
                                         File file = new File(gaDirFile, filename);
 
                                         if (!file.exists())
-                                            out.println(PageFlowUtil.filter(filename));
+                                            out.println(filter(filename));
                                     });
                             }
                         }
@@ -135,6 +157,219 @@ public class ToolsController extends SpringActionController
         {
             appendBeginNavTrail(root);
             root.addChild(".gitattributes File Check");
+            return root;
+        }
+    }
+
+
+    @RequiresPermission(AdminPermission.class)
+    @SuppressWarnings("unused")
+    public class JspFinderAction extends SimpleViewAction<Object>
+    {
+        @Override
+        public ModelAndView getView(Object o, BindException errors)
+        {
+            return new JspFinderView(ModuleLoader.getInstance().getModules());
+        }
+
+        private class JspFinderView extends HttpView
+        {
+            private final Collection<Module> _modules;
+
+            public JspFinderView(Collection<Module> modules)
+            {
+                _modules = modules;
+            }
+
+            @Override
+            protected void renderInternal(Object model, PrintWriter out)
+            {
+                out.println("<pre>");
+
+                Set<String> jspReferences = _modules.stream()
+                    .flatMap(m->findJspReferences(m, out).stream())
+                    .collect(Collectors.toCollection(TreeSet::new));
+
+                out.println();
+                out.println("JSP files that don't seem to be referenced in the code:");
+                out.println();
+
+                Set<String> jspFiles =_modules.stream()
+                    .flatMap(m->findJspFiles(m, out).stream())
+                    .collect(Collectors.toCollection(TreeSet::new));
+
+                Set<String> copyOfJspFiles = new HashSet<>(jspFiles);
+
+                jspFiles.removeAll(jspReferences);
+                jspFiles.forEach(path->out.println(filter(path)));
+
+                out.println();
+                out.println("JSP references that couldn't be resolved to JSP files:");
+                out.println();
+
+                jspReferences.removeAll(copyOfJspFiles);
+                jspReferences.forEach(path->out.println(filter(path)));
+
+                out.println("</pre>");
+                out.flush();
+            }
+
+            private @Nullable Path getSourceRoot(Module m, PrintWriter out)
+            {
+                Path root = null;
+                String sourcePath = m.getSourcePath();
+
+                if (null == sourcePath)
+                {
+                    out.println(m.getName() + " module source path not found");
+                }
+                else
+                {
+                    root = Path.of(sourcePath);
+
+                    if (!root.toFile().isDirectory())
+                    {
+                        out.println("Directory " + root + " not found");
+                        root = null;
+                    }
+                }
+
+                return root;
+            }
+
+            private Collection<String> findJspReferences(Module module, PrintWriter out)
+            {
+                List<String> ret = new LinkedList<>();
+                Path root = getSourceRoot(module, out);
+
+                if (null != root)
+                {
+                    try
+                    {
+                        Files.walkFileTree(root, new SimpleFileVisitor<>()
+                        {
+                            @Override
+                            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                            {
+                                if (file.toString().endsWith(".java"))
+                                {
+                                    String code = PageFlowUtil.getFileContentsAsString(file.toFile());
+                                    JavaScanner scanner = new JavaScanner(code);
+
+                                    // TODO: super("org/labkey/*.jsp")
+                                    // TODO: warn for duplicates - suspicious
+                                    int idx = scanner.indexOf("new JspView");
+
+                                    while (-1 != idx)
+                                    {
+                                        // We've found a JspView constructor; extract its parameters
+                                        Pair<Integer, Integer> startEnd = findParameters(code, idx);
+                                        String parameters = code.substring(startEnd.first, startEnd.second);
+
+                                        // JspView() parameters usually include a path to the JSP, but not always. Try to find a string...
+                                        int openingQuote = parameters.indexOf('"');
+
+                                        if (-1 != openingQuote)
+                                        {
+                                            // TODO: find multiple strings, see ProteinSearchWebPart
+                                            int closingQuote = parameters.indexOf('"', openingQuote + 1);
+                                            String jspPath = parameters.substring(openingQuote + 1, closingQuote);
+                                            handleJspPath(out, root, file, jspPath);
+                                            ret.add(jspPath);
+                                        }
+                                        else
+                                        {
+                                            out.println(filter(module.getName() + ": Could not extract a JSP path from: " + parameters + " [" + file + "]"));
+                                        }
+
+                                        idx = scanner.indexOf("new JspView", startEnd.second + 1);
+                                    }
+                                }
+
+                                return FileVisitResult.CONTINUE;
+                            }
+                        });
+                    }
+                    catch (IOException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                return ret;
+            }
+
+            private Collection<String> findJspFiles(Module module, PrintWriter out)
+            {
+                List<String> ret = new LinkedList<>();
+                Path root = getSourceRoot(module, out);
+
+                if (null != root)
+                {
+                    try
+                    {
+                        Files.walkFileTree(root, new SimpleFileVisitor<>()
+                        {
+                            @Override
+                            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                            {
+                                String filePath = file.toString().replaceAll("\\\\", "/");
+                                if (filePath.endsWith(".jsp"))
+                                {
+                                    int idx = filePath.indexOf("/org/labkey");
+
+                                    if (-1 != idx)
+                                        ret.add(filePath.substring(idx));
+                                    else
+                                        out.println(filter("Can't find \"/org/labkey\": " + filePath));
+                                }
+
+                                return FileVisitResult.CONTINUE;
+                            }
+                        });
+                    }
+                    catch (IOException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                return ret;
+            }
+
+            // Return start and end characters of the parameters
+            private Pair<Integer, Integer> findParameters(String code, int idx)
+            {
+                int start = idx = code.indexOf('(', idx);
+                assert -1 != start;
+                int openingCount = 1;
+
+                while (openingCount > 0)
+                {
+                    char c = code.charAt(++idx);
+                    if (c == '(')
+                        openingCount++;
+                    else if (c == ')')
+                        openingCount--;
+                }
+
+                return Pair.of(start, idx + 1);
+            }
+
+            private void handleJspPath(PrintWriter out, Path root, Path file, String jspPath)
+            {
+                File jsp = new File(root.resolve("src").toFile(), jspPath);
+
+                if (!jsp.exists())
+                    out.println(filter(jspPath + " not found [" + file + "]"));
+            }
+        }
+
+        @Override
+        public NavTree appendNavTrail(NavTree root)
+        {
+            appendBeginNavTrail(root);
+            root.addChild("JSP Finder");
             return root;
         }
     }
--- study/src/org/labkey/study/controllers/specimen/SpecimenController.java
+++ study/src/org/labkey/study/controllers/specimen/SpecimenController.java
@@ -4164,6 +4164,12 @@ public class SpecimenController extends BaseStudyController
         }
 
         @Override
+        protected JspView<StudyImpl> getJspView(StudyImpl study)
+        {
+            return new JspView<>("/org/labkey/study/view/specimen/manageActorOrder.jsp", study);
+        }
+
+        @Override
         public boolean handlePost(BulkEditForm form, BindException errors)
         {
             getUtils().ensureSpecimenRequestsConfigured(false);
@@ -4219,8 +4225,10 @@ public class SpecimenController extends BaseStudyController
         public ModelAndView getView(Form form, boolean reshow, BindException errors)
         {
             getUtils().ensureSpecimenRequestsConfigured(false);
-            return new JspView<>("/org/labkey/study/view/specimen/" + _jsp + ".jsp", getStudyRedirectIfNull());
+            return getJspView(getStudyRedirectIfNull());
         }
+
+        protected abstract JspView<StudyImpl> getJspView(StudyImpl study);
 
         @Override
         public NavTree appendNavTrail(NavTree root)
@@ -4249,6 +4257,12 @@ public class SpecimenController extends BaseStudyController
         public ManageActorsAction()
         {
             super("manageActors", "Manage Specimen Request Actors", "coordinateSpecimens#actor");
+        }
+
+        @Override
+        protected JspView<StudyImpl> getJspView(StudyImpl study)
+        {
+            return new JspView<>("/org/labkey/study/view/specimen/manageActors.jsp", study);
         }
 
         @Override
@@ -4335,6 +4349,12 @@ public class SpecimenController extends BaseStudyController
         }
 
         @Override
+        protected JspView<StudyImpl> getJspView(StudyImpl study)
+        {
+            return new JspView<>("/org/labkey/study/view/specimen/manageStatusOrder.jsp", study);
+        }
+
+        @Override
         public boolean handlePost(BulkEditForm form, BindException errors)
         {
             getUtils().ensureSpecimenRequestsConfigured(false);
@@ -4383,6 +4403,12 @@ public class SpecimenController extends BaseStudyController
         public ManageStatusesAction()
         {
             super("manageStatuses", "Manage Specimen Request Statuses", "specimenRequest#status");
+        }
+
+        @Override
+        protected JspView<StudyImpl> getJspView(StudyImpl study)
+        {
+            return new JspView<>("/org/labkey/study/view/specimen/manageStatuses.jsp", study);
         }
 
         @Override
