59b0361a93cd52ee815455c8f38351958b3f18e1
cnathe <cnathe@labkey.com>
2019-08-23 16:41:22 -0500

    Item 6118: setup assay module for React page development

---
+++ assay/src/client/TestPage/TestPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export class App extends React.Component<any, any> {
+
+    render() {
+        return (
+            <h3>
+                Hello, World!
+            </h3>
+        )
+    }
+}
---
+++ assay/webpack/entryPoints.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+module.exports = {
+    apps: [{
+        name: 'test',
+        title: 'Test Page',
+        permission: 'read',
+        path: './src/client/TestPage'
+    }]
+};
