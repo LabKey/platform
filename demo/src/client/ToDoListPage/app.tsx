e303d499d6d043575bd1024b3197fe0aec3c8928
labkey-martyp <martyp@labkey.com>
2019-09-20 19:29:41 -0700

    Remove demo module from platform
    Moving demo module to tutorialsModules

--- demo/src/client/ToDoListPage/app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-
-import {App} from './ToDoListPage'
-
-// Need to wait for container element to be available in labkey wrapper before render
-window.addEventListener('DOMContentLoaded', (event) => {
-    ReactDOM.render(<App/>, document.getElementById('app'));
-});
