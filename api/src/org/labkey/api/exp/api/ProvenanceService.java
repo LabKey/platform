6a97990818009315f59bfc6332b67c955c2bc0c2
labkey-ankurj <ankurj@labkey.com>
2019-10-20 20:21:37 -0700

    Item 6290: Partial ProvenanceService Api and Impl

--- api/src/org/labkey/api/exp/api/ProvenanceService.java
+++ api/src/org/labkey/api/exp/api/ProvenanceService.java
@@ -1,11 +1,26 @@
 package org.labkey.api.exp.api;
 
+import org.labkey.api.services.ServiceRegistry;
 import org.labkey.api.util.Pair;
 
 import java.util.List;
 
+/**
+ * Service to include non-data {@link ExpObject} and non-material to a run's ProtocolApplication steps for the purposes of lineage.
+ * This service can add provenance to an {@link ExpProtocolApplication} using one of teh add Methods.
+ * */
 public interface ProvenanceService
 {
+    static ProvenanceService get()
+    {
+        return ServiceRegistry.get().getService(ProvenanceService.class);
+    }
+
+    static void setInstance(ProvenanceService impl)
+    {
+        ServiceRegistry.get().registerService(ProvenanceService.class, impl);
+    }
+
     void addProvenanceInputs(ExpProtocolApplication app, List<String> inputLSIDs);
 
     void addProvenanceOutputs(ExpProtocolApplication app, List<String> outputLSIDs);
@@ -13,5 +28,7 @@ public interface ProvenanceService
     void addProvenanceMapping(ExpProtocolApplication app, List<String> inputLSIDs, List<String> outputLSIDs);
 
     void addProvenance(ExpRun run, List<String> runInputLSIDs, List<Pair<List<String>, List<String>>> outputMap);
+
+    Pair<List<String>, List<String>> getProvenance(ExpProtocolApplication app);
 
 }
