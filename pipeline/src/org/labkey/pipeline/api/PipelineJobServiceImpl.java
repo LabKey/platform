e1494fa4f49a9ea14227295ac1077e8a532cc895
labkey-tchad <tchad@labkey.com>
2019-10-23 08:48:20 -0700

    Fix backslash handling

--- pipeline/src/org/labkey/pipeline/api/PipelineJobServiceImpl.java
+++ pipeline/src/org/labkey/pipeline/api/PipelineJobServiceImpl.java
@@ -84,6 +84,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
 public class PipelineJobServiceImpl implements PipelineJobService
@@ -969,7 +970,7 @@ public class PipelineJobServiceImpl implements PipelineJobService
             String javaHome = System.getenv("JAVA_HOME");
             if (javaHome != null)
             {
-                javaHome = javaHome.replaceAll("[/\\\\]", File.separator); // Match normalized file separators from `getExecutablePath`
+                javaHome = javaHome.replaceAll("[/\\\\]", Matcher.quoteReplacement(File.separator)); // Match normalized file separators from `getExecutablePath`
                 String installPath = "${JAVA_HOME}/bin";
                 String found = _impl.getExecutablePath("jar", installPath, null, null, null);
                 assertEquals("Failed to expand environment variable correctly.", javaHome + File.separator + "bin" + File.separator + "jar", found);
