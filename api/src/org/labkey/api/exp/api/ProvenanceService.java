bc643f384a5348505586367bc9b750fe408bf434
labkey-ankurj <ankurj@labkey.com>
2019-10-23 21:44:46 -0700

    Item 6290: storing list of output lsids

--- api/src/org/labkey/api/exp/api/ProvenanceService.java
+++ api/src/org/labkey/api/exp/api/ProvenanceService.java
@@ -1,9 +1,13 @@
 package org.labkey.api.exp.api;
 
+import org.labkey.api.data.DbSchema;
+import org.labkey.api.data.TableInfo;
 import org.labkey.api.services.ServiceRegistry;
 import org.labkey.api.util.Pair;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Service to include non-data {@link ExpObject} and non-material to a run's ProtocolApplication steps for the purposes of lineage.
@@ -23,12 +27,16 @@ public interface ProvenanceService
 
     void addProvenanceInputs(ExpProtocolApplication app, List<String> inputLSIDs);
 
-    void addProvenanceOutputs(ExpProtocolApplication app, List<String> outputLSIDs);
+    Map<Integer, Set<String>> addProvenanceOutputs(ExpProtocolApplication app, Set<String> outputLSIDs);
 
     void addProvenanceMapping(ExpProtocolApplication app, List<String> inputLSIDs, List<String> outputLSIDs);
 
     void addProvenance(ExpRun run, List<String> runInputLSIDs, List<Pair<List<String>, List<String>>> outputMap);
 
     Pair<List<String>, List<String>> getProvenance(ExpProtocolApplication app);
+
+    DbSchema getDbSchema();
+
+    TableInfo getTinfoProtocolApplicationObjectMap();
 
 }
