f60e1842eded6a71fdb1b5be98c909752277be7d
Kevin Krouse <kevink@labkey.com>
2019-11-22 12:24:34 -0800

    Validate the run is from the same container as the request to match GetAssayBatchAction

--- assay/src/org/labkey/assay/actions/GetAssayRunAction.java
+++ assay/src/org/labkey/assay/actions/GetAssayRunAction.java
@@ -3,12 +3,14 @@ package org.labkey.assay.actions;
 import org.json.JSONObject;
 import org.labkey.api.action.ApiResponse;
 import org.labkey.api.action.ApiSimpleResponse;
+import org.labkey.api.action.ApiUsageException;
 import org.labkey.api.action.ReadOnlyApiAction;
 import org.labkey.api.exp.api.AssayJSONConverter;
 import org.labkey.api.exp.api.ExpRun;
 import org.labkey.api.exp.api.ExperimentService;
 import org.labkey.api.security.RequiresPermission;
 import org.labkey.api.security.permissions.ReadPermission;
+import org.labkey.api.view.NotFoundException;
 import org.springframework.validation.BindException;
 
 @RequiresPermission(ReadPermission.class)
@@ -17,7 +19,14 @@ public class GetAssayRunAction extends ReadOnlyApiAction<GetAssayRunAction.LoadA
     @Override
     public ApiResponse execute(LoadAssayRunForm loadAssayRunForm, BindException errors)
     {
-        ExpRun run = ExperimentService.get().getExpRun(loadAssayRunForm.getLsid());
+        ExpRun run = null;
+        if (loadAssayRunForm.getLsid() != null)
+            run = getRun(loadAssayRunForm.getLsid());
+        else if (loadAssayRunForm.getRunId() != null)
+            run = getRun(loadAssayRunForm.getRunId());
+        else
+            throw new ApiUsageException("Either lsid or runId is required");
+
         JSONObject result = new JSONObject();
 
         result.put("run", AssayJSONConverter.serializeRun(run, null, null, getUser()));
@@ -26,9 +35,35 @@ public class GetAssayRunAction extends ReadOnlyApiAction<GetAssayRunAction.LoadA
 
     }
 
+    ExpRun getRun(int runId)
+    {
+        ExpRun run = ExperimentService.get().getExpRun(runId);
+        if (run == null)
+            throw new NotFoundException("Run not found: " + runId);
+
+        if (run.getContainer().equals(getContainer()))
+            throw new NotFoundException("Run not found in folder: " + runId);
+
+        return run;
+    }
+
+    ExpRun getRun(String lsid)
+    {
+        ExpRun run = ExperimentService.get().getExpRun(lsid);
+        if (run == null)
+            throw new NotFoundException("Run not found: " + lsid);
+
+        if (run.getContainer().equals(getContainer()))
+            throw new NotFoundException("Run not found in folder: " + lsid);
+
+        return run;
+    }
+
+
     static class LoadAssayRunForm
     {
         String lsid;
+        Integer runId;
 
         public String getLsid()
         {
@@ -40,6 +75,15 @@ public class GetAssayRunAction extends ReadOnlyApiAction<GetAssayRunAction.LoadA
             this.lsid = lsid;
         }
 
+        public Integer getRunId()
+        {
+            return runId;
+        }
+
+        public void setRunId(Integer runId)
+        {
+            this.runId = runId;
+        }
     }
 }
 
