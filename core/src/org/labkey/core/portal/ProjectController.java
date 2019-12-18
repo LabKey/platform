41087f438c79a3f72c3dd0cc636c1640debd221a
Binal <binalp@labkey.com>
2019-12-18 11:00:23 -0800

    NPE fix - avoid using nullable getPageConfig().

--- core/src/org/labkey/core/portal/ProjectController.java
+++ core/src/org/labkey/core/portal/ProjectController.java
@@ -381,7 +381,7 @@ public class ProjectController extends SpringActionController
 
         public NavTree appendNavTrail(NavTree root)
         {
-            return (null != getPageConfig().getTitle() ? root.addChild(getPageConfig().getTitle()) : root.addChild("Project Main Page"));
+            return (root.hasChildren() ? root : root.addChild("Project Main Page"));
         }
     }
 
