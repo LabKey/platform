f261c875b2698a35a143ced6bcd751e0baf22d41
Adam Rauch <adam@labkey.com>
2019-12-31 08:53:27 -0800

    Issue 39330: Minor search improvements
    Use near-real-time search to make indexed documents visible to search queries more quickly
    
    Avoid redundant calls to clearLastIndexed()

--- search/src/org/labkey/search/model/WritableIndexManagerImpl.java
+++ search/src/org/labkey/search/model/WritableIndexManagerImpl.java
@@ -124,16 +124,19 @@ class WritableIndexManagerImpl extends IndexManager implements WritableIndexMana
     }
 
 
+    @Override
     public void index(String id, Document doc) throws IOException
     {
         synchronized (_writerLock)
         {
             deleteDocument(id);
             getIndexWriter().addDocument(doc);
+            _manager.maybeRefresh(); // Make this document immediately available for searching (i.e., near-real-time searching), see #39330
         }
     }
 
 
+    @Override
     public void deleteDocument(String id)
     {
         try
@@ -167,6 +170,7 @@ class WritableIndexManagerImpl extends IndexManager implements WritableIndexMana
         }
     }
 
+    @Override
     public void deleteQuery(Query query) throws IOException
     {
         try
@@ -192,24 +196,6 @@ class WritableIndexManagerImpl extends IndexManager implements WritableIndexMana
         }
     }
 
-
-    public void clear()
-    {
-        try
-        {
-            synchronized (_writerLock)
-            {
-                getIndexWriter().deleteAll();
-                commit();
-            }
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-
     @Override
     public void close() throws IOException
     {
@@ -223,6 +209,7 @@ class WritableIndexManagerImpl extends IndexManager implements WritableIndexMana
     }
 
     // If this throws then re-initialize the index manager
+    @Override
     public void commit()
     {
         synchronized (_writerLock)
