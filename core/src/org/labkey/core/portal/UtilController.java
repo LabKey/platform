4f6aa7140feb2f8b0b5c54229556ecae05203049
labkey-jeckels <jeckels@labkey.com>
2020-01-23 18:08:42 -0800

    Consolidate checks for POST and GET requests

--- core/src/org/labkey/core/portal/UtilController.java
+++ core/src/org/labkey/core/portal/UtilController.java
@@ -70,7 +70,7 @@ public class UtilController extends SpringActionController
         public ModelAndView getView(DotForm form, BindException errors) throws Exception
         {
             // Don't allow GET to avoid security holes as this may inject script
-            if (!"POST".equals(getViewContext().getRequest().getMethod()))
+            if (!isPost())
                 throw new UnauthorizedException("use POST");
 
             getPageConfig().setTemplate(PageConfig.Template.None);
