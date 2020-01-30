93ca3b0e47ebc5cf0156747bf1c226356eecbfd3
Binal <binalp@labkey.com>
2020-01-29 18:00:27 -0800

    ListDesigner.tsx entryPoint setup and initial wrapper for shared component.

--- list/src/client/ListDesigner/ListDesigner.tsx
+++ list/src/client/ListDesigner/ListDesigner.tsx
@@ -16,16 +16,97 @@
 import * as React from 'react'
 import {Panel} from "react-bootstrap";
 import {ActionURL, Security, Utils} from "@labkey/api";
-import {Alert, LoadingSpinner, PermissionTypes, DomainFieldsDisplay , fetchProtocol} from "@labkey/components";
+import {Alert, LoadingSpinner, PermissionTypes, DomainFieldsDisplay , fetchProtocol} from "@labkey/components"; //TODO: include List model and panel added in labkey-ui-components
 
 import "@labkey/components/dist/components.css"
 
 type State = {
-//TODO
+    listId: number,
+    returnUrl: string,
+    allowFileLinkProperties: 0,
+    allowAttachmentProperties: 1,
+    showDefaultValueSettings: 1,
+    hasDesignListPermission?: boolean,
+    isLoadingModel: boolean,
+    message?: string,
+    dirty: boolean,
+    // model?:  //TODO: define list model in labkey-ui-components
+
+    //TODO: Not sure if these are needed given hasDesignListPermission above, carried over from ListController.EditListDefinitionAction:
+    hasInsertPermission: boolean,
+    hasDeleteListPermission: boolean
 }
 
 export class App extends React.Component<any, State>
 {
-//TODO
+
+    render() {
+        const { isLoadingModel, hasDesignListPermission, message } = this.state;
+
+        if (message) {
+            return <Alert>{message}</Alert>
+        }
+
+        // set as loading until model is loaded and we know if the user has DesignListPerm
+        if (isLoadingModel || hasDesignListPermission === undefined) {
+            return <LoadingSpinner/>
+        }
+
+        // check if this is a create list case with a user that doesn't have permissions
+        // if (model.isNew() && !hasDesignListPerm) { //TODO: use this when ListModel is in place in labkey-ui-components
+        if (!hasDesignListPermission) {
+            return <Alert>You do not have sufficient permissions to create a new list design.</Alert>
+        }
+
+        return (
+            <>
+                {hasDesignListPermission
+                    ? this.renderDesignerView()
+                    : this.renderReadOnlyView()
+                }
+            </>
+        )
+    }
+
+    //TODO: revisit/uncomment once ListDesignerPanels is in place in labkey-ui-components
+
+    renderDesignerView() {
+        // const { model } = this.state;
+        //
+        // return (
+        //     <ListDesignerPanels
+        //         initModel={model}
+        //         onCancel={this.onCancel}
+        //         onComplete={this.onComplete}
+        //         onChange={this.onChange}
+        //         useTheme={true}
+        //     />
+        // )
+    }
+
+    renderReadOnlyView() {
+        //TODO: is this relevant for List?
+    }
+
+    onCancel = () => {
+        this.navigate(ActionURL.buildURL('project', 'begin', LABKEY.container.path));
+    };
+
+    //TODO revisit/uncomment once List Model is in place in labkey-ui-components
+
+    // onComplete = (model: ListModel) => {
+    //     this.navigate(ActionURL.buildURL('list', '??', LABKEY.container.path, {rowId: model.??}));
+    // };
+    //
+    // onChange = (model: ListModel) => {
+    //     this.setState(() => ({dirty: true}));
+    // };
+
+    navigate(defaultUrl: string) {
+        const { returnUrl } = this.state;
+
+        this.setState(() => ({dirty: false}), () => {
+            window.location.href = returnUrl || defaultUrl;
+        });
+    }
 }
-//TODO: Represent all the props mentioned in ListController.EditListDefinitionAction
