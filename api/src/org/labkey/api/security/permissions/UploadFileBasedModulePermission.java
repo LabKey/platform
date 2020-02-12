ce2feccdf959013ad4cbb496328e01c002c6f5f5
Matthew Bellew <matthewb@Labkey.com>
2020-02-12 12:37:28 -0800

    public

--- api/src/org/labkey/api/security/permissions/UploadFileBasedModulePermission.java
+++ api/src/org/labkey/api/security/permissions/UploadFileBasedModulePermission.java
@@ -2,7 +2,7 @@ package org.labkey.api.security.permissions;
 
 public class UploadFileBasedModulePermission extends AbstractSitePermission
 {
-    UploadFileBasedModulePermission()
+    public UploadFileBasedModulePermission()
     {
         super("Upload Module", "Allow updating file-based modules through the web-server user interface.");
     }
