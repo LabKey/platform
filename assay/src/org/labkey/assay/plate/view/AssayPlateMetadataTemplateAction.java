80db0f516d657ed0e1f2c07e79d5e85cb0dcb2ad
Kevin Krouse <kevink@labkey.com>
2020-02-15 11:13:39 -0800

    generate metadata example based on the selected plate template

--- assay/src/org/labkey/assay/plate/view/AssayPlateMetadataTemplateAction.java
+++ assay/src/org/labkey/assay/plate/view/AssayPlateMetadataTemplateAction.java
@@ -1,6 +1,9 @@
 package org.labkey.assay.plate.view;
 
+import org.json.JSONObject;
 import org.labkey.api.action.ExportAction;
+import org.labkey.api.assay.plate.PlateService;
+import org.labkey.api.assay.plate.PlateTemplate;
 import org.labkey.api.security.RequiresPermission;
 import org.labkey.api.security.permissions.ReadPermission;
 import org.labkey.api.util.PageFlowUtil;
@@ -8,37 +11,95 @@ import org.labkey.api.util.StringUtilsLabKey;
 import org.springframework.validation.BindException;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
 
 @RequiresPermission(ReadPermission.class)
-public class AssayPlateMetadataTemplateAction extends ExportAction
+public class AssayPlateMetadataTemplateAction extends ExportAction<AssayPlateMetadataTemplateAction.Form>
 {
-    @Override
-    public void export(Object o, HttpServletResponse response, BindException errors) throws Exception
+    public static class Form
     {
-        String template =
+        private String _template;
+
+        public String getTemplate()
+        {
+            return _template;
+        }
+
+        public void setTemplate(String template)
+        {
+            _template = template;
+        }
+    }
+
+    @Override
+    public void export(AssayPlateMetadataTemplateAction.Form form, HttpServletResponse response, BindException errors) throws Exception
+    {
+        String template = null;
+        if (form.getTemplate() != null)
+        {
+            PlateTemplate plateTemplate = PlateService.get().getPlateTemplateFromLsid(getContainer(), form.getTemplate());
+            if (plateTemplate != null)
+            {
+                Random rand = new Random();
+
+                JSONObject json = new JSONObject();
+                var wellGroupMap = plateTemplate.getWellGroupTemplateMap();
+                for (var typeGroupEntry : wellGroupMap.entrySet())
+                {
+                    var type = typeGroupEntry.getKey();
+                    var groups = typeGroupEntry.getValue();
+
+                    var layerJson = new LinkedHashMap<>();
+
+                    for (var name : groups.keySet())
+                    {
+                        // CONSIDER: create more realistic example data based on the group type.
+                        // CONSIDER: get the assay's template domain to create real prop values
+                        var propsJson = Map.of("dilution", String.format("%.2f", rand.nextDouble()));
+                        layerJson.put(name, propsJson);
+                    }
+
+                    json.put(type.name(), layerJson);
+                }
+
+                template = json.toString(2);
+            }
+        }
+
+        // create a generic template
+        if (template == null)
+        {
+            template = "" +
+                    "{\n" +
+                    "    -- each layer in the plate template can be matched by name\n" +
+                    "    -- with a JSON object, in this example both control and specimen\n" +
+                    "    -- are defined in the plate template that will be selected in\n" +
+                    "    -- the run properties during import\n" +
+                    "    \"control\" : {\n" +
+                    "      -- each well group in the layer can be matched by name\n" +
+                    "      -- with a JSON object, in this case POSITIVE and NEGATIVE well group names\n" +
+                    "      \"POSITIVE\" : {\"dilution\":  0.5},\n" +
+                    "      \"NEGATIVE\" : {\"dilution\":  5.0}\n" +
+                    "    },\n" +
+                    "    \"sample\" : {\n" +
+                    "      -- arbitrary properties can be associated with the \n" +
+                    "      -- well group, these will be imported into the results grid\n" +
+                    "      -- as columns for example : dilution and ID fields\n" +
+                    "      \"SA01\" : {\"dilution\": 1.0, \"ID\" : 111},\n" +
+                    "      \"SA02\" : {\"dilution\": 2.0, \"ID\" : 222},\n" +
+                    "      \"SA03\" : {\"dilution\": 3.0, \"ID\" : 333},\n" +
+                    "      \"SA04\" : {\"dilution\": 4.0, \"ID\" : 444}\n" +
+                    "    }\n" +
+                    "}";
+        }
+
+        String payload = "" +
                 "-- this is not valid JSON but is an example of the required structure\n" +
                 "-- of the plate metadata file contents (removing the comments will make it valid)\n" +
-                "{\n" +
-                "    -- each layer in the plate template can be matched by name\n" +
-                "    -- with a JSON object, in this example both control and specimen\n" +
-                "    -- are defined in the plate template that will be selected in\n" +
-                "    -- the run properties during import\n" +
-                "    \"control\" : {\n" +
-                "      -- each well group in the layer can be matched by name\n" +
-                "      -- with a JSON object, in this case POSITIVE and NEGATIVE well group names\n" +
-                "      \"POSITIVE\" : {\"dilution\":  0.5},\n" +
-                "      \"NEGATIVE\" : {\"dilution\":  5.0}\n" +
-                "    },\n" +
-                "    \"sample\" : {\n" +
-                "      -- arbitrary properties can be associated with the \n" +
-                "      -- well group, these will be imported into the results grid\n" +
-                "      -- as columns for example : dilution and ID fields\n" +
-                "      \"SA01\" : {\"dilution\": 1.0, \"ID\" : 111},\n" +
-                "      \"SA02\" : {\"dilution\": 2.0, \"ID\" : 222},\n" +
-                "      \"SA03\" : {\"dilution\": 3.0, \"ID\" : 333},\n" +
-                "      \"SA04\" : {\"dilution\": 4.0, \"ID\" : 444}\n" +
-                "    }\n" +
-                "}";
-        PageFlowUtil.streamFileBytes(getViewContext().getResponse(), "metadataTemplate.json", template.getBytes(StringUtilsLabKey.DEFAULT_CHARSET), true);
+                template;
+
+        PageFlowUtil.streamFileBytes(getViewContext().getResponse(), "metadataTemplate.json", payload.getBytes(StringUtilsLabKey.DEFAULT_CHARSET), true);
     }
 }
--- assay/src/org/labkey/assay/view/plateMetadataFileUpload.jsp
+++ assay/src/org/labkey/assay/view/plateMetadataFileUpload.jsp
@@ -27,13 +27,37 @@
     JspView<PlateMetadataDataCollector> me = (JspView<PlateMetadataDataCollector>) HttpView.currentView();
     PlateMetadataDataCollector<? extends AssayRunUploadContext<? extends AssayProvider>> bean = me.getModelBean();
     AssayProvider provider = bean.getContext().getProvider();
-    ActionURL plateUrl =urlProvider(AssayUrls.class).getPlateMetadataTemplateURL(getContainer(), provider);
+    ActionURL plateUrl = urlProvider(AssayUrls.class).getPlateMetadataTemplateURL(getContainer(), provider);
 %>
 
+<script>
+LABKEY.Utils.onReady(function () {
+    var plateTemplateEl;
+    var nl = document.getElementsByName('plateTemplate');
+    if (nl.length > 0)
+        plateTemplateEl = nl[0];
+
+    var plateMetadataExampleEl = document.getElementById('plateMetadataExample');
+
+    if (plateTemplateEl) {
+        var plateUrl = new URL(<%=q(plateUrl.toString())%>, LABKEY.ActionURL.getBaseURL());
+        plateTemplateEl.addEventListener('change', function () {
+            var templateLsid = plateTemplateEl.value;
+            if (templateLsid)
+                plateUrl.searchParams.set('template', templateLsid);
+            else
+                plateUrl.searchParams.delete('template');
+
+            console.log('updating url: ' + plateUrl.toString());
+            plateMetadataExampleEl.href = plateUrl.toString();
+        });
+    }
+});
+</script>
 <table id="plate-metadata-file-upload-tbl">
     <tr><td>Plate metadata should be uploaded as a specifically formatted JSON file.</td></tr>
     <% if (plateUrl != null) { %>
-        <tr><td><%= link("download example metadata file", plateUrl)%></td></tr>
+        <tr><td><%= link("download example metadata file", plateUrl).id("plateMetadataExample") %></td></tr>
     <% } %>
     <tr>
         <td><input type="file" name="<%=h(bean.getInputName())%>" size="40" style="border: none"></td>
