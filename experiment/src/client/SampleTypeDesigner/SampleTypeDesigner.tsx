1a6b196e4bfdd51cdf06a18e4508e33bb4ff9c9c
ian <ians@labkey.com>
2020-02-25 19:29:58 -0800

    Bump alpha components Fix some SampleTypeDesigner issues clean-up some commented out code

--- experiment/src/client/SampleTypeDesigner/SampleTypeDesigner.tsx
+++ experiment/src/client/SampleTypeDesigner/SampleTypeDesigner.tsx
@@ -16,30 +16,27 @@
 
 import {List} from "immutable";
 import * as React from 'react'
-import {Button, Panel} from "react-bootstrap";
 import {ActionURL} from "@labkey/api";
 import {
-    LoadingSpinner,
-    Alert,
-    ConfirmModal,
-    DomainForm,
-    DomainDesign,
-    fetchDomainDetails,
-    saveDomain,
     IBannerMessage,
-    getActionErrorMessage,
     SampleTypeDesigner,
     getHelpLink,
     LoadingPage,
-    NotFound,
     getSampleTypeDetails,
     SchemaQuery,
-    DomainDetails, SAMPLE_TYPE, IDomainField, User, hasAllPermissions, PermissionTypes, getSampleSet, initQueryGridState
+    DomainDetails,
+    SAMPLE_TYPE,
+    IDomainField,
+    User,
+    hasAllPermissions,
+    PermissionTypes,
+    getSampleSet,
+    initQueryGridState,
+    AppURL
 } from "@labkey/components"
 
 import "@labkey/components/dist/components.css"
 import "./sampleTypeDesigner.scss";
-import {SampleTypeModel} from "@labkey/components/dist/components/domainproperties/samples/models";
 
 interface IAppState {
     dirty: boolean
@@ -120,6 +117,12 @@ export class App extends React.PureComponent<any, Partial<IAppState>> {
                     }));
                 });
         }
+        else {
+            //Creating a new Sample Type
+            this.setState(()=>({
+                sampleType: DomainDetails.create()
+            }));
+        }
 
         window.addEventListener("beforeunload", this.handleWindowBeforeUnload);
     }
@@ -152,78 +155,15 @@ export class App extends React.PureComponent<any, Partial<IAppState>> {
         window.removeEventListener("beforeunload", this.handleWindowBeforeUnload);
     }
 
-    submitHandler = (navigate : boolean) => {
-        // const { domain, submitting, includeWarnings } = this.state;
-        //
-        // if (submitting) {
-        //     return;
-        // }
-        //
-        // this.setState(() => ({submitting: true}));
-        //
-        // saveDomain(domain, undefined, undefined, undefined,  includeWarnings)
-        //     .then((savedDomain) => {
-        //         this.setState(() => ({
-        //             domain: savedDomain,
-        //             submitting: false,
-        //             dirty: false
-        //         }));
-        //
-        //         this.showMessage("Save Successful", 'success', 0);
-        //
-        //         if (navigate) {
-        //             this.navigate();
-        //         }
-        //     })
-        //     .catch((badDomain) => {
-        //         // if there are only warnings then show ConfirmModel
-        //         if (badDomain.domainException.severity === "Warning") {
-        //             this.setState(() => ({
-        //                 showWarnings : true,
-        //                 badDomain: badDomain
-        //             }))
-        //         }
-        //         else {
-        //             this.setState(() => ({
-        //                 domain: badDomain,
-        //                 submitting: false
-        //             }));
-        //         }
-        //     });
-    };
-
-    submitAndNavigate = () => {
-        this.submitHandler(true);
-    };
-
-    confirmWarningAndNavigate = () => {
+    submitAndNavigate = (response: any) => {
         this.setState(() => ({
-            includeWarnings : false,
-            showWarnings : false,
-            submitting : false
-        }), () => {
-            this.submitHandler(true);
-        });
-    };
-
-    onSubmitWarningsCancel = () => {
-        this.setState(() => ({
-            showWarnings : false,
-            submitting : false
-        }))
-    };
-
-    onChangeHandler = (newDomain, dirty) => {
-        // this.setState((state) => ({
-        //     domain: newDomain,
-        //     dirty: state.dirty || dirty // if the state is already dirty, leave it as such
-        // }));
-    };
-
-    dismissAlert = (index: any) => {
-        this.setState(() => ({
-            messages: this.state.messages.setIn([index], [{message: undefined, messageType: undefined}])
+            submitting: false,
+            dirty: false
         }));
+
+        this.showMessage("Save Successful", 'success', 0);
+
+        this.navigate();
     };
 
     showMessage = (message: string, messageType: string, index: number, additionalState?: Partial<IAppState>) => {
@@ -235,12 +175,7 @@ export class App extends React.PureComponent<any, Partial<IAppState>> {
     };
 
     onCancelBtnHandler = () => {
-        if (this.state.dirty) {
-            this.setState(() => ({showConfirm: true}));
-        }
-        else {
-            this.navigate();
-        }
+        this.navigate();
     };
 
     navigate = () => {
@@ -250,95 +185,18 @@ export class App extends React.PureComponent<any, Partial<IAppState>> {
         });
     };
 
-    //TODO move out?
     userCanDesignSampleSets = (user: User): boolean => {
         return hasAllPermissions(user, [PermissionTypes.DesignSampleSet]);
     };
 
-    // renderNavigateConfirm() {
-    //     return (
-    //         <ConfirmModal
-    //             title='Keep unsaved changes?'
-    //             msg='You have made changes to this domain that have not yet been saved. Do you want to save these changes before leaving?'
-    //             confirmVariant='success'
-    //             onConfirm={this.submitAndNavigate}
-    //             onCancel={this.navigate}
-    //             cancelButtonText='No, Discard Changes'
-    //             confirmButtonText='Yes, Save Changes'
-    //         />
-    //     )
-    // }
-    //
-    // renderWarningConfirm() {
-    //     const { badDomain } = this.state;
-    //     const errors = badDomain.domainException.errors;
-    //     const question = <p> {"There are issues with the following fields that you may wish to resolve:"} </p>;
-    //     const warnings = errors.map((error) => {
-    //         return <li> {error.message} </li>
-    //     });
-    //
-    //     // TODO this doc link is specimen specific, we should find a way to pass this in via the domain kind or something like that
-    //     const rollupURI = LABKEY.helpLinkPrefix + 'specimenCustomProperties';
-    //     const suggestion = (
-    //         <p>
-    //             See the following documentation page for further details: <br/>
-    //             <a href={rollupURI} target={'_blank'}> {"Specimen properties and rollup rules"}</a>
-    //         </p>
-    //     );
-    //
-    //     return (
-    //         <ConfirmModal
-    //             title='Save without resolving issues?'
-    //             msg={
-    //                 <>
-    //                     {question}
-    //                     <ul>{warnings}</ul>
-    //                     {suggestion}
-    //                 </>
-    //             }
-    //             confirmVariant='success'
-    //             onConfirm={this.confirmWarningAndNavigate}
-    //             onCancel={this.onSubmitWarningsCancel}
-    //             cancelButtonText='No, edit and resolve issues'
-    //             confirmButtonText='Yes, save changes'
-    //         />
-    //     )
-    // }
-
-    // renderButtons() {
-    //     const { submitting } = this.state;
-    //
-    //     return (
-    //         <div className={'domain-form-panel domain-designer-buttons'}>
-    //             <Button onClick={this.onCancelBtnHandler}>Cancel</Button>
-    //             <Button className='pull-right' bsStyle='success' disabled={submitting} onClick={this.submitAndNavigate}>Save</Button>
-    //         </div>
-    //     )
-    // }
-    //
-    // renderInstructionsPanel() {
-    //     return (
-    //         <Panel>
-    //             <Panel.Heading>Instructions</Panel.Heading>
-    //             <Panel.Body>{this.state.domain.instructions}</Panel.Body>
-    //         </Panel>
-    //     )
-    // }
-
     beforeFinish = ():void => {}; //TODO may need something here...
 
     render() {
-        // const { domain, messages, showConfirm, showWarnings } = this.state;
-        // const isLoading = domain === undefined && messages === undefined;
-        //
-        // if (isLoading) {
-        //     return <LoadingSpinner/>
-        // }
-
-        const { sampleSetItem, menuLoading, user } = this.props;
-        const { sampleType } = this.state;
+        const {menuLoading} = this.props;
+        const {sampleType} = this.state;
         const subtitle = 'Edit Sample Type Details';
 
+        //TODO check permissions
         // if (!this.userCanDesignSampleSets(user)) {
         //     return <InsufficientPermissionsPage title={subtitle}/>
         // }
@@ -346,31 +204,9 @@ export class App extends React.PureComponent<any, Partial<IAppState>> {
         if (menuLoading) {
             return <LoadingPage title={subtitle}/>
         }
-        // else if (!sampleSetItem || sampleSetItem.get('id') === undefined) {
-        //     return <NotFound/>
-        // }
 
         return (
             <>
-                {/*{ showConfirm && this.renderNavigateConfirm() }*/}
-                {/*{ showWarnings && this.renderWarningConfirm() }*/}
-                {/*{ domain && domain.instructions && this.renderInstructionsPanel()}*/}
-                {/*{ domain &&*/}
-                {/*    <DomainForm*/}
-                {/*        headerTitle={'Fields'}*/}
-                {/*        domain={domain}*/}
-                {/*        onChange={this.onChangeHandler}*/}
-                {/*        useTheme={true}*/}
-                {/*    />*/}
-                {/*}*/}
-                {/*{ messages && messages.size > 0 && messages.map((bannerMessage, idx) => {*/}
-                {/*    return (<Alert key={idx} bsStyle={bannerMessage.messageType} onDismiss={() => this.dismissAlert(idx)}>{bannerMessage.message}</Alert>) })*/}
-                {/*}*/}
-                {/*{ domain && this.renderButtons() }*/}
-
-                {/*{hasError &&*/}
-                {/*<Alert>{getActionErrorMessage("There was a problem loading the sample type details.", 'sample type')}</Alert>*/}
-                {/*}*/}
                 {sampleType &&
                 <SampleTypeDesigner
                         initModel={sampleType}
