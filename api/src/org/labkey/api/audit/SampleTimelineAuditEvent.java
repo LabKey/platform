e6b8c8ffa490e4c526d2967039cc06ca98c07dd2
XingY <xyang@labkey.com>
2020-04-23 21:55:24 -0700

    code review changes

--- api/src/org/labkey/api/audit/SampleTimelineAuditEvent.java
+++ api/src/org/labkey/api/audit/SampleTimelineAuditEvent.java
@@ -1,6 +1,8 @@
 package org.labkey.api.audit;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.labkey.api.query.QueryService;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -9,23 +11,21 @@ public class SampleTimelineAuditEvent extends DetailedAuditTypeEvent
 {
     public static final String EVENT_TYPE = "SampleTimelineEvent";
 
+    public static final String SAMPLE_TIMELINE_EVENT_TYPE = "SampleTimelineEventType";
     public enum SampleTimelineEventType
     {
-        insert("Sample was registered.", null, "Registered"),
-        delete("Sample was deleted.", null, "Deleted"),
-        merge("Sample was registered or updated.", null, "Registered"),
-        update("Sample was updated.", false, "Updated"),
-        mergeWithLineageUpdate("Sample was registered or updated.", true, "Registered"),
-        lineageUpdate("Sample was updated.", true, "Updated");
+        INSERT("Sample was registered.", "Registered"),
+        DELETE("Sample was deleted.", "Deleted"),
+        TRUNCATE("Sample was deleted.", "Deleted"),
+        MERGE("Sample was registered or updated.", "Registered"),
+        UPDATE("Sample was updated.", "Updated");
 
         private String _comment;
-        private Boolean _lineageUpdate;
         private String _actionLabel;
 
-        SampleTimelineEventType(String comment, Boolean isLineageUpdate, String actionLabel)
+        SampleTimelineEventType(String comment, String actionLabel)
         {
             _comment = comment;
-            _lineageUpdate = isLineageUpdate;
             _actionLabel = actionLabel;
         }
 
@@ -34,31 +34,37 @@ public class SampleTimelineAuditEvent extends DetailedAuditTypeEvent
             return _comment;
         }
 
-        public Boolean getLineageUpdate()
-        {
-            return _lineageUpdate;
-        }
-
         public String getActionLabel()
         {
             return _actionLabel;
         }
 
-        public static SampleTimelineEventType getTypeFromEvent(@NotNull SampleTimelineAuditEvent event)
+        public static String getActionCommentDetailed(@NotNull QueryService.AuditAction action)
         {
+            SampleTimelineEventType type = getTypeFromAction(action);
+            return type == null ? null : type.getComment();
+        }
+
+        public static SampleTimelineEventType getTypeFromAction(@NotNull QueryService.AuditAction action)
+        {
+            return getTypeFromName(action.name());
+        }
+
+        public static SampleTimelineEventType getTypeFromName(@Nullable String name)
+        {
+            if (null == name)
+                return null;
+
             for (SampleTimelineEventType type : SampleTimelineEventType.values())
             {
-                if (event.getComment().equals(type.getComment()))
+                if (type.name().equalsIgnoreCase(name))
                 {
-                    if (type.getLineageUpdate() == null)
-                        return type;
-
-                    if (type.getLineageUpdate().equals(event.getIsLineageUpdate()))
-                        return type;
+                    return type;
                 }
             }
             return null;
         }
+
     }
 
     private String _sampleLsid;
