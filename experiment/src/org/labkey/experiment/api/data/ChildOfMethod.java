fed75edeac8997a1c34663015c5b0807286d9487
XingY <xyang@labkey.com>
2020-04-30 17:36:53 -0700

    Issue 40329: Sample Manager source samples: limit to 1st generation of samples only

--- experiment/src/org/labkey/experiment/api/data/ChildOfMethod.java
+++ experiment/src/org/labkey/experiment/api/data/ChildOfMethod.java
@@ -6,12 +6,13 @@ import org.labkey.api.data.dialect.SqlDialect;
 import org.labkey.api.query.AbstractMethodInfo;
 
 /**
+ * method takes 3 params: fieldKey, lsid, depth (optional)
  * <code>
  * SELECT
  *   d.*
  * FROM assay.General.MyAssay.Data d
  * WHERE
-    ExpChildOf(d.resultsample.LSID, 'urn:lsid:labkey.com:Data.Folder-123:1aec9396-3fa2-1038-86f4-495e1672e522') OR ExpChildOf(d.Run.runsample.LSID, 'urn:lsid:labkey.com:Data.Folder-123:1aec9396-3fa2-1038-86f4-495e1672e522')
+    ExpChildOf(d.resultsample.LSID, 'urn:lsid:labkey.com:Data.Folder-123:1aec9396-3fa2-1038-86f4-495e1672e522', 1) OR ExpChildOf(d.Run.runsample.LSID, 'urn:lsid:labkey.com:Data.Folder-123:1aec9396-3fa2-1038-86f4-495e1672e522', 0)
  * </code>
  */
 
@@ -29,7 +30,11 @@ public class ChildOfMethod extends AbstractMethodInfo
     {
         SQLFragment fieldKeyFrag = arguments[0];
         SQLFragment lsidFrag = arguments[1];
-        return LineageHelper.createInSQL(fieldKeyFrag, lsidFrag, LineageHelper.createChildOfOptions());
+        int depth = 0;
+        if (arguments.length > 2)
+            depth = Integer.parseInt(arguments[2].getRawSQL());
+
+        return LineageHelper.createInSQL(fieldKeyFrag, lsidFrag, LineageHelper.createChildOfOptions(depth));
     }
 
 }
---
+++ experiment/src/org/labkey/experiment/api/data/LineageCompareType.java
@@ -0,0 +1,68 @@
+package org.labkey.experiment.api.data;
+
+import org.jetbrains.annotations.NotNull;
+import org.labkey.api.data.ColumnRenderProperties;
+import org.labkey.api.data.CompareType;
+import org.labkey.api.data.SimpleFilter;
+import org.labkey.api.query.FieldKey;
+import org.labkey.data.xml.queryCustomView.OperatorType;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * <code>
+ * Filter.create('lsid', '{json:}', Filter.Types.EXP_LINEAGE_OF)
+ * </code>
+ */
+public class LineageCompareType extends CompareType
+{
+    public static final String SEPARATOR = ",";
+
+    public LineageCompareType()
+    {
+        super("In The Lineage Of", "exp:lineageof", "EXP_LINEAGE_OF", true, " in the lineage of", OperatorType.EXP_LINEAGEOF);
+    }
+
+    @Override
+    protected SimpleFilter.FilterClause createFilterClause(@NotNull FieldKey fieldKey, Object value)
+    {
+        Object[] values;
+        Object collection;
+        if (value instanceof Collection)
+        {
+            collection = value;
+            values = ((Collection) value).toArray();
+        }
+        else
+        {
+            Set<String> params = parseParams(value, getValueSeparator());
+            collection = params;
+            values = params.toArray();
+        }
+
+        String lsid = (String) values[0];
+        int depth = 0;
+        if (values.length > 1)
+        {
+            Object depthObj = values[1];
+            if (depthObj instanceof String)
+                depth = Integer.parseInt((String) depthObj);
+            else if (depthObj instanceof Integer)
+                depth = (Integer) depthObj;
+        }
+        return new LineageClause(fieldKey, collection, lsid, depth);
+    }
+
+    @Override
+    public String getValueSeparator()
+    {
+        return SEPARATOR;
+    }
+
+    @Override
+    public boolean meetsCriteria(ColumnRenderProperties col, Object value, Object[] paramVals)
+    {
+        throw new UnsupportedOperationException("Conditional formatting not yet supported for EXP_LINEAGE_OF");
+    }
+}
