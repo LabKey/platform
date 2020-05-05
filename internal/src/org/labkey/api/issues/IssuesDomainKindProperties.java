835684668a1ea02e2393cdaf7c3b3c65fb79b137
RosalineP <rosalinep@labkey.com>
2020-05-05 10:41:19 -0700

    7078: Issue Designer Server Side API Updates (#1105)

---
+++ internal/src/org/labkey/api/issues/IssuesDomainKindProperties.java
@@ -0,0 +1,85 @@
+package org.labkey.api.issues;
+
+public class IssuesDomainKindProperties
+{
+    private String _issueDefName;
+    private String _singularItemName;
+    private String _pluralItemName;
+    private String _commentSortDirection;
+
+    private Integer _assignedToGroup;
+    private Integer _assignedToUser;
+
+    public IssuesDomainKindProperties()
+    {}
+
+    public IssuesDomainKindProperties(String name, String singularName, String pluralName, String commentSortDirection, Integer assignedToGroup, Integer assignedToUser)
+    {
+        _issueDefName = name;
+        _singularItemName = singularName;
+        _pluralItemName = pluralName;
+        _commentSortDirection = commentSortDirection;
+        _assignedToGroup = assignedToGroup;
+        _assignedToUser = assignedToUser;
+    }
+
+    public String getIssueDefName()
+    {
+        return _issueDefName;
+    }
+
+    public void setIssueDefName(String issueDefName)
+    {
+        _issueDefName = issueDefName;
+    }
+
+    public String getSingularItemName()
+    {
+        return _singularItemName;
+    }
+
+    public void setSingularItemName(String singularItemName)
+    {
+        _singularItemName = singularItemName;
+    }
+
+    public String getPluralItemName()
+    {
+        return _pluralItemName;
+    }
+
+    public void setPluralItemName(String pluralItemName)
+    {
+        _pluralItemName = pluralItemName;
+    }
+
+    public String getCommentSortDirection()
+    {
+        return _commentSortDirection;
+    }
+
+    public void setCommentSortDirection(String commentSortDirection)
+    {
+        _commentSortDirection = commentSortDirection;
+    }
+
+    public Integer getAssignedToGroup()
+    {
+        return _assignedToGroup;
+    }
+
+    public void setAssignedToGroup(Integer assignedToGroup)
+    {
+        _assignedToGroup = assignedToGroup;
+    }
+
+    public Integer getAssignedToUser()
+    {
+        return _assignedToUser;
+    }
+
+    public void setAssignedToUser(Integer assignedToUser)
+    {
+        _assignedToUser = assignedToUser;
+    }
+}
