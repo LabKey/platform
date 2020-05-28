01e8608ca83a1ed402f0c124b707de3ef65be007
Rosaline Pyktel <rosalinep@labkey.com>
2020-05-28 13:04:31 -0700

    Make editable

--- study/src/org/labkey/study/query/VisitMapTable.java
+++ study/src/org/labkey/study/query/VisitMapTable.java
@@ -1,5 +1,6 @@
 package org.labkey.study.query;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.labkey.api.data.ContainerFilter;
 import org.labkey.api.data.JdbcType;
@@ -11,6 +12,8 @@ import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.FilteredTable;
 import org.labkey.api.query.LookupForeignKey;
 import org.labkey.api.query.QueryUpdateService;
+import org.labkey.api.security.UserPrincipal;
+import org.labkey.api.security.permissions.Permission;
 import org.labkey.study.StudySchema;
 
 public class VisitMapTable extends BaseStudyTable
@@ -47,14 +50,20 @@ public class VisitMapTable extends BaseStudyTable
         addColumn(datasetLookupCol);
     }
 
-//    static class VisitMapService extends DefaultQueryUpdateService
-//    {
-//        public VisitMapService(FilteredTable table) { super(table, table.getRealTable()); }
-//    }
-//
-//    @Override
-//    public @Nullable QueryUpdateService getUpdateService()
-//    {
-//        return new VisitMapService(this);
-//    }
+    @Override
+    public boolean hasPermission(@NotNull UserPrincipal user, @NotNull Class<? extends Permission> perm)
+    {
+        return getContainer().hasPermission(user, perm);
+    }
+
+    static class VisitMapService extends DefaultQueryUpdateService
+    {
+        public VisitMapService(FilteredTable table) { super(table, table.getRealTable()); }
+    }
+
+    @Override
+    public @Nullable QueryUpdateService getUpdateService()
+    {
+        return new VisitMapService(this);
+    }
 }
