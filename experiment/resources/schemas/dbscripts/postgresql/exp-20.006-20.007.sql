48272eab2908cfe5172c39c5d6822416ef2045b7
labkey-martyp <martyp@labkey.com>
2020-06-13 15:48:54 -0700

    Missed column on pg index

--- experiment/resources/schemas/dbscripts/postgresql/exp-20.006-20.007.sql
+++ experiment/resources/schemas/dbscripts/postgresql/exp-20.006-20.007.sql
@@ -2,5 +2,5 @@
 SELECT core.fn_dropifexists('Data', 'exp', 'INDEX', 'IX_Data_Container');
 SELECT core.fn_dropifexists('Data', 'exp', 'INDEX', 'IX_Data_SourceApplicationId');
 
-CREATE INDEX IX_Data_Container_LSID_Name_ClassId_Modified ON exp.Data (Container, LSID, Name, classId, Modified);
+CREATE INDEX IX_Data_Container_LSID_Name_ClassId_Modified_RowId ON exp.Data (Container, LSID, Name, classId, Modified, RowId);
 CREATE INDEX IX_Data_SourceApplicationId_RowId ON exp.Data (SourceApplicationId, RowId);
