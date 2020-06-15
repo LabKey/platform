119448db37b067a9617e306b948b55152b3f5bf8
labkey-martyp <martyp@labkey.com>
2020-06-15 09:40:51 -0700

    Move classId sooner in index

--- experiment/resources/schemas/dbscripts/postgresql/exp-20.006-20.007.sql
+++ experiment/resources/schemas/dbscripts/postgresql/exp-20.006-20.007.sql
@@ -2,5 +2,5 @@
 SELECT core.fn_dropifexists('Data', 'exp', 'INDEX', 'IX_Data_Container');
 SELECT core.fn_dropifexists('Data', 'exp', 'INDEX', 'IX_Data_SourceApplicationId');
 
-CREATE INDEX IX_Data_Container_LSID_Name_ClassId_Modified_RowId ON exp.Data (Container, LSID, Name, classId, Modified, RowId);
+CREATE INDEX IX_Data_Container_ClassId_LSID_Name_Modified_RowId ON exp.Data (Container, classId, LSID, Name,  Modified, RowId);
 CREATE INDEX IX_Data_SourceApplicationId_RowId ON exp.Data (SourceApplicationId, RowId);
--- experiment/resources/schemas/dbscripts/sqlserver/exp-20.006-20.007.sql
+++ experiment/resources/schemas/dbscripts/sqlserver/exp-20.006-20.007.sql
@@ -6,5 +6,5 @@ GO
 EXEC core.fn_dropifexists 'Data', 'exp', 'INDEX', 'IX_Data_SourceApplicationId';
 GO
 
-CREATE INDEX IX_Data_Container_LSID_Name_ClassId_Modified_RowId ON exp.Data (Container, LSID, Name, classId, Modified, RowId);
+CREATE INDEX IX_Data_Container_ClassId_LSID_Name_Modified_RowId ON exp.Data (Container, classId, LSID, Name,  Modified, RowId);
 CREATE INDEX IX_Data_SourceApplicationId_RowId ON exp.Data (SourceApplicationId, RowId);
