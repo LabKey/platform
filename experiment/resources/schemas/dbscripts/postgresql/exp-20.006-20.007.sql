6804be655be0ba28407704496ecfbcda184d2565
labkey-martyp <martyp@labkey.com>
2020-06-17 08:09:28 -0700

    Remove upgrade scripts

--- experiment/resources/schemas/dbscripts/postgresql/exp-20.006-20.007.sql
+++
@@ -1,4 +0,0 @@
--- Rebuilding Container index with more columns
-SELECT core.fn_dropifexists('Data', 'exp', 'INDEX', 'IX_Data_Container');
-
-CREATE INDEX IX_Data_Container_ClassId_LSID_Name_Modified_RowId ON exp.Data (Container, classId, LSID, Name,  Modified, RowId);
