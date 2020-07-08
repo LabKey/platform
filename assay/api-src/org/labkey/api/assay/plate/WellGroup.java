c765171f70ee5afebe4cbd098ab826ce35c104cb
Susan Hert <susanh@labkey.com>
2020-07-08 14:32:10 -0700

    Use stringUtils from commons-lang3 instead of springframework and remove some unused imports (#1374)

--- assay/api-src/org/labkey/api/assay/plate/WellGroup.java
+++ assay/api-src/org/labkey/api/assay/plate/WellGroup.java
@@ -16,10 +16,6 @@
 
 package org.labkey.api.assay.plate;
 
-import org.jetbrains.annotations.Nullable;
-import org.labkey.api.util.PageFlowUtil;
-import org.labkey.api.view.ActionURL;
-
 import java.util.List;
 import java.util.Set;
 
