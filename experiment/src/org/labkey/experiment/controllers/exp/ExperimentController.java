7c7e4ac68ab9c44d83357af93d1c9a6e3559542d
cnathe <cnathe@labkey.com>
2020-07-13 08:52:06 -0500

    Hide the labelColor field from the default visible list on the sample type overview details webpart

--- experiment/src/org/labkey/experiment/controllers/exp/ExperimentController.java
+++ experiment/src/org/labkey/experiment/controllers/exp/ExperimentController.java
@@ -610,6 +610,7 @@ public class ExperimentController extends SpringActionController
             detailsView.getDataRegion().getDisplayColumn("Name").setURL(null);
             detailsView.getDataRegion().getDisplayColumn("LSID").setVisible(false);
             detailsView.getDataRegion().getDisplayColumn("MaterialLSIDPrefix").setVisible(false);
+            detailsView.getDataRegion().getDisplayColumn("LabelColor").setVisible(false);
             detailsView.setTitle("Sample Type Properties");
             detailsView.getDataRegion().getButtonBar(DataRegion.MODE_DETAILS).setStyle(ButtonBar.Style.separateButtons);
 
