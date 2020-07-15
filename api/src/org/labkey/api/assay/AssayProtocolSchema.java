720db3ae75abd024a1b01cbc27c89f4e143534ed
labkey-martyp <martyp@labkey.com>
2020-07-14 22:21:10 -0700

    Update AssayProtocolSchema.java

--- api/src/org/labkey/api/assay/AssayProtocolSchema.java
+++ api/src/org/labkey/api/assay/AssayProtocolSchema.java
@@ -705,7 +705,7 @@ public abstract class AssayProtocolSchema extends AssaySchema
                                 if (null != r)
                                 {
                                     // In order to get size must first iterate through result set
-                                    r.iterateAll() ;
+                                    while(r.next());
 
                                     final int rowCount = r.getSize();
                                     baseQueryView.setMessageSupplier(dataRegion -> {
