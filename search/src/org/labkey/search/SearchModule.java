b2ec06e52d5293b96017821a1abe761f9a409372
Matthew Bellew <matthewb@labkey.com>
2020-08-21 09:55:47 -0700

    remove umls schema

--- search/src/org/labkey/search/SearchModule.java
+++ search/src/org/labkey/search/SearchModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2019 LabKey Corporation
+ * Copyright (c) 2009-2020 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,7 +65,7 @@ public class SearchModule extends DefaultModule
     @Override
     public Double getSchemaVersion()
     {
-        return 20.001;
+        return 20.002;
     }
 
     @Override
