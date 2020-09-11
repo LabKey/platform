8fb24b145e2cd578ffee1cc00f913ce35b271c22
Matthew Bellew <matthewb@labkey.com>
2020-09-11 16:19:55 -0700

    ValidateQueryAction check getPrimaryConceptCode()

--- api/src/org/labkey/api/data/BaseColumnInfo.java
+++ api/src/org/labkey/api/data/BaseColumnInfo.java
@@ -393,6 +393,8 @@ public class BaseColumnInfo extends ColumnRenderPropertiesImpl implements Mutabl
         setCrosstabColumnMember(col.getCrosstabColumnMember());
 
         setCalculated(col.isCalculated());
+
+        setPrincipalConceptCode(col.getPrincipalConceptCode());
     }
 
     /*
@@ -465,6 +467,8 @@ public class BaseColumnInfo extends ColumnRenderPropertiesImpl implements Mutabl
         setCrosstabColumnMember(col.getCrosstabColumnMember());
 
         setCalculated(col.isCalculated());
+
+        setPrincipalConceptCode(col.getPrincipalConceptCode());
     }
 
 
