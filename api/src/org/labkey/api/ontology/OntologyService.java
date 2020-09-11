371a8bff8463a5d9e5500574ba886adfd2eab8f5
labkey-matthewb <matthewb@labkey.com>
2020-09-11 15:35:49 -0700

    remove umls 41124 (#1527)
    remove UMLS code and schema from search module
    issue 41124

---
+++ api/src/org/labkey/api/ontology/OntologyService.java
@@ -0,0 +1,28 @@
+package org.labkey.api.ontology;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.labkey.api.data.Container;
+import org.labkey.api.services.ServiceRegistry;
+
+import java.util.List;
+
+/** public interface to ontology services, largely implemented by OntologyManager */
+public interface OntologyService
+{
+    void registerProvider(OntologyProvider provider);
+
+    default List<Ontology> getOntologies(Container c)
+    {
+        throw new NotImplementedException("todo");
+    }
+
+    static OntologyService get()
+    {
+        return ServiceRegistry.get().getService(OntologyService.class);
+    }
+
+    static void setInstance(OntologyService impl)
+    {
+        ServiceRegistry.get().registerService(OntologyService.class, impl);
+    }
+}
