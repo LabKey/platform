6632f67cdf298e0510bf24dc9da69dd4466d30a3
labkey-nicka <nickk@labkey.com>
2020-09-18 21:57:14 -0700

    Utilize @labkey/themes

--- core/resources/styles/js/ext3.js
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) 2017-2019 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-import '../scss/lib/ext3/_ext3.scss';
