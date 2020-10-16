46e415a6d354b1139da32123875fc2d73e472aa0
labkey-martyp <martyp@labkey.com>
2020-10-16 08:08:12 -0700

    Update Sas metadata decimal digits key

--- bigiron/src/org/labkey/bigiron/sas/Sas92Dialect.java
+++ bigiron/src/org/labkey/bigiron/sas/Sas92Dialect.java
@@ -49,7 +49,7 @@ public class Sas92Dialect extends SasDialect
             _nameKey = "COLUMN_NAME";
             _sqlTypeKey = "DATA_TYPE";
             _scaleKey = "COLUMN_SIZE";
-            _decimalDigitsKey = "DECIMAL_DIGITS";
+            _decimalDigitsKey = "SCALE";
             _nullableKey = "NULLABLE";
             _postionKey = "ORDINAL_POSITION";
         }
