aade9249d33e6333b4cdf2fd5fda6d0ef890b076
labkey-susanh <susanh@labkey.com>
2020-11-02 13:41:50 -0800

    Handle multiple timeline events from one row import.  Fix conversion from base-26 to integer.  More error checking

--- api/src/org/labkey/api/inventory/InventoryService.java
+++ api/src/org/labkey/api/inventory/InventoryService.java
@@ -52,5 +52,5 @@ public interface InventoryService
 
     List<FieldKey> addInventoryStatusColumns(@Nullable String sampleTypeMetricUnit, ExpMaterialTable table, Container container);
 
-    DataIteratorBuilder getPersistSampleDataIteratorBuilder(DataIteratorBuilder data, Container container, User user);
+    DataIteratorBuilder getStorageDataIteratorBuilder(DataIteratorBuilder data, Container container, User user);
 }
