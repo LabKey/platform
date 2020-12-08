4a11d8580ddf68b3d6b8ffe7b0b03b3b95bd7901
Nick Kerr <nicka@labkey.com>
2020-12-08 09:23:44 -0800

    41986: Remove experimental feature to use clientapi_core.lib.xml (#1760)

--- api/webapp/clientapi/dom/Utils.js
+++ api/webapp/clientapi/dom/Utils.js
@@ -131,7 +131,18 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation available in core/Utils.js -- search for "@name displayAjaxErrorResponse"
+     * Shows an error dialog box to the user in response to an error from an AJAX request, including
+     * any error messages from the server.
+     *
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name displayAjaxErrorResponse
+     * @param {XMLHttpRequest} responseObj The XMLHttpRequest object containing the response data.
+     * @param {Error} [exceptionObj] A JavaScript Error object caught by the calling code.
+     * @param {boolean} [showExceptionClass] Flag to display the java class of the exception.
+     * @param {String} [msgPrefix] Prefix to the error message (defaults to: 'An error occurred trying to load:')
+     * The error dialog will display the Error's name and message, if available.
      */
     impl.displayAjaxErrorResponse = function(responseObj, exceptionObj, showExceptionClass, msgPrefix)
     {
@@ -150,7 +161,55 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation available in core/Utils.js -- search for "@name convertToExcel"
+     * Sends a JSON object to the server which turns it into an Excel file and returns it to the browser to be saved or opened.
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name convertToExcel
+     * @param {Object} spreadsheet the JavaScript representation of the data
+     * @param {String} spreadsheet.fileName name to suggest to the browser for saving the file. If the fileName is
+     * specified and ends with ".xlsx", it will be returned in Excel 2007 format.
+     * @param {String} spreadsheet.sheets array of sheets, which are objects with properties:
+     * <ul>
+     * <li><b>name:</b> name of the Excel sheet</li>
+     * <li><b>data:</b> two dimensional array of values</li>
+     * </ul>
+     * The value array may be either primitives (booleans, numbers, Strings, and dates), or may be a map with
+     * the following structure:
+     * <ul>
+     * <li><b>value:</b> the boolean, number, String, or date value of the cell</li>
+     * <li><b>formatString:</b> for dates and numbers, the Java format string used with SimpleDateFormat
+     * or DecimalFormat to control how the value is formatted</li>
+     * <li><b>timeOnly:</b> for dates, whether the date part should be ignored and only the time value is important</li>
+     * <li><b>forceString:</b> force the value to be treated as a string (i.e. prevent attempt to convert it to a date)</li>
+     * </ul>
+     * @example &lt;script type="text/javascript"&gt;
+     LABKEY.Utils.convertToExcel(
+     {
+         fileName: 'output.xls',
+         sheets:
+         [
+             {
+                 name: 'FirstSheet',
+                 data:
+                 [
+                     ['Row1Col1', 'Row1Col2'],
+                     ['Row2Col1', 'Row2Col2']
+                 ]
+             },
+             {
+                 name: 'SecondSheet',
+                 data:
+                 [
+                     ['Col1Header', 'Col2Header'],
+                     [{value: 1000.5, formatString: '0,000.00'}, {value: '5 Mar 2009 05:14:17', formatString: 'yyyy MMM dd'}],
+                     [{value: 2000.6, formatString: '0,000.00'}, {value: '6 Mar 2009 07:17:10', formatString: 'yyyy MMM dd'}]
+
+                 ]
+             }
+         ]
+     });
+     &lt;/script&gt;
      */
     impl.convertToExcel = function(spreadsheet) {
         var formData = { 'json': JSON.stringify(spreadsheet) };
@@ -158,7 +217,27 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation available in core/Utils.js -- search for "@name convertToTable"
+     * Sends a JSON object to the server which turns it into an TSV or CSV file and returns it to the browser to be saved or opened.
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name convertToTable
+     * @param {Object} config.  The config object
+     * @param {String} config.fileNamePrefix name to suggest to the browser for saving the file. The appropriate extension (either ".txt" or ".csv", will be appended based on the delim character used (see below).  Defaults to 'Export'
+     * @param {String} config.delim The separator between fields.  Allowable values are 'COMMA' or 'TAB'.
+     * @param {String} config.quoteChar The character that will be used to quote each field.  Allowable values are 'DOUBLE' (ie. double-quote character), 'SINGLE' (ie. single-quote character) or 'NONE' (ie. no character used).  Defaults to none.
+     * @param {String} config.newlineChar The character that will be used to separate each line.  Defaults to '\n'
+     * @param {String} config.rows array of rows, which are arrays with values for each cell.
+     * @example &lt;script type="text/javascript"&gt;
+     LABKEY.Utils.convertToTable({
+         fileNamePrefix: 'output',
+         rows: [
+             ['Row1Col1', 'Row1Col2'],
+             ['Row2Col1', 'Row2Col2']
+         ],
+         delim: 'COMMA'
+     });
+     &lt;/script&gt;
      */
     impl.convertToTable = function(config) {
         var formData = { 'json': JSON.stringify(config) };
@@ -166,14 +245,27 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation available in core/Util.js -- search for "@name postToAction"
+     * POSTs to the given href, including CSRF token. Taken from PageFlowUtil.postOnClickJavascript .
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name postToAction
+     * @param {String} href containing action and parameters to be POSTed.
+     * @param {Object} formData values to include on the hidden form
      */
     impl.postToAction = function (href, formData) {
         formSubmit(href, formData);
     };
 
     /**
-     * Documentation available in core/Util.js -- search for "@name confirmAndPost"
+     * Displays a confirmation dialog with the specified message and then, if confirmed, POSTs to the href, using the method above
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name confirmAndPost
+     * @param {String} message confirmation message to display.
+     * @param {String} href containing action and parameters to be POSTed.
+     * @param {Object} formData values to include on the hidden form
      */
     impl.confirmAndPost = function (message, href, formData) {
         if (confirm(message))
@@ -183,7 +275,13 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation specified in core/Utils.js -- search for "@name alert"
+     * Display an error dialog.
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name alert
+     * @param title
+     * @param msg
      */
     impl.alert = function(title, msg) {
         if (window.Ext4) {
@@ -198,14 +296,66 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation specified in core/Utils.js -- search for "@name modal"
+     * Display a modal dialog.
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name modal
+     * @param title Title of the modal dialog.
+     * @param msg Message to be included in the dialog body. Can be null if the function generates its own content.
+     * @param fn {function} This will be called with the provided argument list {args} after the modal is shown. You can generate content in
+     * the modal via the following empty div: &lt;div id="modal-fn-body">&lt;/div>
+     * @param args {array} Array of arguments to be applied to the function when it is called.
+     * @param disableBackdrop {boolean} True to disable closing the modal on background click.
+     *
+     * @example &lt;script type="text/javascript"&gt;
+     *
+     * var myFN = function(arg1) {
+     *     document.getElementById('modal-fn-body').innerHTML = "Hello " + LABKEY.Security.currentUser[arg1] + "!";
+     * }
+     * LABKEY.Utils.modal("Hello", null, myFN, ["displayName"]);
+     * &lt;/script&gt;
      */
     impl.modal = function(title, msg, fn, args, disableBackdrop) {
       displayModal(title, msg, fn, args, disableBackdrop);
     };
 
     /**
-     * Documentation specified in core/Utils.js -- search for "@name onError"
+     * Provides a generic error callback.  This helper show a modal dialog, log the error to the console
+     * and will log the error to the audit log table. The user must have insert permissions on the selected container for
+     * this to work.  By default, it will insert the error into the Shared project.  A containerPath param can be passed to
+     * use a different container.  The intent of this helper is to provide site admins with a mechanism to identify errors associated
+     * with client-side code.  If noAuditLog=true is used, the helper will not log the error.
+     *
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name onError
+     * @param {Object} error The error object passed to the callback function
+     * @param {String} [error.containerPath] Container where errors will be logged. Defaults to /shared
+     * @param {Boolean} [error.noAuditLog] If false, the errors will not be logged in the audit table.  Defaults to true
+     *
+     * @example &lt;script type="text/javascript"&gt;
+     //basic usage
+     LABKEY.Query.selectRows({
+                schemaName: 'core',
+                queryName: 'users',
+                success: function(){},
+                failure: LABKEY.Utils.onError
+            });
+
+     //custom container and turning off logging
+     LABKEY.Query.selectRows({
+                schemaName: 'core',
+                queryName: 'users',
+                success: function(){},
+                failure: function(error){
+                     error.containerPath = 'myContainer';
+                     error.noAuditLog = true;
+                     LABKEY.Utils.onError(error);
+                }
+            });
+     &lt;/script&gt;
      */
     impl.onError = function(error) {
 
@@ -241,7 +391,13 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation specified in core/Utils.js -- search for "@name setWebpartTitle"
+     * Sets the title of the webpart on the page.  This change is not sticky, so it will be reverted on refresh.
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name setWebpartTitle
+     * @param {string} title The title string
+     * @param {integer} webPartId The ID of the webpart
      */
     impl.setWebpartTitle = function(title, webPartId)
     {
@@ -249,7 +405,31 @@ LABKEY.Utils = new function(impl, $) {
     };
 
     /**
-     * Documentation specified in core/Utils.js -- search for "@name onReady"
+     * Adds new listener to be executed when all required scripts are fully loaded.
+     * @memberOf LABKEY.Utils
+     * @function
+     * @static
+     * @name onReady
+     * @param {Mixed} config Either a callback function, or an object with the following properties:
+     *
+     * <li>callback (required) A function that will be called when required scripts are loaded.</li>
+     * <li>scope (optional) The scope to be used for the callback function.  Defaults to the current scope.</li>
+     * <li>scripts (optional) A string with a single script or an array of script names to load.  This will be passed to LABKEY.requiresScript().</li>
+     * @example &lt;script type="text/javascript"&gt;
+     //simple usage
+     LABKEY.onReady(function(){
+                    //your code here.  will be executed once scripts have loaded
+                });
+
+     //
+     LABKEY.Utils.onReady({
+                    scope: this,
+                    scripts: ['/myModule/myScript.js', 'AnotherScript.js],
+                    callback: function(){
+                        //your code here.  will be executed once scripts have loaded
+                    });
+                });
+     &lt;/script&gt;
      */
     impl.onReady = function(config)
     {
--- core/script-fragments/labkey/Utils.header.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2011-2012 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-
-var LABKEY = require("./init");
-LABKEY.ActionURL = require("./ActionURL").ActionURL;
-LABKEY.ExtAdapter = require("ExtAdapter").Adapter;
