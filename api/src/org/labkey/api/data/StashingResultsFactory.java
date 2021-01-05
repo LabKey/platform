57f8fb7ab1d76c5338b2f06e9f510d0393391d3d
Adam Rauch <adam@labkey.com>
2021-01-05 15:41:50 -0800

    Document StashingResultsFactory

--- api/src/org/labkey/api/data/StashingResultsFactory.java
+++ api/src/org/labkey/api/data/StashingResultsFactory.java
@@ -2,6 +2,15 @@ package org.labkey.api.data;
 
 import java.sql.SQLException;
 
+/**
+ * <p>A {@link ResultsFactory} wrapper that stashes the {@link Results} on first reference and ensures the Results gets
+ * closed. Always use try-with-resources when creating an instance of this class.</p>
+ *
+ * <p>This class is useful for cases where code needs to inspect Results before invoking a class that takes a ResultsFactory.
+ * For example, code may want to short-circuit in the case of zero rows or use the Results metadata to configure an
+ * ExcelWriter or TSVGridWriter. Proper use of this class will ensure that the Results is closed in all cases (successful
+ * render, short-circuit, or error).</p>
+ */
 public class StashingResultsFactory implements ResultsFactory, AutoCloseable
 {
     private final ResultsFactory _factory;
