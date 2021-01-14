654a5f70aba37ee0f079ef4fed5298ba8fca1b9d
Matthew Bellew <matthewb@labkey.com>
2021-01-13 16:17:15 -0800

    fix NPE, add missing initializer

--- api/src/org/labkey/api/reader/DataLoader.java
+++ api/src/org/labkey/api/reader/DataLoader.java
@@ -928,7 +928,7 @@ public abstract class DataLoader implements Iterable<Map<String, Object>>, Loade
             return ret;
         }
 
-        private ArrayListMap.FindMap<String> _findMap = null;
+        private ArrayListMap.FindMap<String> _findMap = new ArrayListMap.FindMap<>(new CaseInsensitiveHashMap<>());
 
         @Override
         public boolean next()
