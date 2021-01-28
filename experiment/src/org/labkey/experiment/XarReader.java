e9ee70d97c82e71ed106f52ac5b5b0e6bf4a7d14
labkey-susanh <susanh@labkey.com>
2021-01-28 14:14:59 -0800

    Update to new auditHandler

--- experiment/src/org/labkey/experiment/XarReader.java
+++ experiment/src/org/labkey/experiment/XarReader.java
@@ -1458,7 +1458,7 @@ public class XarReader extends AbstractXarImporter
                 if (dataTable != null)
                 {
                     Map<String, Object> row = BeanObjectFactory.Registry.getFactory(Data.class).toMap(data, null);
-                    dataTable.addAuditEvent(getUser(), getContainer(), _auditBehaviorType, null, QueryService.AuditAction.INSERT, row);
+                    dataTable.getAuditHandler().addAuditEvent(getUser(), getContainer(), dataTable, _auditBehaviorType, null, QueryService.AuditAction.INSERT, Collections.singletonList(row), null);
                 }
             }
 
