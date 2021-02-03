fb1bb5f60b67ed510b68fe078c25426698f5b1cc
labkey-matthewb <matthewb@labkey.com>
2021-02-03 13:14:59 -0800

    fix SerializationTest (#1959)
    * fix SerializationTest

--- api/src/org/labkey/api/reports/report/RReportJob.java
+++ api/src/org/labkey/api/reports/report/RReportJob.java
@@ -142,11 +142,13 @@ public class RReportJob extends PipelineJob implements Serializable
     protected RReport getReport()
     {
         Report report = (_reportId != null) ? _reportId.getReport(getInfo()) : _report;
+        if (null == report)
+            return null;
 
         if (!(report instanceof RReport))
             throw new UnsupportedOperationException("The report is not a valid instance of an RReport");
 
-        String s = _report.getDescriptor().getProperty(ScriptReportDescriptor.Prop.runInBackground);
+        String s = report.getDescriptor().getProperty(ScriptReportDescriptor.Prop.runInBackground);
         Boolean runInBackground = (Boolean)JdbcType.BOOLEAN.convert(s);
         if (Boolean.TRUE != runInBackground)
             throw new UnsupportedOperationException("Report should have runInBackground property set to true.");
