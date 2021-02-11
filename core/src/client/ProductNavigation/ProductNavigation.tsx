846541afe659413fb2f278307c9981118cd2239e
cnathe <cnathe@labkey.com>
2021-02-11 09:57:04 -0600

    Misc cleanup for ProductNavigation.tsx and css overrides for the header menu in LKS

--- core/src/client/ProductNavigation/ProductNavigation.tsx
+++ core/src/client/ProductNavigation/ProductNavigation.tsx
@@ -3,17 +3,13 @@ import { ProductNavigationMenu } from '@labkey/components';
 
 import './productNavigation.scss';
 
-export interface AppContext {
-
-}
+export interface AppContext {}
 
 interface Props {
     context: AppContext;
 }
 
 export const ProductNavigation: FC<Props> = memo(props => {
-    console.log(props.context);
-
     return (
         <ProductNavigationMenu/>
     );
--- core/src/client/ProductNavigation/app.tsx
+++ core/src/client/ProductNavigation/app.tsx
@@ -4,8 +4,6 @@ import { App } from '@labkey/api';
 
 import {AppContext, ProductNavigation} from './ProductNavigation';
 
-// TODO JS error with the core module npm run build-prod
 App.registerApp<AppContext>('productNavigation', (target, ctx) => {
-    console.log(document.getElementById(target));
     ReactDOM.render(<ProductNavigation context={ctx}/>, document.getElementById(target));
 });
