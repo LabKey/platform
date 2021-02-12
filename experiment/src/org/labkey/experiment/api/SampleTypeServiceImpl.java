b18f1361a5eb7a9fd15f6f4ebf7619f1902055c8
labkey-matthewb <matthewb@labkey.com>
2021-02-11 19:28:04 -0800

    Fixes for sample detailed audit logging (#1990)
    * fix case-sensitive compare
    * Get static values for audit log from existing record if available.
    * row -> staticsRow
    
    Co-authored-by: labkey-susanh <susanh@labkey.com>

--- experiment/src/org/labkey/experiment/api/SampleTypeServiceImpl.java
+++ experiment/src/org/labkey/experiment/api/SampleTypeServiceImpl.java
@@ -910,7 +910,7 @@ public class SampleTypeServiceImpl extends AbstractAuditHandler implements Sampl
     public DetailedAuditTypeEvent createDetailedAuditRecord(User user, Container c, AuditConfigurable tInfo, QueryService.AuditAction action, @Nullable String userComment, @Nullable Map<String, Object> row, Map<String, Object> existingRow)
     {
         // not doing anything with userComment at the moment
-        return createAuditRecord(c, getCommentDetailed(action, !existingRow.isEmpty()), row, action);
+        return createAuditRecord(c, getCommentDetailed(action, !existingRow.isEmpty()), action, row, existingRow);
     }
 
     @Override
@@ -936,10 +936,18 @@ public class SampleTypeServiceImpl extends AbstractAuditHandler implements Sampl
 
     private SampleTimelineAuditEvent createAuditRecord(Container c, String comment, @Nullable Map<String, Object> row)
     {
-        return createAuditRecord(c, comment, row, null);
+        return createAuditRecord(c, comment, null, row, null);
     }
 
-    private SampleTimelineAuditEvent createAuditRecord(Container c, String comment, @Nullable Map<String, Object> row, @Nullable QueryService.AuditAction action)
+    // move to UploadSamplesHelper?
+    private boolean isInputFieldKey(String fieldKey)
+    {
+        int slash = fieldKey.indexOf('/');
+        return  slash==ExpData.DATA_INPUT_PARENT.length() && StringUtils.startsWithIgnoreCase(fieldKey,ExpData.DATA_INPUT_PARENT) ||
+                slash==ExpMaterial.MATERIAL_INPUT_PARENT.length() && StringUtils.startsWithIgnoreCase(fieldKey,ExpMaterial.MATERIAL_INPUT_PARENT);
+    }
+
+    private SampleTimelineAuditEvent createAuditRecord(Container c, String comment, @Nullable QueryService.AuditAction action, @Nullable Map<String, Object> row, @Nullable Map<String, Object> existingRow)
     {
         SampleTimelineAuditEvent event = new SampleTimelineAuditEvent(c.getId(), comment);
         var tx = getExpSchema().getScope().getCurrentTransaction();
@@ -949,17 +957,18 @@ public class SampleTypeServiceImpl extends AbstractAuditHandler implements Sampl
         if (c.getProject() != null)
             event.setProjectId(c.getProject().getId());
 
+        var staticsRow = existingRow != null && !existingRow.isEmpty() ? existingRow : row;
         if (row != null)
         {
-            Optional<String> parentFields = row.keySet().stream().filter((fieldKey) -> fieldKey.startsWith(ExpData.DATA_INPUT_PARENT) || fieldKey.startsWith(ExpMaterial.MATERIAL_INPUT_PARENT)).findAny();
+            Optional<String> parentFields = row.keySet().stream().filter(this::isInputFieldKey).findAny();
             event.setLineageUpdate(parentFields.isPresent());
 
             String sampleTypeLsid = null;
-            if (row.containsKey(CPAS_TYPE))
-                sampleTypeLsid =  String.valueOf(row.get(CPAS_TYPE));
+            if (staticsRow.containsKey(CPAS_TYPE))
+                sampleTypeLsid =  String.valueOf(staticsRow.get(CPAS_TYPE));
             // When a sample is deleted, the LSID is provided via the "sampleset" field instead of "LSID"
-            if (sampleTypeLsid == null && row.containsKey("sampleset"))
-                sampleTypeLsid = String.valueOf(row.get("sampleset"));
+            if (sampleTypeLsid == null && staticsRow.containsKey("sampleset"))
+                sampleTypeLsid = String.valueOf(staticsRow.get("sampleset"));
             if (sampleTypeLsid != null)
             {
                 ExpSampleType sampleType = SampleTypeService.get().getSampleTypeByType(sampleTypeLsid, c);
@@ -969,12 +978,12 @@ public class SampleTypeServiceImpl extends AbstractAuditHandler implements Sampl
                     event.setSampleTypeId(sampleType.getRowId());
                 }
             }
-            if (row.containsKey(LSID))
-                event.setSampleLsid(String.valueOf(row.get(LSID)));
-            if (row.containsKey(ROW_ID) && row.get(ROW_ID) != null)
-                event.setSampleId((Integer) row.get(ROW_ID));
-            if (row.containsKey(NAME))
-                event.setSampleName(String.valueOf(row.get(NAME)));
+            if (staticsRow.containsKey(LSID))
+                event.setSampleLsid(String.valueOf(staticsRow.get(LSID)));
+            if (staticsRow.containsKey(ROW_ID) && staticsRow.get(ROW_ID) != null)
+                event.setSampleId((Integer) staticsRow.get(ROW_ID));
+            if (staticsRow.containsKey(NAME))
+                event.setSampleName(String.valueOf(staticsRow.get(NAME)));
         }
 
         if (action != null)
