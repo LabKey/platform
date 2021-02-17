44cef6c34c7cdde28564d9bf166d342d510586df
labkey-tchad <tchad@labkey.com>
2021-02-16 16:03:31 -0800

    Fix upgrade scripts

--- core/resources/schemas/dbscripts/postgresql/prop-21.000-21.001.sql
+++ core/resources/schemas/dbscripts/postgresql/prop-21.000-21.001.sql
@@ -1,5 +1,5 @@
 -- Rename 'MobileAppStudy' module to 'Response'
 UPDATE prop.propertysets SET category = 'moduleProperties.Response' WHERE category = 'moduleProperties.MobileAppStudy';
 UPDATE prop.properties props SET name = 'Response'
-    FROM core.propertysets sets
+    FROM prop.propertysets sets
     WHERE props.set = sets.set AND props.name = 'MobileAppStudy' AND sets.category = 'activeModules';
