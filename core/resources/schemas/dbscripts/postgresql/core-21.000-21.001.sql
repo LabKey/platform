dd650d231791dd049746754fe339756d5cf5279d
labkey-tchad <tchad@labkey.com>
2021-02-17 15:24:34 -0800

    Update folder type and webpart name for MobileAppStudy

--- core/resources/schemas/dbscripts/postgresql/core-21.000-21.001.sql
+++ core/resources/schemas/dbscripts/postgresql/core-21.000-21.001.sql
@@ -1,3 +1,4 @@
 -- Rename 'MobileAppStudy' module to 'Response'
 UPDATE core.sqlscripts SET modulename = 'Response' WHERE modulename = 'MobileAppStudy';
 UPDATE core.modules SET name = 'Response' WHERE name = 'MobileAppStudy';
+UPDATE core.portalwebparts SET name = 'MyStudies Study Setup' WHERE name = 'Mobile App Study Setup';
