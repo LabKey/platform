2837e813565b275cddd8e9b1d5157ad4d0ababe6
labkey-tchad <tchad@labkey.com>
2021-02-18 12:07:30 -0800

    Rename java classes and packages for Response module

--- core/resources/schemas/dbscripts/postgresql/core-21.000-21.001.sql
+++ core/resources/schemas/dbscripts/postgresql/core-21.000-21.001.sql
@@ -1,3 +1,4 @@
 -- Rename 'MobileAppStudy' module to 'Response'
 UPDATE core.sqlscripts SET modulename = 'Response' WHERE modulename = 'MobileAppStudy';
-UPDATE core.modules SET name = 'Response' WHERE name = 'MobileAppStudy';
+UPDATE core.modules SET name = 'Response', classname = 'org.labkey.response.ResponseModule'
+    WHERE name = 'MobileAppStudy';
