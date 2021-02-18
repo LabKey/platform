aa044861bc50f796523255c88134e4ef44eb1128
labkey-tchad <tchad@labkey.com>
2021-02-17 16:24:49 -0800

    Use 'addLegacyNames' to rename webpart without upgrade script

--- core/resources/schemas/dbscripts/postgresql/core-21.000-21.001.sql
+++ core/resources/schemas/dbscripts/postgresql/core-21.000-21.001.sql
@@ -1,4 +1,3 @@
 -- Rename 'MobileAppStudy' module to 'Response'
 UPDATE core.sqlscripts SET modulename = 'Response' WHERE modulename = 'MobileAppStudy';
 UPDATE core.modules SET name = 'Response' WHERE name = 'MobileAppStudy';
-UPDATE core.portalwebparts SET name = 'MyStudies Study Setup' WHERE name = 'Mobile App Study Setup';
