74ff6fee5ada057bea665c6c69696989fa0c9494
labkey-tchad <tchad@labkey.com>
2021-02-23 10:02:05 -0800

    Update folder type name

--- core/resources/schemas/dbscripts/postgresql/prop-21.000-21.001.sql
+++ core/resources/schemas/dbscripts/postgresql/prop-21.000-21.001.sql
@@ -3,6 +3,6 @@ UPDATE prop.propertysets SET category = 'moduleProperties.Response' WHERE catego
 UPDATE prop.properties props SET name = 'Response'
     FROM prop.propertysets sets
     WHERE props.set = sets.set AND props.name = 'MobileAppStudy' AND sets.category = 'activeModules';
-UPDATE prop.properties props SET value = 'MyStudies Responses'
+UPDATE prop.properties props SET value = 'MyStudies Response'
     FROM prop.propertysets sets
     WHERE props.set = sets.set AND props.name = 'name' AND props.value = 'Mobile App Study' AND sets.category = 'folderType';
