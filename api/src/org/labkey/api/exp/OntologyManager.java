6fda0e687736aae59ad70da26ee081f1b96834b1
Matthew Bellew <matthewb@labkey.com>
2021-03-03 12:54:50 -0800

    Test to see of tx fixes constraint violation

--- api/src/org/labkey/api/exp/OntologyManager.java
+++ api/src/org/labkey/api/exp/OntologyManager.java
@@ -1762,10 +1762,11 @@ public class OntologyManager
         {
             try
             {
+                DbSchema expSchema = getExpSchema();
                 // ensureDomainDescriptor() shouldn't fail if there is a race condition, however Table.insert() will throw if row exists, so can't use that
                 // also a constraint violation will kill any current transaction
                 // CONSIDER to generalize add an option to check for existing row to Table.insert(ColumnInfo[] keyCols, Object[] keyValues)
-                String timestamp = getExpSchema().getSqlDialect().getSqlTypeName(JdbcType.TIMESTAMP);
+                String timestamp = expSchema.getSqlDialect().getSqlTypeName(JdbcType.TIMESTAMP);
                 String templateJson = null==ddIn.getTemplateInfo() ? null : ddIn.getTemplateInfo().toJSON();
                 SQLFragment insert = new SQLFragment(
                         "INSERT INTO " + getTinfoDomainDescriptor().getSelectName() +
@@ -1774,7 +1775,12 @@ public class OntologyManager
                         ddIn.getName(), ddIn.getDomainURI(), ddIn.getDescription(), ddIn.getContainer(), ddIn.getProject(), ddIn.getStorageTableName(), ddIn.getStorageSchemaName(), templateJson)
                 .append("WHERE NOT EXISTS (SELECT * FROM "  + getTinfoDomainDescriptor().getSelectName() + " x WHERE x.DomainURI=? AND x.Project=?)\n")
                 .add(ddIn.getDomainURI()).add(ddIn.getProject());
-                int count = new SqlExecutor(getExpSchema().getScope()).execute(insert);
+                int count;
+                try (var tx = expSchema.getScope().ensureTransaction())
+                {
+                    count = new SqlExecutor(expSchema.getScope()).execute(insert);
+                    tx.commit();
+                }
 
                 // alternately we could reselect rowid and then we wouldn't need this separate round trip
                 dd = fetchDomainDescriptorFromDB(ddIn.getDomainURI(), ddIn.getContainer());
