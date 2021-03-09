acd5ba2a8770805dd7f7aaecc4ac26d166b9b65d
cnathe <cnathe@labkey.com>
2021-03-09 14:06:57 -0600

    Configure platform/list module for @labkey/build v2.0.x - remove tsconfig.json and add main.d.ts - update scss files to use alias for @labkey/components-scss - package.json updates for @labkey/components and build package.json - include jest as devDependencies to fix start-link issue

---
+++ list/typings/main.d.ts
@@ -0,0 +1,9 @@
+/* Test dependencies */
+/// <reference types="enzyme" />
+/// <reference types="enzyme-adapter-react-16" />
+/// <reference types="jest" />
+
+/**
+ * @deprecated Use getServerContext() from @labkey/api instead
+ */
+declare const LABKEY: import('@labkey/api').LabKey;
