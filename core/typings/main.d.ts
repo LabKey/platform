d521b97c4410ce71db8c7ff7132d92c274f97758
cnathe <cnathe@labkey.com>
2021-03-10 12:52:29 -0600

    Update platform main.d.ts files to remove test references

--- core/typings/main.d.ts
+++ core/typings/main.d.ts
@@ -1,8 +1,3 @@
-/* Test dependencies */
-/// <reference types="enzyme" />
-/// <reference types="enzyme-adapter-react-16" />
-/// <reference types="jest" />
-
 /**
  * @deprecated Use getServerContext() from @labkey/api instead
  */
