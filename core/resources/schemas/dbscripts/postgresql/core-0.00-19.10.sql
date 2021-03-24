786ba02cb73a558c029d6d183293448a3dd14a11
Adam Rauch <adam@labkey.com>
2021-03-23 17:57:12 -0700

    Consolidate bootstrap SQL scripts to 0.00-19.10.sql

--- core/resources/schemas/dbscripts/postgresql/core-0.00-19.10.sql
+++ core/resources/schemas/dbscripts/postgresql/core-0.00-19.10.sql
@@ -667,3 +667,32 @@ CREATE TABLE core.PrincipalRelations
 
   CONSTRAINT PK_PrincipalRelations PRIMARY KEY (userid, otherid, relationship)
 );
+
+/* core-18.30-19.10.sql */
+
+SELECT core.fn_dropifexists('portalwebparts', 'core', 'CONSTRAINT', 'fk_portalwebpartpages');
+SELECT core.fn_dropifexists('portalpages', 'core', 'CONSTRAINT', 'pk_portalpages');
+SELECT core.fn_dropifexists('portalpages', 'core', 'CONSTRAINT', 'uq_portalpage');
+ALTER TABLE core.portalpages ADD COLUMN rowId SERIAL PRIMARY KEY;
+ALTER TABLE core.portalwebparts ADD COLUMN portalPageId INTEGER;
+UPDATE core.portalwebparts web SET portalPageId = page.rowId
+    FROM core.portalpages page
+    WHERE web.pageId = page.pageId;
+ALTER TABLE core.portalwebparts DROP COLUMN pageId;
+ALTER TABLE core.portalwebparts ALTER COLUMN portalPageId SET NOT NULL;
+ALTER TABLE core.portalwebparts ADD CONSTRAINT fk_portalwebpartpages FOREIGN KEY (portalPageId) REFERENCES core.portalpages (rowId);
+
+-- Fix webparts that referenced incorrect portal pages
+UPDATE core.portalwebparts parts
+    SET portalPageId = (SELECT rowId AS newPortalPageId FROM core.portalpages p2
+                        WHERE p2.pageid = page.pageid AND p2.Container = parts.Container)
+FROM core.portalpages page
+WHERE page.rowid = parts.portalpageid AND page.Container <> parts.Container;
+
+UPDATE core.RoleAssignments SET role = 'org.labkey.api.security.roles.SeeUserDetailsRole' WHERE role = 'org.labkey.api.security.roles.SeeEmailAddressesRole';
+
+UPDATE core.RoleAssignments SET role = 'org.labkey.api.security.roles.QCAnalystRole' WHERE role = 'org.labkey.api.security.roles.QCEditorRole';
+
+ALTER TABLE core.ReportEngineMap DROP CONSTRAINT PK_ReportEngineMap;
+ALTER TABLE core.ReportEngineMap ADD EngineContext VARCHAR(64) NOT NULL DEFAULT 'report';
+ALTER TABLE core.ReportEngineMap ADD CONSTRAINT PK_ReportEngineMap PRIMARY KEY (EngineId, Container, EngineContext);
