2544067d92e5c2e3257a36bea19993ed1e77ffe8
labkey-klum <klum@labkey.com>
2021-03-23 18:17:23 -0700

    filter notebook archives exclude unreferenced assay runs, samples, data classes & experiments

---
+++ api/src/org/labkey/api/exp/XarExportContext.java
@@ -0,0 +1,59 @@
+package org.labkey.api.exp;
+
+import org.labkey.api.admin.FolderExportContext;
+import org.labkey.api.admin.LoggerGetter;
+import org.labkey.api.data.Container;
+import org.labkey.api.security.User;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Folder export context that allows filtering of objects in the Xar
+ */
+public class XarExportContext extends FolderExportContext
+{
+    private Set<Integer> _includedAssayRuns = new HashSet<>();
+    private Map<Integer, Set<Integer>> _includedSamples = new HashMap<>();
+    private Map<Integer, Set<Integer>> _includedDataClasses = new HashMap<>();
+
+    public XarExportContext(User user, Container c, Set<String> dataTypes, String format, LoggerGetter logger)
+    {
+        super(user, c, dataTypes, format, logger);
+    }
+
+    public Set<Integer> getIncludedAssayRuns()
+    {
+        return _includedAssayRuns;
+    }
+
+    public void setIncludedAssayRuns(Set<Integer> includedRuns)
+    {
+        if (includedRuns != null)
+            _includedAssayRuns = includedRuns;
+    }
+
+    public Map<Integer, Set<Integer>> getIncludedSamples()
+    {
+        return _includedSamples;
+    }
+
+    public void setIncludedSamples(Map<Integer, Set<Integer>> includedSamples)
+    {
+        if (includedSamples != null)
+            _includedSamples = includedSamples;
+    }
+
+    public Map<Integer, Set<Integer>> getIncludedDataClasses()
+    {
+        return _includedDataClasses;
+    }
+
+    public void setIncludedDataClasses(Map<Integer, Set<Integer>> includedDataClasses)
+    {
+        if (includedDataClasses != null)
+            _includedDataClasses = includedDataClasses;
+    }
+}
