4077c790db25d50d61599683dab70bac77941855
cnathe <cnathe@labkey.com>
2021-03-29 10:22:05 -0500

    EntityInsertPanel update for core-components page to support file import

--- core/src/client/LabKeyUIComponentsPage/SampleInsertPage.tsx
+++ core/src/client/LabKeyUIComponentsPage/SampleInsertPage.tsx
@@ -3,7 +3,16 @@
  * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
  */
 import React, { FC, memo, useCallback, useEffect, useState } from 'react';
-import { Alert, EntityInsertPanel, initQueryGridState, SampleTypeDataType } from '@labkey/components';
+import {
+    Alert,
+    EntityInsertPanel,
+    importData,
+    initQueryGridState,
+    InsertOptions,
+    QueryInfo,
+    SampleTypeDataType
+} from '@labkey/components';
+import {ActionURL} from "@labkey/api";
 
 export const SampleInsertPage: FC = memo(() => {
     const [message, setMessage] = useState<string>(undefined);
@@ -16,6 +25,35 @@ export const SampleInsertPage: FC = memo(() => {
         setMessage(`Created ${sampleCount} samples in sample type '${sampleSetName}'.`);
     }, []);
 
+    const handleFileImport = useCallback(
+        (queryInfo: QueryInfo, file: File, isMerge: boolean): Promise<any> => {
+            return new Promise((resolve, reject) => {
+                const { schemaQuery } = queryInfo;
+
+                return importData({
+                    schemaName: schemaQuery.getSchema(),
+                    queryName: schemaQuery.getQuery(),
+                    file,
+                    importUrl: ActionURL.buildURL('experiment', 'ImportSamples', null, {
+                        'schemaName': schemaQuery.getSchema(),
+                        'query.queryName': schemaQuery.getQuery()
+                    }),
+                    importLookupByAlternateKey: true,
+                    insertOption: InsertOptions[isMerge ? InsertOptions.MERGE : InsertOptions.IMPORT]
+                }).then((response) => {
+                    if (response.success) {
+                        resolve(response);
+                    } else {
+                        reject({msg: response.errors._form})
+                    }
+                }).catch((error) => {
+                    console.error(error);
+                    reject({msg: error.exception})
+                });
+            });
+        }, []
+    );
+
     return (
         <>
             <Alert bsStyle="info">
@@ -26,6 +64,7 @@ export const SampleInsertPage: FC = memo(() => {
                 afterEntityCreation={afterSampleCreation}
                 entityDataType={SampleTypeDataType}
                 importHelpLinkNode={<>Get help with your samples</>}
+                handleFileImport={handleFileImport}
                 nounPlural="samples"
                 nounSingular="sample"
             />
