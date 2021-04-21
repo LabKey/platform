3278f9e67811010f59aca514480c3da78a66ed66
cnathe <cnathe@labkey.com>
2021-04-21 16:35:28 -0500

    Issue 42990: Allow the NabPopulateFitParametersPipelineJob to be rerun to attempt to populate NAbSpecimen FitParameters for previously failed runs - MaintenanceTask.isConfigurable(), default to true - Update SystemMaintenanceJob to skip over any non-configurable tasks - Configure System Maintenance page display updates to split configurable vs non-configurable tasks into separate panels

--- api/src/org/labkey/api/util/SystemMaintenanceJob.java
+++ api/src/org/labkey/api/util/SystemMaintenanceJob.java
@@ -90,8 +90,10 @@ public class SystemMaintenanceJob implements org.quartz.Job, Callable<String>
             }
             else
             {
-                // If the task can't be disabled or isn't disabled now then include it
-                if (!task.canDisable() || !disabledTasks.contains(task.getName()))
+                // If the task can't be disabled or isn't disabled now, then include it.
+                // Also, skip over any of the non-configurable tasks.
+                boolean taskEnabled = !task.canDisable() || !disabledTasks.contains(task.getName());
+                if (taskEnabled && task.isConfigurable())
                 {
                     tasksToRun.add(task);
                 }
