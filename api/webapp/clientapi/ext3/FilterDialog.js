66c01befdefa2a1e4694298f93b57587a9521dfd
ian <ians@labkey.com>
2021-04-26 16:38:00 -0700

    Fix reference error

--- api/webapp/clientapi/ext3/FilterDialog.js
+++ api/webapp/clientapi/ext3/FilterDialog.js
@@ -1451,8 +1451,8 @@ LABKEY.FilterDialog.View.ConceptFilter = Ext.extend(LABKEY.FilterDialog.View.Def
                 }],
                 listeners: {
                     render: () => {
-                        // const conceptFilterScript = 'http://localhost:3001/conceptFilter.js';
-                        const conceptFilterScript = 'core/gen/conceptFilter';
+                        const conceptFilterScript = 'http://localhost:3001/conceptFilter.js';
+                        // const conceptFilterScript = 'core/gen/conceptFilter';
                         LABKEY.requiresScript(conceptFilterScript, this.loadConceptPickers, {divId, index, scope:this});
                     }
                 },
@@ -1501,8 +1501,10 @@ LABKEY.FilterDialog.View.ConceptFilter = Ext.extend(LABKEY.FilterDialog.View.Def
 
         // Update concept filters if possible
         if (this.updateConceptFilters[f]) {
+            const filter = this.filters[f];
             const conceptBrowserUpdater = this.updateConceptFilters[f];
-            conceptBrowserUpdater.setValue(inputValue);
+
+            conceptBrowserUpdater.setValue(filter.getURLParameterValue());
             conceptBrowserUpdater.setFilterType(filter.getFilterType());
         }
     },
