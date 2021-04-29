a9ebd706a9adf99187b555e68601661d77ef8467
Alan Vezina <alanv@labkey.com>
2021-04-29 17:56:10 -0500

    Cleanup

--- pipeline/src/client/CreatePipelineTrigger/dev.tsx
+++ pipeline/src/client/CreatePipelineTrigger/dev.tsx
@@ -5,7 +5,7 @@ import { App } from '@labkey/api';
 
 import { Props, CreatePipelineTrigger } from './CreatePipelineTrigger';
 
-const render = (target: string, ctx: Props) => {
+const render = (target: string, ctx: Props): void => {
     ReactDOM.render(
         <AppContainer>
             <CreatePipelineTrigger {...ctx} />
@@ -16,6 +16,7 @@ const render = (target: string, ctx: Props) => {
 
 App.registerApp<Props>('createPipelineTrigger', render, true);
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const module: any;
 
 if (module.hot) {
