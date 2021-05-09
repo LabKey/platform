6cf1492322fcb4302d7a3ecbc4a41c3f0c6eb7be
Kevin Krouse <kevink@labkey.com>
2021-05-09 22:30:53 +0800

    source lsid may be null in the study published data maps

--- study/src/org/labkey/study/assay/StudyPublishManager.java
+++ study/src/org/labkey/study/assay/StudyPublishManager.java
@@ -123,6 +123,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -545,20 +546,22 @@ public class StudyPublishManager implements StudyPublishService
     /**
      * To help generate the assay audit record, group the rows by the source type lsid (assay protocol or sample type).
      */
-    private Map<String, List<Map<String, Object>>> groupBySourceLsid(List<Map<String, Object>> dataMaps)
+    private Map<Optional<String>, List<Map<String, Object>>> groupBySourceLsid(List<Map<String, Object>> dataMaps)
     {
-        return dataMaps.stream().collect(Collectors.groupingBy(m -> (String)m.get(SOURCE_LSID_PROPERTY_NAME)));
+        // source LSID may be null
+        return dataMaps.stream().collect(Collectors.groupingBy(m -> Optional.ofNullable((String)m.get(SOURCE_LSID_PROPERTY_NAME))));
     }
 
     // TODO : consider pushing this into PublishSource
     private void logPublishEvent(Dataset.PublishSource publishSource, ExpObject source, List<Map<String, Object>> dataMaps, User user, Container sourceContainer, Container targetContainer, Dataset dataset)
     {
-        Map<String, List<Map<String, Object>>> sourceLSIDCounts = groupBySourceLsid(dataMaps);
+        Map<Optional<String>, List<Map<String, Object>>> sourceLSIDCounts = groupBySourceLsid(dataMaps);
         if (source != null)
         {
-            for (Map.Entry<String, List<Map<String, Object>>> entry : sourceLSIDCounts.entrySet())
+            for (var entry : sourceLSIDCounts.entrySet())
             {
-                String sourceLsid = entry.getKey();
+                // source LSID may be null
+                String sourceLsid = entry.getKey().orElse(null);
                 List<Map<String, Object>> rows = entry.getValue();
                 int recordCount = rows.size();
 
