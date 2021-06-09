e291f31bdee5ec7d635ad8095fc1f33d6470753a
rosalinep <rosalinep@labkey.com>
2021-06-08 17:36:16 -0700

    With paragraph breaks

--- api/src/org/labkey/api/security/SecurityManager.java
+++ api/src/org/labkey/api/security/SecurityManager.java
@@ -2804,6 +2804,15 @@ public class SecurityManager
             {
                 message.append(email.getEmailAddress()).append(" added as a new user to the system and emailed successfully.");
                 UserManager.addToUserHistory(newUser, newUser.getEmail() + " was added to the system. Verification email was sent successfully.");
+
+                if (!context.getContainer().isRoot())
+                {
+                    LinkBuilder link = new LinkBuilder("here").href(PageFlowUtil.urlProvider(SecurityUrls.class).getPermissionsURL(context.getContainer())).target("_blank").clearClasses();
+
+                    message.append(HtmlString.unsafe("<p>"));
+                    message.append("Add ").append(email.getEmailAddress()).append(" to a Project Group ").append(link).append(".");
+                    message.append(HtmlString.unsafe("</p>"));
+                }
             }
             else
             {
@@ -2846,7 +2855,10 @@ public class SecurityManager
         if (messageContentsURL != null && provider == null)
         {
             LinkBuilder link = new LinkBuilder("here").href(messageContentsURL).target("_blank").clearClasses();
-            message.append(" Click ").append(link).append(" to see the email.");
+
+            message.append(HtmlString.unsafe("<p>"));
+            message.append("Click ").append(link).append(" to see the email.");
+            message.append(HtmlString.unsafe("</p>"));
         }
 
         return message.getHtmlString();
