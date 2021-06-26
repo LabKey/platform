b383f933ad9503997170d86b41edfb366986cbc4
Adam Rauch <adam@labkey.com>
2021-06-26 00:41:52 -0700

    Set "home" and "Shared" as excluded projects by default

--- api/src/org/labkey/api/data/ContainerManager.java
+++ api/src/org/labkey/api/data/ContainerManager.java
@@ -101,7 +101,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
