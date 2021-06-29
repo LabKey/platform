d3a9138c969c730dd164998ac0faa11774d26ec2
labkey-sweta <swetaj@labkey.com>
2021-06-29 16:32:28 -0700

    Automation test coverage for assay.

--- study/test/src/org/labkey/test/tests/study/AutoLinkToStudyTest.java
+++ study/test/src/org/labkey/test/tests/study/AutoLinkToStudyTest.java
@@ -7,15 +7,17 @@ import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.TestFileUtils;
+import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.Assays;
 import org.labkey.test.categories.DailyC;
 import org.labkey.test.util.DataRegionTable;
+import org.labkey.test.util.StudyHelper;
 
 import java.io.File;
 import java.util.List;
 
 @Category({DailyC.class, Assays.class})
-@BaseWebDriverTest.ClassTimeout(minutes = 2)
+@BaseWebDriverTest.ClassTimeout(minutes = 4)
 public class AutoLinkToStudyTest extends BaseWebDriverTest
 {
     private final static String ASSAY_NAME = "Test Assay";
@@ -31,10 +33,16 @@ public class AutoLinkToStudyTest extends BaseWebDriverTest
     {
         log("Creating a date based study");
         _containerHelper.createProject(getProjectName(), "Study");
-        clickButton("Create Study");
-        checkRadioButton(Locator.radioButtonById("dateTimepointType"));
-        clickButton("Create Study");
+        _studyHelper.startCreateStudy()
+                .setTimepointType(StudyHelper.TimepointType.DATE)
+                .createStudy();
 
+        log("Creating an assay");
+        goToProjectHome();
+        goToManageAssays();
+        _assayHelper.createAssayDesign("General", ASSAY_NAME)
+                .setAutoLinkTarget("(Data import folder)")
+                .clickSave();
     }
 
     @Override
@@ -54,14 +62,81 @@ public class AutoLinkToStudyTest extends BaseWebDriverTest
     {
         String runName = "Run 1";
         File runFile = new File(TestFileUtils.getSampleData("AssayImportExport"), "GenericAssay_Run1.xls");
+        importAssayRun(runFile, ASSAY_NAME, runName);
 
-        log("Creating an assay");
+        log("Verifying data is auto imported in study");
+        clickTab("Clinical and Assay Data");
+        waitAndClickAndWait(Locator.linkWithText(ASSAY_NAME));
+        DataRegionTable table = new DataRegionTable("Dataset", getDriver());
+        table.setFilter("Run/Name", "Equals", runName);
+        checker().verifyEquals("New dataset is not created in Study from Assay import", 6, table.getDataRowCount());
+    }
+
+    /*
+        Test coverage for : Issue 42937: Assay results grid loading performance can degrade with a large number of "copied to study" columns
+     */
+    @Test
+    public void testLinkedColumnNotDisplayedCase()
+    {
+        String runName = "Link to study run";
+        log("Creating the study projects");
+        _containerHelper.createProject("Study 1", "Study");
+        _studyHelper.startCreateStudy().setTimepointType(StudyHelper.TimepointType.DATE)
+                .createStudy();
+
+        _containerHelper.createProject("Study 2", "Study");
+        _studyHelper.startCreateStudy().setTimepointType(StudyHelper.TimepointType.DATE)
+                .createStudy();
+
+        _containerHelper.createProject("Study 3", "Study");
+        _studyHelper.startCreateStudy().setTimepointType(StudyHelper.TimepointType.DATE)
+                .createStudy();
+
+        File runFile = new File(TestFileUtils.getSampleData("AssayImportExport"), "GenericAssay_Run2.xls");
+        importAssayRun(runFile, ASSAY_NAME, runName);
+
+        linkToStudy(runName, "Study 1", 1);
+        linkToStudy(runName, "Study 2", 1);
+        linkToStudy(runName, "Study 3", 1);
+
+        log("Verifying linked column does not exists because more then 3 studies are linked");
+        goToProjectHome();
         goToManageAssays();
-        _assayHelper.createAssayDesign("General", ASSAY_NAME)
-                .setAutoLinkTarget("(Data import folder)")
-                .clickSave();
+        clickAndWait(Locator.linkWithText(ASSAY_NAME));
+        clickAndWait(Locator.linkWithText(runName));
+        DataRegionTable runsTable = DataRegionTable.DataRegion(getDriver()).withName("Data").waitFor();
+        checker().verifyFalse("Linked column for Study 1 should not be present",
+                runsTable.getColumnNames().contains("linked_to_Study_1_Study"));
+        checker().verifyFalse("Linked column for Study 2 should not be present",
+                runsTable.getColumnNames().contains("linked_to_Study_2_Study"));
+        checker().verifyFalse("Linked column for Study 3 should not be present",
+                runsTable.getColumnNames().contains("linked_to_Study_3_Study"));
+    }
 
+    private void linkToStudy(String runName, String targetStudy, int numOfRows)
+    {
+        goToProjectHome();
+        goToManageAssays();
+        clickAndWait(Locator.linkWithText(ASSAY_NAME));
+        clickAndWait(Locator.linkWithText(runName));
+
+        DataRegionTable runTable = DataRegionTable.DataRegion(getDriver()).withName("Data").waitFor();
+        for (int i = 0; i < numOfRows; i++)
+            runTable.checkCheckbox(i);
+
+        runTable.clickHeaderButtonAndWait("Link to Study");
+
+        log("Link to study: Choose target");
+        selectOptionByText(Locator.id("targetStudy"), "/" + targetStudy + " (" + targetStudy + " Study)");
+        clickButton("Next");
+
+        new DataRegionTable("Data", getDriver()).clickHeaderButtonAndWait("Link to Study");
+    }
+
+    private void importAssayRun(File runFile, String assayName, String runName)
+    {
         log("Importing the Assay run");
+        goToProjectHome();
         goToManageAssays();
         clickAndWait(Locator.linkWithText(ASSAY_NAME));
         DataRegionTable runTable = new DataRegionTable("Runs", getDriver());
@@ -71,12 +146,14 @@ public class AutoLinkToStudyTest extends BaseWebDriverTest
         checkRadioButton(Locator.radioButtonById("Fileupload"));
         setFormElement(Locator.input("__primaryFile__"), runFile);
         clickButton("Save and Finish");
-
-        log("Verifying data is auto imported in study");
-        clickTab("Clinical and Assay Data");
-        waitAndClickAndWait(Locator.linkWithText(ASSAY_NAME));
-        DataRegionTable table = new DataRegionTable("Dataset", getDriver());
-        checker().verifyEquals("New dataset is not created in Study from Assay import", 6, table.getDataRowCount());
     }
 
+    @Override
+    protected void doCleanup(boolean afterTest) throws TestTimeoutException
+    {
+        _containerHelper.deleteProject(getProjectName(), afterTest);
+        _containerHelper.deleteProject("Study 1");
+        _containerHelper.deleteProject("Study 2");
+        _containerHelper.deleteProject("Study 3");
+    }
 }
