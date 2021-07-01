f094686780b7f3f13bda8382249611f6dcebbb5e
cnathe <cnathe@labkey.com>
2021-07-01 15:12:11 -0500

    Item 9172: Use cache.remap() to resolve lookup value in Derive Samples form code path

--- api/src/org/labkey/api/assay/DefaultAssayRunCreator.java
+++ api/src/org/labkey/api/assay/DefaultAssayRunCreator.java
@@ -28,7 +28,9 @@ import org.labkey.api.assay.pipeline.AssayUploadPipelineJob;
 import org.labkey.api.assay.plate.PlateMetadataDataHandler;
 import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.Container;
+import org.labkey.api.data.ContainerFilter;
 import org.labkey.api.data.ContainerManager;
+import org.labkey.api.data.ConvertHelper;
 import org.labkey.api.data.DbScope;
 import org.labkey.api.data.ExpDataFileConverter;
 import org.labkey.api.data.JdbcType;
@@ -68,6 +70,7 @@ import org.labkey.api.qc.TransformDataHandler;
 import org.labkey.api.qc.TransformResult;
 import org.labkey.api.query.BatchValidationException;
 import org.labkey.api.query.PropertyValidationError;
+import org.labkey.api.query.SchemaKey;
 import org.labkey.api.query.SimpleValidationError;
 import org.labkey.api.query.ValidationError;
 import org.labkey.api.query.ValidationException;
@@ -1011,7 +1014,7 @@ public class DefaultAssayRunCreator<ProviderType extends AbstractAssayProvider> 
         List<ValidationError> errors = new ArrayList<>();
         for (Map.Entry<ColumnInfo, String> entry : properties.entrySet())
         {
-            validateProperty(context, ColumnValidators.create(entry.getKey(), null), entry.getValue(), entry.getKey().getName(), false, entry.getKey().getJavaClass(), errors);
+            validateProperty(context, entry.getKey(), entry.getValue(), errors);
         }
         return errors;
     }
@@ -1019,22 +1022,47 @@ public class DefaultAssayRunCreator<ProviderType extends AbstractAssayProvider> 
     public static List<ValidationError> validateProperties(ContainerUser context, Map<DomainProperty, String> properties)
     {
         List<ValidationError> errors = new ArrayList<>();
-
         for (Map.Entry<DomainProperty, String> entry : properties.entrySet())
         {
-            DomainProperty dp = entry.getKey();
-            String value = entry.getValue();
-            String label = dp.getPropertyDescriptor().getNonBlankCaption();
-            PropertyType type = dp.getPropertyDescriptor().getPropertyType();
-            validateProperty(context, ColumnValidators.create(null, dp), value, label, dp.isRequired(), type.getJavaType(), errors);
+            validateProperty(context, entry.getKey(), entry.getValue(), errors);
         }
         return errors;
     }
 
-    private static void validateProperty(ContainerUser context, List<ColumnValidator> validators, String value, String label, Boolean required, Class type, List<ValidationError> errors)
+    private static void validateProperty(ContainerUser context, ColumnInfo columnInfo, String value, List<ValidationError> errors)
+    {
+        String lookupSchemaName = null, lookupQueryName = null;
+        if (columnInfo.isLookup())
+        {
+            lookupSchemaName = columnInfo.getFk().getLookupSchemaName();
+            lookupQueryName = columnInfo.getFk().getLookupTableName();
+        }
+        validateProperty(context, ColumnValidators.create(columnInfo, null), value, columnInfo.getName(), false,
+                lookupSchemaName, lookupQueryName, columnInfo.getJavaClass(), errors);
+    }
+
+    private static void validateProperty(ContainerUser context, DomainProperty dp, String value, List<ValidationError> errors)
+    {
+        String label = dp.getPropertyDescriptor().getNonBlankCaption();
+        PropertyType type = dp.getPropertyDescriptor().getPropertyType();
+        String lookupSchemaName = null, lookupQueryName = null;
+        if (dp.getLookup() != null)
+        {
+            lookupSchemaName = dp.getLookup().getSchemaName();
+            lookupQueryName = dp.getLookup().getQueryName();
+        }
+
+        validateProperty(context, ColumnValidators.create(null, dp), value, label, dp.isRequired(),
+                lookupSchemaName, lookupQueryName, type.getJavaType(), errors);
+    }
+
+    private static void validateProperty(ContainerUser context, List<ColumnValidator> validators, String value,
+                                         String label, Boolean required, String lookupSchemaName, String lookupQueryName,
+                                         Class type, List<ValidationError> errors)
     {
         boolean missing = (value == null || value.length() == 0);
         int rowNum = 0;
+        RemapCache cache = new RemapCache();
 
         if (required && missing)
         {
@@ -1055,14 +1083,23 @@ public class DefaultAssayRunCreator<ProviderType extends AbstractAssayProvider> 
             }
             catch (ConversionException e)
             {
-                String message = label + " must be of type " + ColumnInfo.getFriendlyTypeName(type) + ".";
-                message +=  "  Value \"" + value + "\" could not be converted";
+                String message = ConvertHelper.getStandardConversionErrorMessage(value, label, type);
                 if (e.getCause() instanceof ArithmeticException)
-                    message +=  ": " + e.getCause().getLocalizedMessage();
+                    message += ": " + e.getCause().getLocalizedMessage();
                 else
                     message += ".";
 
-                errors.add(new SimpleValidationError(message));
+                // Attempt to resolve lookups by display value
+                boolean skipError = false;
+                if (lookupSchemaName != null && lookupQueryName != null)
+                {
+                    Object remappedValue = cache.remap(SchemaKey.fromParts(lookupSchemaName), lookupQueryName, context.getUser(), context.getContainer(), ContainerFilter.Type.CurrentPlusProjectAndShared, value);
+                    if (remappedValue != null)
+                        skipError = true;
+                }
+
+                if (!skipError)
+                    errors.add(new SimpleValidationError(message));
             }
         }
     }
