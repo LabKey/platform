ba80886042b0e957503ded650109aa98dadf94e8
Josh Eckels <jeckels@labkey.com>
2021-07-26 09:10:34 -0700

    Issue 43508: Improve visibility of which user ID enabled which ETLs (#2490)
    Show previews of side effects for deleting and disabling user accounts
    Avoid needing to implement no-op methods for UserListeners
    Show tooltip with user who enabled (and "owns") the ETL
    Improve feedback on saving of ETL enabled status

--- core/src/org/labkey/core/notification/EmailPreferenceUserListener.java
+++ core/src/org/labkey/core/notification/EmailPreferenceUserListener.java
@@ -8,29 +8,9 @@ import java.beans.PropertyChangeEvent;
 public class EmailPreferenceUserListener implements UserManager.UserListener
 {
     @Override
-    public void userAddedToSite(User user)
-    {
-    }
-
-    @Override
     public void userDeletedFromSite(User user)
     {
         //when user is deleted from site, remove any corresponding record from EmailPrefs table.
         EmailPreferenceConfigManager.deleteUserEmailPref(user, null);
-    }
-
-    @Override
-    public void userAccountDisabled(User user)
-    {
-    }
-
-    @Override
-    public void userAccountEnabled(User user)
-    {
-    }
-
-    @Override
-    public void propertyChange(PropertyChangeEvent evt)
-    {
     }
 }
