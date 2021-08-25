f459cab7536ea9833a6e9d5399f5bf429b26c3ef
XingY <xyang@labkey.com>
2021-08-25 16:49:31 -0700

    reuse

--- api/src/org/labkey/api/reports/RScriptEngine.java
+++ api/src/org/labkey/api/reports/RScriptEngine.java
@@ -248,8 +248,9 @@ public class RScriptEngine extends ExternalScriptEngine
                 sb.append("render(run_pandoc=TRUE, ");
                 if (useDefaultOutputFormat(context))
                 {
-                    sb.append("html_document(keep_md=TRUE, self_contained=FALSE, fig_caption=TRUE, " +
-                            "theme=NULL, css=NULL, smart=TRUE, highlight=\"default\"), ");
+                    sb.append("html_document(")
+                            .append(PANDOC_DEFAULT_OUTPUT_OPTIONS_LIST)
+                            .append("), ");
                 }
                 else
                 {
