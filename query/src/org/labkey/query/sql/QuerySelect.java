8a5a0fb11d070d9e16ea91f31f6f8462100e6535
labkey-matthewb <matthewb@labkey.com>
2021-08-31 10:11:49 -0700

    43580: SQL generation omits dependent column from parent table when used in subselect expression (#2554)
    43580: SQL generation omits dependent column from parent table when used in subselect expression

--- query/src/org/labkey/query/sql/QuerySelect.java
+++ query/src/org/labkey/query/sql/QuerySelect.java
@@ -97,6 +97,8 @@ public class QuerySelect extends QueryRelation implements Cloneable
     private final AliasManager _aliasManager;
 //    private List<SelectColumn> _medianColumns = new ArrayList<>();                  // Possible way to support SQL Server Median
 
+    private boolean  skipSuggestedColumns = false;  // set to skip normal getSuggestedColumns() code
+
     /**
      * If this node has exactly one FROM table, it may be occasionally possible to skip
      * generating SQL for this node.
@@ -953,6 +955,11 @@ public class QuerySelect extends QueryRelation implements Cloneable
                         ((QIfDefined)location).isDefined = false;
                     else
                         parseError("Could not resolve column: " + declareKey, location);
+                }
+                else
+                {
+                    // If I don't 'own' the column, just addRef() to avoid having to try to find this reference later.
+                    ret.addRef(this);
                 }
                 return ret;
             }
@@ -1938,9 +1945,18 @@ public class QuerySelect extends QueryRelation implements Cloneable
     }
 
 
+    public void setSkipSuggestedColumns(boolean skipSuggestedColumns)
+    {
+        this.skipSuggestedColumns = skipSuggestedColumns;
+    }
+
+
     @Override
     protected Set<RelationColumn> getSuggestedColumns(Set<RelationColumn> selected)
     {
+        if (skipSuggestedColumns)
+            return Collections.emptySet();
+
         resolveFields();
 
         if (!getParseErrors().isEmpty())
