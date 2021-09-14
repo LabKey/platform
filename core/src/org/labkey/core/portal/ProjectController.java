96cefa7d416720438a782fcdc01cb32f40cccffc
Cory Nathe <cnathe@labkey.com>
2021-09-14 10:13:13 -0500

    Issue 43853: Always include "children" array (even if empty) in project-getContainers API response (#2619)
    - Study Publish Wizard relies on this property being in the response object to do a container duplicate name check

--- core/src/org/labkey/core/portal/ProjectController.java
+++ core/src/org/labkey/core/portal/ProjectController.java
@@ -1527,12 +1527,13 @@ public class ProjectController extends SpringActionController
             if (_requestedDepth > 0)
             {
                 List<Map<String, Object>> children = getVisibleChildren(container, user, propertiesToSerialize, 0);
+                resultMap.put("children", children);
+
                 if (children.size() > 0)
                 {
-                    // if use has perm to at least one child container, make sure that at least the parent name is shown
+                    // if use has permissions to at least one child container, make sure that at least the parent name is shown
                     // (even if the user doesn't have perm to that parent container)
                     resultMap.put("name", container.getName());
-                    resultMap.put("children", children);
                 }
             }
 
