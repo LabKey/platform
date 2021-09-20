265bda355150e100e5e0470676c64b5b710b207f
labkey-tchad <tchad@labkey.com>
2021-09-20 09:56:28 -0700

    Wrong branch

--- study/test/src/org/labkey/test/tests/search/SearchTestDefault.java
+++ study/test/src/org/labkey/test/tests/search/SearchTestDefault.java
@@ -16,10 +16,11 @@
 package org.labkey.test.tests.search;
 
 import org.junit.experimental.categories.Category;
+import org.labkey.test.categories.Daily;
 import org.labkey.test.categories.Search;
 import org.labkey.test.util.search.SearchAdminAPIHelper;
 
-@Category({Search.class})
+@Category({Search.class, Daily.class})
 public class SearchTestDefault extends SearchTest
 {
     @Override
