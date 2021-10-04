83c71ca59abcb2a2c0c3f6109f69194247452b7a
labkey-tchad <tchad@labkey.com>
2021-10-04 09:13:57 -0700

    Fix path check

--- pipeline/src/org/labkey/pipeline/api/PipeRootImpl.java
+++ pipeline/src/org/labkey/pipeline/api/PipeRootImpl.java
@@ -356,7 +356,7 @@ public class PipeRootImpl implements PipeRoot
     @Nullable
     public Path resolveToNioPath(String path)
     {
-        if (StringUtils.isBlank(path))
+        if (path == null)
             throw new NotFoundException("Must specify a file path");
 
         try
