de371b91ad07b93de400d18f07326ed37c4b8ba3
Alan Vezina <alanv@labkey.com>
2021-10-14 15:42:24 -0500

    Fix sql server upgrade script

--- experiment/resources/schemas/dbscripts/sqlserver/exp-21.015-21.016.sql
+++ experiment/resources/schemas/dbscripts/sqlserver/exp-21.015-21.016.sql
@@ -1,3 +1,3 @@
-ALTER TABLE exp.ExperimentRun ADD COLUMN WorkflowTask INT;
+ALTER TABLE exp.ExperimentRun ADD WorkflowTask INT;
 GO
 ALTER TABLE exp.ExperimentRun ADD CONSTRAINT FK_Run_WorfklowTask FOREIGN KEY (WorkflowTask) REFERENCES exp.ProtocolApplication (RowId);
