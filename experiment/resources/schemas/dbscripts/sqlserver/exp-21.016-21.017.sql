0963cb055b9d9eaa1da04d85f61af0916a1ac66b
XingY <xyang@labkey.com>
2021-10-21 15:01:45 -0700

    nullable

--- experiment/resources/schemas/dbscripts/sqlserver/exp-21.016-21.017.sql
+++ experiment/resources/schemas/dbscripts/sqlserver/exp-21.016-21.017.sql
@@ -1,4 +1,4 @@
-ALTER TABLE exp.Material ADD RecomputeRollup BIT NOT NULL CONSTRAINT DF_recomputeRollup DEFAULT(0);
+ALTER TABLE exp.Material ADD RecomputeRollup BIT NULL CONSTRAINT DF_recomputeRollup DEFAULT(0);
 ALTER TABLE exp.Material ADD AliquotCount INT NULL;
 ALTER TABLE exp.Material ADD AliquotVolume FLOAT NULL;
 ALTER TABLE exp.Material ADD AliquotUnit NVARCHAR(10) NULL;
