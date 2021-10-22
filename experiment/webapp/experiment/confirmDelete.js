b5e2044857137210243798a5b01bca8de8bfe587
cnathe <cnathe@labkey.com>
2021-10-22 17:20:06 -0500

    Update sample status enabled JS check

--- experiment/webapp/experiment/confirmDelete.js
+++ experiment/webapp/experiment/confirmDelete.js
@@ -22,8 +22,9 @@ LABKEY.experiment.confirmDelete = function(dataRegionName, schemaName, queryName
                 var cannotDeleteNoun = numCannotDelete === 1 ? nounSingular : nounPlural;
                 var totalNum = numCanDelete + numCannotDelete;
                 var totalNoun = totalNum === 1 ? nounSingular : nounPlural;
-                var dependencyText = LABKEY.moduleContext.experiment && (LABKEY.moduleContext.experiment['experimental-sample-status'] === true) ?
-                        "derived sample or assay data dependencies or status that prevents deletion"
+                var sampleStatusEnabled = LABKEY.moduleContext.api.moduleNames.indexOf('samplemanagement') > -1;
+                var dependencyText = sampleStatusEnabled ?
+                        "derived sample, assay data dependencies, or status that prevents deletion"
                         : "derived sample or assay data dependencies";
                 var text;
                 if (totalNum === 0) {
