0305e0ac4c0595c90cfcf06ba6ae7062e35cab10
ian <ians@labkey.com>
2021-10-27 19:17:13 -0700

    Move SQSUrl parameter to Cloud Store configuration.

---
+++ api/src/org/labkey/api/cloud/CloudWatchService.java
@@ -0,0 +1,25 @@
+package org.labkey.api.cloud;
+
+import org.jetbrains.annotations.Nullable;
+import org.labkey.api.services.ServiceRegistry;
+
+import java.nio.file.Path;
+import java.util.function.BiConsumer;
+
+public interface CloudWatchService
+{
+    static @Nullable CloudWatchService get()
+    {
+        return ServiceRegistry.get().getService(CloudWatchService.class);
+    }
+    static void setInstance(CloudWatchService impl)
+    {
+        ServiceRegistry.get().registerService(CloudWatchService.class, impl);
+    }
+
+    // Listeners are Path/ Store based. These are the notification processors.
+    void registerCloudListener(Path resolvedPath, CloudWatcherConfig config, BiConsumer<Path, Runnable> eventProcessor);
+    void unregisterCloudListener(int watcherConfigId);
+
+    void close();
+}
