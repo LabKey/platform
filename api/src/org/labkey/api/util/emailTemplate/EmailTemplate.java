ab9b24f5a72443c5b2e9905caacce7020f6ad2e1
Karl Lum <klum@labkey.com>
2021-11-16 10:47:40 -0800

    44297: email links to announcements aren't properly encoded (#2778)

--- api/src/org/labkey/api/util/emailTemplate/EmailTemplate.java
+++ api/src/org/labkey/api/util/emailTemplate/EmailTemplate.java
@@ -93,7 +93,7 @@ public abstract class EmailTemplate
                 {
                     return sourceValue;
                 }
-                return PageFlowUtil.filter(sourceValue, true, true);
+                return PageFlowUtil.filter(sourceValue, true, false);
             }
         };
 
