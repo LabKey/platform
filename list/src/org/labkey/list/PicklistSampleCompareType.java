b0cc63aa5f544553c5a7cedbae7191fc55bb4d51
Cory Nathe <cnathe@labkey.com>
2021-11-17 14:57:57 -0600

    Item 9500: Improve Sample display when spanning types (#2781)
    - add/register PicklistSampleCompareType to use in picklist per-sample type grid filtering

---
+++ list/src/org/labkey/list/PicklistSampleCompareType.java
@@ -0,0 +1,40 @@
+package org.labkey.list;
+
+import org.jetbrains.annotations.NotNull;
+import org.labkey.api.data.CompareType;
+import org.labkey.api.data.Container;
+import org.labkey.api.data.SimpleFilter;
+import org.labkey.api.data.TableInfo;
+import org.labkey.api.data.TableSelector;
+import org.labkey.api.exp.list.ListDefinition;
+import org.labkey.api.exp.list.ListService;
+import org.labkey.api.query.FieldKey;
+import org.labkey.api.query.QueryService;
+import org.labkey.api.security.User;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class PicklistSampleCompareType extends CompareType
+{
+    public PicklistSampleCompareType()
+    {
+        super("Samples for picklist", "picklistsamples", "PICKLIST_SAMPLES", true, " matches picklist ", null);
+    }
+
+    @Override
+    public SimpleFilter.FilterClause createFilterClause(@NotNull FieldKey fieldKey, Object value)
+    {
+        final Integer listId = Integer.parseInt(value.toString());
+        final User user = (User) QueryService.get().getEnvironment(QueryService.Environment.USER);
+        final Container container = (Container) QueryService.get().getEnvironment(QueryService.Environment.CONTAINER);
+
+        ListDefinition listDef = ListService.get().getList(container, listId);
+        if (listDef == null || !listDef.isPicklist())
+            return new SimpleFilter.FalseClause();
+
+        TableInfo listTable = listDef.getTable(user, container);
+        Integer[] sampleIds = new TableSelector(listTable, Collections.singleton("SampleID")).getArray(Integer.class);
+        return new SimpleFilter.InClause(fieldKey, Arrays.asList(sampleIds));
+    }
+}
