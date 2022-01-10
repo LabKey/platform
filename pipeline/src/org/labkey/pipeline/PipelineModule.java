cbe6f5daa0172ed6d3c6d88d56b88174752b143d
Josh Eckels <jeckels@labkey.com>
2022-01-10 08:07:22 -0800

    Minor cleanup after adding pipeline job metrics for issue 44605 (#2924)

--- pipeline/src/org/labkey/pipeline/PipelineModule.java
+++ pipeline/src/org/labkey/pipeline/PipelineModule.java
@@ -15,7 +15,6 @@
  */
 package org.labkey.pipeline;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.labkey.api.admin.notification.NotificationService;
@@ -26,7 +25,6 @@ import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerManager;
 import org.labkey.api.data.RuntimeSQLException;
 import org.labkey.api.data.SQLFragment;
-import org.labkey.api.data.SqlExecutor;
 import org.labkey.api.data.SqlSelector;
 import org.labkey.api.files.FileContentService;
 import org.labkey.api.files.TableUpdaterFileListener;
@@ -45,11 +43,11 @@ import org.labkey.api.pipeline.file.PathMapperImpl;
 import org.labkey.api.pipeline.trigger.PipelineTriggerRegistry;
 import org.labkey.api.pipeline.trigger.PipelineTriggerType;
 import org.labkey.api.security.User;
-import org.labkey.api.usageMetrics.UsageMetricsProvider;
 import org.labkey.api.usageMetrics.UsageMetricsService;
 import org.labkey.api.util.ContextListener;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.util.emailTemplate.EmailTemplateService;
+import org.labkey.api.util.logging.LogHelper;
 import org.labkey.api.view.BaseWebPartFactory;
 import org.labkey.api.view.DefaultWebPartFactory;
 import org.labkey.api.view.Portal;
@@ -98,11 +96,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- */
 public class PipelineModule extends SpringModule implements ContainerManager.ContainerListener
 {
-    private static final Logger _log = LogManager.getLogger(PipelineModule.class);
+    private static final Logger _log = LogHelper.getLogger(PipelineModule.class, "Module responsible for managing pipeline jobs and logs");
 
     @Override
     public String getName()
@@ -211,21 +207,16 @@ public class PipelineModule extends SpringModule implements ContainerManager.Con
 
         AuditLogService.get().registerAuditType(new ProtocolManagementAuditProvider());
 
-        UsageMetricsService.get().registerUsageMetrics("Pipeline", new UsageMetricsProvider()
-        {
-            @Override
-            public Map<String, Object> getUsageMetrics()
-            {
-                Map<String, Long> jobCounts = new HashMap<>();
-                SQLFragment sql = new SQLFragment("SELECT COUNT(*) AS JobCount, COALESCE(Provider, 'Unknown') AS Provider FROM ");
-                sql.append(PipelineSchema.getInstance().getTableInfoStatusFiles(), "sf");
-                sql.append(" GROUP BY Provider");
+        UsageMetricsService.get().registerUsageMetrics(getName(), () -> {
+            Map<String, Long> jobCounts = new HashMap<>();
+            SQLFragment sql = new SQLFragment("SELECT COUNT(*) AS JobCount, COALESCE(Provider, 'Unknown') AS Provider FROM ");
+            sql.append(PipelineSchema.getInstance().getTableInfoStatusFiles(), "sf");
+            sql.append(" GROUP BY Provider");
 
-                new SqlSelector(PipelineSchema.getInstance().getSchema(), sql).forEach(rs ->
-                        jobCounts.put(rs.getString("Provider"), rs.getLong("JobCount")));
+            new SqlSelector(PipelineSchema.getInstance().getSchema(), sql).forEach(rs ->
+                    jobCounts.put(rs.getString("Provider"), rs.getLong("JobCount")));
 
-                return Collections.singletonMap("jobCounts", jobCounts);
-            }
+            return Collections.singletonMap("jobCounts", jobCounts);
         });
     }
 
@@ -350,7 +341,7 @@ public class PipelineModule extends SpringModule implements ContainerManager.Con
             }
 
             // TODO: is this the correct spot to start all of the trigger configs?
-            for (PipelineTriggerType triggerType : PipelineTriggerRegistry.get().getTypes())
+            for (PipelineTriggerType<?> triggerType : PipelineTriggerRegistry.get().getTypes())
             {
                 triggerType.startAll();
             }
