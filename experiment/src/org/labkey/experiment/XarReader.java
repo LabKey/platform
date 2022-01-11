a9f22d5949ab1e626e99c4a0040b0d03c4798d02
labkey-jeckels <jeckels@labkey.com>
2022-01-10 18:03:06 -0800

    Issue 44579: XARs Allow Creation of Samples Without a Sample Type

--- experiment/src/org/labkey/experiment/XarReader.java
+++ experiment/src/org/labkey/experiment/XarReader.java
@@ -891,8 +891,7 @@ public class XarReader extends AbstractXarImporter
                 experimentDataObject.setBatchProtocolId(batchProtocol.getRowId());
             }
 
-            experimentDataObject = Table.insert(getUser(), tiExperiment, experimentDataObject);
-            experiment = new ExpExperimentImpl(experimentDataObject);
+            Table.insert(getUser(), tiExperiment, experimentDataObject);
 
             ObjectProperty contactProperty = null;
             if (null != exp.getContact())
@@ -1235,7 +1234,7 @@ public class XarReader extends AbstractXarImporter
             protocolApp.setLSID(protAppLSID);
             protocolApp.setName(trimString(xmlProtocolApp.getName()));
             String cpasType = trimString(xmlProtocolApp.getCpasType());
-            checkProtocolApplicationCpasType(cpasType, getLog());
+            checkProtocolApplicationCpasType(cpasType);
             protocolApp.setCpasType(cpasType);
             protocolApp.setProtocolLSID(protocolLSID);
             protocolApp.setActivityDate(sqlDateTime);
@@ -1348,7 +1347,7 @@ public class XarReader extends AbstractXarImporter
     private ExpMaterial loadMaterial(MaterialBaseType xbMaterial,
                                   @Nullable ExperimentRun run,
                                   Integer sourceApplicationId,
-                                  XarContext context) throws XarFormatException
+                                  XarContext context) throws XarFormatException, ExperimentException
     {
         TableInfo tiMaterial = ExperimentServiceImpl.get().getTinfoMaterial();
 
@@ -2414,7 +2413,7 @@ public class XarReader extends AbstractXarImporter
 
         XmlCursor c = xObj.newCursor();
         XmlCursor cTest;
-        Map m = new HashMap();
+        Map<String, String> m = new HashMap<>();
         c.getAllNamespaces(m);
         String nsObject = c.getName().getNamespaceURI();
 
