f1427fdfbad4d98eeb30cf5fc9360960a4bf9a24
Adam Rauch <adam@labkey.com>
2022-01-24 15:24:39 -0800

    Clean up email prefs, formats, unused tables, etc.

--- core/resources/schemas/dbscripts/postgresql/core-0.000-21.000.sql
+++ core/resources/schemas/dbscripts/postgresql/core-0.000-21.000.sql
@@ -473,55 +473,6 @@ INSERT INTO core.emailOptions (EmailOptionId, EmailOption, Type) VALUES
                                                                      (513, '15 minute digest', 'files'),
                                                                      (514, 'Daily digest', 'files');
 
-CREATE TABLE core.EmailFormats
-(
-    EmailFormatId INT4 NOT NULL,
-    EmailFormat VARCHAR(20),
-
-    CONSTRAINT PK_EmailFormats PRIMARY KEY (EmailFormatId)
-);
-
-INSERT INTO core.EmailFormats (EmailFormatId, EmailFormat) VALUES (0, 'Plain Text');
-INSERT INTO core.EmailFormats (EmailFormatId, EmailFormat) VALUES (1, 'HTML');
-
-CREATE TABLE core.PageTypes
-(
-    PageTypeId INT4 NOT NULL,
-    PageType VARCHAR(20),
-
-    CONSTRAINT PK_PageTypes PRIMARY KEY (PageTypeId)
-);
-
-INSERT INTO core.PageTypes (PageTypeId, PageType) VALUES (0, 'Message');
-INSERT INTO core.PageTypes (PageTypeId, PageType) VALUES (1, 'Wiki');
-
-CREATE TABLE core.EmailPrefs
-(
-    Container ENTITYID,
-    UserId USERID,
-    EmailOptionId INT4 NOT NULL,
-    EmailFormatId INT4 NOT NULL,
-    PageTypeId INT4 NOT NULL,
-    LastModifiedBy USERID,
-    Type VARCHAR(60) NOT NULL DEFAULT 'messages',
-    SrcIdentifier VARCHAR(100) NOT NULL, -- allow subscriptions to multiple forums within a single container
-
-    CONSTRAINT PK_EmailPrefs PRIMARY KEY (Container, UserId, Type, SrcIdentifier),
-    CONSTRAINT FK_EmailPrefs_Containers FOREIGN KEY (Container) REFERENCES core.Containers (EntityId),
-    CONSTRAINT FK_EmailPrefs_Principals FOREIGN KEY (UserId) REFERENCES core.Principals (UserId),
-    CONSTRAINT FK_EmailPrefs_EmailOptions FOREIGN KEY (EmailOptionId) REFERENCES core.EmailOptions (EmailOptionId),
-    CONSTRAINT FK_EmailPrefs_EmailFormats FOREIGN KEY (EmailFormatId) REFERENCES core.EmailFormats (EmailFormatId),
-    CONSTRAINT FK_EmailPrefs_PageTypes FOREIGN KEY (PageTypeId) REFERENCES core.PageTypes (PageTypeId)
-);
-
-ALTER TABLE core.EmailPrefs DROP CONSTRAINT FK_EmailPrefs_PageTypes;
-ALTER TABLE core.EmailPrefs DROP COLUMN PageTypeId;
-ALTER TABLE core.EmailPrefs DROP CONSTRAINT FK_EmailPrefs_EmailFormats;
-ALTER TABLE core.EmailPrefs DROP COLUMN EmailFormatId;
-DROP TABLE core.PageTypes;
-
-DROP TABLE core.EmailFormats;
-
 -- sample manager email notification options
 INSERT INTO core.emailOptions (EmailOptionId, EmailOption, Type) VALUES (701, 'No Email', 'samplemanager');
 INSERT INTO core.emailOptions (EmailOptionId, EmailOption, Type) VALUES (702, 'All emails', 'samplemanager');
@@ -529,6 +480,21 @@ INSERT INTO core.emailOptions (EmailOptionId, EmailOption, Type) VALUES (702, 'A
 -- labbook email notification options
 INSERT INTO core.emailOptions (EmailOptionId, EmailOption, Type) VALUES (801, 'No Email', 'labbook');
 INSERT INTO core.emailOptions (EmailOptionId, EmailOption, Type) VALUES (802, 'All emails', 'labbook');
+
+CREATE TABLE core.EmailPrefs
+(
+    Container ENTITYID,
+    UserId USERID,
+    EmailOptionId INT4 NOT NULL,
+    LastModifiedBy USERID,
+    Type VARCHAR(60) NOT NULL DEFAULT 'messages',
+    SrcIdentifier VARCHAR(100) NOT NULL, -- allow subscriptions to multiple forums within a single container
+
+    CONSTRAINT PK_EmailPrefs PRIMARY KEY (Container, UserId, Type, SrcIdentifier),
+    CONSTRAINT FK_EmailPrefs_Containers FOREIGN KEY (Container) REFERENCES core.Containers (EntityId),
+    CONSTRAINT FK_EmailPrefs_Principals FOREIGN KEY (UserId) REFERENCES core.Principals (UserId),
+    CONSTRAINT FK_EmailPrefs_EmailOptions FOREIGN KEY (EmailOptionId) REFERENCES core.EmailOptions (EmailOptionId)
+);
 
 -- This empty stored procedure doesn't directly change the database, but calling it from a sql script signals the
 -- script runner to invoke the specified method at this point in the script running process. See implementations
