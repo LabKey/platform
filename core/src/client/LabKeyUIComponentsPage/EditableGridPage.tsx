faa181a0575ac8b1b8a4c1cca83bc34cf41240ff
cnathe <cnathe@labkey.com>
2022-02-11 17:19:10 -0600

    Core components page conversion for EditableGridPage to use EditableGridPanel

--- core/src/client/LabKeyUIComponentsPage/EditableGridPage.tsx
+++ core/src/client/LabKeyUIComponentsPage/EditableGridPage.tsx
@@ -2,145 +2,108 @@
  * Copyright (c) 2019 LabKey Corporation. All rights reserved. No portion of this work may be reproduced in
  * any form or by any electronic or mechanical means without written permission from LabKey Corporation.
  */
-import React, {PureComponent} from 'react'
-import {List, Map} from "immutable";
+import React, { FC, memo, useCallback, useEffect, useState } from 'react'
+import { List, Map } from 'immutable';
 import {
-    SchemaQuery,
-    IGridLoader,
-    EditableColumnMetadata,
-    EditableGridPanelDeprecated,
-    getQueryGridModel,
-    getStateQueryGridModel,
-    gridInit,
-    QueryGridModel
+    isLoading,
+    QueryModel,
+    withQueryModels,
+    EditableGridPanel,
+    EditorModelProps,
+    EditorModel,
+    loadEditorModelData,
+    InjectedQueryModels,
+    LoadingSpinner,
+    Alert,
+    resolveErrorMessage,
 } from "@labkey/components";
 
 import {SchemaQueryInputContext, SchemaQueryInputProvider} from "./SchemaQueryInputProvider";
 
-const columnMetadata = Map<string, EditableColumnMetadata>({
-    "Name": {
-        readOnly: true
-    },
-    "Description": {
-        placeholder: "Enter a description"
-    }
-});
-
 const defaultProps = {
     title: 'EditableGridPanel',
-    initialEmptyRowCount: 0,
     bordered: true,
-    condensed: false,
     striped: true,
     allowAdd: true,
     allowBulkAdd: true,
     allowBulkRemove: true,
     allowBulkUpdate: true,
-    addControlProps: {
-        nounPlural: 'rows',
-        nounSingular: 'row'
-    },
     bulkAddProps: {
         title: 'Bulk Add'
     },
 };
 
-class EditableGridPageImpl extends PureComponent<SchemaQueryInputContext> {
+const EditableGridPageBody: FC<InjectedQueryModels> = memo(props => {
+    const { queryModels, actions } = props;
+    const { model } = queryModels;
+    const [dataModel, setDataModel] = useState<QueryModel>();
+    const [editorModel, setEditorModel] = useState<EditorModel>();
+    const [error, setError] = useState<String>();
 
-    componentWillMount() {
-        this.initEditableModel();
-    }
-
-    initEditableModel() {
-        const loader: IGridLoader = {
-            fetch: (model) => {
-                return new Promise((resolve) => {
-                    const data = Map<any, any>({
-                        "123": Map<string, any>({
-                            "Name": List([
-                                {
-                                    displayValue: "Amino Acid",
-                                    value: "Amino Acid"
-                                }
-                            ])
-                        }),
-                        "243": Map<string, any>({
-                            "Name": List([
-                                {
-                                    displayValue: "Flow",
-                                    value: "Flow"
-                                }
-                            ])
-                        })
-                    });
-
-                    resolve({
-                        data,
-                        dataIds: data.keySeq().toList(),
-                    });
-                });
+    useEffect(() => {
+        (async () => {
+            if (!isLoading(model.rowsLoadingState) && !editorModel) {
+                try {
+                    const editorModelData = await loadEditorModelData(model);
+                    setEditorModel(new EditorModel({
+                        id: model.id,
+                        ...editorModelData,
+                    }));
+                    setDataModel(model);
+                } catch (err) {
+                    console.error(err);
+                    setError(resolveErrorMessage(err))
+                }
             }
-        };
+        })();
+    }, [model, editorModel]);
 
-        const editorModelWithData = getStateQueryGridModel('edit-with-data',
-            SchemaQuery.create('assay', 'AssayList'), {
-                loader,
-                editable: true
-            }
-        );
-
-        gridInit(editorModelWithData, true, this);
-    }
-
-    getEditorQueryGridModel = (): QueryGridModel => {
-        const { model } = this.props;
-        const editModel = getStateQueryGridModel('component-editormodel', SchemaQuery.create(model.schema, model.query), () => {
-            return {
-                editable: true,
-                queryInfo: model.queryInfo,
-                bindURL: false,
-                loader: {
-                    fetch: () => {
-                        const gridData = Map<string, Map<string, any>>();
-                        return new Promise(resolve => {
-                            resolve({
-                                data: gridData,
-                                dataIds: gridData.keySeq().toList(),
-                            });
-                        });
-                    },
-                },
-            };
-        });
-
-        return getQueryGridModel(editModel.getId()) || editModel;
-    };
-
-    render() {
-        const { model } = this.props;
-        const editorModelWithData  = getQueryGridModel("edit-with-data|assay/assaylist");
-
-        // TODO hack to force rerender after model is loaded
-        if (model && !model.isLoaded) {
-            window.setTimeout(() => this.forceUpdate(), 500);
+    const onGridChange = useCallback((
+        editorModelChanges: Partial<EditorModelProps>,
+        dataKeys?: List<any>,
+        data?: Map<any, Map<string, any>>
+    ) => {
+        const orderedRows = dataKeys?.toJS();
+        const rows = data?.toJS();
+        if (orderedRows !== undefined && rows !== undefined) {
+            setDataModel(dataModel.mutate({ orderedRows, rows }));
         }
 
-        return (
-            <>
-                {model
-                    ? <EditableGridPanelDeprecated
-                        {...defaultProps}
-                        model={this.getEditorQueryGridModel()}
-                    />
-                    : <EditableGridPanelDeprecated
-                        {...defaultProps}
-                        model={editorModelWithData}
-                        columnMetadata={columnMetadata}
-                    />
-                }
-            </>
-        );
+        // console.log('editorModelChanges', editorModelChanges?.cellValues?.toJS());
+        setEditorModel(editorModel.merge(editorModelChanges) as EditorModel);
+    }, [editorModel, dataModel]);
+
+    if (model.loadErrors?.length > 0) {
+        return <Alert>{model.loadErrors.join(' ')}</Alert>;
     }
+    if (!editorModel || !dataModel) {
+        return <LoadingSpinner />
+    }
+    if (error) {
+        return <Alert>{error}</Alert>;
+    }
+    // console.log('editorModel', editorModel?.cellValues?.toJS());
+
+    return (
+        <EditableGridPanel
+            {...defaultProps}
+            model={dataModel}
+            editorModel={editorModel}
+            onChange={onGridChange}
+        />
+    )
+});
+
+export const EditableGridPageWithQueryModels = withQueryModels(EditableGridPageBody);
+
+export const EditableGridPageImpl: FC<SchemaQueryInputContext> = props => {
+    const { queryConfig } = props;
+    const queryConfigs = { model: queryConfig };
+
+    if (!queryConfig)
+        return null;
+
+    return <EditableGridPageWithQueryModels autoLoad key={queryConfig.id} queryConfigs={queryConfigs}/>
 }
 
 export const EditableGridPage = SchemaQueryInputProvider(EditableGridPageImpl);
