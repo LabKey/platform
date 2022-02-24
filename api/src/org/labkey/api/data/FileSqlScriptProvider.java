13b05525faa0b6dce4a8a1e4c9a12f825d90351a
Adam Rauch <adam@labkey.com>
2022-02-23 19:28:11 -0800

    Warn if schema version is less than script end version

--- api/src/org/labkey/api/data/FileSqlScriptProvider.java
+++ api/src/org/labkey/api/data/FileSqlScriptProvider.java
@@ -362,6 +362,14 @@ public class FileSqlScriptProvider implements SqlScriptProvider
                 _validName = true;
             else
                 assert false : fileName + " has bad version numbers!"; // Ignore on production, throw on development
+
+            Double schemaVersion = _provider._module.getSchemaVersion();
+
+            if (schemaVersion == null)
+                throw new IllegalStateException("SQL scripts and null schema version do not compute");
+
+            if (_toVersion > schemaVersion)
+                _log.warn(fileName + " will never execute because " + _provider._module.getName() + " schema version is less than its \"to\" version");
         }
 
         // Used for DROP and CREATE scripts... so we don't bother verifying filename or parsing info from it
