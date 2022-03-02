c3a78c8bc358fbf69132b518b7f57b34c39a2038
labkey-matthewb <matthewb@labkey.com>
2022-03-02 11:30:41 -0800

    Fix OOR display (#3104)
    * OutOfRangeDisplayColumn display column broke because of a small refactor to DataColumn.getFormattedHtml().  I think this version is more inline with the intended uses of these methods.
    
    * TestCase

--- core/src/org/labkey/core/CoreModule.java
+++ core/src/org/labkey/core/CoreModule.java
@@ -1121,6 +1121,7 @@ public class CoreModule extends SpringModule implements SearchService.DocumentPr
         return Set.of(
             CommandLineTokenizer.TestCase.class,
             CopyFileRootPipelineJob.TestCase.class,
+            OutOfRangeDisplayColumn.TestCase.class,
             PostgreSqlVersion.TestCase.class,
             ScriptEngineManagerImpl.TestCase.class,
             StatsServiceImpl.TestCase.class
