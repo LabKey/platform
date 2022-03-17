b40a8888432c32dc235f208e95d6a6f7f1b2e72d
labkey-susanh <susanh@labkey.com>
2022-03-16 17:03:32 -0700

    Comments are good.

--- assay/src/org/labkey/assay/query/AssayListTable.java
+++ assay/src/org/labkey/assay/query/AssayListTable.java
@@ -75,8 +75,6 @@ public class AssayListTable extends FilteredTable<AssaySchemaImpl>
         addWrapColumn(_rootTable.getColumn("ModifiedBy"));
         addWrapColumn("Folder", _rootTable.getColumn("Container"));
 
-
-
         // Generate a CASE statement that matches an LSID to an AssayProvider so we can create a lookup to the AssayProviderTable.
         // The column value is null if no AssayProvider is matched.
         SQLFragment typeFrag = new SQLFragment();
@@ -100,6 +98,7 @@ public class AssayListTable extends FilteredTable<AssaySchemaImpl>
 
         addWrapColumn(_rootTable.getColumn("Status")).setHidden(true);
 
+        // put the hidden column at the end so it will render at the end by default.
         var lsidColumn = addWrapColumn(_rootTable.getColumn("LSID"));
         lsidColumn.setHidden(true);
 
