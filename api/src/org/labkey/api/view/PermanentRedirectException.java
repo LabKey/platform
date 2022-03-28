3bbd9b13bd2f7942507941ef78af485ea8cee61f
Adam Rauch <adam@labkey.com>
2022-03-28 11:57:26 -0700

    Update api/src/org/labkey/api/view/PermanentRedirectException.java
    Co-authored-by: Josh Eckels <jeckels@labkey.com>

--- api/src/org/labkey/api/view/PermanentRedirectException.java
+++ api/src/org/labkey/api/view/PermanentRedirectException.java
@@ -5,6 +5,7 @@ import org.labkey.api.util.URLHelper;
 
 import javax.servlet.http.HttpServletResponse;
 
+/** Use when we want search engines, browsers, etc to assume that the redirecting URL is defunct and the target URL should be used going forward */
 public class PermanentRedirectException extends RedirectException
 {
     public PermanentRedirectException(@NotNull URLHelper url)
