01dd800891ec6810b3d61bc6a3701beb5d1247df
Matthew Bellew <matthewb@labkey.com>
2022-04-05 17:40:52 -0700

    comments

--- api/src/org/labkey/api/action/HasAllowBindParameter.java
+++ api/src/org/labkey/api/action/HasAllowBindParameter.java
@@ -5,6 +5,11 @@ import org.labkey.api.collections.CaseInsensitiveHashSet;
 import java.util.Set;
 import java.util.function.Predicate;
 
+/**
+ * This interface provides a mechanism for MVC Form objects to control which fields will be bound.  By default, only simple
+ * fields (no object path following) are allowed.  Forms may implement this interface to either a) allow any of the properties that
+ * are restricted by default, or b) allow complex binding scenarios.
+ */
 public interface HasAllowBindParameter
 {
     Predicate<String> allowBindParameter();
