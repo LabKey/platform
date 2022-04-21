15206bda4789d0079e06a86baab9c41316c3bf92
Matthew Bellew <matthewb@labkey.com>
2022-04-21 13:17:21 -0700

    DRT suite passes with no inline script

--- pipeline/src/org/labkey/pipeline/status/StatusDataRegion.java
+++ pipeline/src/org/labkey/pipeline/status/StatusDataRegion.java
@@ -79,7 +79,7 @@ public class StatusDataRegion extends DataRegion
         String action = SpringActionController.getActionName(_apiAction);
         PageConfig config = HttpView.currentPageConfig();
 
-        out.write("<script type=\"text/javascript\" nonce=\"" + config.getScriptNonce() + "\">" +
+        out.write("<script type=\"text/javascript\" nonce=\"" + PageFlowUtil.filter(config.getScriptNonce()) + "\">" +
                 "LABKEY.requiresExt4Sandbox(function() {\n" +
                     "LABKEY.requiresScript('pipeline/StatusUpdate.js', function(){\n" +
                         "if (!LABKEY.pipeline.statusUpdateInstance)\n" +
