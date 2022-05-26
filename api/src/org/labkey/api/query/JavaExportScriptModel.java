a174547af15aba6165be137c360661ec90fb4cf3
Adam Rauch <adam@labkey.com>
2022-05-26 09:52:35 -0700

    Properly escape Java strings in exported script

--- api/src/org/labkey/api/query/JavaExportScriptModel.java
+++ api/src/org/labkey/api/query/JavaExportScriptModel.java
@@ -15,6 +15,7 @@
  */
 package org.labkey.api.query;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.labkey.api.collections.CsvSet;
 import org.labkey.api.data.CompareType;
 import org.labkey.api.data.DisplayColumn;
@@ -125,9 +126,9 @@ public class JavaExportScriptModel extends ExportScriptModel
             sb.append("Collections.singletonList(");
     }
 
-    private String quote(String name)
+    private String quote(String value)
     {
-        return "\"" + name + "\"";
+        return "\"" + StringEscapeUtils.escapeJava(value) + "\"";
     }
 
     @Override
