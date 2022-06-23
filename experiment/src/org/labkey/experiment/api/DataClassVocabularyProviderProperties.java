2cae15471b85be8dce79c56f2d52545101d613e4
XingY <xyang@labkey.com>
2022-06-22 17:54:42 -0700

    format

--- experiment/src/org/labkey/experiment/api/DataClassVocabularyProviderProperties.java
+++ experiment/src/org/labkey/experiment/api/DataClassVocabularyProviderProperties.java
@@ -2,6 +2,9 @@ package org.labkey.experiment.api;
 
 import org.labkey.api.exp.property.ConceptURIVocabularyDomainProvider;
 
-public record DataClassVocabularyProviderProperties(String sourceColumnName /* the dataclass column that has attached vocabulary*/, String sourceColumnLabel /* the label of the dataclass column */, String vocabularyDomainName /* vocabulary property column field key */, ConceptURIVocabularyDomainProvider conceptURIVocabularyDomainProvider /*the ConceptURIVocabularyDomainProvider that matches the column's conceptURI*/)
+public record DataClassVocabularyProviderProperties(String sourceColumnName /* the dataclass column that has attached vocabulary*/,
+                                                    String sourceColumnLabel /* the label of the dataclass column */,
+                                                    String vocabularyDomainName /* vocabulary property column field key */,
+                                                    ConceptURIVocabularyDomainProvider conceptURIVocabularyDomainProvider /*the ConceptURIVocabularyDomainProvider that matches the column's conceptURI*/)
 {
 }
