e21f6fae0af5a1b7dc52fb7fa1b2c8136774fb52
Adam Rauch <adam@labkey.com>
2022-06-27 08:41:42 -0700

    Update cranky message

--- api/src/org/labkey/api/settings/StartupProperty.java
+++ api/src/org/labkey/api/settings/StartupProperty.java
@@ -7,7 +7,7 @@ public interface StartupProperty
     // As a convenience for enum implementations, getStartupPropertyName() simply returns Enum.name()
     default String name()
     {
-        throw new IllegalStateException("Must override either name() or getStartupProp()");
+        throw new IllegalStateException("Must override getStartupProp()");
     }
 
     // Implementations can override this to use an alternative property name (not name()) or to filter out specific
