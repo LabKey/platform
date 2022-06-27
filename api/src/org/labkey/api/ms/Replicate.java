0f97b34f4d88ce12a03e14703fecae19b43e9800
ankurjuneja <ankurjuneja05@gmail.com>
2022-06-27 16:51:20 -0700

    use lombok

--- api/src/org/labkey/api/ms/Replicate.java
+++ api/src/org/labkey/api/ms/Replicate.java
@@ -1,27 +1,10 @@
 package org.labkey.api.ms;
 
+import lombok.Data;
+
+@Data
 public class Replicate
 {
     private long _id;
     private String _name;
-
-    public long getId()
-    {
-        return _id;
-    }
-
-    public void setId(long id)
-    {
-        _id = id;
-    }
-
-    public String getName()
-    {
-        return _name;
-    }
-
-    public void setName(String name)
-    {
-        _name = name;
-    }
 }
