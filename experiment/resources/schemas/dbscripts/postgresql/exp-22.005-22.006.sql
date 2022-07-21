bc329054c0d65b4b8e3489c1cf1a4953d70e23d8
labkey-nicka <nickk@labkey.com>
2022-07-21 10:09:12 -0700

    Revise exp.edge constraints/indices

--- experiment/resources/schemas/dbscripts/postgresql/exp-22.005-22.006.sql
+++ experiment/resources/schemas/dbscripts/postgresql/exp-22.005-22.006.sql
@@ -8,5 +8,7 @@ ALTER TABLE exp.Edge
     ADD SourceKey VARCHAR(200) NULL,
 
     ADD CONSTRAINT FK_Edge_SourceId_Object FOREIGN KEY (SourceId) REFERENCES exp.Object (Objectid),
-    ADD CONSTRAINT UQ_Edge_FromTo_RunId_SourceIdSourceKey UNIQUE (FromObjectId, ToObjectId, RunId, SourceId, SourceKey),
-    ADD CONSTRAINT UQ_Edge_ToFrom_RunId_SourceIdSourceKey UNIQUE (ToObjectId, FromObjectId, RunId, SourceId, SourceKey);
+    ADD CONSTRAINT UQ_Edge_FromTo_RunId_SourceId_SourceKey UNIQUE (FromObjectId, ToObjectId, RunId, SourceId, SourceKey);
+
+CREATE INDEX IX_Edge_ToObjectId ON exp.Edge(ToObjectId);
+CREATE INDEX IX_Edge_SourceId ON exp.Edge(SourceId);
--- experiment/resources/schemas/dbscripts/sqlserver/exp-22.005-22.006.sql
+++ experiment/resources/schemas/dbscripts/sqlserver/exp-22.005-22.006.sql
@@ -7,6 +7,8 @@ ALTER TABLE exp.Edge ADD SourceId INT NULL;
 ALTER TABLE exp.Edge ADD SourceKey NVARCHAR(200) NULL;
 
 ALTER TABLE exp.Edge ADD CONSTRAINT FK_Edge_SourceId_Object FOREIGN KEY (SourceId) REFERENCES exp.Object (Objectid);
-ALTER TABLE exp.Edge ADD CONSTRAINT UQ_Edge_FromTo_RunId_SourceIdSourceKey UNIQUE (FromObjectId, ToObjectId, RunId, SourceId, SourceKey);
-ALTER TABLE exp.Edge ADD CONSTRAINT UQ_Edge_ToFrom_RunId_SourceIdSourceKey UNIQUE (ToObjectId, FromObjectId, RunId, SourceId, SourceKey);
+ALTER TABLE exp.Edge ADD CONSTRAINT UQ_Edge_FromTo_RunId_SourceId_SourceKey UNIQUE (FromObjectId, ToObjectId, RunId, SourceId, SourceKey);
+
+CREATE INDEX IX_Edge_ToObjectId ON exp.Edge(ToObjectId);
+CREATE INDEX IX_Edge_SourceId ON exp.Edge(SourceId);
 GO
