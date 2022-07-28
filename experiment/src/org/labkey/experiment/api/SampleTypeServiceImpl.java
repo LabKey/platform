350de943842333234de87c6494ab2bf45644343f
Susan Hert <susanh@labkey.com>
2022-07-27 17:36:42 -0700

    Data generator for Experiment objects (#3558)

--- experiment/src/org/labkey/experiment/api/SampleTypeServiceImpl.java
+++ experiment/src/org/labkey/experiment/api/SampleTypeServiceImpl.java
@@ -622,7 +622,15 @@ public class SampleTypeServiceImpl extends AbstractAuditHandler implements Sampl
     public ExpSampleTypeImpl createSampleType(Container c, User u, String name, String description, List<GWTPropertyDescriptor> properties, List<GWTIndex> indices, int idCol1, int idCol2, int idCol3, int parentCol, String nameExpression)
             throws ExperimentException
     {
-        return createSampleType(c,u,name,description,properties,indices,idCol1,idCol2,idCol3,parentCol,null, null);
+        return createSampleType(c,u,name,description,properties,indices,idCol1,idCol2,idCol3,parentCol,nameExpression, null);
+    }
+
+    @NotNull
+    @Override
+    public ExpSampleTypeImpl createSampleType(Container c, User u, String name, String description, List<GWTPropertyDescriptor> properties, List<GWTIndex> indices, String nameExpression)
+            throws ExperimentException
+    {
+        return createSampleType(c,u,name,description,properties,indices,-1,-1,-1, -1, nameExpression, null);
     }
 
     @NotNull
