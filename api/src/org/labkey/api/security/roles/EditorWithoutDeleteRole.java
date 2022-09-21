5798b27f3ea19c40ff7731d128a4b167fd9921f2
cnathe <cnathe@labkey.com>
2022-09-21 10:00:21 -0500

    EditorWithoutDeleteRole extends EditorRole

--- api/src/org/labkey/api/security/roles/EditorWithoutDeleteRole.java
+++ api/src/org/labkey/api/security/roles/EditorWithoutDeleteRole.java
@@ -15,51 +15,10 @@
  */
 package org.labkey.api.security.roles;
 
-import org.labkey.api.lists.permissions.ManagePicklistsPermission;
-import org.labkey.api.pipeline.PipeRoot;
-import org.labkey.api.reports.permissions.EditSharedReportPermission;
-import org.labkey.api.reports.permissions.ShareReportPermission;
-import org.labkey.api.security.SecurableResource;
-import org.labkey.api.security.SecurityPolicy;
-import org.labkey.api.security.permissions.AssayReadPermission;
-import org.labkey.api.security.permissions.DataClassReadPermission;
-import org.labkey.api.security.permissions.EditSharedViewPermission;
-import org.labkey.api.security.permissions.InsertPermission;
-import org.labkey.api.security.permissions.MediaReadPermission;
-import org.labkey.api.security.permissions.NotebookReadPermission;
-import org.labkey.api.security.permissions.ReadPermission;
-import org.labkey.api.security.permissions.ReadSomePermission;
-import org.labkey.api.security.permissions.SampleWorkflowJobPermission;
-import org.labkey.api.security.permissions.UpdatePermission;
-import org.labkey.api.study.Dataset;
-import org.labkey.api.study.Study;
-import org.labkey.api.study.permissions.SharedParticipantGroupPermission;
-
-public class EditorWithoutDeleteRole extends AbstractRole
+public class EditorWithoutDeleteRole extends EditorRole
 {
     public EditorWithoutDeleteRole()
     {
-        super("Editor without Delete", "Editors in this role may read, add, and update information but not delete.",
-                ReadPermission.class,
-                ReadSomePermission.class,
-                AssayReadPermission.class,
-                DataClassReadPermission.class,
-                MediaReadPermission.class,
-                NotebookReadPermission.class,
-                InsertPermission.class,
-                UpdatePermission.class,
-                EditSharedViewPermission.class,
-                ShareReportPermission.class,
-                EditSharedReportPermission.class,
-                SharedParticipantGroupPermission.class,
-                ManagePicklistsPermission.class,
-                SampleWorkflowJobPermission.class
-        );
-    }
-
-    @Override
-    public boolean isApplicable(SecurityPolicy policy, SecurableResource resource)
-    {
-        return super.isApplicable(policy,resource) || resource instanceof PipeRoot || resource instanceof Study || resource instanceof Dataset;
+        super("Editor without Delete", "Editors in this role may read, add, and update information but not delete.", BASE_EDITOR_PERMISSIONS);
     }
 }
