66b6093a8607d9ad946725bbb08aea1fc200fc1b
Alan Vezina <alanv@labkey.com>
2022-11-16 11:24:39 -0600

    Convert SSOFields to FC, fix styling for images

--- core/src/client/components/SSOFields.tsx
+++ core/src/client/components/SSOFields.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, PureComponent, ReactNode } from 'react';
+import React, { FC, memo, useCallback, useState } from 'react';
 import { FileAttachmentForm } from '@labkey/components';
 import classNames from 'classnames';
 
@@ -9,131 +9,97 @@ const NoImageSelectedDisplay: FC = memo(() => (
     </div>
 ));
 
-interface ImgFileAttachForm_Props {
+interface ImgFileAttachFormProps {
     canEdit: boolean;
     fileTitle: string;
-    handleDeleteLogo: Function;
+    handleDeleteLogo: (name: string) => void;
     imageUrl: string;
     index: number;
-    onFileChange: Function;
+    onFileChange: (attachment, name: string) => void;
     text: string;
 }
 
-interface ImgFileAttachForm_State {
-    imageUrl: string;
-}
+export const ImageAndFileAttachmentForm: FC<ImgFileAttachFormProps> = memo(props => {
+    const { canEdit, fileTitle, handleDeleteLogo, index, onFileChange, text } = props;
+    const [imageUrl, setImageUrl] = useState<string>(props.imageUrl);
+    const imageClassName = classNames('sso-fields__image', {
+        'sso-fields__image__header-logo': fileTitle === 'auth_header_logo',
+        'sso-fields__image__page-logo': fileTitle !== 'auth_header_logo',
+    });
+    const onError = useCallback(() => setImageUrl(null), []);
+    const onDelete = useCallback(() => {
+        setImageUrl(null);
+        handleDeleteLogo(fileTitle);
+    }, [fileTitle, handleDeleteLogo]);
+    const onChange = useCallback(attachment => onFileChange(attachment, fileTitle), [fileTitle, onFileChange]);
+    const img = <img className={imageClassName} src={imageUrl} onError={onError} alt="Sign in" />;
 
-export class ImageAndFileAttachmentForm extends PureComponent<ImgFileAttachForm_Props, ImgFileAttachForm_State> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            imageUrl: this.props.imageUrl,
-        };
-    }
+    return (
+        <div className="sso-logo-pane-container">
+            <div className="sso-fields__label">{text}</div>
 
-    render(): ReactNode {
-        const { canEdit, fileTitle, handleDeleteLogo, onFileChange } = this.props;
-        const { imageUrl } = this.state;
-        const iconClassName = classNames('fa', 'fa-times-circle', 'sso-fields__delete-img clickable', {
-            'sso-fields__delete-img--header-logo': fileTitle === 'auth_header_logo',
-            'sso-fields__delete-img--page-logo': fileTitle !== 'auth_header_logo',
-        });
-        const imageClassName = classNames({
-            'sso-fields__image__header-logo': fileTitle === 'auth_header_logo',
-            'sso-fields__image__page-log': fileTitle !== 'auth_header_logo',
-        });
-        const img = (
-            <img
-                className={imageClassName}
-                src={imageUrl}
-                onError={() => this.setState({ imageUrl: null })}
-                alt="Sign in"
-            />
-        );
-
-        return (
-            <div className="sso-logo-pane-container">
-                <div className="sso-fields__label">{this.props.text}</div>
-
-                {canEdit ? (
-                    <>
-                        <div className="sso-fields__image-holder">
-                            {imageUrl ? (
-                                <div>
-                                    <div>{img}</div>
-                                    <div
-                                        className="sso-fields__delete-img-div"
-                                        onClick={() => {
-                                            this.setState({ imageUrl: null });
-                                            handleDeleteLogo(fileTitle);
-                                        }}
-                                    >
-                                        <span className={iconClassName} />
-                                    </div>
+            {canEdit ? (
+                <>
+                    <div className="sso-fields__image-holder">
+                        {imageUrl ? (
+                            <div className="sso-fields__img_container">
+                                {img}
+                                <div className="sso-fields__delete-img-icon" onClick={onDelete}>
+                                    <span className="fa fa-times-circle sso-fields__delete-img clickable" />
                                 </div>
-                            ) : (
-                                <NoImageSelectedDisplay />
-                            )}
-                        </div>
-
-                        <div className="sso-fields__file-attachment" id={fileTitle}>
-                            <FileAttachmentForm
-                                index={this.props.index}
-                                showLabel={false}
-                                allowMultiple={false}
-                                allowDirectories={false}
-                                acceptedFormats=".jpg,.jpeg,.png,.gif,.tif"
-                                showAcceptedFormats={false}
-                                onFileChange={attachment => {
-                                    onFileChange(attachment, fileTitle);
-                                }}
-                            />
-                        </div>
-                    </>
-                ) : (
-                    <div className="sso-fields__image-holder--view-only">
-                        {imageUrl ? img : <NoImageSelectedDisplay />}
+                            </div>
+                        ) : (
+                            <NoImageSelectedDisplay />
+                        )}
                     </div>
-                )}
-            </div>
-        );
-    }
-}
+
+                    <div className="sso-fields__file-attachment" id={fileTitle}>
+                        <FileAttachmentForm
+                            index={index}
+                            showLabel={false}
+                            allowMultiple={false}
+                            allowDirectories={false}
+                            acceptedFormats=".jpg,.jpeg,.png,.gif,.tif"
+                            showAcceptedFormats={false}
+                            onFileChange={onChange}
+                        />
+                    </div>
+                </>
+            ) : (
+                <div className="sso-fields__image-holder--view-only">{imageUrl ? img : <NoImageSelectedDisplay />}</div>
+            )}
+        </div>
+    );
+});
 
 interface Props {
     canEdit: boolean;
-    handleDeleteLogo: Function;
+    handleDeleteLogo: (name: string) => void;
     headerLogoUrl: string;
     loginLogoUrl: string;
-    onFileChange: Function;
+    onFileChange: (attachment, name: string) => void;
 }
 
-export class SSOFields extends PureComponent<Props> {
-    render() {
-        return (
-            <>
-                <ImageAndFileAttachmentForm
-                    text="Page Header Logo"
-                    imageUrl={this.props.headerLogoUrl}
-                    onFileChange={this.props.onFileChange}
-                    handleDeleteLogo={this.props.handleDeleteLogo}
-                    fileTitle="auth_header_logo"
-                    canEdit={this.props.canEdit}
-                    index={1}
-                />
+export const SSOFields: FC<Props> = memo(({ canEdit, handleDeleteLogo, headerLogoUrl, loginLogoUrl, onFileChange }) => (
+    <div className="sso-fields-container">
+        <ImageAndFileAttachmentForm
+            text="Page Header Logo"
+            imageUrl={headerLogoUrl}
+            onFileChange={onFileChange}
+            handleDeleteLogo={handleDeleteLogo}
+            fileTitle="auth_header_logo"
+            canEdit={canEdit}
+            index={1}
+        />
 
-                <div className="sso-fields__spacer" />
-
-                <ImageAndFileAttachmentForm
-                    text="Login Page Logo"
-                    imageUrl={this.props.loginLogoUrl}
-                    onFileChange={this.props.onFileChange}
-                    handleDeleteLogo={this.props.handleDeleteLogo}
-                    fileTitle="auth_login_page_logo"
-                    canEdit={this.props.canEdit}
-                    index={2}
-                />
-            </>
-        );
-    }
-}
+        <ImageAndFileAttachmentForm
+            text="Login Page Logo"
+            imageUrl={loginLogoUrl}
+            onFileChange={onFileChange}
+            handleDeleteLogo={handleDeleteLogo}
+            fileTitle="auth_login_page_logo"
+            canEdit={canEdit}
+            index={2}
+        />
+    </div>
+));
