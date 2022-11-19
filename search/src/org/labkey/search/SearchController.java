9e96d7ab0b28a951e8138a312eaf52ca7867cf79
Adam Rauch <adam@labkey.com>
2022-11-18 17:04:41 -0800

    All you actions have been misinFORMed...

--- search/src/org/labkey/search/SearchController.java
+++ search/src/org/labkey/search/SearchController.java
@@ -27,6 +27,7 @@ import org.labkey.api.action.ExportAction;
 import org.labkey.api.action.FormHandlerAction;
 import org.labkey.api.action.FormViewAction;
 import org.labkey.api.action.ReadOnlyApiAction;
+import org.labkey.api.action.ReturnUrlForm;
 import org.labkey.api.action.SimpleRedirectAction;
 import org.labkey.api.action.SimpleViewAction;
 import org.labkey.api.action.SpringActionController;
@@ -61,7 +62,6 @@ import org.labkey.api.view.HttpView;
 import org.labkey.api.view.JspView;
 import org.labkey.api.view.NavTree;
 import org.labkey.api.view.NotFoundException;
-import org.labkey.api.view.RedirectException;
 import org.labkey.api.view.VBox;
 import org.labkey.api.view.ViewContext;
 import org.labkey.api.view.WebPartView;
@@ -434,30 +434,24 @@ public class SearchController extends SpringActionController
 
     /** for selenium testing */
     @RequiresSiteAdmin
-    public class WaitForIdleAction extends SimpleViewAction
+    public class WaitForIdleAction extends SimpleRedirectAction<Object>
     {
         @Override
-        public ModelAndView getView(Object o, BindException errors) throws Exception
+        public URLHelper getRedirectURL(Object o) throws Exception
         {
             SearchService ss = SearchService.get();
             ss.waitForIdle();
-            throw new RedirectException(new ActionURL(AdminAction.class, getContainer()));
-        }
-
-        @Override
-        public void addNavTrail(NavTree root)
-        {
+            return new ActionURL(AdminAction.class, getContainer());
         }
     }
 
     // UNDONE: remove; for testing only
     @RequiresSiteAdmin
-    public class CancelAction extends SimpleRedirectAction
+    public class CancelAction extends SimpleRedirectAction<ReturnUrlForm>
     {
         @Override
-        public ActionURL getRedirectURL(Object o)
+        public ActionURL getRedirectURL(ReturnUrlForm form)
         {
-            // SimpleRedirectAction doesn't take a form
             SearchService ss = SearchService.get();
     
             if (null == ss)
@@ -470,16 +464,7 @@ public class SearchController extends SpringActionController
                     task.cancel(true);
             }
 
-            try
-            {
-                String returnUrl = getViewContext().getRequest().getParameter(ActionURL.Param.returnUrl.name());
-                if (null != returnUrl)
-                    return new ActionURL(returnUrl);
-            }
-            catch (Exception x)
-            {
-            }
-            return getSearchURL();
+            return form.getReturnActionURL(getSearchURL());
         }
     }
 
@@ -487,12 +472,11 @@ public class SearchController extends SpringActionController
     // UNDONE: remove; for testing only
     // cause the current directory to be crawled soon
     @RequiresSiteAdmin
-    public class CrawlAction extends SimpleRedirectAction
+    public class CrawlAction extends SimpleRedirectAction<ReturnUrlForm>
     {
         @Override
-        public ActionURL getRedirectURL(Object o)
+        public ActionURL getRedirectURL(ReturnUrlForm form)
         {
-            // SimpleRedirectAction doesn't take a form
             SearchService ss = SearchService.get();
 
             if (null == ss)
@@ -502,33 +486,57 @@ public class SearchController extends SpringActionController
                     WebdavService.getPath().append(getContainer().getParsedPath()),
                     new Date(System.currentTimeMillis()));
 
-            try
-            {
-                String returnUrl = getViewContext().getRequest().getParameter(ActionURL.Param.returnUrl.name());
-                if (null != returnUrl)
-                    return new ActionURL(returnUrl);
-            }
-            catch (Exception x)
-            {
-            }
-            return getSearchURL();
+            return form.getReturnActionURL(getSearchURL());
         }
     }
 
+    public static class IndexForm extends ReturnUrlForm
+    {
+        boolean _full = false;
+        boolean _wait = false;
+        boolean _since = false;
+
+        public boolean isFull()
+        {
+            return _full;
+        }
+
+        @SuppressWarnings("unused")
+        public void setFull(boolean full)
+        {
+            _full = full;
+        }
+
+        public boolean isWait()
+        {
+            return _wait;
+        }
+
+        @SuppressWarnings("unused")
+        public void setWait(boolean wait)
+        {
+            _wait = wait;
+        }
+
+        public boolean isSince()
+        {
+            return _since;
+        }
+
+        @SuppressWarnings("unused")
+        public void setSince(boolean since)
+        {
+            _since = since;
+        }
+    }
 
     // for testing only
     @RequiresSiteAdmin
-    public class IndexAction extends SimpleRedirectAction
+    public class IndexAction extends SimpleRedirectAction<IndexForm>
     {
         @Override
-        public ActionURL getRedirectURL(Object o) throws Exception
+        public ActionURL getRedirectURL(IndexForm form) throws Exception
         {
-            // SimpleRedirectAction doesn't take a form
-            boolean full = "1".equals(getViewContext().getRequest().getParameter("full"));
-            String returnUrl = getViewContext().getRequest().getParameter(ActionURL.Param.returnUrl.name());
-            boolean wait = "1".equals(getViewContext().getRequest().getParameter("wait"));
-            boolean since = "1".equals(getViewContext().getRequest().getParameter("since"));
-
             SearchService ss = SearchService.get();
 
             if (null == ss)
@@ -536,13 +544,13 @@ public class SearchController extends SpringActionController
 
             SearchService.IndexTask task = null;
 
-            try (var x = SpringActionController.ignoreSqlUpdates())
+            try (var ignored = SpringActionController.ignoreSqlUpdates())
             {
-                if (full)
+                if (form.isFull())
                 {
                     ss.indexFull(true);
                 }
-                else if (since)
+                else if (form.isSince())
                 {
                     task = ss.indexContainer(null, getContainer(), new Date(System.currentTimeMillis()- TimeUnit.DAYS.toMillis(1)));
                 }
@@ -552,26 +560,16 @@ public class SearchController extends SpringActionController
                 }
             }
 
-            if (wait && null != task)
+            if (form.isWait() && null != task)
             {
                 task.get(); // wait for completion
                 if (ss instanceof AbstractSearchService)
                     ((AbstractSearchService)ss).commit();
             }
 
-            try
-            {
-                if (null != returnUrl)
-                    return new ActionURL(returnUrl);
-            }
-            catch (Exception x)
-            {
-            }
-            return getSearchURL();
+            return form.getReturnActionURL(getSearchURL());
         }
     }
-
-
 
     @RequiresPermission(ReadPermission.class)
     public class JsonAction extends ReadOnlyApiAction<SearchForm>
