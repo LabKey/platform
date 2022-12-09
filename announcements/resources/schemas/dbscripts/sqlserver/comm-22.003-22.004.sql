fbf3f006291b05c018590d9541835a9dd9fe1e72
labkey-jeckels <jeckels@labkey.com>
2022-12-08 17:53:38 -0800

    Fix syntax

--- announcements/resources/schemas/dbscripts/sqlserver/comm-22.003-22.004.sql
+++ announcements/resources/schemas/dbscripts/sqlserver/comm-22.003-22.004.sql
@@ -1,3 +1,3 @@
-SELECT core.fn_dropifexists('EmailFormats', 'comm', 'TABLE', NULL);
-SELECT core.fn_dropifexists('EmailPrefs', 'comm', 'TABLE', NULL);
-SELECT core.fn_dropifexists('EmailOptions', 'comm', 'TABLE', NULL);
+EXEC core.fn_dropifexists 'EmailFormats', 'comm', 'TABLE', NULL;
+EXEC core.fn_dropifexists 'EmailPrefs', 'comm', 'TABLE', NULL;
+EXEC core.fn_dropifexists 'EmailOptions', 'comm', 'TABLE', NULL;
