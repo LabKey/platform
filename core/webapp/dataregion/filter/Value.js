0e77fd963e5e32a064e713fb2816f43efeb5e9f0
Adam Rauch <adam@labkey.com>
2023-01-10 10:54:49 -0800

    Move non-code resources from internal to core and ms2

---
+++ core/webapp/dataregion/filter/Value.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+Ext4.define('LABKEY.dataregion.filter.Value', {
+    extend: 'LABKEY.dataregion.filter.Base'
+});
