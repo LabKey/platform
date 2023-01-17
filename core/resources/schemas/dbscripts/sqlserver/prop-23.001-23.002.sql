7b48cc22dbe8f124eb32ce4258ddb9caa4e75ee9
labkey-susanh <susanh@labkey.com>
2023-01-17 06:33:23 -0800

    Include NOT NULL constraint

--- core/resources/schemas/dbscripts/sqlserver/prop-23.001-23.002.sql
+++ core/resources/schemas/dbscripts/sqlserver/prop-23.001-23.002.sql
@@ -1,7 +1,7 @@
 ALTER TABLE prop.properties DROP CONSTRAINT PK_Properties;
 GO
 
-ALTER TABLE prop.properties ALTER COLUMN Name NVARCHAR(400);
-ALTER TABLE prop.properties ADD CONSTRAINT PK_Properties PRIMARY KEY CLUSTERED ("Set", Name)
+ALTER TABLE prop.properties ALTER COLUMN Name NVARCHAR(400) NOT NULL;
+ALTER TABLE prop.properties ADD CONSTRAINT PK_Properties PRIMARY KEY CLUSTERED ("Set", Name);
 
 GO
