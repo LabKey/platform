724488ee44b49d64b7d3b9adc225807d2a46c9f2
labkey-jeckels <jeckels@labkey.com>
2023-01-24 11:56:38 -0800

    Fix folder types that don't use the default portal page as their primary tab

--- api/src/org/labkey/api/module/DefaultFolderType.java
+++ api/src/org/labkey/api/module/DefaultFolderType.java
@@ -134,7 +134,7 @@ public class DefaultFolderType implements FolderType
             for (Portal.WebPart part : required)
                 part.setPermanent(true);
 
-        List<Portal.WebPart> all = new ArrayList<>();
+        ArrayList<Portal.WebPart> all = new ArrayList<>();
         List<Portal.WebPart> existingParts = Portal.getEditableParts(c);
 
         if (existingParts.isEmpty())
@@ -191,7 +191,7 @@ public class DefaultFolderType implements FolderType
 
         active.addAll(requiredActive);
         c.setActiveModules(active, user);
-        Portal.saveParts(c, DEFAULT_DASHBOARD, all);
+        Portal.saveParts(c, getDefaultTab().getName(), all);
 
         // A few things left to do; ordering is important
 
