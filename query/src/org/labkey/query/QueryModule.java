95d0e90cbef9afc9803c1cdb727e0b2d7f93fb40
labkey-nicka <nickk@labkey.com>
2023-02-13 16:15:50 -0800

    Register experimental flag

--- query/src/org/labkey/query/QueryModule.java
+++ query/src/org/labkey/query/QueryModule.java
@@ -229,6 +229,8 @@ public class QueryModule extends DefaultModule
         AdminConsole.addExperimentalFeatureFlag(USE_ROW_BY_ROW_UPDATE, "Use row-by-row update", "For Query.updateRows api, do row-by-row update, instead of using a prepared statement that updates rows in batches.", false);
         AdminConsole.addExperimentalFeatureFlag(QueryServiceImpl.EXPERIMENTAL_PRODUCT_ALL_FOLDER_LOOKUPS, "Less restrictive product project lookups",
                 "Allow for lookup fields in product projects to query across all folders within the top-level folder.", false);
+        AdminConsole.addExperimentalFeatureFlag(QueryServiceImpl.EXPERIMENTAL_PRODUCT_PROJECT_DATA_LISTING_SCOPED, "Product projects display project-specific data",
+                "Only list project-specific data within product projects.", false);
     }
 
 
