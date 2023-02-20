6a08b58016e9eb7708b9ea305fa8e0903d628d52
labkey-jeckels <jeckels@labkey.com>
2023-02-20 15:02:03 -0800

    Imports

--- api/src/org/labkey/api/mbean/LabKeyManagement.java
+++ api/src/org/labkey/api/mbean/LabKeyManagement.java
@@ -15,10 +15,8 @@
  */
 package org.labkey.api.mbean;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
-import org.labkey.api.util.UnexpectedException;
 import org.labkey.api.util.logging.LogHelper;
 
 import javax.management.DynamicMBean;
