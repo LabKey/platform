effcc2013c43567dc6fed87e1579754573deee2c
XingY <xyang@labkey.com>
2023-02-23 20:27:15 -0800

    Issue 47280: LKSM: Trailing whitespace in Source name won't resolve when deriving samples

--- api/src/org/labkey/api/dataiterator/SimpleTranslator.java
+++ api/src/org/labkey/api/dataiterator/SimpleTranslator.java
@@ -616,7 +616,10 @@ public class SimpleTranslator extends AbstractDataIterator implements DataIterat
             Object value = getSourceValue();
             try
             {
-                return convert(value);
+                Object val = convert(value);
+                if (val instanceof String)
+                    return ((String) val).trim();
+                return val;
             }
             catch (ConversionException x)
             {
