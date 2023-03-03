be0dfa0ce69ee9d90de8ebf988ce5fc4a04452b7
labkey-tchad <tchad@labkey.com>
2023-03-02 16:04:50 -0800

    Fix typo

--- query/webapp/QWPDemo.js
+++ query/webapp/QWPDemo.js
@@ -218,7 +218,7 @@
                         }
                     }
                     else {
-                        alert("Didn't find enough results row");
+                        alert("Didn't find enough results rows");
                     }
                 },
                 failure: function() {
