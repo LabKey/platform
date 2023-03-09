eee6931cd67ba933ab3c82c82709fb646bad7949
Adam Rauch <adam@labkey.com>
2023-03-08 17:09:24 -0800

    Collect evidence to address domain-related deadlocks (#4187)

--- api/src/org/labkey/api/util/DeadlockPreventingException.java
+++ api/src/org/labkey/api/util/DeadlockPreventingException.java
@@ -2,16 +2,25 @@ package org.labkey.api.util;
 
 /**
  * Thrown after a resource doesn't become available after a timeout in an attempt to avoid deadlocking the whole server.
+ * Dump threads at construction time since throwing this exception will likely release locks and destroy all evidence of
+ * the deadlock.
  */
 public class DeadlockPreventingException extends RuntimeException
 {
     public DeadlockPreventingException(String message)
     {
         super(message);
+        dumpThreads();
     }
 
     public DeadlockPreventingException(String message, Throwable cause)
     {
         super(message, cause);
+        dumpThreads();
+    }
+
+    private void dumpThreads()
+    {
+        DebugInfoDumper.dumpThreads(1);
     }
 }
