162c65c28e48d9539433408b47a88b8a772c16b8
Lum <klum@labkey.com>
2023-03-21 16:37:45 -0700

    null check

--- api/src/org/labkey/api/data/ILineageDisplayColumn.java
+++ api/src/org/labkey/api/data/ILineageDisplayColumn.java
@@ -1,7 +1,11 @@
 package org.labkey.api.data;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface ILineageDisplayColumn
 {
     DisplayColumn getInnerDisplayColumn();
+
+    @Nullable
     ColumnInfo getInnerBoundColumn();
 }
