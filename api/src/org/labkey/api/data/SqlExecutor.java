9c7ef085602e4a223ccbfa8ba3d5232b7da978aa
Matthew Bellew <matthewb@labkey.com>
2023-03-28 13:06:23 -0700

    undo

--- api/src/org/labkey/api/data/SqlExecutor.java
+++ api/src/org/labkey/api/data/SqlExecutor.java
@@ -157,14 +157,7 @@ public class SqlExecutor extends JdbcCommand<SqlExecutor>
                 try (PreparedStatement stmt = conn.prepareStatement(sql);
                      Parameter.ParameterList jdbcParameters = new Parameter.ParameterList())
                 {
-                    try
-                    {
-                        Table.setParameters(stmt, parameters, jdbcParameters);
-                    }
-                    catch (Exception x)
-                    {
-                        x.printStackTrace();
-                    }
+                    Table.setParameters(stmt, parameters, jdbcParameters);
                     int result = stmt.execute() ? -1 : stmt.getUpdateCount();
 
                     // Issue 46004: Churn through results from all statements in case there are errors lurking
