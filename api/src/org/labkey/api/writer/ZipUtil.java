55a90983f03e6f181206fb8c648fabf002f3db4d
labkey-martyp <martyp@labkey.com>
2023-04-05 16:16:11 -0700

    Update error message

--- api/src/org/labkey/api/writer/ZipUtil.java
+++ api/src/org/labkey/api/writer/ZipUtil.java
@@ -108,7 +108,7 @@ public class ZipUtil
 
                 //Verify that the entry target doesn't attempt to push data outside the unzipDir by resolving '..'
                 if (!destFile.toAbsolutePath().normalize().startsWith(unzipDir.toAbsolutePath().normalize().toString())) {
-                    throw new IOException("Zip entry is outside of the target dir: " + entry.getName());
+                    throw new IOException("Zip entry is outside of the target dir. Dest file: " + entry.getName() + ", unzip dir: " + unzipDir);
                 }
 
                 if (entry.isDirectory())
