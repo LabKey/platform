47f365940229f75998ca1b78b9b4fe5da58059b7
labkey-susanh <susanh@labkey.com>
2023-04-24 16:14:52 -0700

    Revert change to imports

--- experiment/src/org/labkey/experiment/controllers/exp/ExperimentController.java
+++ experiment/src/org/labkey/experiment/controllers/exp/ExperimentController.java
@@ -287,7 +287,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
