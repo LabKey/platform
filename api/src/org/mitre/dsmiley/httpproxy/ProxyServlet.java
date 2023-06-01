d2b26bb89a00da288f491f482c1b123e1636c6e2
XingY <xyang@labkey.com>
2023-06-01 11:32:04 -0700

    format

--- api/src/org/mitre/dsmiley/httpproxy/ProxyServlet.java
+++ api/src/org/mitre/dsmiley/httpproxy/ProxyServlet.java
@@ -433,16 +433,15 @@ public class ProxyServlet extends HttpServlet {
 
     /** These are the "hop-by-hop" headers that should not be copied.
      * http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
-     * I use an HttpClient HeaderGroup class instead of Set<String> because this
-     * approach does case-insensitive lookup faster.
+     * I use an HttpClient HeaderGroup class instead of Set&lt;String&gt; because this
+     * approach does case insensitive lookup faster.
      */
     protected static final HeaderGroup hopByHopHeaders;
-
     static {
         hopByHopHeaders = new HeaderGroup();
         String[] headers = new String[] {
                 "Connection", "Keep-Alive", "Proxy-Authenticate", "Proxy-Authorization",
-                "TE", "Trailers", "Transfer-Encoding", "Upgrade", "Upgrade-Insecure-Requests"};
+                "TE", "Trailers", "Transfer-Encoding", "Upgrade" };
         for (String header : headers) {
             hopByHopHeaders.addHeader(new BasicHeader(header, null));
         }
