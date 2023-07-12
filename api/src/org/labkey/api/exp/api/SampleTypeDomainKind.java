f71a71e7db0dbd3b9a2dc376498f4ac728b110a1
Susan Hert <susanh@labkey.com>
2023-07-12 07:36:12 -0700

    Add missing reserved field names for sample types and make menu section more robust (#4573)

--- api/src/org/labkey/api/exp/api/SampleTypeDomainKind.java
+++ api/src/org/labkey/api/exp/api/SampleTypeDomainKind.java
@@ -100,6 +100,8 @@ public class SampleTypeDomainKind extends AbstractDomainKind<SampleTypeDomainKin
 
         RESERVED_NAMES = BASE_PROPERTIES.stream().map(PropertyStorageSpec::getName).collect(Collectors.toSet());
         RESERVED_NAMES.addAll(Arrays.stream(ExpSampleTypeTable.Column.values()).map(ExpSampleTypeTable.Column::name).toList());
+        RESERVED_NAMES.add("Protocol"); // alias for "SourceProtocolApplication"
+        RESERVED_NAMES.add("SampleTypeUnits"); // alias for MetricUnit
         RESERVED_NAMES.add("CpasType");
         RESERVED_NAMES.add("IsAliquot");
         RESERVED_NAMES.add(ExpMaterial.ALIQUOTED_FROM_INPUT);
