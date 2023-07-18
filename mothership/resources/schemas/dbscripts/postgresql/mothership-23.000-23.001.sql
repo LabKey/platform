012ea33a33143cd2b1321ac98668960f72e92e21
labkey-jeckels <jeckels@labkey.com>
2023-07-18 13:26:32 -0700

    Use temp file to speed up deduplication

--- mothership/resources/schemas/dbscripts/postgresql/mothership-23.000-23.001.sql
+++ mothership/resources/schemas/dbscripts/postgresql/mothership-23.000-23.001.sql
@@ -15,27 +15,34 @@
  */
 
 -- Issue 48270 - dedupe redundant rows in SoftwareRelease that have NULLs for VcsTag
-UPDATE mothership.serversession s SET softwarereleaseid =
-                  (SELECT MaxId
-                   FROM mothership.softwarerelease sr1
-                            INNER JOIN (SELECT MAX(SoftwareReleaseId) AS MaxId,
-                                               VcsUrl,
-                                               VcsRevision,
-                                               VcsTag,
-                                               VcsBranch,
-                                               BuildTime
-                                        FROM mothership.softwarerelease
-                                        GROUP BY VcsUrl, VcsRevision, VcsTag, VcsBranch, BuildTime) sr2
-                                       ON
-                                               (sr1.VcsUrl = sr2.VcsUrl OR (sr1.VcsUrl IS NULL AND sr2.VcsUrl IS NULL)) AND
-                                               (sr1.VcsRevision = sr2.VcsRevision OR
-                                                (sr1.VcsRevision IS NULL AND sr2.VcsRevision IS NULL)) AND
-                                               (sr1.VcsTag = sr2.VcsTag OR (sr1.VcsTag IS NULL AND sr2.VcsTag IS NULL)) AND
-                                               (sr1.VcsBranch = sr2.VcsBranch OR
-                                                (sr1.VcsBranch IS NULL AND sr2.VcsBranch IS NULL)) AND
-                                               (sr1.BuildTime = sr2.BuildTime OR
-                                                (sr1.BuildTime IS NULL AND sr2.BuildTime IS NULL))
-                   WHERE s.SoftwareReleaseId = sr1.SoftwareReleaseId);
+CREATE TABLE mothership.Dedupe (MaxId INT NOT NULL, SoftwareReleaseId INT NOT NULL);
+
+INSERT INTO mothership.Dedupe (MaxId, SoftwareReleaseId)
+    SELECT MaxId, sr1.SoftwareReleaseId
+        FROM mothership.softwarerelease sr1
+        INNER JOIN (SELECT MAX(SoftwareReleaseId) AS MaxId,
+                             VcsUrl,
+                             VcsRevision,
+                             VcsTag,
+                             VcsBranch,
+                             BuildTime
+                      FROM mothership.softwarerelease
+                      GROUP BY VcsUrl, VcsRevision, VcsTag, VcsBranch, BuildTime) sr2
+                     ON
+                             (sr1.VcsUrl = sr2.VcsUrl OR (sr1.VcsUrl IS NULL AND sr2.VcsUrl IS NULL)) AND
+                             (sr1.VcsRevision = sr2.VcsRevision OR
+                              (sr1.VcsRevision IS NULL AND sr2.VcsRevision IS NULL)) AND
+                             (sr1.VcsTag = sr2.VcsTag OR (sr1.VcsTag IS NULL AND sr2.VcsTag IS NULL)) AND
+                             (sr1.VcsBranch = sr2.VcsBranch OR
+                              (sr1.VcsBranch IS NULL AND sr2.VcsBranch IS NULL)) AND
+                             (sr1.BuildTime = sr2.BuildTime OR
+                              (sr1.BuildTime IS NULL AND sr2.BuildTime IS NULL)
+                    );
+
+UPDATE mothership.serversession s SET softwarereleaseid = (SELECT MaxId FROM mothership.Dedupe d
+                   WHERE s.SoftwareReleaseId = d.SoftwareReleaseId);
+
+DROP TABLE mothership.Dedupe;
 
 DELETE FROM mothership.softwarerelease WHERE SoftwareReleaseId NOT IN
                                              (SELECT DISTINCT SoftwareReleaseId FROM mothership.serversession);
