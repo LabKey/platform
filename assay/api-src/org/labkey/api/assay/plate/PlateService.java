d5d8eb71687ee218d6220ddb38cce418e8572cb2
Lum <klum@labkey.com>
2023-07-21 16:28:19 -0700

    bad merge

--- assay/api-src/org/labkey/api/assay/plate/PlateService.java
+++ assay/api-src/org/labkey/api/assay/plate/PlateService.java
@@ -61,7 +61,6 @@ public interface PlateService
     /**
      * Instantiates a new plate instance based on the specified plate and well data.
      * This plate is not persisted to the database.
-     * This plate is not persisted to the database.
      * @param plate The plate that this instance is based upon.
      * @param wellValues A two-dimensional array of the machine data.
      * @param excludedWells A two-dimensional array of wells that are excluded (can be null)
@@ -73,7 +72,6 @@ public interface PlateService
 
     /**
      * Instantiates a new plate instance based on the specified plate and well data.
-     * This plate is not persisted to the database.
      * This plate is not persisted to the database.
      * @param plate The plate that this instance is based upon.
      * @param wellValues A two-dimensional array of the machine data.
