3b79ec035eaf4153b0996a639204a35e77089a6a
Adam Rauch <adam@labkey.com>
2023-08-07 16:43:50 -0700

    Don't even attempt to load encrypted properties on a new install

--- api/src/org/labkey/api/security/Encryption.java
+++ api/src/org/labkey/api/security/Encryption.java
@@ -129,12 +129,15 @@ public class Encryption
 
             try
             {
+                // On trial deployments, encryption key can change between initial bootstrap and the "new install"
+                // startup, so always delete if "newinstall" file is present. See Issue 48346.
+                if (ModuleLoader.getInstance().isNewInstall())
+                    PropertyManager.getEncryptedStore().deletePropertySet(TEST_ENCRYPTION_CATEGORY);
+
                 // This will likely throw if the encryption key has changed
                 PropertyMap map = PropertyManager.getEncryptedStore().getWritableProperties(TEST_ENCRYPTION_CATEGORY, true);
 
-                // Second check is important for trial deployments, where encryption key can change between initial
-                // bootstrap and the "new install" startup. See Issue 48346.
-                if (map.isEmpty() || ModuleLoader.getInstance().isNewInstall())
+                if (map.isEmpty())
                 {
                     byte[] randomBytes = generateRandomBytes(TEST_BYTES_LENGTH);
                     MessageDigest sha1 = MessageDigest.getInstance("SHA1");
