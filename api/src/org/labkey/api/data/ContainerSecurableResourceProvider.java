6d4f51cbdeaff2ca5ecfdfb612d15a5c89d4b617
labkey-jeckels <jeckels@labkey.com>
2023-08-27 20:39:36 -0700

    A little JavaDoc

--- api/src/org/labkey/api/data/ContainerSecurableResourceProvider.java
+++ api/src/org/labkey/api/data/ContainerSecurableResourceProvider.java
@@ -5,7 +5,9 @@ import org.labkey.api.security.User;
 
 import java.util.Collection;
 
+/** A provider for SecurableResource implementations that are scoped to a container */
 public interface ContainerSecurableResourceProvider
 {
+    /** @return all of the resources scoped to the container that are visible by the user */
     Collection<? extends SecurableResource> getSecurableResources(Container c, User user);
 }
