b560b3288a79579268634834b3773d3a7294e2f6
labkey-nicka <nickk@labkey.com>
2023-10-20 13:09:13 -0700

    Remove stale comment

--- experiment/resources/schemas/dbscripts/postgresql/exp-23.010-23.011.sql
+++ experiment/resources/schemas/dbscripts/postgresql/exp-23.010-23.011.sql
@@ -1,12 +1,7 @@
 ALTER TABLE exp.material ADD COLUMN RootMaterialRowId INTEGER;
 
--- TODO: Could consider this UPDATE, however, the performance seems to be much worse...
--- UPDATE exp.material
---     SET RootMaterialRowId = Parent.RowId
---     FROM exp.material AS Parent
---     WHERE Material.RootMaterialLSID = Parent.LSID;
 UPDATE exp.material AS mat SET RootMaterialRowId = (
-    -- This DISTINCT here is logically superfluous but it does seem to help Postgresql choose a better query plan
+    -- This DISTINCT is logically superfluous but it does seem to help Postgresql choose a better query plan
     SELECT DISTINCT RowId FROM exp.material AS expmat WHERE mat.RootMaterialLSID = expmat.LSID
 );
 
