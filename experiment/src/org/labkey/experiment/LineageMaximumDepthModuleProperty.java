ea01a5d0b45876eebdd748a1a12046fbe4e7f62e
labkey-nicka <nickk@labkey.com>
2023-12-06 13:01:48 -0800

    Dialect specific maximum depth

--- experiment/src/org/labkey/experiment/LineageMaximumDepthModuleProperty.java
+++ experiment/src/org/labkey/experiment/LineageMaximumDepthModuleProperty.java
@@ -3,6 +3,7 @@ package org.labkey.experiment;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 import org.labkey.api.data.Container;
+import org.labkey.api.data.CoreSchema;
 import org.labkey.api.exp.api.ExpLineageOptions;
 import org.labkey.api.exp.api.ExperimentService;
 import org.labkey.api.module.Module;
@@ -12,14 +13,13 @@ import org.labkey.api.security.User;
 public class LineageMaximumDepthModuleProperty extends ModuleProperty
 {
     private final int MINIMUM_DEPTH = 1;
-    private final int MAXIMUM_DEPTH = 1_000;
 
     public LineageMaximumDepthModuleProperty(Module module)
     {
         super(module, ExperimentService.LINEAGE_DEFAULT_MAXIMUM_DEPTH_PROPERTY_NAME);
 
         setLabel("Lineage Default Maximum Depth");
-        setDescription(String.format("Default maximum depth lineage queries will recurse (%d-%d). Defaults to %d.", MINIMUM_DEPTH, MAXIMUM_DEPTH, ExpLineageOptions.LINEAGE_DEFAULT_MAXIMUM_DEPTH));
+        setDescription(String.format("Default maximum depth lineage queries will recurse (%d-%d). Defaults to %d.", MINIMUM_DEPTH, getMaximumDepth(), ExpLineageOptions.LINEAGE_DEFAULT_MAXIMUM_DEPTH));
         setShowDescriptionInline(true);
     }
 
@@ -34,12 +34,20 @@ public class LineageMaximumDepthModuleProperty extends ModuleProperty
             var depth = Integer.parseInt(value);
             if (depth < MINIMUM_DEPTH)
                 throw new IllegalArgumentException(String.format("Invalid value for \"%s\". Minimum value is %d.", getLabel(), MINIMUM_DEPTH));
-            else if (depth > MAXIMUM_DEPTH)
-                throw new IllegalArgumentException(String.format("Invalid value for \"%s\". Maximum value is %d.", getLabel(), MAXIMUM_DEPTH));
+
+            var maxDepth = getMaximumDepth();
+            if (depth > maxDepth)
+                throw new IllegalArgumentException(String.format("Invalid value for \"%s\". Maximum value is %d.", getLabel(), maxDepth));
         }
         catch (NumberFormatException e)
         {
             throw new IllegalArgumentException(String.format("Invalid value for \"%s\". Must be a number.", getLabel()));
         }
+    }
+
+    private static int getMaximumDepth()
+    {
+        // Issue 37332: SQL Server can hit max recursion depth over 100 generations.
+        return CoreSchema.getInstance().getSqlDialect().isSqlServer() ? 100 : 1_000;
     }
 }
