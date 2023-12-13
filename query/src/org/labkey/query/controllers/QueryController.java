c6b7e1a2fdaba6806d47184a20c712f423186bae
labkey-matthewb <matthewb@labkey.com>
2023-12-12 16:58:24 -0800

    HtmlStringBuilder (#5029)
    * convert from StringBuilder -> HtmlStringBuilder
    checkpoint I
    
    * convert from StringBuilder -> HtmlStringBuilder
    checkpoint II
    
    * convert from StringBuilder -> HtmlStringBuilder
    class Overview
    
    * CR fixes
    
    * HtmlStringBuilder.insert()

--- query/src/org/labkey/query/controllers/QueryController.java
+++ query/src/org/labkey/query/controllers/QueryController.java
@@ -7003,7 +7003,7 @@ public class QueryController extends SpringActionController
             var inputId = config.makeId("submit_");
             config.addHandler(inputId, "click", "Ext.getBody().mask();");
             html.add("<form method=GET><textarea id=\"expression\" cols=100 rows=10 name=q>" + PageFlowUtil.filter(expr) + "</textarea><br><input id=\"" + inputId + "\" type=submit></form>\n" +
-                    "<script type=\"text/javascript\" nonce=\"" + config.getScriptNonce() + "\">" +
+                    "<script type=\"text/javascript\" nonce=\"" + PageFlowUtil.filter(config.getScriptNonce()) + "\">" +
 
                     "    var resizer = new (Ext4||Ext).Resizable(\"expression\", {\n" +
                             "        handles: 'se',\n" +
@@ -7061,7 +7061,7 @@ public class QueryController extends SpringActionController
                 html.add(PageFlowUtil.filter(prefix));
             }
             html.add("</body></html>");
-            return new HtmlView(StringUtils.join(html,""));
+            return HtmlView.unsafe(StringUtils.join(html,""));
         }
 
         @Override
