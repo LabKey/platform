eb6ed8cd9a9d0e95d38789e5ce318567302d4708
Alan Vezina <alanv@labkey.com>
2023-12-21 19:26:17 -0600

    Bump components and build (#5074)
    * Bump components, and build, update dev.tsx files

--- pipeline/src/client/CreatePipelineTrigger/dev.tsx
+++ pipeline/src/client/CreatePipelineTrigger/dev.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import { App } from '@labkey/api';
 
 import { Props, CreatePipelineTrigger } from './CreatePipelineTrigger';
 
 const render = (target: string, ctx: Props): void => {
-    ReactDOM.render(
-        <AppContainer>
-            <CreatePipelineTrigger {...ctx} />
-        </AppContainer>,
-        document.getElementById(target)
-    );
+    ReactDOM.render(<CreatePipelineTrigger {...ctx} />, document.getElementById(target));
 };
 
 App.registerApp<Props>('createPipelineTrigger', render, true);
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const module: any;
-
-if (module.hot) {
-    module.hot.accept();
-}
