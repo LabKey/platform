be32df5c86d0a42b8f34f081a36dc518e23b2d0d
labkey-susanh <susanh@labkey.com>
2024-01-04 19:37:02 -0800

    No nee to override populateButtonBar

--- core/src/org/labkey/core/security/UserApiKeyQueryView.java
+++ core/src/org/labkey/core/security/UserApiKeyQueryView.java
@@ -1,12 +1,8 @@
 package org.labkey.core.security;
 
-import org.labkey.api.data.ActionButton;
-import org.labkey.api.data.ButtonBar;
-import org.labkey.api.data.MenuButton;
 import org.labkey.api.query.QuerySettings;
 import org.labkey.api.query.QueryView;
 import org.labkey.api.query.UserSchema;
-import org.labkey.api.view.DataView;
 import org.labkey.api.view.ViewContext;
 import org.labkey.core.query.CoreQuerySchema;
 
@@ -16,20 +12,11 @@ public class UserApiKeyQueryView extends QueryView
     {
         super(schema);
         QuerySettings settings =schema.getSettings(ctx, QueryView.DATAREGIONNAME_DEFAULT, CoreQuerySchema.USER_API_KEYS_TABLE_NAME);
-        settings.setAllowChooseQuery(true);
         setSettings(settings);
         setShowDeleteButton(true);
         setShowExportButtons(false);
-
-    }
-
-    @Override
-    protected void populateButtonBar(DataView view, ButtonBar bar)
-    {
-        bar.add(createViewButton(null));
-        ActionButton deleteBtn = createDeleteButton();
-        if (deleteBtn != null)
-            deleteBtn.setDisplayPermission(null); // we allow deletion from any container
-        bar.add(deleteBtn);
+        setShowReports(false);
+        setShowInsertNewButton(false);
+        setShowImportDataButton(false);
     }
 }
