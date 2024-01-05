e5d204d9979655048503e43efb9eb36321ac4d76
labkey-susanh <susanh@labkey.com>
2024-01-05 10:58:43 -0800

    Populate button bar needed for use of different ActionURL

--- core/src/org/labkey/core/security/UserApiKeyQueryView.java
+++ core/src/org/labkey/core/security/UserApiKeyQueryView.java
@@ -1,8 +1,12 @@
 package org.labkey.core.security;
 
+import org.labkey.api.data.ActionButton;
+import org.labkey.api.data.ButtonBar;
 import org.labkey.api.query.QuerySettings;
 import org.labkey.api.query.QueryView;
 import org.labkey.api.query.UserSchema;
+import org.labkey.api.view.ActionURL;
+import org.labkey.api.view.DataView;
 import org.labkey.api.view.ViewContext;
 import org.labkey.core.query.CoreQuerySchema;
 
@@ -11,10 +15,25 @@ public class UserApiKeyQueryView extends QueryView
     public UserApiKeyQueryView(ViewContext ctx, UserSchema schema)
     {
         super(schema);
-        QuerySettings settings =schema.getSettings(ctx, QueryView.DATAREGIONNAME_DEFAULT, CoreQuerySchema.USER_API_KEYS_TABLE_NAME);
+        QuerySettings settings = schema.getSettings(ctx, QueryView.DATAREGIONNAME_DEFAULT, CoreQuerySchema.USER_API_KEYS_TABLE_NAME);
         setSettings(settings);
-        setShowDeleteButton(true);
+        // This is needed so the print button doesn't show up.
         setShowExportButtons(false);
+    }
+
+
+    @Override
+    protected void populateButtonBar(DataView view, ButtonBar bar)
+    {
+        bar.add(createViewButton(null));
+
+        ActionButton deleteBtn = new ActionButton(new ActionURL(SecurityController.DeleteApiKeysAction.class, getContainer()).addReturnURL(getReturnURL()), "Delete");
+        deleteBtn.setIconCls("trash");
+        deleteBtn.setActionType(ActionButton.Action.POST);
+        deleteBtn.setDisplayPermission(null); // we allow deletion from any container
+        deleteBtn.setRequiresSelection(true, "Are you sure you want to delete the selected API key?", "Are you sure you want to delete the selected API keys?");
+        bar.add(deleteBtn);
+
         setShowReports(false);
         setShowInsertNewButton(false);
         setShowImportDataButton(false);
