45fbdcd9b937704d7fee6cf4df86c2b192575eaa
Adam Rauch <adam@labkey.com>
2024-01-04 19:21:27 -0800

    Clean up scripts

--- pipeline/resources/schemas/dbscripts/sqlserver/pipeline-0.000-23.000.sql
+++ pipeline/resources/schemas/dbscripts/sqlserver/pipeline-0.000-23.000.sql
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-/* pipeline-0.00-10.10.sql */
-
 CREATE SCHEMA pipeline;
 GO
 
@@ -54,13 +52,9 @@ CREATE TABLE pipeline.StatusFiles
 );
 CREATE CLUSTERED INDEX IX_StatusFiles_Container_JobParent ON pipeline.StatusFiles (Container ASC, JobParent ASC);
 
-/* pipeline-14.10-14.20.sql */
-
 ALTER TABLE pipeline.StatusFiles ADD ActiveHostName NVARCHAR(255) NULL;
-
-/* 22.xxx SQL scripts */
-
 ALTER TABLE pipeline.StatusFiles ADD TaskPipelineId NVARCHAR(255);
+
 CREATE TABLE pipeline.PipelineRoots
 (
     _ts TIMESTAMP,
@@ -88,8 +82,6 @@ CREATE TABLE pipeline.PipelineRoots
 
 ALTER TABLE pipeline.PipelineRoots ADD SupplementalPath NVARCHAR(300);
 
-/* pipeline-15.30-16.10.sql */
-
 ALTER TABLE pipeline.PipelineRoots DROP COLUMN KeyBytes;
 ALTER TABLE pipeline.PipelineRoots DROP COLUMN CertBytes;
 ALTER TABLE pipeline.PipelineRoots DROP COLUMN KeyPassword;
@@ -99,8 +91,6 @@ DELETE FROM pipeline.PipelineRoots WHERE Container NOT IN (SELECT EntityId FROM 
 ALTER TABLE pipeline.PipelineRoots ADD CONSTRAINT FK_PipelineRoots_Container FOREIGN KEY (Container) REFERENCES core.Containers (EntityId);
 
 ALTER TABLE pipeline.PipelineRoots ADD CONSTRAINT UQ_PipelineRoots_Container_Type UNIQUE (Container, Type);
-
-/* pipeline-17.20-17.30.sql */
 
 CREATE TABLE pipeline.TriggerConfigurations
 (
@@ -124,11 +114,7 @@ CREATE TABLE pipeline.TriggerConfigurations
   CONSTRAINT UQ_TriggerConfigurations_Name UNIQUE (Container, Name)
 );
 
-/* pipeline-17.30-18.10.sql */
-
 ALTER TABLE pipeline.TriggerConfigurations ADD customConfiguration NVARCHAR(MAX);
-
-/* pipeline-18.10-18.20.sql */
 
 CREATE TABLE pipeline.TriggeredFiles
 (
@@ -143,4 +129,3 @@ CREATE TABLE pipeline.TriggeredFiles
   CONSTRAINT FK_TriggeredFiles_Container FOREIGN KEY (Container) REFERENCES core.Containers (ENTITYID),
   CONSTRAINT UQ_TriggeredFiles_Container_TriggerId_FilePath UNIQUE (Container, TriggerId, FilePath)
 );
-
