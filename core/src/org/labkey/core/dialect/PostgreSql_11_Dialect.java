e0e05b48e4fa3c5de13d38b8fa30c785a3da953c
Adam Rauch <adam@labkey.com>
2024-02-02 15:46:53 -0800

    Remove support for PostgreSQL 11.x

--- core/src/org/labkey/core/dialect/PostgreSql_11_Dialect.java
+++ core/src/org/labkey/core/dialect/PostgreSql_11_Dialect.java
@@ -15,6 +15,6 @@
  */
 package org.labkey.core.dialect;
 
-public class PostgreSql_11_Dialect extends PostgreSql_10_Dialect
+public abstract class PostgreSql_11_Dialect extends PostgreSql_10_Dialect
 {
 }
