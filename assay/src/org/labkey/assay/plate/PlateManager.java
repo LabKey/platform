971104b48a0edbb4e28603e4b450de3fe5214573
Nick Kerr <nickk@labkey.com>
2024-02-13 15:01:13 -0800

    Plates: fix unit test cleanup (#5230)

--- assay/src/org/labkey/assay/plate/PlateManager.java
+++ assay/src/org/labkey/assay/plate/PlateManager.java
@@ -21,7 +21,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.junit.Before;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.labkey.api.assay.AssayProvider;
@@ -1879,8 +1879,8 @@ public class PlateManager implements PlateService
             PlateManager.get().createPlateMetadataFields(container, user, customFields);
         }
 
-        @Before
-        public void initTest() throws Exception
+        @After
+        public void cleanupTest() throws Exception
         {
             PlateManager.get().deleteAllPlateData(container);
         }
