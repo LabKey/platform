dcac14db54754e4dd94e8faade414f515c7f0095
labkey-susanh <susanh@labkey.com>
2024-02-29 16:09:28 -0800

    Pass through auditUserComment for assay import

--- assay/src/org/labkey/assay/AssayController.java
+++ assay/src/org/labkey/assay/AssayController.java
@@ -792,6 +792,7 @@ public class AssayController extends SpringActionController
     public static class AssayFileUploadForm extends AbstractFileUploadAction.FileUploadForm
     {
         private Integer _protocolId;
+        private String _auditUserComment;
 
         public Integer getProtocolId()
         {
@@ -802,6 +803,17 @@ public class AssayController extends SpringActionController
         public void setProtocolId(Integer protocolId)
         {
             _protocolId = protocolId;
+        }
+
+        public String getAuditUserComment()
+        {
+            return _auditUserComment;
+        }
+
+        @SuppressWarnings("unused")
+        public void setAuditUserComment(String auditUserComment)
+        {
+            _auditUserComment = auditUserComment;
         }
     }
 
