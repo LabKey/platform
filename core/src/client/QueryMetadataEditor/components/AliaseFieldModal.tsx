3db5876f055e5480427cf159c01e3f205cfd2c5c
Alan Vezina <alanv@labkey.com>
2024-03-06 17:29:07 -0600

    Tabs (#5284)
    * Use internal Tabs component
    * Bump components
    * AliasFieldModal: Convert to FC use internal Modal

--- core/src/client/QueryMetadataEditor/components/AliaseFieldModal.tsx
+++ core/src/client/QueryMetadataEditor/components/AliaseFieldModal.tsx
@@ -1,124 +1,61 @@
-import React, {PureComponent} from "react";
-import {Button, Modal, Row, Col} from "react-bootstrap";
-import {List} from "immutable";
-import {DomainField, SelectInput} from "@labkey/components";
+import React, { FC, useCallback, useState } from 'react';
+import { List } from 'immutable';
+import { DomainField, Modal, SelectInput } from '@labkey/components';
 
-interface AliasFieldModalProps
-{
-    domainFields: List<DomainField>
-    showAlias?: boolean
-    onHide: () => any
-    onAdd: (any) => any
+interface AliasFieldModalProps {
+    domainFields: List<DomainField>;
+    onHide: () => any;
+    onAdd: (any) => any;
 }
 
-interface AliasFieldModalState
-{
-    show?: boolean
-    selectedField?: DomainField
-}
-
-export class AliasFieldModal extends PureComponent<AliasFieldModalProps, AliasFieldModalState> {
-
-    // pass fields as props
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            show: this.props.showAlias,
-            selectedField: this.props.domainFields.get(0)
-        }
-    }
-
-    handleOK = () => {
-        const { onAdd } = this.props;
-        const { selectedField } = this.state;
-
+export const AliasFieldModal: FC<AliasFieldModalProps> = ({ domainFields, onAdd, onHide }) => {
+    const [selectedField, setSelectedField] = useState<DomainField>(domainFields.get(0));
+    const onConfirm = useCallback(() => {
         const selectedFieldName = selectedField.name;
-
-        if (onAdd) {
-            const domainFieldChange = selectedField
-                .merge({
-                    name: 'Wrapped' + selectedFieldName,
-                    dataType: undefined,
-                    lockType: undefined,
-                    wrappedColumnName: selectedFieldName,
-                    propertyId: undefined,
-                    label: 'Wrapped' + selectedFieldName,
-                    lockExistingField: false,
-                    rangeURI: selectedField.rangeURI,
-                });
-            const newDomainField =  DomainField.create(domainFieldChange.toJS());
-            onAdd(newDomainField);
-        }
-
-    };
-
-    handleClose = () => {
-        const { onHide } = this.props;
-
-        onHide();
-    };
-
-    handleChange = (name, formValue, selected) => {
-        this.setState( () =>({
-            selectedField: selected
-        }));
-    };
-
-    renderDisplayFieldOptions = () => {
-        const { domainFields } = this.props;
-
-        return (
-            <>
-                <Row>
-                    <Col xs={3}>
+        const domainFieldChange = selectedField.merge({
+            name: 'Wrapped' + selectedFieldName,
+            dataType: undefined,
+            lockType: undefined,
+            wrappedColumnName: selectedFieldName,
+            propertyId: undefined,
+            label: 'Wrapped' + selectedFieldName,
+            lockExistingField: false,
+            rangeURI: selectedField.rangeURI,
+        });
+        const newDomainField = DomainField.create(domainFieldChange.toJS());
+        onAdd(newDomainField);
+    }, [onAdd, selectedField]);
+    const onChange = useCallback((_, __, domainField) => setSelectedField(domainField), []);
+    return (
+        <Modal
+            className="domain-modal"
+            confirmClass="btn-primary"
+            confirmText="OK"
+            onCancel={onHide}
+            onConfirm={onConfirm}
+            title="Choose a field to wrap"
+        >
+            <div className="row">
+                <div className="col-xs-3">
+                    <label>
                         Field Options
-                    </Col>
-                    <Col xs={9}>
-                        <SelectInput
-                            onChange={this.handleChange}
-                            value={domainFields.get(0)}
-                            options={domainFields.toArray()}
-                            inputClass={'col-xs-12'}
-                            valueKey="name"
-                            labelKey="label"
-                            clearable={false}
-                        />
-                    </Col>
-                </Row>
-            </>
-        )
+                    </label>
+                </div>
 
-    };
-
-    render() {
-        const {show} = this.state;
-
-        return (
-            <Modal
-                show={show}
-                onHide={this.handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>{'Choose a field to wrap'}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div className='domain-modal'>
-                        {this.renderDisplayFieldOptions()}
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <div className={'domain-designer-buttons'}>
-                        <Button onClick={this.handleClose} className='domain-adv-footer domain-adv-cancel-btn'>
-                            Cancel
-                        </Button>
-
-                        <Button onClick={this.handleOK} className='domain-adv-footer domain-adv-apply-btn' bsStyle={'primary'}>
-                            OK
-                        </Button>
-                    </div>
-                </Modal.Footer>
-            </Modal>
-        );
-    }
-
+                <div className="col-xs-9">
+                    <SelectInput
+                        id="domain-field-selector"
+                        onChange={onChange}
+                        value={domainFields.get(0)}
+                        options={domainFields.toArray()}
+                        inputClass="col-xs-12"
+                        valueKey="name"
+                        labelKey="label"
+                        clearable={false}
+                    />
+                </div>
+            </div>
+        </Modal>
+    );
 }
+AliasFieldModal.displayName = 'AliasFieldModal';
