664bb160b7d2432a0e4d8d43787a44ef6d9b4861
labkey-nicka <nickk@labkey.com>
2024-03-10 14:48:44 -0700

    MSSQL upgrade script

--- assay/resources/schemas/dbscripts/sqlserver/assay-24.003-24.004.sql
+++ assay/resources/schemas/dbscripts/sqlserver/assay-24.003-24.004.sql
@@ -1,1 +1,28 @@
-;
+ALTER TABLE assay.PlateSet ADD Type NVARCHAR(64);
+ALTER TABLE assay.PlateSet ADD RootPlateSetId INT;
+ALTER TABLE assay.PlateSet ADD PrimaryPlateSetId INT;
+GO
+
+ALTER TABLE assay.PlateSet ADD CONSTRAINT FK_PlateSet_RootPlateSetId FOREIGN KEY (RootPlateSetId) REFERENCES assay.PlateSet(RowId);
+ALTER TABLE assay.PlateSet ADD CONSTRAINT FK_PlateSet_PrimaryPlateSetId FOREIGN KEY (PrimaryPlateSetId) REFERENCES assay.PlateSet(RowId);
+
+-- Update all pre-existing plate sets to type "assay"
+UPDATE assay.PlateSet SET type = 'assay';
+
+ALTER TABLE assay.PlateSet ALTER COLUMN Type NVARCHAR(64) NOT NULL;
+
+CREATE TABLE assay.PlateSetEdge
+(
+    FromPlateSetId INT NOT NULL,
+    ToPlateSetId INT NOT NULL,
+    RootPlateSetId INT NOT NULL,
+
+    CONSTRAINT FK_PlateSet_FromPlate FOREIGN KEY (FromPlateSetId) REFERENCES assay.PlateSet(RowId),
+    CONSTRAINT FK_PlateSet_ToPlate FOREIGN KEY (ToPlateSetId) REFERENCES assay.PlateSet(RowId),
+    CONSTRAINT FK_PlateSet_RootPlate FOREIGN KEY (RootPlateSetId) REFERENCES assay.PlateSet(RowId),
+    CONSTRAINT UQ_PlateSetEdge_FromPlate_ToPlate UNIQUE (FromPlateSetId, ToPlateSetId)
+);
+
+CREATE INDEX IX_PlateSetEdge_FromPlateSetId ON assay.PlateSetEdge(FromPlateSetId);
+CREATE INDEX IX_PlateSetEdge_ToPlateSetId ON assay.PlateSetEdge(ToPlateSetId);
+CREATE INDEX IX_PlateSetEdge_RootPlateSetId ON assay.PlateSetEdge(RootPlateSetId);
