8707a0ebd8264e8884498d1196baa75e2e3fdcf9
rosalinep <rosalinep@labkey.com>
2024-04-03 11:38:22 -0700

    Fix conditional

--- assay/src/org/labkey/assay/PlateController.java
+++ assay/src/org/labkey/assay/PlateController.java
@@ -976,7 +976,7 @@ public class PlateController extends SpringActionController
             if (!form.isStandaloneAssayPlateCase() && !form.isReplateCase() && !form.isRearrayCase() && !form.isEmptyCase() && !form.isDefaultCase())
                 errors.reject(ERROR_GENERIC, "Invalid parameters.");
 
-            if ((form.isStandaloneAssayPlateCase() || form.isRearrayCase()) && form.getType() != PlateSetType.assay)
+            if (form.isRearrayCase() && form.getType() != PlateSetType.assay)
                 errors.reject(ERROR_GENERIC, "This Plate Set must be of type Assay.");
         }
 
