10ed89fb2a233a751414ea13e2fd795d49bafd0b
labkey-susanh <susanh@labkey.com>
2024-04-05 08:31:10 -0700

    Don't set colors in upgrade script; let UI handle with default values

--- core/resources/schemas/dbscripts/postgresql/core-24.002-24.003.sql
+++ core/resources/schemas/dbscripts/postgresql/core-24.002-24.003.sql
@@ -1,5 +1,2 @@
 ALTER TABLE core.datastates ADD COLUMN Color VARCHAR(7);
 
-UPDATE core.datastates SET Color='#F0F8ED' WHERE StateType='Available';
-UPDATE core.datastates SET Color='#FDE6E6' WHERE StateType='Locked';
-UPDATE core.datastates SET Color='#FCF8E3' WHERE StateType='Consumed';
--- core/resources/schemas/dbscripts/sqlserver/core-24.002-24.003.sql
+++ core/resources/schemas/dbscripts/sqlserver/core-24.002-24.003.sql
@@ -1,6 +1,1 @@
 ALTER TABLE core.datastates ADD Color NVARCHAR(7) NULL;
-GO
-
-UPDATE core.datastates SET Color='#F0F8ED' WHERE StateType='Available';
-UPDATE core.datastates SET Color='#FDE6E6' WHERE StateType='Locked';
-UPDATE core.datastates SET Color='#FCF8E3' WHERE StateType='Consumed';
