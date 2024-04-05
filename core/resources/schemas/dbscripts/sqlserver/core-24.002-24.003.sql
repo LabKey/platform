28bf137970704f021c7488bdf09f0c019afef2fa
labkey-susanh <susanh@labkey.com>
2024-04-05 07:11:32 -0700

    Update to use background colors not border colors

--- core/resources/schemas/dbscripts/sqlserver/core-24.002-24.003.sql
+++ core/resources/schemas/dbscripts/sqlserver/core-24.002-24.003.sql
@@ -1,6 +1,6 @@
 ALTER TABLE core.datastates ADD Color NVARCHAR(7) NULL;
 GO
 
-UPDATE core.datastates SET Color='#D6E9C6' WHERE StateType='Available';
-UPDATE core.datastates SET Color='#F9B3B3' WHERE StateType='Locked';
-UPDATE core.datastates SET Color='#FAEBCC' WHERE StateType='Consumed';
+UPDATE core.datastates SET Color='#F0F8ED' WHERE StateType='Available';
+UPDATE core.datastates SET Color='#FDE6E6' WHERE StateType='Locked';
+UPDATE core.datastates SET Color='#FCF8E3' WHERE StateType='Consumed';
