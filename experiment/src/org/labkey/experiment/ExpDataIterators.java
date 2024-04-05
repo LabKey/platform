1a59952b3239d64c45c8a29ec691d7bda0626e47
Xing Yang <5168106+XingY@users.noreply.github.com>
2024-04-05 14:29:40 -0700

    Issue 50030: sample update data iterator (#5390)

--- experiment/src/org/labkey/experiment/ExpDataIterators.java
+++ experiment/src/org/labkey/experiment/ExpDataIterators.java
@@ -136,6 +136,8 @@ import java.util.stream.Collectors;
 
 import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
 import static org.labkey.api.data.CompareType.IN;
+import static org.labkey.api.dataiterator.ExistingRecordDataIterator.EXISTING_RECORD_COLUMN_NAME;
+import static org.labkey.api.dataiterator.SampleUpdateAddColumnsDataIterator.CURRENT_SAMPLE_STATUS_COLUMN_NAME;
 import static org.labkey.api.exp.api.ExpData.DATA_INPUTS_PREFIX_LC;
 import static org.labkey.api.exp.api.ExpData.DATA_INPUT_PARENT;
 import static org.labkey.api.exp.api.ExpMaterial.ALIQUOTED_FROM_INPUT;
@@ -145,14 +147,19 @@ import static org.labkey.api.exp.api.ExpRunItem.INPUTS_PREFIX_LC;
 import static org.labkey.api.exp.api.ExperimentService.ALIASCOLUMNALIAS;
 import static org.labkey.api.exp.api.ExperimentService.QueryOptions.SkipBulkRemapCache;
 import static org.labkey.api.exp.query.ExpMaterialTable.Column.AliquotedFromLSID;
+import static org.labkey.api.exp.query.ExpMaterialTable.Column.Folder;
+import static org.labkey.api.exp.query.ExpMaterialTable.Column.MaterialSourceId;
+import static org.labkey.api.exp.query.ExpMaterialTable.Column.Name;
 import static org.labkey.api.exp.query.ExpMaterialTable.Column.RootMaterialRowId;
 import static org.labkey.api.exp.query.ExpMaterialTable.Column.RowId;
 import static org.labkey.api.exp.query.ExpMaterialTable.Column.SampleState;
 import static org.labkey.api.exp.query.ExpMaterialTable.Column.StoredAmount;
 import static org.labkey.api.exp.query.ExpMaterialTable.Column.Units;
 import static org.labkey.api.query.AbstractQueryImportAction.configureLoader;
+import static org.labkey.experiment.api.SampleTypeUpdateServiceDI.PARENT_RECOMPUTE_NAME_SET;
 import static org.labkey.experiment.api.SampleTypeUpdateServiceDI.ROOT_RECOMPUTE_ROWID_COL;
 import static org.labkey.experiment.api.SampleTypeUpdateServiceDI.PARENT_RECOMPUTE_NAME_COL;
+import static org.labkey.experiment.api.SampleTypeUpdateServiceDI.ROOT_RECOMPUTE_ROWID_SET;
 
 
 public class ExpDataIterators
@@ -2260,9 +2267,15 @@ public class ExpDataIterators
                 return ret;
             };
 
+            DataIteratorBuilder step2c = step2b;
+            if (isSample && isMergeOrUpdate)
+            {
+                step2c = LoggingDataIterator.wrap(new ExpDataIterators.SampleStatusCheckIteratorBuilder(step2b, _container));
+            }
+
             // Insert into exp.data then the provisioned table
             // Use embargo data iterator to ensure rows are committed before being sent along Issue 26082 (row at a time, reselect rowid)
-            DataIteratorBuilder step3 = LoggingDataIterator.wrap(new TableInsertDataIteratorBuilder(step2b, _expTable, _container)
+            DataIteratorBuilder step3 = LoggingDataIterator.wrap(new TableInsertDataIteratorBuilder(step2c, _expTable, _container)
                     .setKeyColumns(keyColumns)
                     .setDontUpdate(dontUpdate)
                     .setAddlSkipColumns(_excludedColumns)
@@ -2930,4 +2943,129 @@ public class ExpDataIterators
             return LoggingDataIterator.wrap(new MultiDataTypeCrossProjectDataIterator(pre, context, _container, _user, _isCrossType, _isCrossFolder, _dataType, _isSamples));
         }
     }
+
+    public static class SampleStatusCheckIteratorBuilder implements DataIteratorBuilder
+    {
+        private final DataIteratorBuilder _in;
+        private final Container _container;
+
+        public SampleStatusCheckIteratorBuilder(@NotNull DataIteratorBuilder in, Container container)
+        {
+            _in = in;
+            _container = container;
+        }
+
+        @Override
+        public DataIterator getDataIterator(DataIteratorContext context)
+        {
+            DataIterator pre = _in.getDataIterator(context);
+            return LoggingDataIterator.wrap(new SampleStatusCheckDataIterator(pre, context, _container));
+        }
+    }
+
+    public static class SampleStatusCheckDataIterator extends WrapperDataIterator
+    {
+        private final Set<String> SAMPLE_IMPORT_BASE_FIELDS = new CaseInsensitiveHashSet(
+                "LSID",
+                "CreatedBy",
+                "Modified",
+                "ModifiedBy",
+                "Created",
+                "_rowNumber",
+                RowId.name(),
+                "genId",
+                AliquotedFromLSID.name(),
+                ALIQUOTED_FROM_INPUT,
+                ROOT_RECOMPUTE_ROWID_COL,
+                PARENT_RECOMPUTE_NAME_COL,
+                ROOT_RECOMPUTE_ROWID_SET,
+                PARENT_RECOMPUTE_NAME_SET,
+                "cpasType",
+                Folder.name(),
+                Name.name(),
+                "EntityId",
+                EXISTING_RECORD_COLUMN_NAME,
+                CURRENT_SAMPLE_STATUS_COLUMN_NAME,
+                MaterialSourceId.name(),
+                RootMaterialRowId.name()
+        );
+
+        final DataIteratorContext _context;
+        private final Integer _sampleStateCol;
+        private final Integer _oldSampleStateCol;
+        private Map<Integer, DataState> _allStates;
+        private final boolean _noStatusChangeCol;
+        private final boolean _hasNonStatusChangeCol;
+
+        protected SampleStatusCheckDataIterator(DataIterator di, DataIteratorContext context, Container container)
+        {
+            super(di);
+            _context = context;
+            Map<String, Integer> map = DataIteratorUtil.createColumnNameMap(di);
+            _sampleStateCol = map.get(SampleState.name());
+            _noStatusChangeCol = _sampleStateCol == null;
+            _oldSampleStateCol = map.get(CURRENT_SAMPLE_STATUS_COLUMN_NAME);
+
+            _allStates = SampleStatusService.get()
+                    .getAllProjectStates(container)
+                    .stream().collect(Collectors.toMap(DataState::getRowId, data -> data));
+
+            boolean hasNonStatusChangeCol = false;
+            for (String col : map.keySet())
+            {
+                if (SampleState.name().equalsIgnoreCase(col))
+                    continue;
+                if (!SAMPLE_IMPORT_BASE_FIELDS.contains(col))
+                {
+                    hasNonStatusChangeCol = true;
+                    break;
+                }
+            }
+            _hasNonStatusChangeCol = hasNonStatusChangeCol;
+        }
+
+        @Override
+        public boolean next() throws BatchValidationException
+        {
+            boolean hasNext = super.next();
+            if (!hasNext)
+                return false;
+
+            if (_context.getErrors().hasErrors())
+                return true;
+
+            if (_oldSampleStateCol == null && getExistingRecord() == null)
+                return true;
+
+            Integer oldState;
+            if (_oldSampleStateCol != null)
+                oldState = (Integer) get(_oldSampleStateCol);
+            else
+                oldState = (Integer) getExistingRecord().get(SampleState.name());
+
+            if (oldState == null)
+                return true;
+
+            DataState oldStatus = _allStates.get(oldState);
+            boolean oldAllowsOp = SampleStatusService.get().isOperationPermitted(oldStatus, SampleTypeService.SampleOperations.EditMetadata);
+            if (oldAllowsOp)
+                return true;
+
+            if (_noStatusChangeCol)
+            {
+                _context.getErrors().addRowError(new ValidationException(String.format("Updating sample data when status is %s is not allowed.", oldStatus.getLabel())));
+                return true;
+            }
+
+            Integer newState = (Integer) get(_sampleStateCol);
+            DataState newStatus = _allStates.get(newState);
+            boolean newAllowsOp = SampleStatusService.get().isOperationPermitted(newStatus, SampleTypeService.SampleOperations.EditMetadata);
+
+            if (!newAllowsOp && _hasNonStatusChangeCol)
+                _context.getErrors().addRowError(new ValidationException(String.format("Updating sample data when status is %s is not allowed.", oldStatus.getLabel())));
+
+            return true;
+        }
+    }
+
 }
