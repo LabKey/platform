f655699f4f2e00e954129839163bec27346add4b
labkey-nicka <nickk@labkey.com>
2024-05-01 13:34:31 -0700

    SQL Server upgrade

--- assay/resources/schemas/dbscripts/sqlserver/assay-24.006-24.007.sql
+++ assay/resources/schemas/dbscripts/sqlserver/assay-24.006-24.007.sql
@@ -2,3 +2,6 @@ ALTER TABLE assay.PlateSet ADD Template BIT NOT NULL DEFAULT 0;
 UPDATE assay.Plate SET Template = 0 WHERE Template = 1;
 
 ALTER TABLE assay.Plate ADD Archived BIT NOT NULL DEFAULT 0;
+
+UPDATE assay.Plate SET AssayType = 'Standard' WHERE AssayType IS NULL;
+ALTER TABLE assay.Plate ALTER COLUMN AssayType NVARCHAR(200) NOT NULL;
