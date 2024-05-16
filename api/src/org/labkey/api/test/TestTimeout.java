aa01bbb5c9b7e9de7c87743468f1219711275a10
Trey Chadick <tchad@labkey.com>
2024-05-16 13:51:08 -0700

    Increase timeout for TikaTestCase (#5509)

--- api/src/org/labkey/api/test/TestTimeout.java
+++ api/src/org/labkey/api/test/TestTimeout.java
@@ -20,18 +20,13 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-/**
- * Timeout for server-side unit tests, in seconds
- * User: jeckels
- * Date: 1/14/13
- */
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Inherited
 public @interface TestTimeout
 {
     /** One minute is our standard timeout */
-    public static final int DEFAULT = 60;
+    int DEFAULT = 60;
 
     int value() default DEFAULT;
 }
