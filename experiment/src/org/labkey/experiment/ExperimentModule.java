22d4bf2a1066724cfcc564637bc8ca8d13b32b89
labkey-susanh <susanh@labkey.com>
2024-05-22 10:39:01 -0700

    Update version number

--- experiment/src/org/labkey/experiment/ExperimentModule.java
+++ experiment/src/org/labkey/experiment/ExperimentModule.java
@@ -175,7 +175,7 @@ public class ExperimentModule extends SpringModule
     @Override
     public Double getSchemaVersion()
     {
-        return 24.002;
+        return 24.003;
     }
 
     @Nullable
