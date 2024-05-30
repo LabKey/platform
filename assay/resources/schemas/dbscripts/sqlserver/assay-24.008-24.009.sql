3ba7b187a54704ccf2f54fd84fcef757757b2e73
labkey-nicka <nickk@labkey.com>
2024-05-30 14:25:47 -0700

    Add index on assay.Well.SampleId - Improves performance of DELETE operation on exp.Material table

--- assay/resources/schemas/dbscripts/sqlserver/assay-24.008-24.009.sql
+++ assay/resources/schemas/dbscripts/sqlserver/assay-24.008-24.009.sql
@@ -1,2 +1,5 @@
+-- Add index on assay.Well.SampleId to improve performance of DELETE operation on exp.Material table.
+CREATE INDEX IX_Well_SampleId ON assay.Well (SampleId);
+
 -- Add index on assay.WellGroupPositions.WellId to improve performance of DELETE operation on assay.Well table.
 CREATE INDEX IX_WellGroupPositions_WellId ON assay.WellGroupPositions (WellId);
