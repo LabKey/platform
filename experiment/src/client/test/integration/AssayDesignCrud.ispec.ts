69587b19aad2af463ba6a1f1a88c232a964df5cf
XingY <xyang@labkey.com>
2024-06-03 10:01:08 -0700

    assay

---
+++ experiment/src/client/test/integration/AssayDesignCrud.ispec.ts
@@ -0,0 +1,7 @@
+import { checkLackDesignerOrReaderPerm } from './utils';
+
+
+describe('Assay Designer - Permissions', () => {
+    it('Lack designer or Reader permission', async () => {
+    });
+})
