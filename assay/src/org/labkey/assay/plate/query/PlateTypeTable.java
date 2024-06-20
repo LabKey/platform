a17e1347fab1682cf6320dc752a24d8fbdb8edd4
labkey-nicka <nickk@labkey.com>
2024-06-20 09:59:15 -0700

    PlateTypeTable: suppress mutations

--- assay/src/org/labkey/assay/plate/query/PlateTypeTable.java
+++ assay/src/org/labkey/assay/plate/query/PlateTypeTable.java
@@ -1,10 +1,16 @@
 package org.labkey.assay.plate.query;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.labkey.api.data.ContainerFilter;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.SimpleUserSchema;
 import org.labkey.api.query.UserSchema;
+import org.labkey.api.security.UserPrincipal;
+import org.labkey.api.security.permissions.DeletePermission;
+import org.labkey.api.security.permissions.InsertPermission;
+import org.labkey.api.security.permissions.Permission;
+import org.labkey.api.security.permissions.UpdatePermission;
 import org.labkey.assay.query.AssayDbSchema;
 
 import java.util.ArrayList;
@@ -12,6 +18,7 @@ import java.util.List;
 
 public class PlateTypeTable extends SimpleUserSchema.SimpleTable<UserSchema>
 {
+    public static final String NAME = "PlateType";
     private static final List<FieldKey> defaultVisibleColumns = new ArrayList<>();
 
     static
@@ -20,8 +27,6 @@ public class PlateTypeTable extends SimpleUserSchema.SimpleTable<UserSchema>
         defaultVisibleColumns.add(FieldKey.fromParts("Rows"));
         defaultVisibleColumns.add(FieldKey.fromParts("Columns"));
     }
-
-    public static final String NAME = "PlateType";
 
     public PlateTypeTable(PlateSchema schema, @Nullable ContainerFilter cf)
     {
@@ -33,5 +38,13 @@ public class PlateTypeTable extends SimpleUserSchema.SimpleTable<UserSchema>
     public List<FieldKey> getDefaultVisibleColumns()
     {
         return defaultVisibleColumns;
+    }
+
+    @Override
+    public boolean hasPermission(@NotNull UserPrincipal user, @NotNull Class<? extends Permission> perm)
+    {
+        if (InsertPermission.class.equals(perm) || UpdatePermission.class.equals(perm) || DeletePermission.class.equals(perm))
+            return false;
+        return super.hasPermission(user, perm);
     }
 }
