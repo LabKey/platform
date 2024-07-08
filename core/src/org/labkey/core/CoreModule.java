91d26e903024f13d66a17d2c97f590f5a15176ed
Adam Rauch <adam@labkey.com>
2024-07-08 15:53:22 -0700

    Metrics include all feature flags -> true/false map

--- core/src/org/labkey/core/CoreModule.java
+++ core/src/org/labkey/core/CoreModule.java
@@ -1133,9 +1133,11 @@ public class CoreModule extends SpringModule implements SearchService.DocumentPr
             javaInfo.put("java.vm.name", System.getProperty("java.vm.name"));
             results.put("javaRuntime", javaInfo);
             results.put("applicationMenuDisplayMode", LookAndFeelProperties.getInstance(ContainerManager.getRoot()).getApplicationMenuDisplayMode());
-            results.put("optionalFeaturesEnabled", AdminConsole.getOptionalFeatureFlags().stream()
-                .filter(OptionalFeatureFlag::isEnabled)
-                .collect(Collectors.groupingBy(optionalFeatureFlag -> optionalFeatureFlag.getType().name().toLowerCase(), Collectors.mapping(OptionalFeatureFlag::getFlag, Collectors.toList()))));
+            results.put("optionalFeatures", AdminConsole.getOptionalFeatureFlags().stream()
+                .collect(Collectors.groupingBy(optionalFeatureFlag -> optionalFeatureFlag.getType().name().toLowerCase(),
+                    Collectors.mapping(flag -> flag, Collectors.toMap(OptionalFeatureFlag::getFlag, OptionalFeatureFlag::isEnabled))
+                ))
+            );
             results.put("productFeaturesEnabled", AdminConsole.getProductFeatureSet());
             results.put("analyticsTrackingStatus", AnalyticsServiceImpl.get().getTrackingStatus().toString());
             String labkeyContextPath = AppProps.getInstance().getContextPath();
