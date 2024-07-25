2a54df19abcda332abf4522ce6a1f014cc1affa5
labkey-susanh <susanh@labkey.com>
2024-07-25 15:01:56 -0700

    Don't populate the ancestors table twice

--- experiment/resources/schemas/dbscripts/postgresql/exp-24.002-24.003.sql
+++ experiment/resources/schemas/dbscripts/postgresql/exp-24.002-24.003.sql
@@ -7,8 +7,6 @@ CREATE TABLE exp.MaterialAncestors
     CONSTRAINT FK_MaterialAncestors_MaterialId FOREIGN KEY (RowId) REFERENCES exp.Material (RowId) ON DELETE CASCADE
 );
 
-SELECT core.executeJavaUpgradeCode('populateMaterialAncestors');
-
 CREATE UNIQUE INDEX UQ_MaterialAncestors_AncestorTypeId_RowId ON exp.MaterialAncestors (AncestorTypeId, RowId);
 CREATE INDEX IDX_MaterialAncestors_AncestorTypeId_RowId_AncestorRowId ON exp.MaterialAncestors (AncestorTypeId, RowId, AncestorRowId);
 
@@ -21,8 +19,6 @@ CREATE TABLE exp.DataAncestors
 
     CONSTRAINT FK_DataAncestors_DataId FOREIGN KEY (RowId) REFERENCES exp.Data (RowId) ON DELETE CASCADE
 );
-
-SELECT core.executeJavaUpgradeCode('populateDataAncestors');
 
 CREATE UNIQUE INDEX UQ_DataAncestors_AncestorTypeId_RowId ON exp.DataAncestors (AncestorTypeId, RowId);
 CREATE INDEX IDX_DataAncestors_AncestorTypeId_RowId_AncestorRowId ON exp.DataAncestors (AncestorTypeId, RowId, AncestorRowId);
