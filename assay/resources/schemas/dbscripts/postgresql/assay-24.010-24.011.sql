e5e747780836b85e8d98cb035c9b028e7a818a85
rosalinep <rosalinep@labkey.com>
2024-08-07 09:14:50 -0700

    Change col size

--- assay/resources/schemas/dbscripts/postgresql/assay-24.010-24.011.sql
+++ assay/resources/schemas/dbscripts/postgresql/assay-24.010-24.011.sql
@@ -1,4 +1,4 @@
-ALTER TABLE assay.plate ADD Barcode VARCHAR(400);
+ALTER TABLE assay.plate ADD Barcode VARCHAR(255);
 ALTER TABLE assay.plate ADD CONSTRAINT UQ_Barcode UNIQUE (Barcode);
 
 UPDATE assay.plate SET Barcode = LPAD(rowid::text, 9, '0') WHERE plate.Barcode IS NULL AND plate.template IS FALSE;
