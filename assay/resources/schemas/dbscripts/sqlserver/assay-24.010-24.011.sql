c23818bb754db5a001215302939f14c2a2822645
rosalinep <rosalinep@labkey.com>
2024-08-06 17:20:36 -0700

    Add db constraint

--- assay/resources/schemas/dbscripts/sqlserver/assay-24.010-24.011.sql
+++ assay/resources/schemas/dbscripts/sqlserver/assay-24.010-24.011.sql
@@ -4,5 +4,7 @@ ALTER TABLE assay.plate ADD CONSTRAINT UQ_Barcode UNIQUE (Barcode);
 
 UPDATE assay.plate SET Barcode = FORMAT(rowid, '000000000') WHERE Barcode IS NULL AND template = 0;
 
+ALTER TABLE assay.plate ADD CONSTRAINT check_template_true_barcode_null CHECK (NOT template OR Barcode IS NULL);
+
 
 EXEC core.executeJavaUpgradeCode 'updateBarcodeSequence';
