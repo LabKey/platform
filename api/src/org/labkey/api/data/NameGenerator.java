e4866aa11fb694a8b4b5daba8b8917e19f467744
Xing Yang <5168106+XingY@users.noreply.github.com>
2024-08-14 09:29:06 -0700

    Issue 50924: LKSM: Importing samples using naming expression referencing parent inputs with # (#5732)

--- api/src/org/labkey/api/data/NameGenerator.java
+++ api/src/org/labkey/api/data/NameGenerator.java
@@ -656,12 +656,23 @@ public class NameGenerator
             if (StringUtils.isEmpty((valueStr).trim()))
                 return Stream.empty();
 
+            TSVWriter tsvWriter = new TSVWriter() // Used to quote values with newline/tabs/quotes
+            {
+                @Override
+                protected int write()
+                {
+                    throw new UnsupportedOperationException();
+                }
+            };
+
             // Issue 44841: The names of the parents may include commas, so we parse the set of parent names
             // using TabLoader instead of just splitting on the comma.
-            try (TabLoader tabLoader = new TabLoader(valueStr))
+            try (TabLoader tabLoader = new TabLoader(tsvWriter.quoteValue(valueStr)))
             {
                 tabLoader.setDelimiterCharacter(',');
                 tabLoader.setUnescapeBackslashes(false);
+                // Issue 50924: LKSM: Importing samples using naming expression referencing parent inputs with # result in error
+                tabLoader.setIncludeComments(true);
                 try
                 {
                     String[][] parsedValues = tabLoader.getFirstNLines(1);
--- api/src/org/labkey/api/reader/TabLoader.java
+++ api/src/org/labkey/api/reader/TabLoader.java
@@ -70,6 +70,8 @@ public class TabLoader extends DataLoader
     public static final FileType TSV_FILE_TYPE = new TabFileType(Arrays.asList(".tsv", ".txt"), ".tsv", "text/tab-separated-values");
     public static final FileType CSV_FILE_TYPE = new TabFileType(Collections.singletonList(".csv"), ".csv", "text/comma-separated-values");
 
+    private boolean _includeComments = false;
+
     private static final Logger _log = LogManager.getLogger(TabLoader.class);
 
     public static class TsvFactory extends AbstractDataLoaderFactory
@@ -298,6 +300,15 @@ public class TabLoader extends DataLoader
         return Collections.unmodifiableMap(_comments);
     }
 
+    public boolean isIncludeComments()
+    {
+        return _includeComments;
+    }
+
+    public void setIncludeComments(boolean includeComments)
+    {
+        _includeComments = includeComments;
+    }
 
     /**
      * called for non-quoted strings
@@ -380,7 +391,7 @@ public class TabLoader extends DataLoader
 
     private String[] readFields(TabBufferedReader r, @Nullable ColumnDescriptor[] columns)
     {
-        CharSequence line = readLine(r, true, !isIncludeBlankLines());
+        CharSequence line = readLine(r, !isIncludeComments(), !isIncludeBlankLines());
 
         if (line == null)
             return null;
