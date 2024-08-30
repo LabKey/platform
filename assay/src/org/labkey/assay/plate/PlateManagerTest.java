380b048842b7067e35e660e57b0981a0ef78e5f1
rosalinep <rosalinep@labkey.com>
2024-08-30 08:29:59 -0700

    Manager test update

--- assay/src/org/labkey/assay/plate/PlateManagerTest.java
+++ assay/src/org/labkey/assay/plate/PlateManagerTest.java
@@ -1,5 +1,6 @@
 package org.labkey.assay.plate;
 
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.junit.AfterClass;
@@ -31,6 +32,8 @@ import org.labkey.api.exp.api.ExperimentService;
 import org.labkey.api.exp.api.SampleTypeService;
 import org.labkey.api.exp.property.Domain;
 import org.labkey.api.gwt.client.model.GWTPropertyDescriptor;
+import org.labkey.api.module.Module;
+import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.query.BatchValidationException;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.QueryService;
@@ -39,6 +42,8 @@ import org.labkey.api.security.User;
 import org.labkey.api.util.JunitUtil;
 import org.labkey.api.util.Pair;
 import org.labkey.api.util.TestContext;
+import org.labkey.api.util.logging.LogHelper;
+import org.labkey.assay.AssayModule;
 import org.labkey.assay.plate.model.ReformatOptions;
 import org.labkey.assay.plate.model.WellBean;
 import org.labkey.assay.plate.query.PlateSchema;
@@ -48,9 +53,13 @@ import org.labkey.assay.query.AssayDbSchema;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
@@ -64,6 +73,8 @@ import static org.labkey.api.util.JunitUtil.deleteTestContainer;
 
 public final class PlateManagerTest
 {
+    private static final Logger LOG = LogHelper.getLogger(PlateManagerTest.class, "jUnit tests for Plates");
+
     private static Integer ARCHIVED_PLATE_SET_ID;
     private static Integer EMPTY_PLATE_SET_ID;
     private static Integer FULL_PLATE_SET_ID;
@@ -90,6 +101,18 @@ public final class PlateManagerTest
 
         container = JunitUtil.getTestContainer();
         user = TestContext.get().getUser();
+
+        ModuleLoader moduleLoader = ModuleLoader.getInstance();
+        Module assayModule = moduleLoader.getModule(AssayModule.NAME);
+        Set<Module> activeModules = container.getActiveModules();
+        LOG.info("rosalineCurrentActiveModules: " + activeModules.stream().map(Module::getName).collect(Collectors.joining(", ")));
+
+        if (!activeModules.contains(assayModule))
+        {
+            Set<Module> assayModuleSet = new HashSet<>(Arrays.asList(assayModule));
+            Set<Module> newActiveModules = Stream.concat(assayModuleSet.stream(), activeModules.stream()).collect(Collectors.toSet());
+            container.setActiveModules(newActiveModules);
+        }
 
         Domain domain = PlateManager.get().getPlateMetadataDomain(container, user);
         if (domain != null)
