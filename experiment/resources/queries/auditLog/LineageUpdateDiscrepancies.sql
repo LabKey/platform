3e5eae6085d1620b8bede518aaf8ffa7a09e3f18
labkey-susanh <susanh@labkey.com>
2024-09-07 17:20:56 -0700

    Issues 51215: query revision to include problem transaction ids

--- experiment/resources/queries/auditLog/LineageUpdateDiscrepancies.sql
+++ experiment/resources/queries/auditLog/LineageUpdateDiscrepancies.sql
@@ -1,46 +1,55 @@
--- lineage update events for samples that had lineage and were involved in a transaction that
--- updated lineage on some but not all samples.
--- Includes samples that may have had their lineage intentionally removed before this transaction happened.
+-- transaction ids where some, but not all, samples had lineage update events prior to the fix for Issue 51210
 WITH MixedTransactions AS
-         (SELECT a1.transactionId
-          FROM (SELECT DISTINCT transactionId
-                FROM auditlog.SampleTimelineEvent
-                -- created before the bug fix was introduced
-                WHERE islineageupdate = true
-                  AND comment = 'Sample was updated.'
-                  AND created < '2024-10-01') a1
-                   JOIN
-               -- but have associated entries that are not lineage updates
-                   (SELECT DISTINCT transactionid
-                    FROM auditlog.SampleTimelineEvent
-                    WHERE islineageupdate = false) a2
-               ON a1.transactionid = a2.transactionid
-          )
-SELECT
-    rowid,
-    container,
-   sampletype,
-   sampletypeid,
-   samplename,
-   d.sampleid,
-    latestLineageEventDate,
-   oldrecordmap,
-   newrecordmap
-   --transactionid,  TODO this should be the transactionid of the problematic transaction
+         (
+             SELECT a1.transactionId FROM
+                 (
+                     SELECT DISTINCT transactionId
+                     FROM auditlog.SampleTimelineEvent
+                     -- created before the Issue 51210 fix was introduced
+                     WHERE islineageupdate = true AND comment = 'Sample was updated.' and created < '2024-10-01'
+                 ) a1
+                     JOIN
+                 -- but have associated entries that are not lineage updates
+                     (
+                         SELECT DISTINCT transactionid
+                         FROM auditlog.SampleTimelineEvent
+                         WHERE islineageupdate = false
 
+                     ) a2
+                 ON a1.transactionid = a2.transactionid
+         )
+
+SELECT d.container,
+       d.sampletype,
+       d.sampletypeid,
+       d.samplename,
+       d.sampleid,
+       d.oldrecordmap AS lineageEventOldData,
+       d.newrecordmap AS lineageEventNewData,
+       d.rowId AS lineageEventRowId,
+       b.latestLineageEventDate,
+       m.transactionid as problemTransaction,
+       m.oldrecordmap AS problemOldRecordMap,
+       m.newRecordmap AS problemNewRecordMap,
+       m.rowId AS problemEventRowId
 FROM (
+
          SELECT a.sampleId, max(created) as latestLineageEventDate
          FROM auditlog.SampleTimelineEvent a
          WHERE sampleId IN (
-             -- samples involved in transactions where some, but not all, samples had lineage update events prior to the fix for Issue 51210
              SELECT DISTINCT sampleId
              FROM auditLog.SampleTimelineEvent sa
-             WHERE transactionId IN ( SELECT transactionId FROM MixedTransactions )
+             WHERE transactionId IN (
+                 SELECT transactionId FROM MixedTransactions
+             )
          )
-         -- samples had an audit log creating lineage
-         AND (a.newrecordmap LIKE '%MaterialInputs%' OR a.newrecordmap LIKE '%DataInputs%')
+           -- samples had an audit log creating lineage
+           AND (a.newrecordmap LIKE '%MaterialInputs%' OR a.newrecordmap LIKE '%DataInputs%')
          GROUP BY sampleid
      ) b
-
          JOIN auditLog.SampleTimelineEvent d on d.sampleid = b.sampleid AND d.created = b.latestLineageEventDate
-ORDER BY sampleId
+         JOIN (
+    SELECT DISTINCT sampleId, transactionId, oldrecordmap, newrecordmap, rowId
+    FROM auditlog.SampleTimelineEvent WHERE transactionID IN (SELECT transactionId FROM MixedTransactions)
+) m ON m.sampleId = b.sampleId
+ORDER BY problemTransaction, sampleId
