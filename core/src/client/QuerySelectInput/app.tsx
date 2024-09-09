359e87cb9b6c639a6120acc50463a1db40b51199
Nick Kerr <nickk@labkey.com>
2024-09-09 09:15:44 -0700

    React 18: switch dnd library (#5832)

--- core/src/client/QuerySelectInput/app.tsx
+++ core/src/client/QuerySelectInput/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from '@labkey/api';
 
 import { AppContext, QuerySelectInput } from './QuerySelectInput';
 
 App.registerApp<AppContext>('querySelectInput', (target, ctx) => {
-    ReactDOM.render(<QuerySelectInput context={ctx} />, document.getElementById(target));
+    createRoot(document.getElementById(target)).render(<QuerySelectInput context={ctx} />);
 });
--- experiment/src/client/RunGraph/app.tsx
+++ experiment/src/client/RunGraph/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from '@labkey/api';
 
 import { AppContext, RunGraph } from './RunGraph';
 
 App.registerApp<AppContext>('runGraph', (target, ctx) => {
-    ReactDOM.render(<RunGraph context={ctx} />, document.getElementById(target));
+    createRoot(document.getElementById(target)).render(<RunGraph context={ctx} />);
 });
