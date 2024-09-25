e40bb07b76e6fc7f94ff8c04ad28f3c9aebdfe90
rosalinep <rosalinep@labkey.com>
2024-09-25 12:21:09 -0700

    sqlserver-specific update

--- assay/resources/schemas/dbscripts/sqlserver/assay-24.012-24.013.sql
+++ assay/resources/schemas/dbscripts/sqlserver/assay-24.012-24.013.sql
@@ -8,3 +8,6 @@ ALTER TABLE assay.platesetproperty
 
 ALTER TABLE assay.platesetproperty ALTER COLUMN PropertyURI NVARCHAR(300) NULL;
 ALTER TABLE assay.platesetproperty ALTER COLUMN PropertyId INT NULL;
+
+ALTER TABLE assay.platesetproperty DROP CONSTRAINT UQ_PlateSetProperty_PlateSetId_PropertyId;
+CREATE UNIQUE INDEX UQ_PlateSetProperty_PlateSetId_PropertyId ON assay.platesetproperty (PlateSetId, PropertyId) WHERE PropertyId IS NOT NULL;
