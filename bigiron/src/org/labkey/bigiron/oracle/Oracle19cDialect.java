b324f90dbc1038165bafeaf1305d16e1a88778f1
labkey-tchad <tchad@labkey.com>
2024-10-21 11:57:37 -0700

    Remove bigiron module. Moved to https://github.com/LabKey/premiumModules

--- bigiron/src/org/labkey/bigiron/oracle/Oracle19cDialect.java
+++
@@ -1,5 +0,0 @@
-package org.labkey.bigiron.oracle;
-
-public class Oracle19cDialect extends Oracle18cDialect
-{
-}
--- bigiron/src/org/labkey/bigiron/oracle/Oracle23cDialect.java
+++
@@ -1,5 +0,0 @@
-package org.labkey.bigiron.oracle;
-
-public class Oracle23cDialect extends Oracle21cDialect
-{
-}
