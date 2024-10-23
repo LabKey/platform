46c335a71c543c395a1e2e2c9fc74c0b7f89ea44
Adam Rauch <adam@labkey.com>
2024-10-23 15:47:24 -0700

    Revert PK add to core.Containers

---
+++ core/resources/schemas/dbscripts/postgresql/core-24.006-24.007.sql
@@ -0,0 +1,4 @@
+-- Adding a PK on RowId seemed like a good idea, but it broke existing lookups to core.Containers and other assumptions.
+-- We could add a PK on EntityId, but that column is currently nullable. For now, we'll just live without a PK.
+ALTER TABLE core.Containers DROP CONSTRAINT PK_Containers;
+ALTER TABLE core.Containers ADD CONSTRAINT UQ_Containers_RowId UNIQUE (RowId);
