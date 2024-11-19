6724881a0029646d92792848c75a01f3a9367c4c
Josh Eckels <jeckels@labkey.com>
2024-11-18 16:49:27 -0800

    Update api/src/org/labkey/api/util/AbortedRequestException.java
    Co-authored-by: Adam Rauch <adam@labkey.com>

--- api/src/org/labkey/api/util/AbortedRequestException.java
+++ api/src/org/labkey/api/util/AbortedRequestException.java
@@ -1,7 +1,7 @@
 package org.labkey.api.util;
 
 /**
- * Signals that we're trying to abort processing. Examples include a HTTP request that's exceeded a timeout or a
+ * Signals that we're trying to abort processing. Examples include an HTTP request that's exceeded a timeout or a
  * PipelineJob that's been canceled.
  */
 public class AbortedRequestException extends RuntimeException implements SkipMothershipLogging
