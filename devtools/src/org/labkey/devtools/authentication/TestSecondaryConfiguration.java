142f84e5c1013bfbeabad24772ea753c4f26fcb1
Binal Patel <binalp@labkey.com>
2024-12-17 15:21:17 -0800

    Refactor AuthenticationConfiguration.getRedirectURL() to return URLHelper.

--- devtools/src/org/labkey/devtools/authentication/TestSecondaryConfiguration.java
+++ devtools/src/org/labkey/devtools/authentication/TestSecondaryConfiguration.java
@@ -3,8 +3,9 @@ package org.labkey.devtools.authentication;
 import org.labkey.api.data.Container;
 import org.labkey.api.security.BaseSecondaryAuthenticationConfiguration;
 import org.labkey.api.security.User;
-import org.labkey.api.view.ActionURL;
+import org.labkey.api.util.URLHelper;
 
+import java.net.URISyntaxException;
 import java.util.Map;
 
 public class TestSecondaryConfiguration extends BaseSecondaryAuthenticationConfiguration<TestSecondaryProvider>
@@ -15,8 +16,15 @@ public class TestSecondaryConfiguration extends BaseSecondaryAuthenticationConfi
     }
 
     @Override
-    public ActionURL getRedirectURL(User candidate, Container c)
+    public URLHelper getRedirectURL(User candidate, Container c)
     {
-        return TestSecondaryController.getTestSecondaryURL(c, getRowId());
+        try
+        {
+            return new URLHelper(TestSecondaryController.getTestSecondaryURL(c, getRowId()).getURIString());
+        }
+        catch (URISyntaxException e)
+        {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
     }
 }
