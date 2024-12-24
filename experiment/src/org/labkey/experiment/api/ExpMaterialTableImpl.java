533759e581cf1c475747cf52e4ef34c351faccb7
RosalineP <rosalinep@labkey.com>
2024-12-23 19:23:06 -0800

    AbDisc: Sample to Plate Nav (#6166)

--- experiment/src/org/labkey/experiment/api/ExpMaterialTableImpl.java
+++ experiment/src/org/labkey/experiment/api/ExpMaterialTableImpl.java
@@ -769,6 +769,37 @@ public class ExpMaterialTableImpl extends ExpRunItemTableImpl<ExpMaterialTable.C
         if (InventoryService.get() != null && (st == null || !st.isMedia()))
             defaultCols.addAll(InventoryService.get().addInventoryStatusColumns(st == null ? null : st.getMetricUnit(), this, getContainer(), _userSchema.getUser()));
 
+        UserSchema plateUserSchema = QueryService.get().getUserSchema(_userSchema.getUser(), getContainer(), "plate");
+        SQLFragment sql;
+        if (plateUserSchema != null)
+        {
+            String rowIdField = ExprColumn.STR_TABLE_ALIAS + "." + Column.RowId.name();
+            SQLFragment existsSubquery = new SQLFragment()
+                    .append("SELECT 1 FROM ")
+                    .append(plateUserSchema.getTable("Well"), "well")
+                    .append(" WHERE well.sampleid = ").append(rowIdField);
+
+            sql = new SQLFragment()
+                    .append("CASE WHEN EXISTS (")
+                    .append(existsSubquery)
+                    .append(") THEN 'Plated' ELSE 'Not Plated'")
+                    .append(" END");
+        }
+        else
+        {
+            sql = new SQLFragment("SELECT NULL");
+        }
+        var col = new ExprColumn(this, Column.IsPlated.name(), sql, JdbcType.VARCHAR);
+        col.setDescription("Whether the sample that has been plated, if plating is supported.");
+        col.setUserEditable(false);
+        col.setReadOnly(true);
+        col.setShownInDetailsView(false);
+        col.setShownInInsertView(false);
+        col.setShownInUpdateView(false);
+        if (plateUserSchema != null)
+           col.setURL(DetailsURL.fromString("plate-isPlated.api?sampleId=${" + Column.RowId.name() + "}"));
+        addColumn(col);
+
         addVocabularyDomains();
 
         addColumn(Column.Properties);
