283598e00ca239e28f52a86be5c026f727b126db
Binal Patel <binalp@labkey.com>
2025-01-11 09:04:55 -0800

    Revert changes

--- devtools/src/org/labkey/devtools/authentication/TestSecondaryConfiguration.java
+++ devtools/src/org/labkey/devtools/authentication/TestSecondaryConfiguration.java
@@ -3,7 +3,6 @@ package org.labkey.devtools.authentication;
 import org.labkey.api.data.Container;
 import org.labkey.api.security.BaseSecondaryAuthenticationConfiguration;
 import org.labkey.api.security.User;
-import org.labkey.api.util.URLHelper;
 import org.labkey.api.view.ActionURL;
 
 import java.util.Map;
@@ -16,7 +15,7 @@ public class TestSecondaryConfiguration extends BaseSecondaryAuthenticationConfi
     }
 
     @Override
-    public URLHelper getRedirectURL(User candidate, Container c)
+    public ActionURL getRedirectURL(User candidate, Container c)
     {
         return TestSecondaryController.getTestSecondaryURL(c, getRowId());
     }
