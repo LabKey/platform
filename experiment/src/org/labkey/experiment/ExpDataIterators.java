4e09925a3d099fe44a9bc5812298c9a26e1911aa
labkey-matthewb <matthewb@labkey.com>
2025-01-22 20:39:54 -0800

    don't duplicate rowNumber column in ListOfMapsDataIterator constructor (#6241)

--- experiment/src/org/labkey/experiment/ExpDataIterators.java
+++ experiment/src/org/labkey/experiment/ExpDataIterators.java
@@ -3184,7 +3184,7 @@ public class ExpDataIterators
                 "Modified",
                 "ModifiedBy",
                 "Created",
-                "_rowNumber",
+                ROWNUMBER_COLUMNNAME,
                 RowId.name(),
                 "genId",
                 AliquotedFromLSID.name(),
