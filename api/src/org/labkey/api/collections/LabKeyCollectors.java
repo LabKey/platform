b0dcff1506a76e270ca2c588358767074a212e3a
Adam Rauch <adam@labkey.com>
2025-02-21 13:00:47 -0800

    Fix many issues with lists and special characters (#6325)

--- api/src/org/labkey/api/collections/LabKeyCollectors.java
+++ api/src/org/labkey/api/collections/LabKeyCollectors.java
@@ -11,17 +11,19 @@ import org.labkey.api.util.HtmlStringBuilder;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toMap;
 
 /**
  * Static methods that return custom {@link Collector}s that can be used to collect elements of a {@link Stream} into a
@@ -30,53 +32,99 @@ import static java.util.stream.Collectors.toMap;
 public class LabKeyCollectors
 {
     /**
+     * A {@link Collectors#toMap(Function, Function, BinaryOperator, Supplier)} alternative that allows {@code null} values.
+     * The standard method does not allow {@code null} values because it uses {@link Map#merge(Object, Object, BiFunction)}
+     * which throws NPE on {@code null}. See <a href="https://bugs.openjdk.org/browse/JDK-8148463">JDK bug</a>.
+     */
+    public static <T, K, V, M extends Map<K, V>> Collector<T, ?, M> toMapNullSafe(
+        Function<? super T, ? extends K> keyMapper,
+        Function<? super T, ? extends V> valueMapper,
+        BinaryOperator<V> mergeFunction,
+        Supplier<M> mapFactory)
+    {
+        // Effectively the same as toMap() except for the use of the null-supporting merge() method below
+        return Collector.of(
+            mapFactory,
+            (M map, T t) -> merge(map, keyMapper.apply(t), valueMapper.apply(t), mergeFunction),
+            (M map1, M map2) -> {
+                for (Map.Entry<K, V> e : map2.entrySet())
+                    merge(map1, e.getKey(), e.getValue(), mergeFunction);
+                return map1;
+            }
+        );
+    }
+
+    /**
+     * A {@link Map#merge(Object, Object, BiFunction)} alternative that allows null values and maintains null mappings
+     */
+    private static <K, V> void merge(Map<K, V> map, K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction)
+    {
+        Objects.requireNonNull(remappingFunction);
+        V newValue = map.containsKey(key) ? remappingFunction.apply(map.get(key), value) : value;
+        map.put(key, newValue);
+    }
+
+    /**
+     * Standard map collector that accepts null values and rejects duplicate keys
+     * @throws IllegalStateException if duplicate keys are detected
+     */
+    public static <T, K, V, M extends Map<K, V>> Collector<T, ?, M> toMapNullSafe(
+        Function<? super T, ? extends K> keyMapper,
+        Function<? super T, ? extends V> valueMapper,
+        Supplier<M> mapFactory)
+    {
+        return toMapNullSafe(
+            keyMapper,
+            valueMapper,
+            (u, v) -> {
+                throw new IllegalStateException(String.format("Duplicate key %s", u));
+            },
+            mapFactory);
+    }
+
+    /**
      * Returns a {@link Collector} that builds a {@link LinkedHashMap}, for cases where caller wants a map that preserves {@link Stream} order.
-     * https://stackoverflow.com/questions/29090277/how-do-i-keep-the-iteration-order-of-a-list-when-using-collections-tomap-on-a
+     * <a href="https://stackoverflow.com/questions/29090277/how-do-i-keep-the-iteration-order-of-a-list-when-using-collections-tomap-on-a">Stackoverflow source</a>
+     * Accepts {@code null} values.
+     * @throws IllegalStateException if duplicate keys are detected
      */
     public static <T, K, U> Collector<T, ?, Map<K, U>> toLinkedMap(
         Function<? super T, ? extends K> keyMapper,
         Function<? super T, ? extends U> valueMapper)
     {
-        return toMap(
+        return toMapNullSafe(
             keyMapper,
             valueMapper,
-            (u, v) -> {
-                throw new IllegalStateException(String.format("Duplicate key %s", u));
-            },
             LinkedHashMap::new
         );
     }
 
     /**
-     * Returns a {@link Collector} that builds a {@link CaseInsensitiveHashMap}
+     * Returns a {@link Collector} that builds a {@link CaseInsensitiveHashMap}. Accepts {@code null} values.
+     * @throws IllegalStateException if duplicate keys are detected
      */
     public static <T, U> Collector<T, ?, Map<String, U>> toCaseInsensitiveMap(
         Function<? super T, String> keyMapper,
         Function<? super T, ? extends U> valueMapper)
     {
-        return toMap(
+        return toMapNullSafe(
             keyMapper,
             valueMapper,
-            (u, v) -> {
-                throw new IllegalStateException(String.format("Duplicate key %s", u));
-            },
             CaseInsensitiveHashMap::new
         );
     }
 
     /**
-     * Returns a {@link Collector} that builds a {@link CaseInsensitiveLinkedHashMap}
+     * Returns a {@link Collector} that builds a {@link CaseInsensitiveLinkedHashMap}. Accepts {@code null} values.
+     * @throws IllegalStateException if duplicate keys are detected
      */
     public static <T, U> Collector<T, ?, Map<String, U>> toCaseInsensitiveLinkedMap(
         Function<? super T, String> keyMapper,
         Function<? super T, ? extends U> valueMapper)
     {
-        return toMap(
+        return toMapNullSafe(
             keyMapper,
             valueMapper,
-            (u, v) -> {
-                throw new IllegalStateException(String.format("Duplicate key %s", u));
-            },
             CaseInsensitiveLinkedHashMap::new
         );
     }
@@ -122,10 +170,8 @@ public class LabKeyCollectors
             (MultiValuedMap<K, V> mmap, T t) -> mmap.put(keyMapper.apply(t), valueMapper.apply(t)),
             (mmap1, mmap2) ->
             {
-                MultiValuedMap<K, V> mmap = supplier.get();
-                mmap.putAll(mmap1);
-                mmap.putAll(mmap2);
-                return mmap;
+                mmap1.putAll(mmap2);
+                return mmap1;
             });
     }
 
@@ -181,7 +227,7 @@ public class LabKeyCollectors
             assertEquals(4, mmap.size());
 
             Map<String, String> lhmap = map.entrySet().stream().sorted(Map.Entry.comparingByKey()).collect(toLinkedMap(Map.Entry::getKey, Map.Entry::getValue));
-            Comparators.isInOrder(lhmap.keySet(), Comparator.naturalOrder());
+            assertTrue(Comparators.isInOrder(lhmap.keySet(), Comparator.naturalOrder()));
 
             JSONArray jsonArray = map.keySet().stream().sorted().collect(toJSONArray());
             assertEquals(4, jsonArray.length());
@@ -193,8 +239,37 @@ public class LabKeyCollectors
             assertEquals(10, mmap2.keySet().size());
         }
 
+        @Test
+        public void testNullSafe()
+        {
+            Map<String, String> someNulls = new HashMap<>();
+            someNulls.put("ABC", "one");
+            someNulls.put("B", "two");
+            someNulls.put("C", null);
+            someNulls.put("D", "four");
+            someNulls.put("E", null);
+
+            Map<String, String> insensitive = someNulls.entrySet().stream()
+                .collect(toCaseInsensitiveMap(Map.Entry::getKey, Map.Entry::getValue));
+            assertEquals(5, insensitive.size());
+
+            assertEquals("one", insensitive.get("abc"));
+            assertEquals("two", insensitive.get("b"));
+            assertNull(insensitive.get("c"));
+            assertEquals("four", insensitive.get("D"));
+            assertNull(insensitive.get("E"));
+
+            Map<String, String> linked = someNulls.entrySet().stream()
+                .collect(toLinkedMap(Map.Entry::getKey, Map.Entry::getValue));
+            assertEquals(5, linked.size());
+
+            Map<String, String> insensitiveLinked = someNulls.entrySet().stream()
+                    .collect(toCaseInsensitiveLinkedMap(Map.Entry::getKey, Map.Entry::getValue));
+            assertEquals(5, insensitiveLinked.size());
+        }
+
         @Test(expected = IllegalStateException.class)
-        public void testDuplicates()
+        public void testDuplicatesLinkedMap()
         {
             List<String> list = List.of(
                 "one",
@@ -208,6 +283,23 @@ public class LabKeyCollectors
 
             //noinspection ResultOfMethodCallIgnored
             list.stream().collect(toLinkedMap(v->v, v->v));
+        }
+
+        @Test(expected = IllegalStateException.class)
+        public void testDuplicatesCaseInsensitive()
+        {
+            List<String> list = List.of(
+                "one",
+                "two",
+                "three",
+                "four",
+                "six",
+                "two",
+                "five"
+            );
+
+            //noinspection ResultOfMethodCallIgnored
+            list.stream().collect(toCaseInsensitiveMap(v->v, v->v));
         }
     }
 }
