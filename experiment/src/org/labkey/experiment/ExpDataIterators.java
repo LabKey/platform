6944af1c2fc943f554a051f5add3b446ec2b1cbc
Susan Hert <susanh@labkey.com>
2025-02-24 07:08:15 -0800

    Issue 52355: Assure we always have non-blank rows for rows provided for cross-type import (#6357)

--- experiment/src/org/labkey/experiment/ExpDataIterators.java
+++ experiment/src/org/labkey/experiment/ExpDataIterators.java
@@ -2961,7 +2961,12 @@ public class ExpDataIterators
                 ColumnInfo colInfo = getColumnInfo(i);
                 String name = colInfo.getName();
                 String lcName = name.toLowerCase();
-                if (validFields.contains(name))
+                if (_typeColIndex != null && _typeColIndex == i) // Issue 52355: assure we have some data in the row by including the type
+                {
+                    fieldIndexes.add(i);
+                    header.add(_typeColName);
+                }
+                else if (validFields.contains(name))
                 {
                     fieldIndexes.add(i);
                     header.add(_tsvWriter.quoteValue(name));
