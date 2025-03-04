e359e8d23d0f5f7bf81bc5a8c439c8ba6b1eabf9
labkey-susanh <susanh@labkey.com>
2025-03-03 17:07:33 -0800

    isEmpty

--- api/src/org/labkey/api/action/ApiQueryResponse.java
+++ api/src/org/labkey/api/action/ApiQueryResponse.java
@@ -386,7 +386,7 @@ public class ApiQueryResponse implements ApiResponse
             String[] sortStrings = sortString.split(",");
             for (String sort : sortStrings)
             {
-                if (sort.length() == 0)
+                if (sort.isEmpty())
                     continue;
                 String dir = "ASC";
                 if (sort.charAt(0) == '-' || sort.charAt(0) == '+')
