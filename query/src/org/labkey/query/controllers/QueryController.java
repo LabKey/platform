7b414c67e5023809d08f10dc922f7477c7f4cb7b
bbimber <bbimber@gmail.com>
2025-03-18 12:45:14 -0700

    Pass something other than 'null' in the template name when not provided (#6448)

--- query/src/org/labkey/query/controllers/QueryController.java
+++ query/src/org/labkey/query/controllers/QueryController.java
@@ -2196,7 +2196,7 @@ public class QueryController extends SpringActionController
                     .setIncludeColumns(form.getIncludeColumns())
                     .setExcludeColumns(form.getExcludeColumns())
                     .setRenamedColumns(form.getRenameColumnMap())
-                    .setPrefix(form.getFilenamePrefix() + "_Template") // Issue 48028: Change template file names
+                    .setPrefix((StringUtils.isEmpty(form.getFilenamePrefix()) ? "Import" : form.getFilenamePrefix()) + "_Template") // Issue 48028: Change template file names
             );
         }
     }
