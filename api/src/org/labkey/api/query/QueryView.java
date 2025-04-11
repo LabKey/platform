02d30e72c65bcf14c0d72d39437f89b95a1c1657
Adam Rauch <adam@labkey.com>
2025-04-11 12:21:46 -0700

    Ensure that QueryView always has a non-null Errors

--- api/src/org/labkey/api/query/QueryView.java
+++ api/src/org/labkey/api/query/QueryView.java
@@ -282,7 +282,8 @@ public class QueryView extends WebPartView<Object>
     public QueryView(UserSchema schema, QuerySettings settings, @Nullable Errors errors)
     {
         this(schema);
-        _errors = errors;
+        // TODO: stop passing in null Errors. For now, new one up if null.
+        _errors = errors != null ? errors : new BindException(new Object(), "form");
         if (null != settings)
             setSettings(settings);
     }
