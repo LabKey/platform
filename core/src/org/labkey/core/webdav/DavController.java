cb0169a73eaddd43a4b2e3a7a2108655fe427a7e
labkey-jeckels <jeckels@labkey.com>
2025-04-22 16:32:59 -0700

    Simpler handling for conditional logging

--- core/src/org/labkey/core/webdav/DavController.java
+++ core/src/org/labkey/core/webdav/DavController.java
@@ -351,11 +351,7 @@ public class DavController extends SpringActionController
             }
             catch (Exception ex)
             {
-                if (!ExceptionUtil.isIgnorable(ex))
-                {
-                    _log.error("unexpected exception", ex);
-                    ExceptionUtil.logExceptionToMothership(request, ex);
-                }
+                ExceptionUtil.logExceptionToMothership(request, ex);
                 _webdavresponse.sendError(WebdavStatus.SC_INTERNAL_SERVER_ERROR, ex);
             }
         }
