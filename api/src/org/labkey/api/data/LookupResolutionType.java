1973843fc21d0d8d3ad2a2444a6d05c29653272f
labkey-susanh <susanh@labkey.com>
2025-05-07 09:11:27 -0700

    Remove unused option

--- api/src/org/labkey/api/data/LookupResolutionType.java
+++ api/src/org/labkey/api/data/LookupResolutionType.java
@@ -3,9 +3,7 @@ package org.labkey.api.data;
 public enum LookupResolutionType
 {
     primaryKey(true, false, true), // known that the use will always supply the pk value
-    alternateKey(false, true, false), // known that the use will never supply the pk value
     alternateThenPrimaryKey(true, true, false), // If there is a situation where it's sometimes a primary and sometimes an alternate key, check for the alternate key first
-    // TODO do we need to support this? If not, we can change the properties here to just have a "supportsAlternateKey" since alternate will always be checked first.
     primaryThenAlternateKey(true, true, true); // this most closely matches previous behavior when allowImportLookupByAlternateKey was true (added for compatibility; prefer the other options)
 
     final boolean _useAlternateKey;
