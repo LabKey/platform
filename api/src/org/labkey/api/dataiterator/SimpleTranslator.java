af15eeb898ae02c974e66ee54b6894fb37cffb72
labkey-susanh <susanh@labkey.com>
2025-05-08 19:05:53 -0700

    Remove reference to deleted method.

--- api/src/org/labkey/api/dataiterator/SimpleTranslator.java
+++ api/src/org/labkey/api/dataiterator/SimpleTranslator.java
@@ -961,7 +961,7 @@ public class SimpleTranslator extends AbstractDataIterator implements DataIterat
 
         private Object convertWithPrimaryColumn(Object o, boolean primaryTried)
         {
-            if (_lookupResolutionType.usePrimaryKey() && _lookupResolutionType.usePrimaryFirst() != primaryTried)
+            if (_lookupResolutionType.usePrimaryFirst() != primaryTried)
             {
                 try
                 {
@@ -1353,7 +1353,7 @@ public class SimpleTranslator extends AbstractDataIterator implements DataIterat
 
             RemapMissingBehavior missing = remapMissingBehavior;
             if (missing == null)
-                missing = col.isRequired() || hasValidator || !lookupResolutionType.usePrimaryKey() ? RemapMissingBehavior.Error : RemapMissingBehavior.Null;
+                missing = col.isRequired() || hasValidator ? RemapMissingBehavior.Error : RemapMissingBehavior.Null;
             // For enum tables, we should not be using number names for the values, so we will look up by primary key first (resolving rowIds) then alternate.
             // This assures the type is an Integer when a rowId is given (e.g., from a row read from the database), not a string.
             if (fk.getLookupTableInfo() instanceof EnumTableInfo || _context.hasBeenCoerced())
