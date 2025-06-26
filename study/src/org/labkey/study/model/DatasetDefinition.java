e1ad9c9903cd211431f100c4432b1c3c29b50a9e
Adam Rauch <adam@labkey.com>
2025-06-26 13:24:15 -0700

    Restore getter used by reflection

--- study/src/org/labkey/study/model/DatasetDefinition.java
+++ study/src/org/labkey/study/model/DatasetDefinition.java
@@ -488,6 +488,12 @@ public class DatasetDefinition extends AbstractStudyEntity<Integer, DatasetDefin
             throw new IllegalStateException();
     }
 
+    @SuppressWarnings("unused") // Used via reflection
+    public String getDataSharing()
+    {
+        return getDataSharingEnum().name();
+    }
+
     @Override
     public DataSharing getDataSharingEnum()
     {
