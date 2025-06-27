258f7346261575c24d67b4d21971e59be4697b7a
labkey-matthewb <matthewb@labkey.com>
2025-06-27 11:45:42 -0700

    LOCK keyword

--- api/src/org/labkey/api/data/dialect/MutatingSqlDetector.java
+++ api/src/org/labkey/api/data/dialect/MutatingSqlDetector.java
@@ -129,6 +129,7 @@ public class MutatingSqlDetector
         // Needed for PostgreSQL
         WORD_MUTATING_MAP.putAll(Map.of(
             "ANALYZE", true,   // Typically executed after UPDATE, CREATE INDEX, et al
+            "LOCK", true,      // Not technically mutating. However, it is currently only used in mutating TX.
             "VACUUM", true,    // VACUUM is mutating
             "{call", true      // Execute a stored procedure, which is likely to be mutating
         ));
