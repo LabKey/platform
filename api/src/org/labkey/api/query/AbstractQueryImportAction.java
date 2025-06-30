6f76debc2f036c704723c1452892bc9da751c4c0
labkey-susanh <susanh@labkey.com>
2025-06-30 10:31:49 -0700

    Make it private

--- api/src/org/labkey/api/query/AbstractQueryImportAction.java
+++ api/src/org/labkey/api/query/AbstractQueryImportAction.java
@@ -918,7 +918,7 @@ public abstract class AbstractQueryImportAction<FORM> extends FormApiAction<FORM
             SimpleMetricsService.get().increment("query", featureArea, targetType);
     }
 
-    public static String getMetricPrefix(TableInfo target)
+    private static String getMetricPrefix(TableInfo target)
     {
         String metricPrefix = target.getUserSchema() == null ? target.getSchema().getName() : target.getUserSchema().getSchemaName();
         return metricPrefix.replace("exp.", "");
