7680cd6170f5764086af54abad7ab40fb122649f
Adam Rauch <adam@labkey.com>
2025-07-10 13:16:09 -0700

    Don't turn Jupyter JSON output into HTML

--- api/src/org/labkey/api/reports/report/r/view/IpynbOutput.java
+++ api/src/org/labkey/api/reports/report/r/view/IpynbOutput.java
@@ -31,6 +31,7 @@ import org.labkey.api.thumbnail.Thumbnail;
 import org.labkey.api.util.HtmlString;
 import org.labkey.api.util.HtmlStringBuilder;
 import org.labkey.api.util.ImageUtil;
+import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.view.HttpView;
 import org.labkey.api.view.ViewContext;
 
@@ -149,7 +150,8 @@ public class IpynbOutput extends HtmlOutput
         @Override
         protected String renderInternalAsString(File file) throws Exception
         {
-            String result = StringUtils.trimToEmpty(super.renderInternalAsString(file));
+            // Don't call super.renderInternalAsString(file) since we expect JSON
+            String result = file.exists() ? StringUtils.trimToEmpty(PageFlowUtil.getFileContentsAsString(file)) : "";
             try
             {
                 final JSONObject obj = new JSONObject(result);
