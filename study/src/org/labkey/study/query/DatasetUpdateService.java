dddcfa0b414820a3f458569a45fd192269694184
Lum <klum@labkey.com>
2025-07-15 12:32:05 -0700

    add import

--- study/src/org/labkey/study/query/DatasetUpdateService.java
+++ study/src/org/labkey/study/query/DatasetUpdateService.java
@@ -58,6 +58,7 @@ import org.labkey.api.query.DefaultSchema;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.InvalidKeyException;
 import org.labkey.api.query.QueryService;
+import org.labkey.api.query.QueryUpdateService;
 import org.labkey.api.query.QueryUpdateServiceException;
 import org.labkey.api.query.SimpleValidationError;
 import org.labkey.api.query.ValidationException;
