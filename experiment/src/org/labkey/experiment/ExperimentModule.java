5f19c89728b3741024eacec923f2a83239d736bd
Cory Nathe <cnathe@labkey.com>
2025-07-18 14:35:38 -0500

    Issue 53478: Error deriving sample from a source where the source type has a name > 50 characters (#6857)
    - update exp.DataInput from varchar 50 to 200

--- experiment/src/org/labkey/experiment/ExperimentModule.java
+++ experiment/src/org/labkey/experiment/ExperimentModule.java
@@ -183,7 +183,7 @@ public class ExperimentModule extends SpringModule
     @Override
     public Double getSchemaVersion()
     {
-        return 25.003;
+        return 25.004;
     }
 
     @Nullable
