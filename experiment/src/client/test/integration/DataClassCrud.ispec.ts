6199e6cff388916a229228a649f6ce5f9d4a644a
XingY <xyang@labkey.com>
2025-07-23 14:05:44 -0700

    fix merge and put back index fix

--- experiment/src/client/test/integration/DataClassCrud.ispec.ts
+++ experiment/src/client/test/integration/DataClassCrud.ispec.ts
@@ -233,7 +233,7 @@ describe('Import with update / merge', () => {
    it ("Issue 52922: Blank sample id in the file are getting ignored in update from file", async () => {
        const BLANK_KEY_UPDATE_ERROR_NO_EXPRESSION = 'Missing value for required property: Name';
        const BLANK_KEY_UPDATE_ERROR_WITH_EXPRESSION = 'Name value not provided on row ';
-       const BOGUS_KEY_UPDATE_ERROR = 'Data not found: ';
+       const BOGUS_KEY_UPDATE_ERROR = 'Data not found for ';
        const DUPLICATE_KEY_ERROR = 'duplicate key value';
 
        const dataType = "NoExpressionNameRequired52922";
