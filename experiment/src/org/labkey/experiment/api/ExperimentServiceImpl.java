9ba473b5e257e6b7b2d84cc0fb94f0242fed5e21
XingY <xyang@labkey.com>
2025-08-20 11:14:12 -0700

    metric

--- experiment/src/org/labkey/experiment/api/ExperimentServiceImpl.java
+++ experiment/src/org/labkey/experiment/api/ExperimentServiceImpl.java
@@ -9138,7 +9138,36 @@ public class ExperimentServiceImpl implements ExperimentService, ObjectReference
         metrics.put("nameexpression", getNameExpressionMetrics());
         metrics.put("parentalias", getParentAliasMetrics());
         metrics.put("importTemplates", getImportTemplatesMetrics());
+        metrics.put("auditLevelOverride", getAuditLevelOverrideMetrics());
         return metrics;
+    }
+
+    private Map<String, Object> getAuditLevelOverrideMetrics()
+    {
+        DbSchema dbSchema = CoreSchema.getInstance().getSchema();
+        SQLFragment sql = new SQLFragment("SELECT \"schema\", metadata FROM query.querydef WHERE metadata LIKE '%<auditLogging>%'");
+        Map<String, Object>[] results = new SqlSelector(dbSchema, sql).getMapArray();
+        Map<String, Long> counts = new HashMap<>();
+        final String auditNone = "<auditlogging>none";
+        final String auditSummary = "<auditlogging>summary";
+        for (Map<String, Object> result : results)
+        {
+            String schema = (String) result.get("schema");
+            String metadata = (String) result.get("metadata");
+            metadata = metadata.toLowerCase();
+            if (schema.toLowerCase().startsWith("assay.general."))
+                schema = "assay";
+            if (schema.equals("assay") || schema.equals("exp.data") || schema.equals("samples"))
+            {
+                if (!metadata.contains(auditNone) && !metadata.contains(auditSummary))
+                    continue;
+                long count = counts.get(schema) == null ? 0L : counts.get(schema);
+                counts.put(schema, ++count);
+            }
+        }
+        return Map.of("SampleType", counts.getOrDefault("samples", 0L),
+                "DataClass", counts.getOrDefault("exp.data", 0L),
+                "AssayDesign", counts.getOrDefault("assay", 0L));
     }
 
     private Map<String, Object> getImportTemplatesMetrics()
