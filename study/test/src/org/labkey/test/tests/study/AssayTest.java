05e214313f2e8838dfe7a91e77506b326e33fef7
labkey-jeckels <jeckels@labkey.com>
2025-08-22 15:05:06 -0700

    Another try

--- study/test/src/org/labkey/test/tests/study/AssayTest.java
+++ study/test/src/org/labkey/test/tests/study/AssayTest.java
@@ -131,7 +131,6 @@ public class AssayTest extends AbstractAssayTest
         verifyStudyList();
         verifyRunDeletionRecallsDatasetRows();
         verifyWebdavTree();
-        goBack();
     }
 
     @Test
