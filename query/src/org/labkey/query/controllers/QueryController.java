767de2c1f75b0e4a0f8710d6663b3a4491576e7c
labkey-nicka <nickk@labkey.com>
2025-08-25 09:53:38 -0700

    Undo out of scope change

--- query/src/org/labkey/query/controllers/QueryController.java
+++ query/src/org/labkey/query/controllers/QueryController.java
@@ -4813,18 +4813,18 @@ public class QueryController extends SpringActionController
         }
 
         @NotNull
-        protected TableInfo getTableInfo(Container container, User user, String schemaName, String queryName) throws ValidationException
+        protected TableInfo getTableInfo(Container container, User user, String schemaName, String queryName)
         {
             if (null == schemaName || null == queryName)
-                throw new ValidationException("You must supply a schemaName and queryName!");
+                throw new IllegalArgumentException("You must supply a schemaName and queryName!");
 
             UserSchema schema = QueryService.get().getUserSchema(user, container, schemaName);
             if (null == schema)
-                throw new ValidationException("The schema '" + schemaName + "' does not exist.", "schemaName");
+                throw new IllegalArgumentException("The schema '" + schemaName + "' does not exist.");
 
             TableInfo table = schema.getTableForInsert(queryName);
             if (table == null)
-                throw new ValidationException("The query '" + queryName + "' in the schema '" + schemaName + "' does not exist.", "queryName");
+                throw new IllegalArgumentException("The query '" + queryName + "' in the schema '" + schemaName + "' does not exist.");
             return table;
         }
     }
