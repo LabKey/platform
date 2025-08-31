f485a4dfc1fd88dad340de8121c0e9c487a439f7
XingY <xyang@labkey.com>
2025-08-31 14:14:13 -0700

    more null check

--- experiment/src/org/labkey/experiment/api/ExperimentServiceImpl.java
+++ experiment/src/org/labkey/experiment/api/ExperimentServiceImpl.java
@@ -4230,6 +4230,7 @@ public class ExperimentServiceImpl implements ExperimentService, ObjectReference
 
         try (DbScope.Transaction transaction = ensureTransaction())
         {
+            AssayService assayService = AssayService.get();
             // This can be slightly expensive to fetch, so don't do it multiple times if runs share protocols
             Map<ExpProtocol, ProtocolImplementation> protocolImpls = new HashMap<>();
 
@@ -4265,18 +4266,21 @@ public class ExperimentServiceImpl implements ExperimentService, ObjectReference
                                 throw new UnauthorizedException("Cannot delete rows from dataset " + dataset);
                             }
 
-                            AssayProvider provider = AssayService.get().getProvider(protocol);
-                            if (provider != null)
+                            if (assayService != null)
                             {
-                                AssayTableMetadata tableMetadata = provider.getTableMetadata(protocol);
-                                SimpleFilter filter = new SimpleFilter(tableMetadata.getRunRowIdFieldKeyFromResults(), run.getRowId());
-                                Collection<String> lsids = new TableSelector(tableInfo, singleton("LSID"), filter, null).getCollection(String.class);
+                                AssayProvider provider = assayService.getProvider(protocol);
+                                if (provider != null)
+                                {
+                                    AssayTableMetadata tableMetadata = provider.getTableMetadata(protocol);
+                                    SimpleFilter filter = new SimpleFilter(tableMetadata.getRunRowIdFieldKeyFromResults(), run.getRowId());
+                                    Collection<String> lsids = new TableSelector(tableInfo, singleton("LSID"), filter, null).getCollection(String.class);
 
-                                // Add an audit event to the link to study history
-                                publishService.addRecallAuditEvent(run.getContainer(), user, dataset, lsids.size(), null);
+                                    // Add an audit event to the link to study history
+                                    publishService.addRecallAuditEvent(run.getContainer(), user, dataset, lsids.size(), null);
 
-                                // Do the actual delete on the dataset for the rows in question
-                                dataset.deleteDatasetRows(user, lsids);
+                                    // Do the actual delete on the dataset for the rows in question
+                                    dataset.deleteDatasetRows(user, lsids);
+                                }
                             }
                         }
                     }
@@ -4340,12 +4344,12 @@ public class ExperimentServiceImpl implements ExperimentService, ObjectReference
                 //  ideally this would be transacted as a commit task but we decided against it due to complications
                 run.archiveDataFiles(user);
                 // Re-index replaced run if replacing run is deleted
-                if (run.getReplacesRuns() != null)
+                if (assayService != null && run.getReplacesRuns() != null)
                 {
                     List<ExpRunImpl> replacedRuns = run.getReplacesRuns();
                     transaction.addCommitTask(() ->
                         replacedRuns.forEach(replacedRun ->
-                                AssayService.get().indexAssayRun(SearchService.get().defaultTask().getQueue(container, SearchService.PRIORITY.modified), replacedRun.getRowId())
+                                assayService.indexAssayRun(SearchService.get().defaultTask().getQueue(container, SearchService.PRIORITY.modified), replacedRun.getRowId())
                         ),
                         DbScope.CommitTaskOption.POSTCOMMIT
                     );
@@ -9798,26 +9802,30 @@ public class ExperimentServiceImpl implements ExperimentService, ObjectReference
                 AbstractQueryUpdateService.addTransactionAuditEvent(transaction, user, auditEvent);
             }
 
-            for (Map.Entry<ExpProtocol, List<ExpRun>> entry: protocolMap.entrySet())
+            AssayService assayService = AssayService.get();
+            if (assayService != null)
             {
-                ExpProtocol protocol = entry.getKey();
-                AssayProvider provider = AssayService.get().getProvider(protocol);
-                List<ExpRun> runs = entry.getValue();
-                if (provider != null)
+                for (Map.Entry<ExpProtocol, List<ExpRun>> entry: protocolMap.entrySet())
                 {
-                    provider.moveRuns(runs, targetContainer, user, assayMoveData);
-                    Map<String, Integer> counts = assayMoveData.counts();
-                    int auditEventCount = expService.moveAuditEvents(targetContainer, runLsids);
-                    counts.put("auditEvents", counts.getOrDefault("auditEvents", 0) + auditEventCount);
-                    if (auditBehavior != null && AuditBehaviorType.NONE != auditBehavior)
+                    ExpProtocol protocol = entry.getKey();
+                    AssayProvider provider = assayService.getProvider(protocol);
+                    List<ExpRun> runs = entry.getValue();
+                    if (provider != null)
                     {
-                        for (ExpRun run : runs)
+                        provider.moveRuns(runs, targetContainer, user, assayMoveData);
+                        Map<String, Integer> counts = assayMoveData.counts();
+                        int auditEventCount = expService.moveAuditEvents(targetContainer, runLsids);
+                        counts.put("auditEvents", counts.getOrDefault("auditEvents", 0) + auditEventCount);
+                        if (auditBehavior != null && AuditBehaviorType.NONE != auditBehavior)
                         {
-                            run.setContainer(targetContainer);
+                            for (ExpRun run : runs)
+                            {
+                                run.setContainer(targetContainer);
 
-                            // Issue 52570: include source and target containers in the audit event message
-                            String message = String.format("Moved from %s to %s", container.getPath(), targetContainer.getPath());
-                            auditRunEvent(user, protocol, run, null, "Assay run was moved.", userComment, message);
+                                // Issue 52570: include source and target containers in the audit event message
+                                String message = String.format("Moved from %s to %s", container.getPath(), targetContainer.getPath());
+                                auditRunEvent(user, protocol, run, null, "Assay run was moved.", userComment, message);
+                            }
                         }
                     }
                 }
