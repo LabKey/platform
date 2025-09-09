b886980cd828c80fc0c6216ca09931daf2e245c6
Adam Rauch <adam@labkey.com>
2025-09-09 01:38:09 -0700

    Consistent ordering SQL Server vs. PostgreSQL

--- assay/resources/schemas/dbscripts/sqlserver/assay-25.001-25.002.sql
+++ assay/resources/schemas/dbscripts/sqlserver/assay-25.001-25.002.sql
@@ -1,7 +1,7 @@
--- This index overlaps with uq_wellgroup_plateid_typename_name
-DROP INDEX ix_wellgroup_plateid ON assay.WellGroup;
 -- This index overlaps with uq_platesetedge_fromplate_toplate
 DROP INDEX ix_platesetedge_fromplatesetid ON assay.PlateSetEdge;
+-- This index overlaps with uq_wellgroup_plateid_typename_name
+DROP INDEX ix_wellgroup_plateid ON assay.WellGroup;
 -- This index overlaps with uq_well_plateid_row_col
 DROP INDEX ix_well_plateid ON assay.Well;
 -- This index overlaps with uq_plate_container_name_template
