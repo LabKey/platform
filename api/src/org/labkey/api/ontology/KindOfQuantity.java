d4d5f1a14248b86048016a72ce921ec6ed4931ad
cnathe <cnathe@labkey.com>
2025-09-11 08:48:47 -0500

    remove Unit.count from getCommonUnits()

--- api/src/org/labkey/api/ontology/KindOfQuantity.java
+++ api/src/org/labkey/api/ontology/KindOfQuantity.java
@@ -39,7 +39,7 @@ public enum KindOfQuantity
         @Override
         public List<Unit> getCommonUnits()
         {
-            return List.of(Unit.count, Unit.unit);
+            return List.of(Unit.unit);
         }
     };
 
