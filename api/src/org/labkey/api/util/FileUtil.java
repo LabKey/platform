ce428d8f441433d7bf1766cf2383c1cd08d69f85
Trey Chadick <tchad@labkey.com>
2025-09-19 12:33:11 -0700

    Throw InvalidPathException for prohibited path appending (#7052)

--- api/src/org/labkey/api/util/FileUtil.java
+++ api/src/org/labkey/api/util/FileUtil.java
@@ -65,6 +65,7 @@ import java.nio.channels.ReadableByteChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -822,11 +823,11 @@ public class FileUtil
     {
         org.labkey.api.util.Path path = originalPath.normalize();
         if (path == null || (!path.isEmpty() && "..".equals(path.get(0))))
-            throw new IllegalArgumentException("Bad path: " + originalPath);
+            throw new InvalidPathException(originalPath.toString(), "Path to parent not allowed");
         @SuppressWarnings("SSBasedInspection")
         var ret = new File(dir, path.toString());
         if (!ret.toPath().normalize().startsWith(dir.toPath().normalize()))
-            throw new IllegalArgumentException(path.toString());
+            throw new InvalidPathException(originalPath.toString(), "Path to parent not allowed");
         return ret;
     }
 
@@ -836,7 +837,7 @@ public class FileUtil
     {
         path = path.normalize();
         if (!path.isEmpty() && "..".equals(path.get(0)))
-            throw new IllegalArgumentException(path.toString());
+            throw new InvalidPathException(path.toString(), "Path to parent not allowed");
         return dir.resolveFile(path);
     }
 
@@ -867,7 +868,7 @@ public class FileUtil
         var ret = new File(dir, name);
 
         if (!ret.toPath().normalize().startsWith(dir.toPath().normalize()))
-            throw new IllegalArgumentException(name);
+            throw new InvalidPathException(name, "Path to parent not allowed");
         return ret;
     }
 
@@ -878,7 +879,7 @@ public class FileUtil
         var ret = dir.resolve(name);
 
         if (!ret.normalize().startsWith(dir.normalize()))
-            throw new IllegalArgumentException(name);
+            throw new InvalidPathException(name, "Path to parent not allowed");
         return ret;
     }
 
@@ -887,12 +888,11 @@ public class FileUtil
     // this check that a name is a valid path part (e.g. filename) and is not path like.
     private static void legalPathPartThrow(String name)
     {
-        if (StringUtils.contains(name, '/'))
-            throw new IllegalArgumentException(name);
-        if (StringUtils.contains(name, File.separatorChar))
-            throw new IllegalArgumentException(name);
+        int invalidCharacterIndex = StringUtils.indexOfAny(name, '/', File.separatorChar);
+        if (invalidCharacterIndex >= 0)
+            throw new InvalidPathException(name, "Invalid file or directory name", invalidCharacterIndex);
         if (".".equals(name) || "..".equals(name))
-            throw new IllegalArgumentException(name);
+            throw new InvalidPathException(name, "Invalid file or directory name");
     }
 
 
