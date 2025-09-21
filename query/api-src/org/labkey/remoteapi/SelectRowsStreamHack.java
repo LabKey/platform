a78fba1b070e82e633d644c829a30d593e912daa
Marty Pradere <martyp@labkey.com>
2025-09-21 14:22:38 -0700

    Remote api stream to file

--- query/api-src/org/labkey/remoteapi/SelectRowsStreamHack.java
+++ query/api-src/org/labkey/remoteapi/SelectRowsStreamHack.java
@@ -16,6 +16,7 @@
 package org.labkey.remoteapi;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.Logger;
 import org.labkey.api.data.Container;
 import org.labkey.api.dataiterator.DataIterator;
 import org.labkey.api.dataiterator.DataIteratorBuilder;
@@ -23,10 +24,20 @@ import org.labkey.api.dataiterator.DataIteratorContext;
 import org.labkey.api.dataiterator.WrapperDataIterator;
 import org.labkey.api.query.ValidationException;
 import org.labkey.api.reader.JSONDataLoader;
+import org.labkey.api.util.FileUtil;
+import org.labkey.api.util.logging.LogHelper;
 import org.labkey.remoteapi.query.SelectRowsCommand;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * NOTE: This class only exists to use internal classes of the remoteapi package that we haven't exposed to the public yet.
@@ -39,6 +50,8 @@ import java.io.InputStream;
  */
 public class SelectRowsStreamHack
 {
+    private static final Logger _log = LogHelper.getLogger(SelectRowsStreamHack.class, "Information related to streaming of SelectRows results");
+
     public static DataIteratorBuilder go(Connection cn, String container, SelectRowsCommand cmd, Container targetContainer) throws IOException, CommandException
     {
         return new DataIteratorBuilder()
@@ -60,7 +73,36 @@ public class SelectRowsStreamHack
                 }
                 try
                 {
-                    final InputStream is = response.getInputStream();
+                    final InputStream is;
+                    final File tempFile = FileUtil.createTempFile("SelectRows-", ".tmp.gz");
+                    tempFile.deleteOnExit();
+
+                    try (OutputStream os = new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(tempFile))); InputStream ris = response.getInputStream())
+                    {
+                        _log.debug("Downloading SelectRows JSON to file: " + tempFile);
+                        IOUtils.copy(ris, os);
+                        _log.debug("Finished saving SelectRows results");
+
+                    }
+
+                    is = new BufferedInputStream(new GZIPInputStream(new FileInputStream(tempFile)))
+                    {
+                        @Override
+                        public void close() throws IOException
+                        {
+                            super.close();
+
+                            if (tempFile.exists())
+                            {
+                                _log.debug("Deleting temporary file used to download SelectRows results: " + tempFile);
+                                if (!tempFile.delete())
+                                {
+                                    _log.error("Unable to delete SelectRowsStreamHack temp file: " + tempFile);
+                                }
+                            }
+                        }
+                    };
+
                     final JSONDataLoader loader = new JSONDataLoader(is, targetContainer);
                     WrapperDataIterator wrapper = new WrapperDataIterator(loader.getDataIterator(context))
                     {
@@ -88,5 +130,4 @@ public class SelectRowsStreamHack
             }
         };
     }
-
 }
