075c0e18a3cc9f7fac593a863ce1b6401412943f
Adam Rauch <adam@labkey.com>
2025-09-21 12:04:56 -0700

    Add a couple foreign keys

--- announcements/src/org/labkey/announcements/AnnouncementModule.java
+++ announcements/src/org/labkey/announcements/AnnouncementModule.java
@@ -98,7 +98,7 @@ public class AnnouncementModule extends DefaultModule implements SearchService.D
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 25.001;
+        return 25.002;
     }
 
     @Override
