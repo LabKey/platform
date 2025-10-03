1918e2185b139a25bee06a82bcde1f34fee6d200
labkey-nicka <nickk@labkey.com>
2025-10-03 14:09:04 -0700

    Fix audit list detail links/resolution

--- list/src/org/labkey/list/model/ListAuditProvider.java
+++ list/src/org/labkey/list/model/ListAuditProvider.java
@@ -104,7 +104,7 @@ public class ListAuditProvider extends AbstractAuditTypeProvider implements Audi
         appendValueMapColumns(table, null, true);
 
         // Render a details URL only for rows that have a listItemEntityId
-        DetailsURL url = DetailsURL.fromString("list/listItemDetails.view?listId=${listId}&entityId=${listItemEntityId}&rowId=${rowId}", null, StringExpressionFactory.AbstractStringExpression.NullValueBehavior.NullResult);
+        DetailsURL url = DetailsURL.fromString("list/listItemDetails.view?listId=${listId}&name=${listName}&entityId=${listItemEntityId}&rowId=${rowId}", null, StringExpressionFactory.AbstractStringExpression.NullValueBehavior.NullResult);
         table.setDetailsURL(url);
 
         return table;
