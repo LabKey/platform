86d353bef80c0d6a88b6a92947b8b5c91739a0a7
cnathe <cnathe@labkey.com>
2025-10-27 10:51:41 -0500

    Issue 53831: check the true assay name length since we append onto the name when creating the assay domains (ex. "<assay nam> Batch Fields")

--- api/src/org/labkey/api/exp/property/DomainUtil.java
+++ api/src/org/labkey/api/exp/property/DomainUtil.java
@@ -1018,6 +1018,11 @@ public class DomainUtil
                 return prefix + invalidPattenError;
         }
 
+        // Issue 53831: check the true assay name length since we append onto the name when creating the assay domains (ex. "<assay nam> Batch Fields")
+        int actualAssayNameLengthMax = 186;
+        if ("Assay Design".equalsIgnoreCase(kindName) && domainName.length() > actualAssayNameLengthMax)
+            return "Value is too long for assay design name, a maximum length of " + actualAssayNameLengthMax + " is allowed. The supplied value, '" + StringUtils.abbreviateMiddle(domainName, "...", 50) + "', was " + domainName.length() + " characters long.";
+
         return null;
     }
 
