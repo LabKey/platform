4567289613b387686152ed09810fa205ce82154c
labkey-jeckels <jeckels@labkey.com>
2025-11-03 15:26:26 -0800

    Support tables with quotes in names, BaseColumnInfo

--- core/src/org/labkey/core/query/PostgresTableSizesTable.java
+++ core/src/org/labkey/core/query/PostgresTableSizesTable.java
@@ -1,9 +1,9 @@
 package org.labkey.core.query;
 
 import org.jetbrains.annotations.NotNull;
+import org.labkey.api.data.BaseColumnInfo;
 import org.labkey.api.data.JdbcType;
 import org.labkey.api.data.SQLFragment;
-import org.labkey.api.query.ExprColumn;
 
 /** Backed by pg_locks view */
 public class PostgresTableSizesTable extends AbstractPostgresAdminOnlyTable
@@ -14,11 +14,11 @@ public class PostgresTableSizesTable extends AbstractPostgresAdminOnlyTable
 
         setDescription("Shows info Postgres table sizes");
 
-        addColumn(new ExprColumn(this, "table_schema", new SQLFragment(ExprColumn.STR_TABLE_ALIAS + ".table_schema"), JdbcType.VARCHAR));
-        addColumn(new ExprColumn(this, "table_name", new SQLFragment(ExprColumn.STR_TABLE_ALIAS + ".table_name"), JdbcType.VARCHAR));
-        addColumn(new ExprColumn(this, "table_size", new SQLFragment(ExprColumn.STR_TABLE_ALIAS + ".table_size"), JdbcType.BIGINT)).setFormat("#,##0");
-        addColumn(new ExprColumn(this, "index_size", new SQLFragment(ExprColumn.STR_TABLE_ALIAS + ".index_size"), JdbcType.BIGINT)).setFormat("#,##0");
-        addColumn(new ExprColumn(this, "total_size", new SQLFragment(ExprColumn.STR_TABLE_ALIAS + ".total_size"), JdbcType.BIGINT)).setFormat("#,##0");
+        addColumn(new BaseColumnInfo("table_schema", this, JdbcType.VARCHAR));
+        addColumn(new BaseColumnInfo("table_name", this, JdbcType.VARCHAR));
+        addColumn(new BaseColumnInfo("table_size", this, JdbcType.BIGINT)).setFormat("#,##0");
+        addColumn(new BaseColumnInfo("index_size", this, JdbcType.BIGINT)).setFormat("#,##0");
+        addColumn(new BaseColumnInfo("total_size", this, JdbcType.BIGINT)).setFormat("#,##0");
     }
 
 
@@ -30,9 +30,9 @@ public class PostgresTableSizesTable extends AbstractPostgresAdminOnlyTable
                 SELECT
                     table_schema,
                     table_name,
-                    pg_table_size('"' || table_schema || '"."' || table_name || '"') AS table_size,
-                    pg_indexes_size('"' || table_schema || '"."' || table_name || '"') AS index_size,
-                    pg_total_relation_size('"' || table_schema || '"."' || table_name || '"') AS total_size
+                    pg_table_size(quote_ident(table_schema) || '.' || quote_ident(table_name)) AS table_size,
+                    pg_indexes_size(quote_ident(table_schema) || '.' || quote_ident(table_name)) AS index_size,
+                    pg_total_relation_size(quote_ident(table_schema) || '.' || quote_ident(table_name)) AS total_size
                 FROM information_schema.tables""");
         return result;
     }
