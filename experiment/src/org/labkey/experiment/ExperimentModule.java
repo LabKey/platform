b3bbeaa820f7eabda2d964d95bebe86f88991c07
cnathe <cnathe@labkey.com>
2025-11-13 08:19:49 -0600

    Change exp.ExperimentRun WorkflowTask column to reference workflow.task RowId instead of exp.ProtocolApplication

--- experiment/src/org/labkey/experiment/ExperimentModule.java
+++ experiment/src/org/labkey/experiment/ExperimentModule.java
@@ -196,7 +196,7 @@ public class ExperimentModule extends SpringModule
     @Override
     public Double getSchemaVersion()
     {
-        return 25.013;
+        return 25.014;
     }
 
     @Nullable
