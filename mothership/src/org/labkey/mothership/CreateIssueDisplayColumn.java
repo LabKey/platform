04a6cddff3acab1f5b473ddd39d18885a5de4369
labkey-jeckels <jeckels@labkey.com>
2025-11-20 12:29:45 -0800

    Hide Create Issue button when repo is empty string

--- mothership/src/org/labkey/mothership/CreateIssueDisplayColumn.java
+++ mothership/src/org/labkey/mothership/CreateIssueDisplayColumn.java
@@ -16,6 +16,7 @@
 
 package org.labkey.mothership;
 
+import org.apache.commons.lang3.StringUtils;
 import org.labkey.api.data.ActionButton;
 import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.ConnectionWrapper;
@@ -51,7 +52,7 @@ public class CreateIssueDisplayColumn extends DataColumn
         _saveButton.render(ctx, out);
 
         String repo = MothershipManager.get().getGitHubRepo();
-        if (repo != null)
+        if (!StringUtils.isEmpty(repo))
         {
             out.write("\t");
 
