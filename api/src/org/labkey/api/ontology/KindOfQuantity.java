fee03aff694bc19d57a2d98baf7c72fe38738b6f
XingY <xyang@labkey.com>
2025-11-26 10:03:24 -0800

    fix test

--- api/src/org/labkey/api/ontology/KindOfQuantity.java
+++ api/src/org/labkey/api/ontology/KindOfQuantity.java
@@ -39,7 +39,7 @@ public enum KindOfQuantity
         @Override
         public List<Unit> getCommonUnits()
         {
-            return List.of(Unit.unit);
+            return List.of(Unit.unit, Unit.pcs, Unit.pack, Unit.blocks, Unit.slides, Unit.cells, Unit.box, Unit.kit, Unit.tests, Unit.bottle);
         }
     };
 
