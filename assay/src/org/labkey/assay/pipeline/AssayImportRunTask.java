abd4c445e3074ec6502a40b0dfc7dcdb361249cc
labkey-jeckels <jeckels@labkey.com>
2025-12-04 10:20:19 -0800

    Fix FileWatcherAssayIntegrationTest

--- assay/src/org/labkey/assay/pipeline/AssayImportRunTask.java
+++ assay/src/org/labkey/assay/pipeline/AssayImportRunTask.java
@@ -46,7 +46,6 @@ import org.labkey.api.pipeline.PipelineStatusFile;
 import org.labkey.api.pipeline.PipelineValidationException;
 import org.labkey.api.pipeline.RecordedAction;
 import org.labkey.api.pipeline.RecordedActionSet;
-import org.labkey.api.pipeline.TaskFactory;
 import org.labkey.api.pipeline.TaskId;
 import org.labkey.api.pipeline.XMLBeanTaskFactoryFactory;
 import org.labkey.api.pipeline.file.AbstractFileAnalysisJob;
@@ -70,6 +69,7 @@ import org.labkey.pipeline.xml.TaskType;
 import org.labkey.vfs.FileLike;
 import org.labkey.vfs.FileSystemLike;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -95,7 +95,7 @@ public class AssayImportRunTask extends PipelineJob.Task<AssayImportRunTask.Fact
     public static class FactoryFactory implements XMLBeanTaskFactoryFactory
     {
         @Override
-        public TaskFactory create(TaskId taskId, TaskType xobj, Path taskDir)
+        public Factory create(TaskId taskId, TaskType xobj, Path taskDir)
         {
             if (taskId.getModuleName() == null)
                 throw new IllegalArgumentException("Task factory must be defined by a module");
@@ -245,7 +245,7 @@ public class AssayImportRunTask extends PipelineJob.Task<AssayImportRunTask.Fact
                 {
                     job.getLogger().info("Processing excel file: " + dataFile.getName());
                     // check to see if this is a multi-sheet format
-                    try (ExcelLoader loader = new ExcelLoader(dataFile.openInputStream(), true, null))
+                    try (ExcelLoader loader = new ExcelLoader(new BufferedInputStream(dataFile.openInputStream()), true, null))
                     {
                         List<String> sheets = loader.getSheetNames();
                         if (sheets.size() > 1)
@@ -434,7 +434,7 @@ public class AssayImportRunTask extends PipelineJob.Task<AssayImportRunTask.Fact
             super(AssayImportRunTaskId.class);
         }
 
-        public Factory(Class namespaceClass)
+        public Factory(Class<?> namespaceClass)
         {
             super(namespaceClass);
         }
@@ -468,7 +468,7 @@ public class AssayImportRunTask extends PipelineJob.Task<AssayImportRunTask.Fact
             return Collections.emptyList();
         }
 
-        public FileType getOutputType()
+        public FileType ßgetOutputType()
         {
             return _outputType;
         }
@@ -802,7 +802,7 @@ public class AssayImportRunTask extends PipelineJob.Task<AssayImportRunTask.Fact
             createData(matchedFile, assayDataType);
 
 
-            AssayRunUploadContext.Factory<? extends AssayProvider, ? extends AssayRunUploadContext.Factory> factory
+            AssayRunUploadContext.Factory<? extends AssayProvider, ? extends AssayRunUploadContext.Factory<?, ?>> factory
                     = provider.createRunUploadFactory(protocol, user, container);
 
             factory.setName(getName());
@@ -828,7 +828,7 @@ public class AssayImportRunTask extends PipelineJob.Task<AssayImportRunTask.Fact
 
             factory.setLogger(getJob().getLogger());
 
-            AssayRunUploadContext uploadContext = factory.create();
+            AssayRunUploadContext<?> uploadContext = factory.create();
 
             Long batchId = null;
 
@@ -857,7 +857,7 @@ public class AssayImportRunTask extends PipelineJob.Task<AssayImportRunTask.Fact
 //            }
 
             // Check if we've been cancelled. If so, delete any newly created runs from the database
-            PipelineStatusFile statusFile = PipelineService.get().getStatusFile(getJob().getLogFile());
+            PipelineStatusFile statusFile = PipelineService.get().getStatusFile(getJob().getContainer(), getJob().getLogFileLike());
             if (statusFile != null && (PipelineJob.TaskStatus.cancelled.matches(statusFile.getStatus()) || PipelineJob.TaskStatus.cancelling.matches(statusFile.getStatus())))
             {
                 getJob().info("Deleting run " + run.getName() + " due to cancellation request");
