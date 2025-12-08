93b5a0d2b703c9b15ce5b69e3bbdfa2d6e0add6b
Binal Patel <binalp@labkey.com>
2025-12-08 09:50:41 -0800

    Update error message

--- core/src/org/labkey/core/webdav/DavController.java
+++ core/src/org/labkey/core/webdav/DavController.java
@@ -3002,7 +3002,7 @@ public class DavController extends SpringActionController
                 if (result.result == AntiVirusService.Result.CONFIGURATION_ERROR)
                     throw new ConfigurationException(result.message);
                 else
-                    throw new DavException(WebdavStatus.SC_BAD_REQUEST, result.message + ". For security reasons, we did not upload this file. Please contact your IT Team for assistance in dealing with this potentially harmful file.");
+                    throw new DavException(WebdavStatus.SC_BAD_REQUEST, result.message + ". For security reasons, we did not upload this file. Please contact your internal IT or Information Security department for assistance in dealing with this potentially harmful file.");
             }
         }
 
