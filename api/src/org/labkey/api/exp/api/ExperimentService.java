05ae6578db956c6f81a279a0318e1c380b31a19d
Adam Rauch <adam@labkey.com>
2025-12-29 18:41:29 -0800

    @Override

--- api/src/org/labkey/api/exp/api/ExperimentService.java
+++ api/src/org/labkey/api/exp/api/ExperimentService.java
@@ -720,10 +720,8 @@ public interface ExperimentService extends ExperimentRunTypeSource
     ExpData getExpDataByURL(Path p, @Nullable Container c);
 
     /**
-     * Get all ExpData for the dataFileUrl.
-     *
-     * Having more than one ExpData for the same file path doesn't happen often but is allowed.
-     * Some examples:
+     * Get all ExpData for the dataFileUrl. Having more than one ExpData for the same file path doesn't happen often but
+     * is allowed. Some examples:
      * - The file or pipeline root may be shared by more than one container and an exp.data may be created in each container when importing assay data.
      * - In the MS2 analysis pipeline, there are tools that rewrite an input file to add more data. We model them as separate exp.data.
      */
