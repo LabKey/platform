d342ba0aa0ad1eb7d5c072fbc205fa3579d90b29
labkey-jeckels <jeckels@labkey.com>
2026-01-29 10:00:45 -0800

    Avoid including temp tables to avoid race condition when reporting

--- core/src/org/labkey/core/CoreModule.java
+++ core/src/org/labkey/core/CoreModule.java
@@ -1237,7 +1237,7 @@ public class CoreModule extends SpringModule implements SearchService.DocumentPr
             {
                 SQLFragment sql = new SQLFragment("SELECT table_schema, SUM(total_size) FROM ");
                 sql.append(new PostgresTableSizesTable(new PostgresUserSchema(User.getAdminServiceUser(), ContainerManager.getRoot())), "t");
-                sql.append(" GROUP BY table_schema");
+                sql.append(" WHERE table_schema != 'temp' GROUP BY table_schema");
 
                 var schemaSizes = new SqlSelector(CoreSchema.getInstance().getSchema(), sql).getValueMap();
                 results.put("databaseSchemaSize", schemaSizes);
