489a65206d02b18ce9345e2157a73bfcb1759f6a
Marty Pradere <martyp@labkey.com>
2026-02-03 10:34:47 -0800

    don't throw

--- issues/src/org/labkey/issue/model/IssueManager.java
+++ issues/src/org/labkey/issue/model/IssueManager.java
@@ -1319,7 +1319,7 @@ public class IssueManager
             return null;
         }
 
-        final IssueObject issue = getIssue(null, User.getSearchUser(), issueId);
+        final IssueObject issue = getIssue(null, User.getSearchUser(), issueId, false);
         if (null == issue)
             return null;
 
