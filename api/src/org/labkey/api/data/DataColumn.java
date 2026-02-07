2bc2ff787949033d5ab92d54301a6f17b662bb66
XingY <xyang@labkey.com>
2026-02-06 16:59:10 -0800

    Fix mvfk input

--- api/src/org/labkey/api/data/DataColumn.java
+++ api/src/org/labkey/api/data/DataColumn.java
@@ -742,6 +742,8 @@ public class DataColumn extends DisplayColumn
     private void renderSelectFormInput(HtmlWriter out, String formFieldName, Object value, List<String> strValues, boolean disabledInput, NamedObjectList entryList)
     {
         boolean isMultiple = "select.multiple".equalsIgnoreCase(_inputType);
+        if (isMultiple && !formFieldName.startsWith(MultiChoice.ARRAY_MARKER))
+            formFieldName = MultiChoice.ARRAY_MARKER + formFieldName;
         SelectBuilder select = new SelectBuilder()
             .disabled(disabledInput)
             .multiple(isMultiple)
