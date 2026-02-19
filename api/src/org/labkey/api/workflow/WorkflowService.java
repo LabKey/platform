a100b4fe9453d79e19053a7b77ea63f9dff73e7e
labkey-susanh <susanh@labkey.com>
2026-02-19 14:07:24 -0800

    Add new ActionType enum values

--- api/src/org/labkey/api/workflow/WorkflowService.java
+++ api/src/org/labkey/api/workflow/WorkflowService.java
@@ -7,7 +7,10 @@ public interface WorkflowService
 {
     enum ActionType
     {
-        AssayImport("assay types", "Imported assay data");
+        AssayImport("assay types", "Imported assay data"),
+        DeriveSamples("sample type parameters", "Derived samples"),
+        AliquotSamples("sample type parameters", "Aliquot samples"),
+        PoolSamples("sample type parameters", "Pooled samples");
 
         private final String _inputDescription;
         private final String _auditMessage;
