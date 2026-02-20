b3a351f8bbae8341fd119ed47ba86d31c69288a9
cnathe <cnathe@labkey.com>
2026-02-20 10:56:46 -0600

    Product.shouldIncludeViaStartupProperty static method to get startup props and check if a given product name is set to "include"

--- api/src/org/labkey/api/products/Product.java
+++ api/src/org/labkey/api/products/Product.java
@@ -1,7 +1,11 @@
 package org.labkey.api.products;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+import org.labkey.api.module.ModuleLoader;
+import org.labkey.api.settings.StartupPropertyEntry;
 
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class Product implements Comparable<Product>
@@ -17,6 +21,16 @@ public abstract class Product implements Comparable<Product>
     public abstract boolean isEnabled();
 
     public abstract @NotNull List<String> getFeatureFlags();
+
+    // return true if the ProductRegistry.include startup prop is set to include the product by name
+    public static boolean shouldIncludeViaStartupProperty(String productName)
+    {
+        ModuleLoader loader = ModuleLoader.getInstance();
+        StartupPropertyEntry entry = loader.getStartupPropertyEntries("ProductRegistry").stream()
+                .filter(e -> "include".equals(e.getName()))
+                .findFirst().orElse(null);
+        return entry != null && Arrays.stream(StringUtils.split(entry.getValue(), ",")).anyMatch(val -> productName.equals(val.trim()));
+    }
 
     @Override
     public int compareTo(@NotNull Product o)
