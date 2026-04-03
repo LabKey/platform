0150443a65e0488be074f82e8870e4b3bd4a9596
XingY <xyang@labkey.com>
2026-04-03 09:28:41 -0700

    fix import

--- api/src/org/labkey/api/dataiterator/AttachmentDataIterator.java
+++ api/src/org/labkey/api/dataiterator/AttachmentDataIterator.java
@@ -15,7 +15,7 @@
  */
 package org.labkey.api.dataiterator;
 
-import io.micrometer.common.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.labkey.api.attachments.AttachmentFile;
