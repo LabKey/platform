d35b630e1c8c2e3676316b30c77e6dbce2abb24c
Adam Rauch <adam@labkey.com>
2026-04-04 08:57:47 -0700

    MCP Server MVP (#7506) (#7548)

--- api/src/org/labkey/api/security/RequiresPermission.java
+++ api/src/org/labkey/api/security/RequiresPermission.java
@@ -22,10 +22,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Specifies the required permission for an action. It does not imply that the user needs to be logged in or otherwise
- * authenticated.
+ * Specifies the required permission for an action class or an MCP tool method. It does not imply that the user needs
+ * to be logged in or otherwise authenticated.
  */
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME) @Target(ElementType.TYPE)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME) @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface RequiresPermission
 {
     Class<? extends Permission> value();
