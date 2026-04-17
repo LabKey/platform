103a571feb5f3a869f852010aa7dfb12858f1de2
labkey-tchad <tchad@labkey.com>
2026-04-17 15:04:06 -0700

    Log warning if graph SVG generation fails

--- experiment/src/org/labkey/experiment/ExperimentRunGraph.java
+++ experiment/src/org/labkey/experiment/ExperimentRunGraph.java
@@ -53,11 +53,19 @@ public class ExperimentRunGraph
         if (graph.isEmpty())
             return ""; // Graphviz.toSvgStr() throws an exception if the graph is empty.
 
-        String svg = graph.toSvgStr();
+        try
+        {
+            String svg = graph.toSvgStr();
 
-        // Scale down to 50% of default size. This is arbitrary but seems reasonable. Diagrams are larger than
-        // the old image-based ones, but monitors are much higher resolution than when those were scaled.
-        return SvgUtil.scaleSize(svg, 0.5f);
+            // Scale down to 50% of default size. This is arbitrary but seems reasonable. Diagrams are larger than
+            // the old image-based ones, but monitors are much higher resolution than when those were scaled.
+            return SvgUtil.scaleSize(svg, 0.5f);
+        }
+        catch (ExecuteException ex)
+        {
+            LOG.warn("Error generating graph", ex);
+            throw ex;
+        }
     }
 
     private static String getDotGraph(Container c, ExpRunImpl run, boolean detail, String focus, String focusType)
