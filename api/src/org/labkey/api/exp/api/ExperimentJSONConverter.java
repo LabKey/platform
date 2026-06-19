38dbfce4199ed07d2ce09005ff9fa9635a79628e
labkey-susanh <susanh@labkey.com>
2026-06-19 07:09:35 -0700

    Support filtering sample lineage graph by sample status

--- api/src/org/labkey/api/exp/api/ExperimentJSONConverter.java
+++ api/src/org/labkey/api/exp/api/ExperimentJSONConverter.java
@@ -754,6 +754,8 @@ public class ExperimentJSONConverter
             isDerivative = material.getRunId() != null && material.getRunId() > 0;
 
         jsonObject.put("materialLineageType", isAliquot ? "Aliquot" : (isDerivative ? "Derivative" : "RootMaterial"));
+        if (material.getSampleState() != null)
+            jsonObject.put("sampleStatus", material.getSampleStateId());
 
         return jsonObject;
     }
