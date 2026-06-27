b2522aee5d1898a345a00a660f810af4cf4f6212
Adam Rauch <adam@labkey.com>
2026-06-27 08:20:09 -0700

    Don't warn for ConfirmAction

--- api/src/org/labkey/api/action/SpringActionController.java
+++ api/src/org/labkey/api/action/SpringActionController.java
@@ -1258,6 +1258,8 @@ public abstract class SpringActionController implements Controller, HasViewConte
         ViewContext vc = HttpView.currentContext();
         boolean readonly = false;
 
+        // Action classes listed below have a single code path that responds to GET and POST, so check for mutating SQL
+        // regardless of the current method
         if (
             ReadOnlyApiAction.class.isAssignableFrom(actionClass) ||
             SimpleAction.class.isAssignableFrom(actionClass) ||
@@ -1272,19 +1274,24 @@ public abstract class SpringActionController implements Controller, HasViewConte
             if (null != vc && "GET".equals(vc.getRequest().getMethod()))
             {
                 readonly = true;
-                if (!FormViewAction.class.isAssignableFrom(actionClass) && AppProps.getInstance().isDevMode())
+                // Action classes listed below have different code paths for GET vs. POST. Treat them as read-only when
+                // current method is GET.
+                if (AppProps.getInstance().isDevMode() && !(
+                    ConfirmAction.class.isAssignableFrom(actionClass) ||
+                    FormViewAction.class.isAssignableFrom(actionClass)
+                ))
                     _log.warn("Action {} accepted GET unexpectedly... might need to update checkForMutatingSql()", actionClass.getName());
             }
         }
 
         if (readonly)
         {
-            // Note: This defers the mutating SQL parsing & detection until after the quick checks above
+            // Supplier allows the quick checks above us to short-circuit the mutating SQL parsing & detection work
             String mutatingSql = mutatingSqlSupplier.get();
             if (mutatingSql != null)
             {
                 // Checking late in the game to ensure OptionalFeatureService has been initialized and to avoid
-                // reentrancy when querying this optional feature when it's not already cached.
+                // reentrancy when querying this optional feature flag's value when it's not already cached.
                 if (OptionalFeatureService.get().isFeatureEnabled(ALLOW_MUTATING_SQL_VIA_GET))
                     return;
 
