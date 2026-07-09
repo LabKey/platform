a7a27ed59b120a8e50c1f7a236957ac092df5ca1
Karl Lum <klum@labkey.com>
2026-07-09 09:42:25 -0700

    Skip batch trigger if we have prior errors. (#7830)

--- api/src/org/labkey/api/query/AbstractQueryUpdateService.java
+++ api/src/org/labkey/api/query/AbstractQueryUpdateService.java
@@ -889,6 +889,8 @@ public abstract class AbstractQueryUpdateService implements QueryUpdateService
         }
 
         // Fire triggers, if any, and also throw if there are any errors
+        if (errors.hasErrors())
+            throw errors;
         getQueryTable().fireBatchTrigger(container, user, TableInfo.TriggerType.UPDATE, false, errors, extraScriptContext);
         afterInsertUpdate(null==result?0:result.size(), errors, true);
 
