746da0a486c273419d8bb2c857d716cff2f76550
XingY <xyang@labkey.com>
2026-07-20 11:00:58 -0700

    metrics

--- experiment/src/org/labkey/experiment/ExperimentModule.java
+++ experiment/src/org/labkey/experiment/ExperimentModule.java
@@ -796,6 +796,24 @@ public class ExperimentModule extends SpringModule
                 results.put("sampleTypesWithVolumeTypeUnit", new SqlSelector(schema, "SELECT COUNT(*) from exp.materialSource WHERE category IS NULL AND metricunit IN ('L', 'mL', 'uL')").getObject(Long.class));
                 results.put("sampleTypesWithCountTypeUnit", new SqlSelector(schema, "SELECT COUNT(*) from exp.materialSource WHERE category IS NULL AND metricunit = ?", "unit").getObject(Long.class));
 
+                Map<String, Object> sampleColorMetrics = new HashMap<>();
+                Long colorCount = new SqlSelector(schema, "SELECT COUNT(*) FROM exp.datacolors").getObject(Long.class);
+                results.put("colorCount", colorCount);
+                if (colorCount > 0)
+                {
+                    Long archivedColorCount = new SqlSelector(schema, new SQLFragment("SELECT COUNT(*) FROM exp.datacolors WHERE archived = " + schema.getSqlDialect().getBooleanTRUE())).getObject(Long.class);
+                    results.put("archivedColorCount", archivedColorCount);
+                    results.put("samplesWithColorCount", new SqlSelector(schema, "SELECT COUNT(*) FROM exp.material WHERE samplecolor IS NOT NULL").getObject(Long.class));
+                    results.put("samplesWithArchivedColorCount", new SqlSelector(schema, new SQLFragment("SELECT COUNT(*) FROM exp.material m JOIN exp.datacolors dc ON m.samplecolor = dc.rowid WHERE dc.archived = " + schema.getSqlDialect().getBooleanTRUE())).getObject(Long.class));
+
+                    results.put("sampleTypesWithColorsEnabledCount", new SqlSelector(schema, new SQLFragment(
+                            "SELECT COUNT(*) FROM exp.materialsource ms WHERE EXISTS (" +
+                                    "SELECT 1 FROM exp.datacolors dc WHERE dc.container = ms.container AND dc.archived = " + schema.getSqlDialect().getBooleanFALSE() + " AND NOT EXISTS (" +
+                                    "SELECT 1 FROM exp.datatypecolorexclusion e WHERE e.datatype = ? AND e.datatyperowid = ms.rowid AND e.colorrowid = dc.rowid))")
+                            .add(ExperimentService.DataTypeForExclusion.SampleType.name())).getObject(Long.class));
+                }
+                results.put("sampleColors", sampleColorMetrics);
+
                 results.put("duplicateSampleMaterialNameCount", new SqlSelector(schema, "SELECT COUNT(*) as duplicateCount FROM " +
                         "(SELECT name, cpastype FROM exp.material WHERE cpastype <> 'Material' GROUP BY name, cpastype HAVING COUNT(*) > 1) d").getObject(Long.class));
                 results.put("duplicateSpecimenMaterialNameCount", new SqlSelector(schema, "SELECT COUNT(*) as duplicateCount FROM " +
