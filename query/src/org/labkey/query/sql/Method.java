8abc1ded40ba5ad77958dd6e293e4bcead3a9be0
labkey-matthewb <matthewb@labkey.com>
2026-07-24 10:44:21 -0700

    fix is_distinct_from() and is_not_distinct_from().  Enables using postgres "IS DISTINCT FROM" syntax in the future.

--- query/src/org/labkey/query/sql/Method.java
+++ query/src/org/labkey/query/sql/Method.java
@@ -27,10 +27,12 @@ import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.CompareType;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.CoreSchema;
+import org.labkey.api.data.DbScope;
 import org.labkey.api.data.JdbcType;
 import org.labkey.api.data.MethodInfo;
 import org.labkey.api.data.MutableColumnInfo;
 import org.labkey.api.data.SQLFragment;
+import org.labkey.api.data.SqlSelector;
 import org.labkey.api.data.TableInfo;
 import org.labkey.api.data.dialect.SqlDialect;
 import org.labkey.api.module.Module;
@@ -1664,6 +1666,24 @@ public abstract class Method
                 }
             };
         }
+
+        // "is distinct from" and "is not distinct from" operators in method form
+        labkeyMethod.put("is_distinct_from", new Method(JdbcType.BOOLEAN, 2, 2)
+        {
+            @Override
+            public MethodInfo getMethodInfo()
+            {
+                return new IsDistinctFromMethodInfo(IS);
+            }
+        });
+        labkeyMethod.put("is_not_distinct_from", new Method(JdbcType.BOOLEAN, 2, 2)
+        {
+            @Override
+            public MethodInfo getMethodInfo()
+            {
+                return new IsDistinctFromMethodInfo(IS_NOT);
+            }
+        });
     }
 
     final static Map<String, Method> postgresMethods = Collections.synchronizedMap(new CaseInsensitiveHashMap<>());
@@ -1843,24 +1863,6 @@ public abstract class Method
         postgresMethods.put("jsonb_path_query_tz", new PassthroughMethod("jsonb_path_query_tz", JdbcType.VARCHAR, 2, 4));
         postgresMethods.put("jsonb_path_query_array_tz", new PassthroughMethod("jsonb_path_query_array_tz", JdbcType.VARCHAR, 2, 4));
         postgresMethods.put("jsonb_path_query_first_tz", new PassthroughMethod("jsonb_path_query_first_tz", JdbcType.VARCHAR, 2, 4));
-
-        // "is distinct from" and "is not distinct from" operators in method form
-        labkeyMethod.put("is_distinct_from", new Method(JdbcType.BOOLEAN, 2, 2)
-        {
-            @Override
-            public MethodInfo getMethodInfo()
-            {
-                return new IsDistinctFromMethodInfo(IS);
-            }
-        });
-        labkeyMethod.put("is_not_distinct_from", new Method(JdbcType.BOOLEAN, 2, 2)
-        {
-            @Override
-            public MethodInfo getMethodInfo()
-            {
-                return new IsDistinctFromMethodInfo(IS_NOT);
-            }
-        });
     }
 
     private static class IsDistinctFromMethodInfo extends AbstractMethodInfo
@@ -1876,13 +1878,35 @@ public abstract class Method
         @Override
         public SQLFragment getSQL(SqlDialect dialect, SQLFragment[] arguments)
         {
+            SQLFragment a = arguments[0];
+            SQLFragment b = arguments[1];
             SQLFragment ret = new SQLFragment();
-            ret.append(" ((").append(arguments[0]).append(")");
-            if (token == IS)
-                ret.append(" IS DISTINCT FROM ");
+
+            if (dialect.supportsNativeIsDistinctFrom())
+            {
+                ret.append(" ((").append(a).append(")");
+                if (token == IS)
+                    ret.append(" IS DISTINCT FROM ");
+                else
+                    ret.append(" IS NOT DISTINCT FROM ");
+                ret.append("(").append(b).append(")) ");
+            }
             else
-                ret.append(" IS NOT DISTINCT FROM ");
-            ret.append("(").append(arguments[1]).append(")) ");
+            {
+                // "IS [NOT] DISTINCT FROM" isn't standard/portable SQL -- it's native only on PostgreSQL-family and
+                // Snowflake dialects. Elsewhere (e.g. SQL Server), rewrite as a CASE expression that always
+                // evaluates to a real TRUE/FALSE -- never NULL, even when exactly one side is null -- so it behaves
+                // the same as the native predicate would.
+                String notDistinctValue = token == IS ? dialect.getBooleanFALSE() : dialect.getBooleanTRUE();
+                String distinctValue = token == IS ? dialect.getBooleanTRUE() : dialect.getBooleanFALSE();
+
+                ret.append("(CASE WHEN (").append(a).append(") = (").append(b).append(")");
+                ret.append(" OR ((").append(a).append(") IS NULL AND (").append(b).append(") IS NULL)");
+                ret.append(" THEN ").append(notDistinctValue);
+                ret.append(" ELSE ").append(distinctValue);
+                ret.append(" END)");
+            }
+
             return ret;
         }
     }
@@ -2012,6 +2036,43 @@ public abstract class Method
             assertNotSimpleString(new SQLFragment("?").add(5));
             assertNotSimpleString(new SQLFragment("SELECT 'test'"));
             assertNotSimpleString(new SQLFragment("'test''string'"));
+        }
+
+        // Exercises both the native and portable-fallback branches of IsDistinctFromMethodInfo.getSQL() against every
+        // dialect that's actually connected in this environment, not just whichever dialect the current CI leg happens
+        // to be running against. A FROM-less SELECT works everywhere except Oracle, which requires FROM DUAL.
+        @Test
+        public void testIsDistinctFrom()
+        {
+            record Case(String a, String b, boolean distinct) {}
+            List<Case> cases = List.of(
+                new Case("1", "2", true),
+                new Case("1", "1", false),
+                new Case("NULL", "NULL", false),
+                new Case("1", "NULL", true)
+            );
+
+            for (DbScope scope : DbScope.getDbScopesToTest())
+            {
+                SqlDialect d = scope.getSqlDialect();
+
+                for (Case c : cases)
+                {
+                    assertIsDistinctFrom(scope, d, IS, c.a(), c.b(), c.distinct());
+                    assertIsDistinctFrom(scope, d, IS_NOT, c.a(), c.b(), !c.distinct());
+                }
+            }
+        }
+
+        private void assertIsDistinctFrom(DbScope scope, SqlDialect d, int token, String a, String b, boolean expected)
+        {
+            SQLFragment expr = new IsDistinctFromMethodInfo(token).getSQL(d, new SQLFragment[]{new SQLFragment(a), new SQLFragment(b)});
+            SQLFragment sql = new SQLFragment("SELECT ").append(expr);
+            if (d.isOracle())
+                sql.append(" FROM DUAL");
+
+            Boolean result = new SqlSelector(scope, sql).getObject(Boolean.class);
+            assertEquals(d.getClass().getSimpleName() + ": " + sql.toDebugString(), expected, result);
         }
     }
 }
