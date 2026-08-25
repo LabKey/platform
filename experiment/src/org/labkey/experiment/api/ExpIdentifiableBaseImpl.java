c1ba2cdc713e0354e9b4b218f5f8392ea1bcba07
XingY <xyang@labkey.com>
2026-08-24 18:46:17 -0700

    update comment

--- experiment/src/org/labkey/experiment/api/ExpIdentifiableBaseImpl.java
+++ experiment/src/org/labkey/experiment/api/ExpIdentifiableBaseImpl.java
@@ -192,7 +192,7 @@ public abstract class ExpIdentifiableBaseImpl<Type extends IdentifiableBase> ext
 
             for (String nameSuffix : nameSuffixes)
             {
-                // \d+ admits suffixes beyond Long.MAX_VALUE, so skip any that don't fit
+                // \d+ admits suffixes beyond Long.MAX_VALUE, so skip any that don't fit so generated sequence won't overflow
                 if (nameSuffix.matches("\\d+"))
                 {
                     try
