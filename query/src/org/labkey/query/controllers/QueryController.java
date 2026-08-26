1ed157ef79584eeb7658e99a0a88d6c180b5f17a
XingY <xyang@labkey.com>
2026-08-25 20:01:17 -0700

    GitHub Issue 1470: Manage Templates end up with sample type not usable in Apps

--- query/src/org/labkey/query/controllers/QueryController.java
+++ query/src/org/labkey/query/controllers/QueryController.java
@@ -8174,7 +8174,8 @@ public class QueryController extends SpringActionController
             User user = getUser();
             Container container = getContainer();
             String schemaName = form.getSchemaName();
-            String queryName = form.getQueryName();
+            // GitHub Issue 1470: use the resolved table's name instead of user provided name that might have different casing
+            String queryName = _tInfo.getName();
             QueryDef queryDef = QueryManager.get().getQueryDef(container, schemaName, queryName, false);
             if (queryDef != null && queryDef.getQueryDefId() != 0)
             {
@@ -8208,7 +8209,7 @@ public class QueryController extends SpringActionController
                     {
                         throw new MetadataUnavailableException(e.getMessage());
                     }
-                    xmlTable = getTableType(form.getQueryName(), doc);
+                    xmlTable = getTableType(queryName, doc);
                     // when there is a queryDef but xmlTable is null it means the xmlMetaData contains tableName which does not
                     // match with actual queryName then reconstruct the xml table metadata : See Issue 43523
                     if (xmlTable == null)
