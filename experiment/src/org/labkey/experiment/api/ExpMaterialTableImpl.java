f2a190a77888c954a021e19b42d2f743e004703a
labkey-jeckels <jeckels@labkey.com>
2026-08-31 10:59:32 -0700

    Strip comment

--- experiment/src/org/labkey/experiment/api/ExpMaterialTableImpl.java
+++ experiment/src/org/labkey/experiment/api/ExpMaterialTableImpl.java
@@ -1260,7 +1260,6 @@ public class ExpMaterialTableImpl extends ExpRunItemTableImpl<ExpMaterialTable.C
         }
     }
 
-    // TTL is a backstop for missed invalidations. Admins can manually clear via Admin Console->Caches
     static final BlockingCache<String,_MaterializedQueryHelper> _materializedQueries = CacheManager.getBlockingStringKeyCache(CacheManager.UNLIMITED, 12 * CacheManager.HOUR, "materialized sample types", null);
     static final Map<String, InvalidationCounters> _invalidationCounters = Collections.synchronizedMap(new HashMap<>());
     static final AtomicBoolean initializedListeners = new AtomicBoolean(false);
