74d962085deb974cc4d45e0958d20e26a81d7260
Daria Bodiakova <70635654+DariaBod@users.noreply.github.com>
2026-09-02 11:28:39 -0700

    delete comment

--- core/src/client/AuthenticationConfiguration/AuthenticationConfiguration.tsx
+++ core/src/client/AuthenticationConfiguration/AuthenticationConfiguration.tsx
@@ -212,7 +212,6 @@ class AuthenticationConfiguration extends PureComponent<{}, Partial<State>> {
                 const oldOrdering = state.dirtinessData[configType];
                 return !isEquivalent(newOrdering, oldOrdering);
             });
-           // const dirty = dirtyStateAreas.length > 0 || !isEquivalent({ ...globalSettings }, dirtinessData);
             const dirty = dirtyStateAreas.length > 0 || !isEquivalent({ ...globalSettings }, dirtinessData.globalSettings);
 
             return { ...newState, dirty };
