ff35ad0458f72e89a6c44cf11fd8e7c758501d8b
Susan Hert <susanh@labkey.com>
2026-09-02 10:44:11 -0700

    GH Issue 1412: Don't allow job task ids for assay types not referenced in actions (#8010)

--- assay/src/org/labkey/assay/actions/ImportRunApiAction.java
+++ assay/src/org/labkey/assay/actions/ImportRunApiAction.java
@@ -201,6 +201,14 @@ public class ImportRunApiAction extends MutatingApiAction<ImportRunApiAction.Imp
             auditDetailsJsonStr = form.getAuditDetails();
         }
 
+        if (workflowTaskId != null)
+        {
+            WorkflowService workService = WorkflowService.get();
+
+            if (workService != null && protocol != null && !workService.isTaskAssayType(workflowTaskId, protocol.getRowId()))
+                throw new ExperimentException("Invalid job task id " + workflowTaskId + ". Either the task does not exist or does not reference this assay type.");
+        }
+
         if (reImportOption == null)
         {
             if (provider != null && protocol != null && provider.isPlateMetadataEnabled(protocol))
