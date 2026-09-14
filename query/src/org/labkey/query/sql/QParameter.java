50eee860e0c35dc5d5237df4f783d7e303fe77cf
alanv <alanv@labkey.com>
2026-09-14 10:06:44 -0500

    remove unused import

--- query/src/org/labkey/query/sql/QParameter.java
+++ query/src/org/labkey/query/sql/QParameter.java
@@ -16,7 +16,6 @@
 package org.labkey.query.sql;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
 import org.labkey.api.data.JdbcType;
 import org.labkey.api.query.QueryService;
 
