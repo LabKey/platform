61c300c3654e7e3f95e64825630c02fd3e0cb8da
Adam Rauch <adam@labkey.com>
2026-09-19 04:53:43 -0700

    Optimize full-text search's SecurityQuery (#8041)
    ## Rationale
    Permission checking done by `SecurityQuery` is slow with many
    containers. Details here:
    https://github.com/LabKey/internal-issues/issues/819
    
    ---------
    
    Co-authored-by: labkey-jeckels <jeckels@labkey.com>

--- experiment/src/org/labkey/experiment/api/ExpDataClassImpl.java
+++ experiment/src/org/labkey/experiment/api/ExpDataClassImpl.java
@@ -48,6 +48,7 @@ import org.labkey.api.search.SearchService;
 import org.labkey.api.security.User;
 import org.labkey.api.security.permissions.DataClassReadPermission;
 import org.labkey.api.security.permissions.MediaReadPermission;
+import org.labkey.api.security.permissions.Permission;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.util.Path;
 import org.labkey.api.util.UnexpectedException;
@@ -76,16 +77,16 @@ public class ExpDataClassImpl extends ExpIdentifiableEntityImpl<DataClass> imple
     private static final String MEDIA_SEARCH_CATEGORY_NAME = "media";
     public static final SearchService.SearchCategory SEARCH_CATEGORY = new SearchService.SearchCategory(SEARCH_CATEGORY_NAME, "Collections of data objects", false) {
         @Override
-        public Set<String> getPermittedContainerIds(User user, Map<String, Container> containers)
+        public Class<? extends Permission> getRequiredPermission()
         {
-            return getPermittedContainerIds(user, containers, DataClassReadPermission.class);
+            return DataClassReadPermission.class;
         }
     };
     public static final SearchService.SearchCategory MEDIA_SEARCH_CATEGORY = new SearchService.SearchCategory(MEDIA_SEARCH_CATEGORY_NAME, "Collections of media data and samples", false) {
         @Override
-        public Set<String> getPermittedContainerIds(User user, Map<String, Container> containers)
+        public Class<? extends Permission> getRequiredPermission()
         {
-            return getPermittedContainerIds(user, containers, MediaReadPermission.class);
+            return MediaReadPermission.class;
         }
     };
 
